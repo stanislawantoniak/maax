786d5ee3683aa642e6d40bcff460850b9a982760
Maciej Babol <maciej.babol@orba.pl>
2014-02-11 15:09:20 +0100

    Dodanie helepra i fn sprawdzajacej czy produkt z listy do moejej wishlisty

---
+++ app/code/local/Zolago/Wishlist/Helper/Data.php
@@ -0,0 +1,32 @@
+<?php
+class Zolago_Wishlist_Helper_Data extends Mage_Core_Helper_Abstract{
+    
+    protected $_wishlist;
+    
+    public function productBelongsToMyWishlist(Mage_Catalog_Model_Product $product) {
+        $session = Mage::getSingleton("customer/session");
+        $customerId = $session->getCustomerId();
+        $coll = Mage::getResourceModel("wishlist/item_collection");
+        /* @var $coll Mage_Wishlist_Model_Resource_Item_Collection */
+        
+        $coll->addCustomerIdFilter($customerId);
+        $coll->addFieldToFilter("product_id", $product->getId());
+        
+        return (bool)$coll->count();
+    }
+    
+    /**
+     * @param int $customerId
+     * @return Mage_Wishlist_Model_Wishlist
+     */
+    protected function _getWishlist($customerId){
+        if(!$this->_wishlist){
+            $wishlist = Mage::getModel('wishlist/wishlist');
+            if($customerId){
+                $wishlist->loadByCustomer($customerId, true);
+            }
+            $this->_wishlist = $wishlist;
+        }
+        return $this->_wishlist;
+    }
+}
