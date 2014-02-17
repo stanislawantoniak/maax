0864f2729bce2c7284da7b4d7564354d26c73d83
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-02-17 13:29:43 +0100

    testy

--- test/Zolago/Pos/Model/Resource/PosTest.php
+++ test/Zolago/Pos/Model/Resource/PosTest.php
@@ -1,5 +1,5 @@
 <?php
-class Zolago_Pos_Model_Resource_Pos extends Zolago_TestCase {
+class Zolago_Pos_Model_Resource_PosTest extends Zolago_TestCase {
     
     /**
      * new object test
@@ -8,5 +8,6 @@ class Zolago_Pos_Model_Resource_Pos extends Zolago_TestCase {
         $obj = Mage::getModel('zolagopos_resource/pos');
         $this->assertEquals('Zolago_Pos_Model_Resource_Pos',get_class($obj));    
     }    
+    
 }
 ?>
