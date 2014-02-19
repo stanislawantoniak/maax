4d59bfb13099abfdfa990d2234403dd1fb575c6e
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-02-19 12:06:17 +0100

    wypełnianie czasów

--- app/code/local/Zolago/Operator/Model/Resource/Operator.php
+++ app/code/local/Zolago/Operator/Model/Resource/Operator.php
@@ -5,6 +5,19 @@ class Zolago_Operator_Model_Resource_Operator extends Mage_Core_Model_Resource_D
 	protected function _construct() {
 		$this->_init('zolagooperator/operator', "operator_id");
 	}
-	
+
+    /**
+     * fill times
+     */	
+     protected function _prepareDataForSave(Mage_Core_Abstract_Object $object) {
+ 		// Times
+		$currentTime = Varien_Date::now();
+		if ((!$object->getId() || $object->isObjectNew()) && !$object->getCreatedAt()) {
+
+			$object->setCreatedAt($currentTime);
+		}
+		$object->setUpdatedAt($currentTime);
+		return parent::_prepareDataForSave($object);     	
+     }
 }
 
