eceb12b84534c33f8f70dd5be8307783e8023c7a
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-02-19 12:33:22 +0100

    testy i zwracanie vendora

--- app/code/local/Zolago/Operator/Model/Operator.php
+++ app/code/local/Zolago/Operator/Model/Operator.php
@@ -4,6 +4,12 @@
  */
 class Zolago_Operator_Model_Operator extends Mage_Core_Model_Abstract {
     protected function _construct() {   
-        $this->init('zolagooperator/operator');
+        $this->_init('zolagooperator/operator');
+    }
+    public function getVendor() {
+        if (!$this->hasData('vendor')) {            
+            $this->setData('vendor',Mage::getModel('udropship/vendor')->load($this->vendor_id));
+        }
+        return $this->getData('vendor');
     }
 }
