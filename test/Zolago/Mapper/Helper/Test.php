35b6b6d88853d553a00d9da6ee1642fa7e6bcad6
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-03-04 13:04:58 +0100

    błąd w testach - nie były sprawdzane

--- test/Zolago/Mapper/Helper/Test.php
+++ test/Zolago/Mapper/Helper/Test.php
@@ -3,16 +3,16 @@
  * helper for mapper test
  */
 class Zolago_Mapper_Helper_Test {
-    static public class getNewMapperData() {        
+    static public function getNewMapperData() {        
         $data = array (
-            'website_id' = 1,
+            'website_id' => 1,
             'attribute_set_id' => 1,
             'is_active' => 1,
             'name' => 'mapper testowy',
             'priority' => 0,
         );
     }
-    static public class getNewMapper($data = null) {
+    static public function getNewMapper($data = null) {
         if (!$data) {
             $data = self::getNewMapperData();        
         }
