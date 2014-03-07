d06bc44ed7a4f997222db5ff44d9930e9421ef0a
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-03-07 13:44:09 +0100

    testy pobrania atrybutów

--- test/Zolago/Catalog/Model/Category/FilterTest.php
+++ test/Zolago/Catalog/Model/Category/FilterTest.php
@@ -49,9 +49,118 @@ class Zolago_Catalog_Model_Category_FilterTest extends ZolagoDb_TestCase {
         $collection->addCategoryFilter($category);
         $this->assertGreaterThan(0,count($collection));        
     }
-    public function testAttributeList() {
-        // create mapper
-        
+    protected function _createCategory($parentId) {
+        $newCategory = Mage::getModel('catalog/category');
+        $data = Zolago_Helper_Test::getCategoryData();
+        $data['parent_id'] = $cid;
+        $newCategory->setData($data);
+        $newCategory->save();
+        $this->assertNotEmpty($newCategory->getId());
+        return $newCategory;        
     }
+    public function testAttributeListSameSet() {
+        if (!no_coverage()) {
+            $this->markTestSkipped('Coverage');
+            return;
+        }
+        // create child categories
+        $category = Zolago_Helper_Test::getItem('catalog/category');
+        $this->assertNotEmpty($category);
+        $cid = $category->getId();
+        $this->assertNotEmpty($cid);
 
+        $cid_1 = $this->_createCategory($cid);
+        $cid_2 = $this->_createCategory($cid);
+        
+        // create attribute set
+        $newSet = Zolago_Helper_Test::addAttributeSet('zestaw testowy');
+        $this->assertNotEmpty($newSet->getId());
+        
+        // add attributes
+        $attr_1 = Zolago_Helper_Test::addNewAttribute('code_1','label_1');
+        $this->assertNotEmpty($attr_1->getId());
+        $attr_2 = Zolago_Helper_Test::addNewAttribute('code_2','label_2');
+        $this->assertNotEmpty($attr_2->getId());
+        
+        // connect attributes to set
+		$catalogInstaller = Zolago_Helper_Test::getCatalogInstaller();
+		$entityType = Zolago_Helper_Test::getEntityType();
+		$catalogInstaller->addAttributeToSet($entityType->getId(), $newSet->getId(), 
+				"General Information", $attr_1->getId());
+		$catalogInstaller->addAttributeToSet($entityType->getId(), $newSet->getId(), 
+				"General Information", $attr_2->getId());
+        // add mappers
+        $mapper_1 = Zolago_Helper_Test::addNewMapper($cid_1,$newSet,serialize(Zolago_Helper_Test::getConditionsArray('code_1')));
+        $this->assertNotEmpty($mapper_1->getId());
+        $mapper_2 = Zolago_Helper_Test::addNewMapper($cid_2,$newSet,serialize(Zolago_Helper_Test::getConditionsArray('code_2')));
+        $this->assertNotEmpty($mapper_2->getId());
+        
+        // test!
+        $model = Mage::getResourceModel('zolagomapper/mapper');
+        $this->assertNotEmpty($model);
+        $ret = $model->getAttributesByCategory($cid_1->getId());
+        $expected = array (
+            $attr_1->getId() => 'label_1',
+            $attr_2->getId() => 'label_2',
+        );
+        $this->assertEquals($expected,$ret);
+        $ret = $model->getAttributesByCategory($cid_2->getId());
+        $this->assertEquals($expected,$ret);        
+    }
+    public function testAttributeListAnotherSet() {
+        if (!no_coverage()) {
+            $this->markTestSkipped('Coverage');
+            return;
+        }
+        // create child categories
+        $category = Zolago_Helper_Test::getItem('catalog/category');
+        $this->assertNotEmpty($category);
+        $cid = $category->getId();
+        $this->assertNotEmpty($cid);
+
+        $cid_1 = $this->_createCategory($cid);
+        $cid_2 = $this->_createCategory($cid);
+        
+        // create attribute sets
+        $newSet1 = Zolago_Helper_Test::addAttributeSet('zestaw testowy 1');
+        $this->assertNotEmpty($newSet1->getId());
+        $newSet2 = Zolago_Helper_Test::addAttributeSet('zestaw testowy 2');
+        $this->assertNotEmpty($newSet2->getId());
+        
+        // add attributes
+        $attr_1 = Zolago_Helper_Test::addNewAttribute('code_b_1','label_3');
+        $this->assertNotEmpty($attr_1->getId());
+        $attr_2 = Zolago_Helper_Test::addNewAttribute('code_b_2','label_4');
+        $this->assertNotEmpty($attr_2->getId());
+        
+        // connect attributes to set
+		$catalogInstaller = Zolago_Helper_Test::getCatalogInstaller();
+		$entityType = Zolago_Helper_Test::getEntityType();
+		$catalogInstaller->addAttributeToSet($entityType->getId(), $newSet1->getId(), 
+				"General Information", $attr_1->getId());
+		$catalogInstaller->addAttributeToSet($entityType->getId(), $newSet2->getId(), 
+				"General Information", $attr_2->getId());
+        // add mappers
+        $mapper = Zolago_Helper_Test::getNewMapper('nowy mapper 1');
+        $mapper_1 = Zolago_Helper_Test::addNewMapper($cid_1,$newSet1,serialize(Zolago_Helper_Test::getConditionsArray('code_b_1')),$mapper);
+        $this->assertNotEmpty($mapper_1->getId());
+        $mapper = Zolago_Helper_Test::getNewMapper('nowy mapper 2');
+        $mapper_2 = Zolago_Helper_Test::addNewMapper($cid_2,$newSet2,serialize(Zolago_Helper_Test::getConditionsArray('code_b_2')),$mapper);
+        $this->assertNotEmpty($mapper_2->getId());
+        
+        // test!
+        $model = Mage::getResourceModel('zolagomapper/mapper');
+        $this->assertNotEmpty($model);
+        $ret = $model->getAttributesByCategory($cid_1->getId());
+        $expected = array (
+            $attr_1->getId() => 'label_3',
+        );
+        $this->assertEquals($expected,$ret);
+        $expected = array (
+            $attr_2->getId() => 'label_4',
+        );
+        $ret = $model->getAttributesByCategory($cid_2->getId());
+        $this->assertEquals($expected,$ret);        
+    }
+    
 }
--- test/Zolago/Mapper/Model/IndexTest.php
+++ test/Zolago/Mapper/Model/IndexTest.php
@@ -11,46 +11,13 @@ class Zolago_Mapper_Model_IndexTest extends ZolagoDb_TestCase {
 		$attributeCode = "my_test";
 		$attributeLabel = "My test";
 		
-		$eav = Mage::getSingleton("eav/config");
-		/* @var $eav Mage_Eav_Model_Config */
-		$catalogInstaller = Mage::getResourceModel("catalog/setup", "core_setup");
-		/* @var $catalogInstaller Mage_Catalog_Model_Resource_Setup */
-		
-		/**
-		 * 1: Add new attribute set
-		 */
-        $entityType = $eav->getEntityType(Mage_Catalog_Model_Product::ENTITY);
-		/* @var $entityType Mage_Eav_Model_Entity_Type */
-		
-        $newSet = Mage::getModel("eav/entity_attribute_set");
-            /* @var $newSet Mage_Eav_Model_Entity_Attribute_Set */
-            
-		$newSet->setAttributeSetName($attributeSetName);
-		$newSet->setEntityTypeId($entityType->getId());
-		$newSet->save();
-		$newSet->setAttributeSetName($newSet->getAttributeSetName() . " " . $newSet->getId());
-		$newSet->initFromSkeleton($entityType->getDefaultAttributeSetId());
-		$newSet->save();
-			
+		$newSet = Zolago_Helper_Test::addAttributeSet($attributeSetName);
 		/* @var $newSet Mage_Eav_Model_Entity_Attribute_Set */
 		
 		// Test is object saved?
 		$this->assertTrue((bool)$newSet->getId());
 		
-		/**
-		 * 2: Add new mapped attribute
-		 */
-		$catalogInstaller->addAttribute($entityType->getId(), $attributeCode, array(
-			'type'                       => 'int',
-			'input'                      => 'select',
-			'source'                     => 'eav/entity_attribute_source_boolean',
-			'global'                     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-			'label'						 => $attributeLabel
-		));
-		$newAttribute = $eav->getAttribute($entityType->getId(), $attributeCode);
-		$newAttribute->setIsMappable(1)->save();
-		/* @var $newAttribute Mage_Eav_Model_Entity_Attribute_Abstract */
-		
+        $newAttribute = Zolago_Helper_Test::addNewAttribute($attributeCode,$attributeLabel);		
 		// Test is object saved?
 		$this->assertTrue((bool)$newAttribute->getId());
 		
@@ -58,6 +25,8 @@ class Zolago_Mapper_Model_IndexTest extends ZolagoDb_TestCase {
 		/**
 		 * 3: Add mapped attribute to set and group
 		 */
+		$catalogInstaller = Zolago_Helper_Test::getCatalogInstaller();
+		$entityType = Zolago_Helper_Test::getEntityType();
 		$catalogInstaller->addAttributeToSet($entityType->getId(), $newSet->getId(), 
 				"General Information", $newAttribute->getId());
 		
@@ -69,26 +38,18 @@ class Zolago_Mapper_Model_IndexTest extends ZolagoDb_TestCase {
 		$category->save();
 		$this->assertTrue((bool)$category->getId());
 		
-		/**
-		 * 5: Add mapper with condiotions (A) and action to category (B)
-		 */
-		$newMapper = $this->_getMapper();
-		$newMapper->setCategoryIds(array($category->getId()));
-		$newMapper->setWebsiteId($this->_getStore()->getWebsiteId());
-		$newMapper->setAttributeSetId($newSet->getId());
-		$newMapper->setData('conditions_serialized', $this->_getConditions());
-		$newMapper->save();
+        $newMapper = Zolago_Helper_Test::addNewMapper($category,$newSet);
 		$this->assertTrue((bool)$newMapper->getId());
-
+		
 		/**
 		 * 6: Add new product with created set and positive matched attribute (P1)
 		 */
-		$productP1 = $this->_getTestedProduct($newSet->getId(), $attributeCode, 1);
+		$productP1 = Zolago_Helper_Test::getNewProduct($newSet->getId(), $attributeCode, 1);
 
 		/**
 		 * 7: Add new product with created set and negative matched attribute (P2)
 		 */	
-		$productP2 = $this->_getTestedProduct($newSet->getId(), $attributeCode, 0);
+		$productP2 = Zolago_Helper_Test::getNewProduct($newSet->getId(), $attributeCode, 0);
 
 		/**
 		 * 8: Save event - save item to queue
@@ -120,31 +81,6 @@ class Zolago_Mapper_Model_IndexTest extends ZolagoDb_TestCase {
 		$this->assertFalse(in_array($category->getId(), $productP2->getCategoryIds()));
 	}
 	
-	/**
-	 * @param type $attributeSetId
-	 * @param type $testCode
-	 * @param type $testValue
-	 * @return Mage_Catalog_Model_Product
-	 */
-	protected function _getTestedProduct($attributeSetId, $testCode, $testValue) {
-		$product = Mage::getModel("catalog/product");
-		$product->setAttributeSetId($attributeSetId);
-		$product->setData($testCode, $testValue);
-		$product->setSku('some-sku-' . (int)$testValue);
-		$product->setTypeId('simple');
-		$product->setName('Some cool product name ' . (int)$testValue);
-		$product->setWebsiteIds(array($this->_getStore()->getWebsite()->getId()));
-		$product->setDescription('Full description here');
-		$product->setShortDescription('Short description here');
-		$product->setPrice(39.99); # Set some price
-		
-		//Default Magento attribute
-		$product->setWeight(4.0000);
-		$product->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH);
-		$product->setStatus(1);
-		$product->setCreatedAt(strtotime('now'));
-		return $product;
-	}
 	
 	/**
 	 * @return Mage_Core_Model_Store
@@ -171,21 +107,5 @@ class Zolago_Mapper_Model_IndexTest extends ZolagoDb_TestCase {
 		return $category;
 	}
 	
-	/**
-	 * @return Zolago_Mapper_Model_Mapper
-	 */
-	protected function _getMapper() {
-		$newMapper = Mage::getModel("zolagomapper/mapper");
-		$newMapper->setData(array(
-            'is_active' => 1,
-            'name' => 'Mapper testowy',
-            'priority' => 0,
-			'category_ids' => array(),
-		));
-		return $newMapper;
-	}
 	
-	protected function _getConditions() {
-		return 'a:7:{s:4:"type";s:22:"rule/condition_combine";s:9:"attribute";N;s:8:"operator";N;s:5:"value";s:1:"1";s:18:"is_value_processed";N;s:10:"aggregator";s:3:"all";s:10:"conditions";a:1:{i:0;a:5:{s:4:"type";s:37:"zolagomapper/mapper_condition_product";s:9:"attribute";s:7:"my_test";s:8:"operator";s:2:"==";s:5:"value";s:1:"1";s:18:"is_value_processed";b:0;}}}';
-	}
 }
