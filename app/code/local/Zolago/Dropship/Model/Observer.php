70c2f89e6e4eecbc1dde3640f5780bbc9c983f45
Piotr Siejczuk <piotr.siejczuk@orba.pl>
2014-03-11 13:27:24 +0100

    re#66: 'Next Cron Check Fix';

--- app/code/local/Zolago/Dropship/Model/Observer.php
+++ app/code/local/Zolago/Dropship/Model/Observer.php
@@ -18,6 +18,24 @@ class Zolago_Dropship_Model_Observer {
         }
         return $this;
     }
-	
 
+    /**
+     * Add Zolago Dhl Data to Save Action
+     *
+     * @param $observer Varien_Event_Observer
+	 * 
+     * @return Zolago_Dropship_Model_Observer
+     */	
+	public function addZolagoDhlData(Varien_Event_Observer $observer)
+	{
+        $track = $observer->getEvent()->getTrack();
+		$carrierCode = $track->getCarrierCode();
+		
+		if ($carrierCode == 'zolagodhl'
+			&& Mage::getSingleton('shipping/config')->getCarrierInstance($carrierCode)->isTrackingAvailable()) {
+			$track->setNextCheck(date('Y-m-d H:i:s', time()));
+		}
+
+		return $this;
+	}
 }
