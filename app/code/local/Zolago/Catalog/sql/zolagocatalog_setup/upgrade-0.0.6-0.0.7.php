41850adeb1533587afd0654005b62183f2433bfe
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-03-14 10:17:40 +0100

    próby poprawienia atrybutów

--- app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.0.6-0.0.7.php
+++ app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.0.6-0.0.7.php
@@ -23,34 +23,38 @@ $installer->addAttribute('catalog_category', 'price_filter_settings', array(
 
 // new attributes (flags, reviews)
 $installer->addAttribute('catalog_product','product_flag', array (
+                             'label' => 'Product Flags',
+                             'type' => 'varchar',
                              'group' => 'General',
                              'input' => 'multiselect',
-                             'source' => 'zolagocatalog/product_source_flag',
                              'backend' => 'eav/entity_attribute_backend_array',
-                             'type' => 'varchar',
-                             'label' => 'Product Flags',
+                             'frontend' => '',
+                             'source' => '',
+                             'global'                    => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
                              'visible' => true,
                              'required' => false,
-                             'visible_on_front' => true,
-                             'global'                    => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
                              'user_defined'              => true,
-                             'default'                   => '',
-                             'position'                  => 110,
+                             'searchable' => false,
+                             'filterable' => false,
+                             'comparable' => false,                                                     
+                             'visible_on_front' => true,
+                             'unique' => 'false',
                              'option' => array (
                                  'value' => array (
-                                      '1' => 'New',
-                                      '2' => 'Sale',
-                                      '3' => 'Promotion',
-                                      '4' => 'Bestseller',
+                                      '1' => array('New'),
+                                      '2' => array('Sale'),
+                                      '3' => array('Promotion'),
+                                      '4' => array('Bestseller'),
                                   ),
-                               )
-                             
+                               )                             
 
                          ));
 $installer->addAttribute('catalog_product','product_rating', array (
                              'group' => 'General',
                              'input' => 'select',
-                             'source' => 'zolagocatalog/product_source_rating',
+                             'source' => '',
+                             'frontend' => '',
+                             'backend' => 'eav/entity_attribute_backend_array',
                              'type' => 'decimal',
                              'label' => 'Product Rating',
                              'visible' => true,
@@ -59,7 +63,17 @@ $installer->addAttribute('catalog_product','product_rating', array (
                              'global'                    => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
                              'user_defined'              => true,
                              'default'                   => '0',
-                             'position'                  => 120
+                             'position'                  => 120,
+                             'option' => array (
+                                 'value' => array (
+                                      '0' => array('no rating'),
+                                      '1' => array('1'),
+                                      '2' => array('2'),
+                                      '3' => array('3'),
+                                      '4' => array('4'),
+                                      '5' => array('5'),
+                                  ),
+                               )                             
 
                          ));
 
@@ -69,4 +83,3 @@ $installer->endSetup();
 
 
 
-$installer = $this;
