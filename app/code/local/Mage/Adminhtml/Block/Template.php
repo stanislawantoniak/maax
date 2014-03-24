27cf85b4077afadc7a7bf2cdaafd52a90f855899
Maciej Babol <maciej.babol@orba.pl>
2014-03-24 17:34:30 +0100

    Fix dziedziecznia

--- app/code/local/Mage/Adminhtml/Block/Template.php
+++ app/code/local/Mage/Adminhtml/Block/Template.php
@@ -1,37 +1,21 @@
 <?php
-/**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Adminhtml
- * @copyright   Copyright (c) 2013 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 
 /**
- * Adminhtml abstract block
- *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @author      Magento Core Team <core@magentocommerce.com>
+ * Fix do dziedziczenia po oryginalnej klasie
+ * Odkomentowac w razei potrzby oryginal
  */
-class Mage_Adminhtml_Block_Template extends Mage_Core_Block_Template
+
+$fileContent = preg_replace(
+		"/Mage_Adminhtml_Block_Template/", 
+		"Mage_Adminhtml_Block_Template_Tmp", 
+		file_get_contents(
+				Mage::getModuleDir("block", "Mage_Adminhtml") . DS . 
+				"Block". DS. "Template.php"
+		));
+
+eval('?>' . $fileContent);
+
+class Mage_Adminhtml_Block_Template extends Mage_Adminhtml_Block_Template_Tmp
 {
     /**
      * Fixed
@@ -45,41 +29,59 @@ class Mage_Adminhtml_Block_Template extends Mage_Core_Block_Template
 		return 'adminhtml/url';
 	}
 
-    /**
-     * Retrieve Session Form Key
-     *
-     * @return string
-     */
-    public function getFormKey()
-    {
-        return Mage::getSingleton('core/session')->getFormKey();
-    }
-
-    /**
-     * Check whether or not the module output is enabled
-     *
-     * Because many module blocks belong to Adminhtml module,
-     * the feature "Disable module output" doesn't cover Admin area
-     *
-     * @param string $moduleName Full module name
-     * @return boolean
-     */
-    public function isOutputEnabled($moduleName = null)
-    {
-        if ($moduleName === null) {
-            $moduleName = $this->getModuleName();
-        }
-        return !Mage::getStoreConfigFlag('advanced/modules_disable_output/' . $moduleName);
-    }
-
-    /**
-     * Prepare html output
-     *
-     * @return string
-     */
-    protected function _toHtml()
-    {
-        Mage::dispatchEvent('adminhtml_block_html_before', array('block' => $this));
-        return parent::_toHtml();
-    }
 }
+
+//class Mage_Adminhtml_Block_Template extends Mage_Core_Block_Template
+//{
+//    /**
+//     * Fixed
+//     *
+//     * @return string
+//     */
+//    public function _getUrlModelClass() {
+//		if($this->getAsFrontend() || Mage::registry('as_frontend')){
+//			return 'core/url';
+//		}
+//		return 'adminhtml/url';
+//	}
+//
+//    /**
+//     * Retrieve Session Form Key
+//     *
+//     * @return string
+//     */
+//    public function getFormKey()
+//    {
+//        return Mage::getSingleton('core/session')->getFormKey();
+//    }
+//
+//    /**
+//     * Check whether or not the module output is enabled
+//     *
+//     * Because many module blocks belong to Adminhtml module,
+//     * the feature "Disable module output" doesn't cover Admin area
+//     *
+//     * @param string $moduleName Full module name
+//     * @return boolean
+//     */
+//    public function isOutputEnabled($moduleName = null)
+//    {
+//        if ($moduleName === null) {
+//            $moduleName = $this->getModuleName();
+//        }
+//        return !Mage::getStoreConfigFlag('advanced/modules_disable_output/' . $moduleName);
+//    }
+//
+//    /**
+//     * Prepare html output
+//     *
+//     * @return string
+//     */
+//    protected function _toHtml()
+//    {
+//        Mage::dispatchEvent('adminhtml_block_html_before', array('block' => $this));
+//        return parent::_toHtml();
+//    }
+//
+//}
+
