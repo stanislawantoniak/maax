8ab2ac6b51dcf6e19933a271d7adcc55834c3d60
Maciej Babol <maciej.babol@orba.pl>
2014-03-25 18:12:29 +0100

    Fix 12

--- app/code/local/Unirgy/Dropship/Controller/VendorAbstract.php
+++ app/code/local/Unirgy/Dropship/Controller/VendorAbstract.php
@@ -61,6 +61,8 @@ class Unirgy_Dropship_Controller_VendorAbstract extends Mage_Core_Controller_Fro
      */
     public function preDispatch()
     {
+		
+		Mage::log("Predispath-start: " . ($session->isOperatorMode() ? "1" : "0") . " " . Mage::app()->getRequest()->getRequestUri());
 		/***********************************************************************
 		 *  Changning locale
 		 ***********************************************************************/
@@ -112,12 +114,12 @@ class Unirgy_Dropship_Controller_VendorAbstract extends Mage_Core_Controller_Fro
                 Mage::getConfig()->setNode('global/models/core/rewrite/url', 'Unirgy_Dropship_Model_Url');
             }
         }
-		Mage::log("Predispath: " . ($session->isOperatorMode() ? "1" : "0") . " " . Mage::app()->getRequest()->getRequestUri());
+		Mage::log("Predispath-stop: " . ($session->isOperatorMode() ? "1" : "0") . " " . Mage::app()->getRequest()->getRequestUri());
     }
 
 	public function postDispatch() {
 		$session = Mage::getSingleton('udropship/session');
-		Mage::log("Postdispathc: " . ($session->isOperatorMode() ? "1" : "0") . " " . Mage::app()->getRequest()->getRequestUri());
+		Mage::log("Postdispatch: " . ($session->isOperatorMode() ? "1" : "0") . " " . Mage::app()->getRequest()->getRequestUri());
 		parent::postDispatch();
 	}
 	
