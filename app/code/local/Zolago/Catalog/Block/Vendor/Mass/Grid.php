a2eaba1c9da3400c0d2c50c302ac88c4786485e7
Maciej Babol <maciej.babol@orba.pl>
2014-03-26 17:16:44 +0100

    Pierwsze podejscie do kolumn

--- app/code/local/Zolago/Catalog/Block/Vendor/Mass/Grid.php
+++ app/code/local/Zolago/Catalog/Block/Vendor/Mass/Grid.php
@@ -13,53 +13,148 @@ class Zolago_Catalog_Block_Vendor_Mass_Grid extends Mage_Adminhtml_Block_Widget_
 	protected function _prepareCollection(){
         $collection = Mage::getResourceModel('catalog/product_collection');
         /* @var $collection Mage_Catalog_Model_Resource_Product_Collection */
-		$collection->addAttributeToSelect("sku");
-		$collection->addAttributeToSelect("name");
-		$collection->addPriceData();
+		// Add non-grid filters
 		$collection->addAttributeToFilter("udropship_vendor", $this->getVendorId());
 		$collection->addAttributeToFilter("attribute_set_id", $this->getAttributeSet()->getId());
+		// Set store id
 		$store = $this->getStore();
 		if(!Mage::app()->isSingleStoreMode() && !$store->isAdmin()){
 			$collection->setStoreId($store->getId());
 			$collection->addWebsiteFilter($store->getWebsite());
 		}
+		// Add static attrs
+		$collection->addAttributeToSelect("sku");
+		$collection->addAttributeToSelect("name");
+		$collection->addPriceData();
+		
+		foreach($this->_getGridVisibleAttributes() as $attribute){
+			/* @var $attribute Mage_Catalog_Model_Resource_Eav_Attribute */
+			$collection->addAttributeToSelect($attribute->getAttributeCode());
+		}
+		
         $this->setCollection($collection);
         return parent::_prepareCollection();
     }
+	
+	protected function _prepareStaticStartColumns(){
+		 $static = $this->_getFixedColumns();
+		 if(isset($static['start'])){
+			 foreach($static['start'] as $key=>$column){
+				 $this->addColumn($key, $column);
+			 }
+		 }
+	}
+	
+	protected function _prepareStaticEndColumns(){
+		 $static = $this->_getFixedColumns();
+		 if(isset($static['end'])){
+			 foreach($static['end'] as $key=>$column){
+				 $this->addColumn($key, $column);
+			 }
+		 }
+	}
+
+	protected function	_getFixedColumns(){
+		return array(
+			"start" => array(
+				"entity_id"=> array(
+					"index"=>"entity_id", 
+					"type"=>"number",
+					"header"=> Mage::helper("zolagocatalog")->__("ID"),
+					"width" => "50px"
+				)
+			),
+			"end" => array(
+				"price" => array(
+					"index"			=> "price",
+					'type'			=> 'price',
+					'currency_code' => $this->getStore()->getBaseCurrency()->getCode(),
+					"header"		=> Mage::helper("zolagocatalog")->__("Price"),
+				)
+			)
+		);
+	}
 
     protected function _prepareColumns() {
-        $store = Mage::app()->getStore();
-        $this->addColumn("entity_id", array(
-			"index"=>"entity_id", 
-			"type"=>"number",
-			"header"=> Mage::helper("zolagocatalog")->__("ID"),
-			"width" => "100px"
-		));
+       
+		$this->_prepareStaticStartColumns();
         
-        $this->addColumn("sku", array(
-			"index"=>"sku", 
-			"width" => "150px",
-			"header"=>Mage::helper("zolagocatalog")->__("SKU"))
-		);
-        
-        
-        $this->addColumn("name", array(
-            "index"     =>"name",
-            "header"    => Mage::helper("zolagocatalog")->__("Name"),
-        ));
-        $this->addColumn("price", array(
-            "index"     => "price",
-			'type'  => 'price',
-			'currency_code' => $store->getBaseCurrency()->getCode(),
-            "header"    => Mage::helper("zolagocatalog")->__("Price"),
-        ));
+		$attributeCollection = $this->_getGridVisibleAttributes();
+		$count = $attributeCollection->count();
+		
+		foreach($attributeCollection as $attribute){
+			/* @var $attribute Mage_Catalog_Model_Resource_Eav_Attribute */
+			$attribute->setStoreId($this->getStore()->getId());
+			$code = $attribute->getAttributeCode();
+			$data = array(
+				"index"     => $code,
+				'type'		=> $this->_getColumnType($attribute),
+				"header"    => $this->_getColumnLabel($attribute),
+			);
+			$this->addColumn($code,  $this->_processColumnConfig($attribute, $data));
+		}
+		
+        $this->_prepareStaticEndColumns();
+		
         return parent::_prepareColumns();
     }
 
 	public function getGridUrl() {
 		return $this->getUrl("*/*/grid", array("_current"=>true));
 	}
+	
+	protected function _processColumnConfig(Mage_Catalog_Model_Resource_Eav_Attribute $attribute, array $config){
+		$extend = array();
+		// Process select
+		if(in_array($attribute->getFrontendInput(), array("select", "multiselect", "boolean"))){
+			$extend['type'] = "options";
+			if($attribute->getSource()){
+				$extend['options']  = array();
+				foreach($attribute->getSource()->getAllOptions(false) as $option){
+					$extend['options'][$option['value']]=$option['label'];
+				}
+			}
+		}
+		return array_merge($config, $extend);
+	}
+	
+	protected function _getColumnLabel(Mage_Catalog_Model_Resource_Eav_Attribute $attribute){
+		 return $attribute->getStoreLabel($this->getStore()->getId());
+	}
 
+
+	protected function _getColumnType(Mage_Catalog_Model_Resource_Eav_Attribute $attribute) {
+		switch ($attribute->getBackendType()) {
+			case "text":
+			case "varchar":
+				return "text";
+			break;
+			case "int":
+			case "decimal":
+				return "number";
+			break;
+			case "datetime":
+				return "datetime";
+			break;
+			
+		}
+		return "text";
+	}
+	
+	/**
+	 * @return Mage_Catalog_Model_Resource_Product_Attribute_Collection
+	 */
+	protected function _getGridVisibleAttributes() {
+		$collection = Mage::getResourceModel("catalog/product_attribute_collection");
+		/* @var $collection Mage_Catalog_Model_Resource_Product_Attribute_Collection */
+		$collection->setAttributeSetFilter($this->getAttributeSet()->getId());
+		$collection->addFieldToFilter("grid_permission", array("in"=>array(
+			Zolago_Eav_Model_Entity_Attribute_Source_GridPermission::DISPLAY,
+			Zolago_Eav_Model_Entity_Attribute_Source_GridPermission::EDITION,
+			Zolago_Eav_Model_Entity_Attribute_Source_GridPermission::INLINE_EDITION,
+		)));
+		return $collection;
+	}
 
 	protected function _prepareMassaction()
     {
@@ -96,14 +191,24 @@ class Zolago_Catalog_Block_Vendor_Mass_Grid extends Mage_Adminhtml_Block_Widget_
 	 * @return Mage_Eav_Model_Entity_Attribute_Set
 	 */
 	public function getAttributeSet() {
-		return $this->getParentBlock()->getCurrentAttributeSet();
+		if($this->getParentBlock()){
+			return $this->getParentBlock()->getCurrentAttributeSet();
+		}
+		return Mage::getModel("eav/entity_attribute_set")->load(
+			Mage::app()->getRequest()->getParam("attribute_set")
+		);
 	}
 	
 	/**
 	 * @return Mage_Core_Model_Store
 	 */
 	public function getStore() {
-		return $this->getParentBlock()->getCurrentStore();
+		if($this->getParentBlock()){
+			return $this->getParentBlock()->getCurrentStore();
+		}
+		return Mage::app()->getStore(
+			Mage::app()->getRequest()->getParam("store", 0)
+		);
 	}
     
 
