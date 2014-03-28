bb736e6ef3558e7232e6d351fd7532b2605d2674
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-03-28 10:07:52 +0100

    zmiany unirgy

--- app/code/community/Unirgy/DropshipVendorAskQuestion/Block/Vendor/Question.php
+++ app/code/community/Unirgy/DropshipVendorAskQuestion/Block/Vendor/Question.php
@@ -1,140 +1,128 @@
 <?php
 
-class Unirgy_DropshipVendorAskQuestion_Block_Vendor_Question extends Mage_Core_Block_Template
+class Unirgy_DropshipVendorAskQuestion_Block_Vendor_Questions extends Mage_Core_Block_Template
 {
-    protected $_form;
-    protected $_question;
+    protected $_collection;
+    protected $_oldStoreId;
+    protected $_unregUrlStore;
 
     protected function _beforeToHtml()
     {
         parent::_beforeToHtml();
 
-        Varien_Data_Form::setFieldsetRenderer(
-            $this->getLayout()->createBlock('udqa/vendor_question_renderer_fieldset')
-        );
-        Varien_Data_Form::setFieldsetElementRenderer(
-            $this->getLayout()->createBlock('udqa/vendor_question_renderer_fieldsetElement')
-        );
+        if (!Mage::registry('url_store')) {
+            $this->_unregUrlStore = true;
+            Mage::register('url_store', Mage::app()->getStore());
+        }
+        $this->_oldStoreId = Mage::app()->getStore()->getId();
+        Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+        if ($toolbar = $this->getLayout()->getBlock('udqa.grid.toolbar')) {
+            $toolbar->setCollection($this->getQuestionsCollection());
+        }
 
         return $this;
     }
-    public function getForm()
-    {
-        if (null === $this->_form) {
-            $question = $this->getQuestion();
-            $this->_form = new Varien_Data_Form();
-            $this->_form->setDataObject($question);
-            $values = $question->getData();
 
-            if (($udFormData = Mage::getSingleton('udropship/session')->getUdqaData(true))
-                && is_array($udFormData)
-            ) {
-                $values = array_merge($values, $udFormData);
+    protected function _getUrlModelClass()
+    {
+        return 'core/url';
+    }
+    public function getUrl($route = '', $params = array())
+    {
+        if (!isset($params['_store']) && $this->_oldStoreId) {
+            $params['_store'] = $this->_oldStoreId;
+        }
+        return parent::getUrl($route, $params);
+    }
+
+    protected function _afterToHtml($html)
+    {
+        if ($this->_unregUrlStore) {
+            $this->_unregUrlStore = false;
+            Mage::unregister('url_store');
+        }
+        Mage::app()->setCurrentStore($this->_oldStoreId);
+        return parent::_afterToHtml($html);
+    }
+
+    protected function _applyRequestFilters($collection)
+    {
+        $r = Mage::app()->getRequest();
+        $param = $r->getParam('filter_product_sku');
+        if (!is_null($param) && $param!=='') {
+            $collection->addFieldToFilter('product_sku', array('like'=>$param.'%'));
+        }
+        $param = $r->getParam('filter_product_name');
+        if (!is_null($param) && $param!=='') {
+            $collection->addFieldToFilter('product_name', array('like'=>'%'.$param.'%'));
+        }
+        $param = $r->getParam('filter_customer_name');
+        if (!is_null($param) && $param!=='') {
+            $collection->addFieldToFilter('customer_name', array('like'=>'%'.$param.'%'));
+        }
+        $param = $r->getParam('filter_replied');
+        if (!is_null($param) && $param!=='') {
+            if ($param) {
+                $collection->addFieldToFilter('answer_text', array('gt'=>0,'field_expr'=>'LENGTH(#?)'));
+            } else {
+                $collection->addFieldToFilter('answer_text', array(array('null'=>1),array('eq'=>0,'field_expr'=>'LENGTH(#?)')));
             }
-
-            $this->_addDetailsFieldset($question, $values);
-
-            $this->_form->addValues($values);
-
-            $this->_form->setFieldNameSuffix('question');
         }
-        return $this->_form;
+        $param = $r->getParam('filter_question');
+        if (!is_null($param) && $param!=='') {
+            $collection->addFieldToFilter('question_text', array('like'=>'%'.$param.'%'));
+        }
+        $param = $r->getParam('filter_answer');
+        if (!is_null($param) && $param!=='') {
+            $collection->addFieldToFilter('answer_text', array('like'=>'%'.$param.'%'));
+        }
+        if (($v = $r->getParam('filter_order_id_from'))) {
+            $collection->addFieldToFilter('order_increment_id', array('gteq'=>$v));
+        }
+        if (($v = $r->getParam('filter_order_id_to'))) {
+            $collection->addFieldToFilter('order_increment_id', array('lteq'=>$v));
+        }
+
+        if (($v = $r->getParam('filter_question_date_from'))) {
+            $collection->addFieldToFilter('question_date', array('gteq'=>Mage::helper('udropship')->dateLocaleToInternal($v, null, true)));
+        }
+        if (($v = $r->getParam('filter_question_date_to'))) {
+            $_filterDate = Mage::app()->getLocale()->date();
+            $_filterDate->set($v, Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT));
+            $_filterDate->addDay(1);
+            $_filterDate->setTimezone(Mage_Core_Model_Locale::DEFAULT_TIMEZONE);
+            $collection->addFieldToFilter('question_date', array('lteq'=>$_filterDate->toString(Varien_Date::DATETIME_INTERNAL_FORMAT)));
+
+        }
+        $collection->addVendorFilter($this->getVendor()->getId());
+        $collection->addApprovedQuestionsFilter();
+        return $this;
     }
-    public function getQuestion()
+
+    public function getVendor()
     {
-        if (null === $this->_question) {
-            $this->_question = Mage::getModel('udqa/question')->load(
-                Mage::app()->getRequest()->getParam('id')
-            );
-        }
-        return $this->_question;
+        return Mage::getSingleton('udropship/session')->getVendor();
     }
-    protected function _addDetailsFieldset($question, &$values)
+
+    public function getQuestionsCollection()
     {
-        $fieldset = $this->_form->addFieldset('details',
-            array(
-                'legend'=>Mage::helper('udprod')->__('Question Details'),
-                'class'=>'fieldset-wide',
-        ));
-        $this->_addElementTypes($fieldset);
+        if (!$this->_collection) {
+            $v = Mage::getSingleton('udropship/session')->getVendor();
+            if (!$v || !$v->getId()) {
+                return array();
+            }
+            $r = Mage::app()->getRequest();
+            $res = Mage::getSingleton('core/resource');
+            $collection = Mage::getModel('udqa/question')->getCollection();
+            $collection->joinShipments()->joinProducts()->joinVendors();
+            $collection->getSelect()->columns(array('is_replied' => new Zend_Db_Expr('if(LENGTH(answer_text)>0,1,0)')));
 
-        $data = new Varien_Object($values);
+            $this->_applyRequestFilters($collection);
 
-        $fieldset->addField('question_date', 'note', array(
-            'name' => 'question_date',
-            'label' => $this->__('QUESTION DATE'),
-            'image' => $this->getSkinUrl('images/grid-cal.gif'),
-            'format' => Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT),
-            'input_format' => Varien_Date::DATE_INTERNAL_FORMAT,
-            'text' => $this->formatDate($data->getQuestionDate()),
-            'is_wide'=>true,
-            'is_top'=>true,
-            'disabled'=>'disabled'
-        ));
-
-        $fieldset->addField('customer_name', 'note', array(
-            'name' => 'customer_name',
-            'label' => $this->__('CUSTOMER NAME'),
-            'text' => $data->getCustomerName(),
-            'is_wide'=>true,
-            'is_top'=>true,
-        ));
-
-        if ($data->getShipmentId()) {
-            $fieldset->addField('shipment_id', 'note', array(
-                'name' => 'shipment_id',
-                'label' => $this->__('SHIPMENT'),
-                'text' => '<a href="'.$this->getShipmentUrl($data).'">'.$this->__('#%s for order #%s', $data->getShipmentIncrementId(), $data->getOrderIncrementId()).'</a>',
-                'is_wide'=>true,
-                'is_top'=>true,
-            ));
+            $this->_collection = $collection;
         }
-
-        if ($data->getProductId()) {
-            $fieldset->addField('product_id', 'note', array(
-                'name' => 'product_id',
-                'label' => $this->__('PRODUCT'),
-                'text' => '<a href="'.$this->getProductUrl($data).'">SKU: '.$data->getProductSku().' '.$data->getProductName().'</a>',
-                'is_wide'=>true,
-                'is_top'=>true,
-            ));
-        }
-
-        /*
-        $fieldset->addField('customer_email', 'text', array(
-            'name' => 'customer_email',
-            'label' => $this->__('Customer email'),
-            'disabled'=>'disabled'
-        ));
-        */
-
-        $fieldset->addField('vendor_id', 'hidden', array(
-            'name' => 'vendor_id',
-            'label' => $this->__('Vendor'),
-            'is_wide'=>true,
-            'is_hidden'=>true
-        ));
-
-        $fieldset->addField('question_text', 'note', array(
-            'name' => 'question_text',
-            'label' => $this->__('Question Text'),
-            'required' => true,
-            'is_wide'=>true,
-            'is_bottom'=>true,
-            'text' => $data->getQuestionText()
-        ));
-
-        $fieldset->addField('answer_text', 'editor', array(
-            'name' => 'answer_text',
-            'label' => $this->__('Answer Text'),
-            'title' => $this->__('Answer Text'),
-            'wysiwyg' => false,
-            'required' => true,
-            'is_wide'=>true,
-            'is_bottom'=>true,
-        ));
-        
-        $this->_prepareFieldsetColumns($fieldset);
+        return $this->_collection;
     }
 
     public function getShipmentUrl($question)
@@ -157,84 +145,6 @@ class Unirgy_DropshipVendorAskQuestion_Block_Vendor_Question extends Mage_Core_B
             return $hlp->getUrl('adminhtml/catalog_product/edit', $params);
         } else {
             return Mage::getUrl('udropship/vendor/product', array('_query'=>'filter_sku='.$question->getProductSku()));
-        }
-    }
-
-    protected function _prepareFieldsetColumns($fieldset)
-    {
-        $elements = $fieldset->getElements()->getIterator();
-        reset($elements);
-        $fullCnt = count($elements);
-        $wideElementsBottom = $wideElements = $lcElements = $rcElements = array();
-        while($element=current($elements)) {
-            if ($element->getIsWide()) {
-                if ($element->getIsBottom()) {
-                    $wideElementsBottom[] = $element->getId();
-                } else {
-                    $wideElements[] = $element->getId();
-                }
-                $fullCnt--;
-            }
-            next($elements);
-        }
-        $halfCnt = ceil($fullCnt/2);
-        reset($elements);
-        $i=0; while ($element=current($elements)) {
-            if (!$element->getIsWide()) {
-                $lcElements[] = $element->getId();
-                $i++;
-            }
-            next($elements);
-            if ($i>=$halfCnt) break;
-        }
-        while ($element=current($elements)) {
-            if (!$element->getIsWide()) {
-                $rcElements[] = $element->getId();
-            }
-            next($elements);
-        }
-        $fieldset->setWideColumnTop($wideElements);
-        $fieldset->setWideColumnBottom($wideElementsBottom);
-        $fieldset->setLeftColumn($lcElements);
-        $fieldset->setRightColumn($rcElements);
-        reset($elements);
-        return $this;
-    }
-    protected $_additionalElementTypes = null;
-    protected function _initAdditionalElementTypes()
-    {
-        if (is_null($this->_additionalElementTypes)) {
-            $result = array();
-
-            $response = new Varien_Object();
-            $response->setTypes(array());
-            Mage::dispatchEvent('udqa_question_edit_element_types', array('response'=>$response));
-
-            foreach ($response->getTypes() as $typeName=>$typeClass) {
-                $result[$typeName] = $typeClass;
-            }
-            $this->_additionalElementTypes = $result;
-        }
-        return $this;
-    }
-
-    protected function _getAdditionalElementTypes()
-    {
-        $this->_initAdditionalElementTypes();
-        return $this->_additionalElementTypes;
-    }
-    public function addAdditionalElementType($code, $class)
-    {
-        $this->_initAdditionalElementTypes();
-        $this->_additionalElementTypes[$code] = Mage::getConfig()->getBlockClassName($class);
-        return $this;
-    }
-
-    protected function _addElementTypes(Varien_Data_Form_Abstract $baseElement)
-    {
-        $types = $this->_getAdditionalElementTypes();
-        foreach ($types as $code => $className) {
-            $baseElement->addType($code, $className);
         }
     }
 }
