37d1b9849d064cafdac70b9968a25f21ddca7d02
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-04-18 13:03:19 +0200

    kolejka do wczytywania zdjęć

--- app/code/local/Zolago/Catalog/Block/Vendor/Image/Queue.php
+++ app/code/local/Zolago/Catalog/Block/Vendor/Image/Queue.php
@@ -1,9 +1,10 @@
 <?php
-class Zolago_Catalog_Block_Vendor_Image extends Mage_Core_Block_Template {
-    
+class Zolago_Catalog_Block_Vendor_Image_Queue extends Mage_Core_Block_Template {
+
     protected function _prepareLayout() {   
         $locale = Mage::app()->getLocale()->getLocaleCode();
         $this->getLayout()->getBlock('head')->addItem('skin_js','plugins/plupload/i18n/'.$locale.'.js');
         parent::_prepareLayout();
     }    
+    
 }
