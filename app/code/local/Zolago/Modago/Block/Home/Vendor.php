cfb86fd9eda5d26038bc5c50b27cd148385a3cbb
Paweł Chyl <pawel.chyl@orba.pl>
2014-04-25 16:24:19 +0200

    Block naming convention changed

--- app/code/local/Zolago/Modago/Block/Home/Vendor.php
+++ app/code/local/Zolago/Modago/Block/Home/Vendor.php
@@ -6,5 +6,12 @@
 
 class Zolago_Modago_Block_Home_Vendor extends Mage_Core_Block_Template
 {
-
+/*
+ * Potrzebuję tutaj kolekcję, która będzie zawierać:
+ * - obrazek maksymalnie 130x74px
+ * - url gdzie ma kierować element listy lub klucz i nazwę routa
+ * - tekst - nazwę vendora
+ *
+ * Dodatkowo URL do ZOBACZ WIĘCEJ MAREK
+ */
 } 
