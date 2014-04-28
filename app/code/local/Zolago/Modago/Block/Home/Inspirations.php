9a2afc70b1b906f977cb2c326fc95c65f54520cc
Paweł Chyl <pawel.chyl@orba.pl>
2014-04-28 11:36:05 +0200

    Inspirations block added

---
+++ app/code/local/Zolago/Modago/Block/Home/Inspirations.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Paweł Chyl <pawel.chyl@orba.pl>
+ * Date: 28.04.14
+ */
+
+class Zolago_Modago_Block_Home_Inspirations extends Mage_Core_Block_Template
+{
+/**
+ * Potrzebuję kolekcji inspiracji, której każdy element będzie zawierał:
+ * - Zdjęcie inspiracji o wymiarach maksymalnie: 208x312 px
+ * - opis najlepiej podzielony na tytuł inspiracji oraz drugi wiersz opisu
+ * - URL lub routing z kluczami prowadzący do inspiracji
+ */
+} 
