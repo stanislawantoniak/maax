405c521117fc135461a264d20d52b5c3110b57eb
Maciej Babol <maciej.babol@orba.pl>
2014-04-28 11:29:04 +0200

    Dodanie logiki do bloku vendors

--- app/code/local/Zolago/Modago/controllers/IndexController.php
+++ app/code/local/Zolago/Modago/controllers/IndexController.php
@@ -2,6 +2,16 @@
 class Zolago_Modago_IndexController extends Mage_Core_Controller_Front_Action{
 	public function indexAction() {
 		$this->loadLayout();
+		/*
+		$block = $this->getLayout()->getBlock("zolago_modago_home_popularvendors");
+		foreach($block->getVendorColleciton() as $vendor){
+			echo $block->getVendorName($vendor) . "<br/>";
+			echo $block->getVendorMarkUrl($vendor) . "<br/>";
+			echo $block->getVendorBaseUrl($vendor) . "<br/>";
+			echo $block->getVendorResizedLogoUrl($vendor) . "<br/>";
+		}
+		die;
+		*/
 		$this->renderLayout();
 	}
 }
