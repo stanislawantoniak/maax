ddf634d8b5ac414ca264bb13c9421434be352d8d
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-05-29 19:35:12 +0300

    Refs#59  Configurable product price should be the least of the associated simple products. The options should have price diff set up accordingly
    updateAttributeNoIndex

--- app/code/local/Zolago/Catalog/Model/Product/Action.php
+++ app/code/local/Zolago/Catalog/Model/Product/Action.php
@@ -1,22 +1,38 @@
 <?php
 class Zolago_Catalog_Model_Product_Action extends Mage_Catalog_Model_Product_Action {
 
+
+
     public function updateAttributes($productIds, $attrData, $storeId)
     {
+        $ret = parent::updateAttributes($productIds, $attrData, $storeId);
+        // Add after event
+        Mage::dispatchEvent('catalog_product_attribute_update_after', array(
+            'attributes_data' => $attrData,
+            'product_ids' => $productIds,
+            'store_id' => $storeId
+        ));
+        return $ret;
+    }
+
+    public function updateAttributesNoIndex($productIds, $attrData, $storeId)
+    {
+
         Mage::dispatchEvent('catalog_product_attribute_update_before', array(
             'attributes_data' => &$attrData,
-            'product_ids'   => &$productIds,
-            'store_id'      => &$storeId
+            'product_ids' => &$productIds,
+            'store_id' => &$storeId
         ));
 
         $this->_getResource()->updateAttributes($productIds, $attrData, $storeId);
         $this->setData(array(
-            'product_ids'       => array_unique($productIds),
-            'attributes_data'   => $attrData,
-            'store_id'          => $storeId
+            'product_ids' => array_unique($productIds),
+            'attributes_data' => $attrData,
+            'store_id' => $storeId
         ));
 
 
         return $this;
     }
+
 }
