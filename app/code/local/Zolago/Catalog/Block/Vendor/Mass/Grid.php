f3216d15ba7476d8bafa6ee56bc00c9de18eccaf
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-06-03 11:37:50 +0300

    Refs#226 Being a vendor I want to see in nice bootstrap theme in product mass action grid
    ajax turn off

--- app/code/local/Zolago/Catalog/Block/Vendor/Mass/Grid.php
+++ app/code/local/Zolago/Catalog/Block/Vendor/Mass/Grid.php
@@ -10,7 +10,7 @@ class Zolago_Catalog_Block_Vendor_Mass_Grid extends Mage_Adminhtml_Block_Widget_
         $this->setDefaultSort('entity_id');
         $this->setDefaultDir('desc');
         $this->setGridClass('z-grid');
-        $this->setUseAjax(true);
+
         $this->setSaveParametersInSession(true);
 		$this->setTemplate("zolagocatalog/widget/grid.phtml");
 		// Add custom renderes
@@ -50,10 +50,7 @@ class Zolago_Catalog_Block_Vendor_Mass_Grid extends Mage_Adminhtml_Block_Widget_
 		}
 		return parent::_setCollectionOrder($column);
 	}
-    public function getGridUrl()
-    {
-        return $this->getUrl('*/*/grid', array('_current'=>true));
-    }
+
 	public function getPopupContent() {
 		return $this->getChildHtml('popup_content');
 	}
