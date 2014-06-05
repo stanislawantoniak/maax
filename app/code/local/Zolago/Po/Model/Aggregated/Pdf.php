9a7e11c4c8589d931c22b336cdac890e63fd8b90
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-06-05 10:39:49 +0200

    tłumaczenia

--- app/code/local/Zolago/Po/Model/Aggregated/Pdf.php
+++ app/code/local/Zolago/Po/Model/Aggregated/Pdf.php
@@ -199,9 +199,9 @@ class Zolago_Po_Model_Aggregated_Pdf extends Varien_Object {
         $page->setLineWidth(1);
         $page->setLineDashingPattern(array(2, 1, 2, 1), 1.6);
         $page->drawLine(45,$rel-98,210,$rel-98);
+        $page->drawText(Mage::helper('zolagopo')->__('Customer signature'),305,$rel-110,'UTF-8');
         $page->drawText(Mage::helper('zolagopo')->__('DHL signature'),45,$rel-110,'UTF-8');
         $page->drawLine(305,$rel-98,500,$rel-98);
-        $page->drawText(Mage::helper('zolagopo')->__('Customer signature'),305,$rel-110,'UTF-8');
     }
     // preparing date from - to
     // additionally gets courier name
