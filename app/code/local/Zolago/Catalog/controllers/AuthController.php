a7c5201a2fe2e018520a02d7f2676dd2883fd530
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-06-05 18:22:31 +0300

    price A

--- app/code/local/Zolago/Catalog/controllers/AuthController.php
+++ app/code/local/Zolago/Catalog/controllers/AuthController.php
@@ -3,6 +3,57 @@
 class Zolago_Catalog_AuthController extends Mage_Core_Controller_Front_Action
 {
 
+    public function indexAction(){
+        $data = array(
+            'cmd' => 'ProductPricesUpdate',
+            'merchant' => 4,
+            'data' => array(
+                array('price' => 584.63,
+                    'price_id' => "marketPrice"
+                ),
+                array('price' => 575.52,
+                    'price_id' => "A"
+                )
+            ),
+            'sku' => "32345-01X-65C"
+        );
+
+
+        if (!empty($data)) {
+            $productAction = Mage::getSingleton('catalog/product_action');
+            $merchant = $data['merchant'];
+            $skuV = $data['sku'];
+
+            $sku = $merchant . '-' . $skuV;
+            $productId = Zolago_Catalog_Helper_Data::getSkuAssocId($sku);
+            if ($productId) {
+
+                $prices = isset($data['data']) ? $data['data'] : array();
+                if(!empty($prices)){
+
+                    $priceA = FALSE;
+                    foreach ($prices as $pricesItem) {
+                        if ($pricesItem['price_id'] == "A") {
+                            $priceA = $pricesItem['price'];
+                        }
+                    }
+
+
+                    $productIds = array($productId);
+                    $attrData = array('price' => $priceA);
+
+                    $productAction->updateAttributesNoIndex($productIds, $attrData, 0);
+                    $productAction->updateAttributesNoIndex($productIds, $attrData, 1);
+                    $productAction->updateAttributesNoIndex($productIds, $attrData, 2);
+
+                    Zolago_Catalog_Helper_Configurable::queueProduct($productId);
+                }
+
+
+            }
+        }
+    }
+
     public function configurableAction()
     {
         Zolago_Catalog_Model_Observer::processConfigurableQueue();
