c4e38f2bd520e89cfc3f8ce91485bbff235b8b6a
Maciej Babol <maciej.babol@orba.pl>
2014-06-09 17:05:42 +0200

    Grid rma

---
+++ app/code/local/Zolago/Rma/Block/Vendor/Rma/Grid/Column/Renderer/Products.php
@@ -0,0 +1,18 @@
+<?php
+
+class Zolago_Rma_Block_Vendor_Rma_Grid_Column_Renderer_Products 
+	extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+	public function render(Varien_Object $row) {
+		$items = $row->getData($this->getColumn()->getIndex());
+		$return = "";
+		if(is_array($items)){
+			foreach($items as $item){
+				/* @var $item Zolago_Rma_Model_Rma_Item */
+				$return .=  "[" . $item->getItemConditionName() . "] " .
+						$this->escapeHtml($item->getName()) . "<br/>" ;
+			}
+		}
+		return $return;
+	}
+}
