848386385d16de6f34bcdb278ee327f8c4e1b359
Maciej Babol <maciej.babol@orba.pl>
2014-06-11 15:09:32 +0200

    Dodanie wlasciwosci vendora i konfiguracji max czas wysylki

---
+++ app/code/local/Zolago/Dropship/Model/Vendor.php
@@ -0,0 +1,21 @@
+<?php
+
+class Zolago_Dropship_Model_Vendor extends Unirgy_Dropship_Model_Vendor
+{
+	public function getMaxShippingDate($storeId=null)
+    {
+        $maxShippingDate = $this->getData('max_shipping_date');
+        if (is_null($maxShippingDate) || $maxShippingDate=="" || $maxShippingDate==0) {
+            $maxShippingDate = Mage::getStoreConfig('udropship/vendor/max_shipping_date', $storeId);
+        }
+        return $maxShippingDate;
+    }
+	
+	protected function _beforeSave() {
+		if($this->getData("max_shipping_date")=="" || $maxShippingDate==0){
+			$this->setData("max_shipping_date", null);
+		}
+		return parent::_beforeSave();
+	}
+
+}
