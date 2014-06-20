9b2ab2461132f386a333750e6d2d7cb888d95179
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-06-20 14:11:39 +0300

    Refs#250 Being a system admin I want to set product price to A, B, D or Z converter value with a % margin
    attributes
    price type
    margin
    installation

---
+++ app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.0.12-0.0.13.php
@@ -0,0 +1,91 @@
+<?php
+
+$installer = $this;
+/* @var $installer Mage_Core_Model_Resource_Setup */
+
+$installer->startSetup();
+
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
+//Adding Attribute converter_price_type
+$attributePriceTypeCode = "converter_price_type";
+$attributePriceType = $setup->addAttribute(
+    Mage_Catalog_Model_Product::ENTITY, $attributePriceTypeCode,
+    array(
+         'group'      => 'Price',
+         'type'       => 'int',
+         'input'      => 'select',
+         'label'      => 'Converter Price Type',
+         'sort_order' => 2,
+         'set_id'     => 4,
+         'required'   => false,
+         'global'     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+         'backend'    => 'eav/entity_attribute_backend_array',
+         'option'     => array(
+             'values' => array(
+                 0 => 'A',
+                 1 => 'B',
+                 2 => 'D',
+                 3 => 'Z',
+             )
+         ),
+
+    )
+);
+//Adding Attribute price_margin
+$attributePriceMarginCode = "price_margin";
+$attributePriceMargin = $setup->addAttribute(
+    Mage_Catalog_Model_Product::ENTITY, $attributePriceMarginCode,
+    array(
+         'group'      => 'Price',
+         'type'       => 'text',
+         'input'      => 'text',
+         'label'      => 'Price margin, %',
+         'sort_order' => 3,
+         'set_id'     => 4,
+         'required'   => false,
+         'global'     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+         'backend'    => ''
+    )
+);
+
+$entityTypeId = Mage::getModel('eav/entity')
+    ->setType('catalog_product')
+    ->getTypeId();
+
+$attributePriceTypeId = $attributePriceType->getAttributeId($entityTypeId, $attributePriceTypeCode);
+$attributePriceMarginId = $attributePriceMargin->getAttributeId($entityTypeId, $attributePriceMarginCode);
+
+$collection = Mage::getResourceModel('eav/entity_attribute_set_collection');
+$collection
+    ->getSelect()
+    ->reset('columns')
+    ->join(
+        array('attribute_group' => 'eav_attribute_group'),
+        'attribute_group.attribute_set_id=main_table.attribute_set_id',
+        array(
+             'main_table.attribute_set_id AS set',
+             'attribute_group_id AS group'
+        )
+    )
+    ->where("attribute_group_name='Prices'");
+$attributeSetsGroups = $collection->getData();
+
+if (!empty($attributeSetsGroups)) {
+    foreach ($attributeSetsGroups as $attributeSetsGroupsItem) {
+        $setup->addAttributeToSet(
+            Mage_Catalog_Model_Product::ENTITY,
+            (int)$attributeSetsGroupsItem['set'],
+            (int)$attributeSetsGroupsItem['group'],
+            $attributePriceTypeId
+        );
+        $setup->addAttributeToSet(
+            Mage_Catalog_Model_Product::ENTITY,
+            (int)$attributeSetsGroupsItem['set'],
+            (int)$attributeSetsGroupsItem['group'],
+            $attributePriceMarginId
+        );
+    }
+}
+
+$installer->endSetup();
