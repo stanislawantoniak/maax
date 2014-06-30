aeac3ed99527c7810b9be77805ef334e889ca3cf
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-06-30 14:39:28 +0300

    multiupdate
    test

--- app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
+++ app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
@@ -58,7 +58,7 @@ class Zolago_Catalog_Model_Api2_Restapi_Rest_Admin_V1
                         if(!empty($batch)){
                             $batch = (array)$batch;
                             foreach($batch as $dataPrice){
-                                Mage::log(microtime() . "Test {$dataPrice}", 0, $batchFile);
+Zend_Debug::dump($dataPrice);
                                 $merchant = $dataPrice->merchant;
                                 $prices = $dataPrice->data;
 
