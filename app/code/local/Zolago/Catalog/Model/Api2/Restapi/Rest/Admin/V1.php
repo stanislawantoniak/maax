3a021a575a130fc0d73e6812fd1685c77c8bb4a9
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-06-30 14:23:46 +0300

    multiupdate

--- app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
+++ app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
@@ -58,6 +58,7 @@ class Zolago_Catalog_Model_Api2_Restapi_Rest_Admin_V1
                         if(!empty($batch)){
                             $batch = (array)$batch;
                             foreach($batch as $dataPrice){
+                                Mage::log(microtime() . "Test {$dataPrice}", 0, $batchFile);
                                 $merchant = $dataPrice->merchant;
                                 $prices = $dataPrice->data;
 
