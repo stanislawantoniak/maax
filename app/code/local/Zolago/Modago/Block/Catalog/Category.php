9980441b6d516f9cd855d3ca934545cb92d4c214
Pawel Chyl <pawel.chyl@orba.pl>
2014-07-09 15:09:07 +0200

    Adds hamburger menu and corresponding functions refs #206

--- app/code/local/Zolago/Modago/Block/Catalog/Category.php
+++ app/code/local/Zolago/Modago/Block/Catalog/Category.php
@@ -37,7 +37,62 @@ class Zolago_Modago_Block_Catalog_Category extends Mage_Core_Block_Template
         );
     }
 
+    /**
+     * Returns main categories for mobile navigation menu under black header
+     *
+     * @todo implement true login here, preserver data structure
+     * @return array
+     */
     public function getMainCategoriesMobile()
+    {
+        return array(
+            array(
+                'name' => 'Ona',
+                'url' => '/',
+                'category_id' => 1,
+                'has_dropdown' => true,
+                'children' => array(
+                    array(
+                        'name' => 'podkategoria 1',
+                        'url' => '/pod-1',
+                        'category_id' => 10
+                    ),
+                    array(
+                        'name' => 'podkategoria 1',
+                        'url' => '/pod-1',
+                        'category_id' => 10
+                    ),
+                    array(
+                        'name' => 'podkategoria 1',
+                        'url' => '/pod-1',
+                        'category_id' => 10
+                    ),
+                )
+            ),
+            array(
+                'name' => 'On',
+                'url' => '/',
+                'category_id' => 2,
+                'has_dropdown' => false,
+                'children' => array()
+            ),
+            array(
+                'name' => 'Dziecko',
+                'url' => '/dziecko',
+                'category_id' => 3,
+                'has_dropdown' => false,
+                'children' => array()
+            ),
+        );
+    }
+
+    /**
+     * Returns categories for sliding menu(hamburger menu)
+     *
+     * @todo implement true logic here, preserve data structure.
+     * @return array
+     */
+    public function getMainCategoriesForSlidingMenu()
     {
         return array(
             array(
