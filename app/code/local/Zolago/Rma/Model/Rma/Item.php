033b1d7992c0986fd3002adcba4ad10a7083f6dd
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-07-16 14:06:16 +0200

    Refs#85 Rma details - reason is a code, should be text

--- app/code/local/Zolago/Rma/Model/Rma/Item.php
+++ app/code/local/Zolago/Rma/Model/Rma/Item.php
@@ -23,9 +23,18 @@ class Zolago_Rma_Model_Rma_Item extends Unirgy_Rma_Model_Rma_Item
     public function getItemConditionName()
     {
         $id = $this->getItemCondition();
-        $itemConditions = Mage::getStoreConfig('urma/general/item_conditions');
-        $itemConditionsA = json_decode($itemConditions);
-        $code = (!empty($itemConditionsA) && isset($itemConditionsA[$id])) ? $itemConditionsA[$id]->code : $id;
+
+        $itemConditionsA = Mage::helper('urma')->getItemConditionTitles();
+
+        $code = $id;
+
+        if (!empty($itemConditionsA)) {
+            foreach ($itemConditionsA as $idD => $dataD) {
+                if ($id == $idD)
+                    $code = $dataD;
+            }
+        }
+
         return Mage::helper('urma')->getItemConditionTitle($code);
     }
 }
