03e9527163e9aa3db10161635916eb84dee61ab5
Maciej Babol <maciej.babol@orba.pl>
2014-07-21 14:55:59 +0200

    Zaslepka rozmiarow tabel

---
+++ app/code/local/Zolago/Catalog/Block/Product/Vendor/Sizecomments.php
@@ -0,0 +1,14 @@
+<?php
+class Zolago_Catalog_Block_Product_Vendor_Sizecomments
+	extends Zolago_Catalog_Block_Product_Vendor_Abstract
+{
+	
+	/**
+	 * @todo implement
+	 * @return string
+	 */
+	protected function _toHtml() {
+		return "";
+	}
+	
+}
