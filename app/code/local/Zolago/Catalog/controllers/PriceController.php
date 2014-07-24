255140fd7cd6c72ea36ed662e4fbbef7ad6c63d3
Maciej Babol <maciej.babol@orba.pl>
2014-07-24 09:52:29 +0200

    Kontorler testowy

---
+++ app/code/local/Zolago/Catalog/controllers/PriceController.php
@@ -0,0 +1,20 @@
+<?php
+class Zolago_Catalog_PriceController extends Mage_Core_Controller_Front_Action
+{
+	
+	public function indexAction() {
+		echo "Works";
+	}
+
+	public function pricetypeAction() {
+		Zolago_Catalog_Model_Observer::processPriceTypeQueue();
+	}
+	
+	public function configurableAction() {
+		Zolago_Catalog_Model_Observer::processConfigurableQueue();
+	}
+
+}
+
+
+
