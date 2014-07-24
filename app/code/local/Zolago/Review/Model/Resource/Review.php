467c572d9c5ca65ed070cd95ff95b039b63865ff
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-07-24 14:13:56 +0200

    Refs#323 product review extension - recommend product - yes/no

---
+++ app/code/local/Zolago/Review/Model/Resource/Review.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Review resource model
+ *
+ * @category    Zolago
+ * @package     Zolago_Review
+ */
+class Zolago_Review_Model_Resource_Review extends Mage_Review_Model_Resource_Review
+{
+    /**
+     * Perform actions after object save
+     *
+     * @param Varien_Object $object
+     * @return Mage_Review_Model_Resource_Review
+     */
+    protected function _afterSave(Mage_Core_Model_Abstract $object)
+    {
+        $adapter = $this->_getWriteAdapter();
+        /**
+         * save detail
+         */
+        $detail = array(
+            'title'     => $object->getTitle(),
+            'detail'    => $object->getDetail(),
+            'nickname'  => $object->getNickname(),
+            'recommend_product'  => $object->getRecommendProduct()
+        );
+
+        $select = $adapter->select()
+            ->from($this->_reviewDetailTable, 'detail_id')
+            ->where('review_id = :review_id');
+        $detailId = $adapter->fetchOne($select, array(':review_id' => $object->getId()));
+
+        if ($detailId) {
+            $condition = array("detail_id = ?" => $detailId);
+            $adapter->update($this->_reviewDetailTable, $detail, $condition);
+        } else {
+            $detail['store_id']   = $object->getStoreId();
+            $detail['customer_id']= $object->getCustomerId();
+            $detail['review_id']  = $object->getId();
+            $adapter->insert($this->_reviewDetailTable, $detail);
+        }
+
+
+        /**
+         * save stores
+         */
+        $stores = $object->getStores();
+        if (!empty($stores)) {
+            $condition = array('review_id = ?' => $object->getId());
+            $adapter->delete($this->_reviewStoreTable, $condition);
+
+            $insertedStoreIds = array();
+            foreach ($stores as $storeId) {
+                if (in_array($storeId, $insertedStoreIds)) {
+                    continue;
+                }
+
+                $insertedStoreIds[] = $storeId;
+                $storeInsert = array(
+                    'store_id' => $storeId,
+                    'review_id'=> $object->getId()
+                );
+                $adapter->insert($this->_reviewStoreTable, $storeInsert);
+            }
+        }
+
+        // reaggregate ratings, that depend on this review
+        $this->_aggregateRatings(
+            $this->_loadVotedRatingIds($object->getId()),
+            $object->getEntityPkValue()
+        );
+
+        return $this;
+    }
+}
