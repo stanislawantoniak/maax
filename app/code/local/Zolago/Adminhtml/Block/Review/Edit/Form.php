cd900d976142f8b485480a74858922b299edb217
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-07-24 14:22:17 +0200

    Refs#323 product review extension - recommend product - yes/no

--- app/code/local/Zolago/Adminhtml/Block/Review/Edit/Form.php
+++ app/code/local/Zolago/Adminhtml/Block/Review/Edit/Form.php
@@ -1,41 +1,16 @@
 <?php
 /**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Adminhtml
- * @copyright   Copyright (c) 2013 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
  * Adminhtml Review Edit Form
  *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @category   Zolago
+ * @package    Zolago_Adminhtml
  */
 
 class Zolago_Adminhtml_Block_Review_Edit_Form extends Mage_Adminhtml_Block_Review_Edit_Form
 {
     protected function _prepareForm()
     {
+        parent::_prepareForm();
         $review = Mage::registry('review_data');
         $product = Mage::getModel('catalog/product')->load($review->getEntityPkValue());
         $customer = Mage::getModel('customer/customer')->load($review->getCustomerId());
@@ -144,6 +119,6 @@ class Zolago_Adminhtml_Block_Review_Edit_Form extends Mage_Adminhtml_Block_Revie
         $form->setUseContainer(true);
         $form->setValues($review->getData());
         $this->setForm($form);
-        return parent::_prepareForm();
+        return $this;
     }
 }
