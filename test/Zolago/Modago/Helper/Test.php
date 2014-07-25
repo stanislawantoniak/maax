d51cdd07efa0bf46767ffa968c2bf5fbfa98dd22
victoriasultanovska <victoria.sultanovskaya@gmail.com>
2014-07-25 12:58:48 +0200

    Refs#286 Zolago_Modago_Block_Catalog_Category - add methods to serve categories in main menu

--- test/Zolago/Modago/Helper/Test.php
+++ test/Zolago/Modago/Helper/Test.php
@@ -4,16 +4,28 @@
  */
 class Zolago_Modago_Helper_Test extends Zolago_TestCase
 {
-    public function testCategoriesTree()
+    public static function setMainCategoriesData()
     {
-        $rootCatId = Mage::app()->getStore()->getRootCategoryId();
-        $categories = Mage::getModel('catalog/category')->getCategories($rootCatId);
-
-        $this->assertInstanceOf('Varien_Data_Tree_Node_Collection',$categories);
-
-        $zolagoModagoH = Mage::helper('zolagomodago');
-        $tree = $zolagoModagoH->getCategoriesTree($categories, 1, 3);
-        $this->assertInstanceOf('Zolago_Modago_Helper_Data', $zolagoModagoH);
-        $this->assertNotEmpty($tree);
+        $layout = Mage::app()->getLayout();
+        return array(
+            array(
+                'name'         => 'Ona',
+                'url'          => '/',
+                'category_id'  => 1,
+                'has_dropdown' => $layout->createBlock('cms/block')->setBlockId('navigation-dropdown-c-1')->toHtml()
+            ),
+            array(
+                'name'         => 'On',
+                'url'          => '/',
+                'category_id'  => 2,
+                'has_dropdown' => $layout->createBlock('cms/block')->setBlockId('navigation-dropdown-c-2')->toHtml()
+            ),
+            array(
+                'name'         => 'Dziecko',
+                'url'          => '/dziecko',
+                'category_id'  => 3,
+                'has_dropdown' => $layout->createBlock('cms/block')->setBlockId('navigation-dropdown-c-3')->toHtml()
+            ),
+        );
     }
 }
