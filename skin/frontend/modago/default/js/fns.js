a693a4c1fca9c162bc070da49a95efce1d6eae53
Pawel Chyl <pawel.chyl@orba.pl>
2014-07-25 15:24:30 +0200

    Frontend changes

--- skin/frontend/modago/default/js/fns.js
+++ skin/frontend/modago/default/js/fns.js
@@ -3,6 +3,38 @@ jQuery.noConflict();
 (function( $ ) {
   $(function() {
 
+    
+    $(window).on('load resize', function(){
+      var link = $('.forgot-password');
+      $.each( link, function( key, value ) {
+        var p = $( "#pass" );    
+        var pw = p.innerWidth();
+        var offset = p.offset();
+        
+        var position = parseInt(pw - 108);
+          $(this).css({
+          left:position+'px'
+          });
+      });
+    })
+    
+
+
+
+    $('.hint').not('input').tooltip({
+      placement: function(a, element) {
+             var position = $(element).parent().position();
+             var viewport = $(window).innerWidth();
+
+              if (viewport > 768) {
+                  return "right";
+              }
+              if (viewport <= 767) {
+                  return "top";
+              }
+              return "right";
+          }
+    })
    
 
 
@@ -268,10 +300,12 @@ var view_default_pay = $('#view_default_pay');
 var view_block_default_pay = $('.default_pay > .panel > .panel-body > .panel');
 var form_group_default_pay = $('.form-group.default_pay');
 view_default_pay.on('click', function(e){
+  var txt = $(this).closest('.panel').children('.panel-body').find('.panel-default').is(':visible') ? 'zmień sposób płatności' : 'porzuć zmianę';
   e.preventDefault();
   view_block_default_pay.toggle();
-  form_group_default_pay.closest('.row').css({marginBottom:'15px'})
-  $(this).closest('.panel-footer').hide();
+  form_group_default_pay.closest('.row').css({marginBottom:'15px'});
+  $(this).text(txt);
+  //$(this).closest('.panel-footer').hide();
 });
 
 var stara_cena = jQuery('.table-summary-product .cena .stara_cena');
@@ -339,19 +373,23 @@ $('.open-details-order-info').on('click',function(e){
 $('#show-table-summary-product').on('click', function(){
   if ($('#order-show-products').is(':visible')) {
   $('#order-show-products').hide();
+  $('#zmien_produkty').show(50);
   } else {
     $('#order-show-products').show();
+    $('#zmien_produkty').hide(50);
   };
 });
 
 $('#zwin_produkty').on('click',function(){
   $(this).closest('.panel-group').hide(50);
+  $('#zmien_produkty').hide(50);
    $('#order-show-products').show();
   hide_panel.find('i').removeClass('bullet-strzalka-up').addClass('bullet-strzalka-down');
 });
 
 $('#pokaz_produkty').on('click',function(){
   $('#summary-panel-product').show(50);
+  $('#zmien_produkty').show(50);
   $('#order-show-products').hide();
   hide_panel.find('i').removeClass('bullet-strzalka-down').addClass('bullet-strzalka-up');
 });
@@ -441,6 +479,13 @@ $(window).resize(function(){
     mouseDrag:false,
     afterInit : function(el){
       el.find(".rwd-item").eq(0).addClass("synced");
+      var sync2Item = $('#sync2 .rwd-item');
+      var sync2ItemLength = sync2Item.length;
+      if (sync2ItemLength <= 4 ) {
+        $('#product-gallery #wrapper-sync2 .up').addClass('disabled');
+        $('#product-gallery #wrapper-sync2 .down').addClass('disabled');
+        //console.log(sync2ItemLength)
+      };
     }
   });
 
@@ -462,6 +507,8 @@ $(window).resize(function(){
     var number = $(this).data("rwdItem");
     sync1.trigger("rwd.goTo",number);
   });
+
+
 
 
 
@@ -594,8 +641,21 @@ function galeriaProduktu(){
         mouseDrag:false,
         touchDrag: false,
         transitionStyle: "fade",
+        afterUpdate: function() {
+
+        },
         afterInit : function(el){
             el.find(".rwd-item").eq(0).addClass("synced");
+            
+              var sync4Item = $('#sync4 .rwd-item');
+              var sync4ItemLength = sync4Item.length;
+              if (sync4ItemLength <= 4 ) {
+                $('#wrapper-sync4 .up').addClass('disabled');
+                $('#wrapper-sync4 .down').addClass('disabled');
+                console.log(sync4ItemLength)
+              };
+           
+            
         }
     });
   sync3.rwdCarousel({
@@ -610,10 +670,16 @@ function galeriaProduktu(){
     itemsScaleUp:true,
     transitionStyle : "fade",
     slideSpeed:10,
-    afterUpdate: function() {
+    afterMove: function() {
         //var number_trigger = $(this).closest('.rwd-item').data("currentItem");
         //console.log(number_trigger)
         //$('#sync3').trigger("rwd.jumpTo",number_trigger);
+        var body = $("#inner-lightbox");
+        var top = body.scrollTop();
+        if(top!=0) {
+          $("#inner-lightbox").animate({ scrollTop: 0 }, "slow");
+          console.log(top);
+        }
     },
     afterInit:function() {
 
@@ -621,6 +687,14 @@ function galeriaProduktu(){
         $('#galeria-lightbox #sync3 .rwd-item').each(function(index, el) {
           var numberProduct = $('#sync1').data("rwdCarousel").rwd.visibleItems;
           $('#sync4').find( ".rwd-item" ).eq( numberProduct ).trigger("click");
+
+
+
+             
+
+
+
+
 
           flagProductGallery();
           var img = $(this).find('img');
@@ -685,7 +759,7 @@ function galeriaProduktu(){
                 divWidth = parseInt(divWidth);
             var divHeight  = $(this).closest('.item').css("height") ;
             
-            $('#lightbox .rwd-buttons').hide()
+            $('#lightbox .rwd-buttons').hide();
             $(img).width(divWidth) //Set the width to the div's width
             //$(this).closest('.inner-item').css({width:'100%',margin:0});
             $(img).css("height", 'auto');
@@ -708,6 +782,10 @@ function galeriaProduktu(){
                   $(this).addClass('full disabled');
                   $(this).next('.zoom_minus').addClass('full').removeClass('disabled');
                   console.log('WidthScale:'+ imgScaleWidth + '|| WidthOriginal:' + imgWidth );
+         
+                  
+
+
           });   
 
 
@@ -2500,6 +2578,7 @@ var rwd_banners = $("#rwd-banners .rwd-carousel");
               itemsScaleUp:false,
               jsonPath : '/orbacommon/ajax_product/get_recently_viewed',
               jsonSuccess : customDataSuccess,
+              navigationText : false,
               afterUpdate: function(){
                   var imgHeight = rwd_recently_viewed.find('img').height()/2;
                   var imgHeightplus = rwd_recently_viewed.find('img').height()/2-10;
