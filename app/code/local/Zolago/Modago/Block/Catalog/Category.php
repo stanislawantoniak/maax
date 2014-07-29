f4e29b56e71727a0201290ad0859a37d05fe2173
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2014-07-29 14:49:34 +0200

    poprawki do kategorii na głównym menu

--- app/code/local/Zolago/Modago/Block/Catalog/Category.php
+++ app/code/local/Zolago/Modago/Block/Catalog/Category.php
@@ -10,88 +10,49 @@ class Zolago_Modago_Block_Catalog_Category extends Mage_Core_Block_Template
     /**
      * Returns main categories for dropdown menu
      *
-     * @todo implement full login
      * @return array
      */
     public function getMainCategories()
     {
-        return array(
-            array(
-                'name' => 'Ona',
-                'url' => '/',
-                'category_id' => 1,
-                'has_dropdown' => (bool) $this->getLayout()->createBlock('cms/block')->setBlockId('navigation-dropdown-c-1')->toHtml()
-            ),
-            array(
-                'name' => 'On',
-                'url' => '/',
-                'category_id' => 2,
-                'has_dropdown' => (bool) $this->getLayout()->createBlock('cms/block')->setBlockId('navigation-dropdown-c-2')->toHtml()
-            ),
-            array(
-                'name' => 'Dziecko',
-                'url' => '/dziecko',
-                'category_id' => 3,
-                'has_dropdown' => (bool) $this->getLayout()->createBlock('cms/block')->setBlockId('navigation-dropdown-c-3')->toHtml()
-            ),
-        );
+        $rootCatId = Mage::app()->getStore()->getRootCategoryId();
+        $categories = Mage::getModel('catalog/category')->getCategories($rootCatId);
+        return Mage::helper('zolagomodago')->getCategoriesTree($categories, 1, 3);
     }
 
     /**
      * Returns main categories for mobile navigation menu under black header
      *
-     * @todo implement true login here, preserver data structure
      * @return array
      */
     public function getMainCategoriesMobile()
     {
-        return array(
-            array(
-                'name' => 'Ona',
-                'url' => '/',
-                'category_id' => 1,
-                'has_dropdown' => true,
-                'children' => array(
-                    array(
-                        'name' => 'podkategoria 1',
-                        'url' => '/pod-1',
-                        'category_id' => 10
-                    ),
-                    array(
-                        'name' => 'podkategoria 1',
-                        'url' => '/pod-1',
-                        'category_id' => 10
-                    ),
-                    array(
-                        'name' => 'podkategoria 1',
-                        'url' => '/pod-1',
-                        'category_id' => 10
-                    ),
-                )
-            ),
-            array(
-                'name' => 'On',
-                'url' => '/',
-                'category_id' => 2,
-                'has_dropdown' => false,
-                'children' => array()
-            ),
-            array(
-                'name' => 'Dziecko',
-                'url' => '/dziecko',
-                'category_id' => 3,
-                'has_dropdown' => false,
-                'children' => array()
-            ),
-        );
+        $rootCatId = Mage::app()->getStore()->getRootCategoryId();
+        $categories = Mage::getModel('catalog/category')->getCategories($rootCatId);
+        return Mage::helper('zolagomodago')->getCategoriesTree($categories, 1, 2);
     }
 
     /**
      * Returns categories for sliding menu(hamburger menu)
      *
-     * @todo implement true logic here, preserve data structure.
      * @return array
      */
+    public function getMainCategoriesForSlidingMenu()
+    {
+    
+        $rootCatId = Mage::app()->getStore()->getRootCategoryId();
+        $categories = Mage::getModel('catalog/category')
+            ->getCategories($rootCatId);
+            
+        $tree = Mage::helper('zolagomodago')->getCategoriesTree($categories, 1, 2);
+        return $tree;
+    }
+/**/
+    /**
+     * Returns categories for sliding menu(hamburger menu)
+     *
+     * @todo implement true logic here, preserve data structure.
+     * @return array
+     *
     public function getMainCategoriesForSlidingMenu()
     {
         return array(
@@ -134,7 +95,7 @@ class Zolago_Modago_Block_Catalog_Category extends Mage_Core_Block_Template
             ),
         );
     }
-
+    */
     /**
      * Returns category label for mobile menu in main category page
      *
