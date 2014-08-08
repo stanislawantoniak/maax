6b9d0c0176a57ee3ff3de97bba41b8d87a203935
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-08-08 17:17:21 +0200

    #362 - Campaigns: banner types definition in admin

--- app/code/local/Zolago/Banner/Helper/Data.php
+++ app/code/local/Zolago/Banner/Helper/Data.php
@@ -14,10 +14,58 @@ class Zolago_Banner_Helper_Data extends Mage_Core_Helper_Abstract {
         return Mage::getUrl('zolagobanner/vendor/edit', array('type' => $type));
     }
 
-
+    public function getHideEditFields()
+    {
+        $hideFields = explode(',', Mage::getStoreConfig('udropship/microsite/hide_product_attributes'));
+        $hideFields[] = 'udropship_vendor';
+        //$hideFields[] = 'tier_price';
+        $hideFields[] = 'gallery';
+        $hideFields[] = 'media_gallery';
+        $hideFields[] = 'small_image';
+        $hideFields[] = 'thumbnail';
+        $hideFields[] = 'image';
+        $hideFields[] = 'recurring_profile';
+        $hideFields[] = '';
+        return $hideFields;
+    }
     public function getEditFieldsConfig()
     {
-        return array();
+        $entityType = Mage::getSingleton('eav/config')->getEntityType('catalog_product');
+        $hideFields = $this->getHideEditFields();
+        $attrs = $entityType->getAttributeCollection()
+            ->addFieldToFilter('is_visible', 1)
+            ->setOrder('frontend_label', 'asc');
+        $editFields = array();
+        $paValues = array();
+        foreach ($attrs as $a) {
+            if (!in_array($a->getAttributeCode(), $hideFields)) {
+                $paValues['product.'.$a->getAttributeCode()] = $a->getFrontendLabel().' ['.$a->getAttributeCode().']';
+            }
+        }
+        $editFields['product']['label'] = 'Product Attributes';
+        $editFields['product']['values'] = $paValues;
+
+        $editFields['system']['label'] = 'System Attributes';
+        $editFields['system']['values'] = array(
+            'system.product_categories' => Mage::helper('catalog')->__('Categories'),
+            'system.product_websites'   => Mage::helper('catalog')->__('Websites')
+        );
+
+        if (Mage::helper('udropship')->isUdmultiActive()) {
+            $editFields['udmulti']['label'] = Mage::helper('udmulti')->__('Vendor Specific Fields');
+            $editFields['udmulti']['values']  = $this->getVendorEditFieldsConfig();
+        } else {
+            $sdValues['stock_data.qty'] = Mage::helper('cataloginventory')->__('Stock Qty').' [stock_item.qty]';
+            $sdValues['stock_data.is_in_stock'] = Mage::helper('cataloginventory')->__('Stock Status').' [stock_item.is_in_stock]';
+            $sdValues['stock_data.manage_stock'] = Mage::helper('cataloginventory')->__('Manage Stock').' [stock_item.manage_stock]';
+            $sdValues['stock_data.backorders'] = Mage::helper('cataloginventory')->__('Backorders').' [stock_item.backorders]';
+            $sdValues['stock_data.min_qty'] = Mage::helper('cataloginventory')->__('Qty for Item\'s Status to Become Out of Stock').' [stock_item.min_qty]';
+            $sdValues['stock_data.min_sale_qty'] = Mage::helper('cataloginventory')->__('Minimum Qty Allowed in Shopping Cart').' [stock_item.min_sale_qty]';
+            $sdValues['stock_data.max_sale_qty'] = Mage::helper('cataloginventory')->__('Maximum Qty Allowed in Shopping Cart').' [stock_item.max_sale_qty]';
+            $editFields['stock_data']['label'] = Mage::helper('udropship')->__('Stock Item Fields');
+            $editFields['stock_data']['values']  = $sdValues;
+        }
+        return $editFields;
     }
 
     public function getEditFieldsConfigSelect2Json()
--- app/code/local/Zolago/Banner/Model/SystemConfig/Backend/TypesConfig.php
+++ app/code/local/Zolago/Banner/Model/SystemConfig/Backend/TypesConfig.php
@@ -6,33 +6,31 @@ class Zolago_Banner_Model_SystemConfig_Backend_TypesConfig extends Mage_Core_Mod
     public function setValue($value)
     {
         $value = $this->_unserialize($value);
-        Zend_Debug::dump($value);
+        //Zend_Debug::dump($value);
         if (is_array($value)) {
             unset($value['$$ROW']);
-            //usort($value, array($this, 'sortBySortOrder'));
             foreach ($value as &$_val) {
-                $colDef = array(
-                    'fields_extra'=>array(),
-                    'required_fields'=>array(),
-                );
-                foreach (array('image_def', 'caption_def') as $colKey) {
-
-                    if (is_array(@$_val[$colKey])) {
-                        unset($_val[$colKey]['$ROW']);
-                        //usort($_val[$colKey], array($this, 'sortBySortOrder'));
-                        foreach ($_val[$colKey] as $r) {
-//                            $colDef[substr($colKey, 0, -4)][] = $r['column_field'];
-//                            $colDef['fields_extra'][$r['column_field']] = array(
-//                                'use_limit_type' => @$r['use_limit_type'],
-//                                'limit_type' => @$r['limit_type'],
-//                            );
+//                $colDef = array(
+//                    'fields_extra'=>array(),
+//                    'required_fields'=>array(),
+//                );
+//                foreach (array('image_def', 'caption_def') as $colKey) {
+//                    if (is_array(@$_val[$colKey])) {
+//                        unset($_val[$colKey]['$ROW']);
+//
+//                        foreach ($_val[$colKey] as $r) {
+////                            $colDef[substr($colKey, 0, -4)][] = $r['column_field'];
+////                            $colDef['fields_extra'][$r['column_field']] = array(
+////                                'use_limit_type' => @$r['use_limit_type'],
+////                                'limit_type' => @$r['limit_type'],
+////                            );
 //                            if (!empty($r['is_required'])) {
-//                                $colDef['required_fields'][] = $r['column_field'];
+//                                $colDef['required_fields'][] = $r['is_required'];
 //                            }
-                        }
-                    }
-                }
-                $_val = array_merge($_val, $colDef);
+//                        }
+//                    }
+//                }
+//                //$_val = array_merge($_val, $colDef);
             }
             unset($_val);
         }
