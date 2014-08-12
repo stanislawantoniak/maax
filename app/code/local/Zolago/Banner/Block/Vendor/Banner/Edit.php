7a2a7fcc7e015b6455a4a40a9216d57dcf26476c
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-08-12 17:16:29 +0200

    Refs#356

--- app/code/local/Zolago/Banner/Block/Vendor/Banner/Edit.php
+++ app/code/local/Zolago/Banner/Block/Vendor/Banner/Edit.php
@@ -68,7 +68,136 @@ class Zolago_Banner_Block_Vendor_Banner_Edit extends Mage_Core_Block_Template
                 $data = $typesConfigType;
             }
         }
+        krumo('-----------------',$data);
+        $bannerContent = $form->addFieldset("banner_content", array(
+            "legend" => $helper->__("Content")
+        ));
+        switch ($data->show_as) {
+            case Zolago_Banner_Model_Banner_Show::BANNER_SHOW_IMAGE:
+                $picturesNumber = $data->pictures_number;
+                $pictureUrlCanBeEmpty = $data->picture_can_be_empty;
+                $pictureUrlRequired = ($pictureUrlCanBeEmpty == 1) ? FALSE : TRUE;
+                if ($picturesNumber > 0) {
+                    foreach ($data->picture as $n => $picture) {
+                        $bannerContent->addField("image_" . $n, "image", array(
+                            "name" => "image[" . $n . "]",
+                            "class" => "form-control",
+                            "required" => true,
+                            "label" => $picture->picture_label
+                        ));
+                        $bannerContent->addField("image_url_" . $n, "text", array(
+                            "name" => "image_url[" . $n . "]",
+                            "class" => "form-control",
+                            "required" => $pictureUrlRequired,
+                            "label" => $picture->picture_label . ": url"
+                        ));
+                    }
+                    unset($n);
+                }
 
+                $captionsNumber = $data->captions_number;
+                $captionUrlCanBeEmpty = $data->caption_can_be_empty;
+                $captionUrlRequired = ($captionUrlCanBeEmpty == 1) ? FALSE : TRUE;
+                if ($captionsNumber > 0) {
+                    foreach ($data->caption as $n => $caption) {
+                        $bannerContent->addField("caption_url_" . $n, "text", array(
+                            "name" => "caption_url[" . $n . "]",
+                            "class" => "form-control",
+                            "required" => true,
+                            "label" => $caption->caption_label . ": url"
+                        ));
+
+                        $bannerContent->addField("caption_text_" . $n, "text", array(
+                            "name" => "caption_text[" . $n . "]",
+                            "class" => "form-control",
+                            "required" => $captionUrlRequired,
+                            "label" => $caption->caption_label . ": text"
+                        ));
+
+                    }
+                }
+                break;
+            case Zolago_Banner_Model_Banner_Show::BANNER_SHOW_HTML:
+                $bannerContent->addField("banner_html", "textarea", array(
+                    "name" => "banner_html",
+                    "class" => "form-control",
+                    "required" => true,
+                    "label" => $helper->__('HTML')
+                ));
+                break;
+        }
+
+//        switch ($type) {
+//            case Zolago_Banner_Model_Banner_Type::BANNER_TYPE_SLIDER:
+//                $content = $form->addFieldset("banner_content", array(
+//                    "legend" => $helper->__("Banner Content Type")
+//                ));
+//                $content->addField("slider_type", "select", array(
+//                    "name" => "slider_type",
+//                    "class" => "form-control",
+//                    "required" => true,
+//                    "label" => $helper->__('Slider Type'),
+//                    'values' => array('html' => 'HTML' , 'image' => 'Image')
+//                ));
+//
+//
+//                $sliderTypeHtml = $form->addFieldset("slider_type_html", array(
+//                    "legend" => $helper->__("Slider HTML"),
+//                    'attr_data' => array((object)array('name' => 'type', 'value' => 'html'), (object)array('name' => 'name', 'value' => 'slider_type'))
+//                ));
+//                $sliderTypeHtml->addField("html", "textarea", array(
+//                    "name" => "html",
+//                    "class" => "form-control",
+//                    "required" => false,
+//                    "label" => $helper->__('HTML')
+//                ));
+//
+//                $sliderTypeImage = $form->addFieldset("slider_type_image", array(
+//                    "legend" => $helper->__("Slider Content"),
+//                    'class' => 'hidden',
+//                    'attr_data' => array((object)array('name' => 'type', 'value' => 'image'), (object)array('name' => 'name', 'value' => 'slider_type'))
+//                ));
+////                $sliderTypeImage->addField("add_slider", "link", array(
+////                    "name" => "add_slider",
+////                    "label" => $helper->__("ADD"),
+////                    "href" => "",
+////                    'inside'  => '<span class="glyphicon glyphicon-plus"></span>',
+////                ));
+//                $sliderTypeImage->addField("slider_image_desktop", "image", array(
+//                    "name" => "slider[0][image_desktop]",
+//                    "class" => "form-control",
+//                    "required" => false,
+//                    "label" => $helper->__('Slider Image Desktop')
+//                ));
+//                $sliderTypeImage->addField("slider_image_mobile", "image", array(
+//                    "name" => "slider[0][image_mobile]",
+//                    "class" => "form-control",
+//                    "required" => false,
+//                    "label" => $helper->__('Slider Image Mobile')
+//                ));
+//                $sliderTypeImage->addField("slider_link_url", "text", array(
+//                    "name" => "slider[0][link_url][0]",
+//                    "class" => "form-control",
+//                    "required" => true,
+//                    "label" => $helper->__('Caption Url')
+//                ));
+//
+//                $sliderTypeImage->addField("slider_link_text", "text", array(
+//                    "name" => "slider[0][link_text][0]",
+//                    "class" => "form-control",
+//                    "required" => true,
+//                    'wrapper_class' => 'col-md-9',
+//                    "label" => $helper->__('Caption Text')
+//                ));
+//
+//                break;
+//            case Zolago_Banner_Model_Banner_Type::BANNER_TYPE_BOX:
+//                echo '2';
+//                break;
+//            case Zolago_Banner_Model_Banner_Type::BANNER_TYPE_INSPIRATION:
+//                echo '3';
+//                break;
+//        }
         return $form;
     }
 
