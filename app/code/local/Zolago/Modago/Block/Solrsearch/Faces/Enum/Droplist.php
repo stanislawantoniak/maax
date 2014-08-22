6e8d83bb3e9e31c38a8a37e540a9c7c5b5527084
Pawel Chyl <pawel.chyl@orba.pl>
2014-08-22 15:25:37 +0200

    Adds Droplist block

---
+++ app/code/local/Zolago/Modago/Block/Solrsearch/Faces/Enum/Droplist.php
@@ -0,0 +1,8 @@
+<?php
+class Zolago_Modago_Block_Solrsearch_Faces_Enum_Droplist extends Zolago_Solrsearch_Block_Faces_Enum_Droplist
+{
+    public function __construct()
+    {
+        $this->setTemplate('zolagosolrsearch/standard/searchfaces/enum/droplist.phtml');
+	}
+}
