619c815ae96166426dcc4ade166340a104f62662
Maciej Babol <maciej.babol@orba.pl>
2014-09-03 15:28:18 +0200

    Login refs #444

---
+++ app/code/local/Zolago/Checkout/controllers/GuestController.php
@@ -0,0 +1,56 @@
+<?php
+
+class Zolago_Checkout_GuestController extends Zolago_Checkout_Controller_Abstract {
+	
+	/**
+	 * 1. Is logged - move onepage for logged
+	 * 2. Has no guest flag - move login
+	 * 3. Else dipslay guest chekcout
+	 * 
+	 * @return type
+	 */
+	public function indexAction() {
+		// Customer logged in - forward to logged controller
+		if($this->_getCustomerSession()->isLoggedIn()){
+			return $this->_forward("index", "onepage");
+		}
+		// No is guest checkout flag - go to login
+		// Reset flag - every refresh will show login
+		// Devs can remove true argument for tests
+		if(!$this->_getCustomerSession()->getIsCheckout(true)){
+			return $this->_redirect("*/*/login");
+		}
+		parent::indexAction();
+	}
+	
+	/**
+	 * Do login if nesseery
+	 * @return type
+	 */
+	public function loginAction() {
+		
+		// Is logged in ?
+		if($this->_getCustomerSession()->isLoggedIn()){
+			return $this->_forward("index", "onepage");
+		}
+		
+		// Set redirect to onepage logged afetr login
+		$this->_getCustomerSession()->setAfterAuthUrl(
+				Mage::getUrl("checkout/onepage/index")
+		);
+		
+		// Set checkout context
+		$this->getRequest()->setParam("is_checkout", true);
+		
+		// Forward to Magento login
+		return $this->_forward("login", "account", "customer");
+	}
+	
+	/**
+	 * Set guest mode
+	 */
+	public function continueAction() {
+		$this->_getCustomerSession()->setIsCheckout(true);
+		return $this->_redirect("*/*/index");
+	}
+}
