c6ed7ed9aefacbda717b83ec5b7afbfab5109679
Andrzej Spólnicki <andrzej.spolnicki@convertica.pl>
2014-09-10 13:00:49 +0200

    thank you page refs#474

--- app/code/local/Zolago/Checkout/controllers/GuestController.php
+++ app/code/local/Zolago/Checkout/controllers/GuestController.php
@@ -57,4 +57,30 @@ class Zolago_Checkout_GuestController extends Zolago_Checkout_Controller_Abstrac
 
         $this->renderLayout();
     }
+
+    public function successAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+
+//        $session = $this->getOnepage()->getCheckout();
+//        if (!$session->getLastSuccessQuoteId()) {
+//            $this->_redirect('checkout/cart');
+//            return;
+//        }
+//
+//        $lastQuoteId = $session->getLastQuoteId();
+//        $lastOrderId = $session->getLastOrderId();
+//        $lastRecurringProfiles = $session->getLastRecurringProfileIds();
+//        if (!$lastQuoteId || (!$lastOrderId && empty($lastRecurringProfiles))) {
+//            $this->_redirect('checkout/cart');
+//            return;
+//        }
+//
+//        $session->clear();
+//        $this->loadLayout();
+//        $this->_initLayoutMessages('checkout/session');
+//        Mage::dispatchEvent('checkout_onepage_controller_success_action', array('order_ids' => array($lastOrderId)));
+//        $this->renderLayout();
+    }
 }
