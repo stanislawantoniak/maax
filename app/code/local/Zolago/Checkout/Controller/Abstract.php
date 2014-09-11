8f00cd9ac44cacc195bd4b6ab0d029e333182f38
Andrzej Spólnicki <andrzej.spolnicki@convertica.pl>
2014-09-11 17:39:00 +0200

    some merge problem - fixed

--- app/code/local/Zolago/Checkout/Controller/Abstract.php
+++ app/code/local/Zolago/Checkout/Controller/Abstract.php
@@ -184,32 +184,7 @@ abstract class Zolago_Checkout_Controller_Abstract
 			$this->_prepareJsonResponse($response);
 		}
 	}
-    /**
-     * Save addresses
-     */
-    public function saveShippingpaymentAction() {
-        if (!$this->_validateFormKey()) {
-            $this->_redirect('*/*');
-            return;
-        }
-        $response = array(
-            "status"=>true,
-            "content" => array()
-        );
-        try{
-            $this->importPostData();
-        } catch (Exception $ex) {
-            $response = array(
-                "status"=>0,
-                "content"=>$ex->getMessage()
-            );
-        }
-        if($this->getRequest()->isAjax()){
-            $this->_prepareJsonResponse($response);
-        }else{
-            $this->_redirectReferer();
-        }
-    }
+    
 	/**
 	 * Save shipping data
 	 */
