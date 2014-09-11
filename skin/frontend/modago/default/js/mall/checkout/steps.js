bba1a81c080bb7acd3433bdd26691e2c73482a4a
Pawel Chyl <pawel.chyl@orba.pl>
2014-09-11 12:45:45 +0200

    Fixes setting same_as_biiling flag refs #480

--- skin/frontend/modago/default/js/mall/checkout/steps.js
+++ skin/frontend/modago/default/js/mall/checkout/steps.js
@@ -99,6 +99,7 @@
 					if (jQuery("#invoice_data_address").is(":checked")) {
 						self.invoiceCopyShippingData();
 						self.invoiceDisableFields(self._invoice_copy_shipping_fields);
+                        self.setSameAsBilling(1);
 					}
 				});
 
