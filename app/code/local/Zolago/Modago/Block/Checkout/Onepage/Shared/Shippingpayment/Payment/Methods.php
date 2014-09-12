40c424a520167d2b9dfe8ad64ad5782e7774237d
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-09-12 14:22:47 +0200

    Refs#473 checkout: delivery and payment data 2nd step
    shipping method from rate (not from dropshipping details)

--- app/code/local/Zolago/Modago/Block/Checkout/Onepage/Shared/Shippingpayment/Payment/Methods.php
+++ app/code/local/Zolago/Modago/Block/Checkout/Onepage/Shared/Shippingpayment/Payment/Methods.php
@@ -4,4 +4,29 @@ class Zolago_Modago_Block_Checkout_Onepage_Shared_Shippingpayment_Payment_Method
 	extends Mage_Checkout_Block_Onepage_Payment_Methods
 {
 
+    /**
+     * @return Mage_Customer_Model_Session
+     */
+    public function getSession() {
+        return Mage::getSingleton('customer/session');
+    }
+
+    /**
+     * @return bool|mixed
+     */
+    public function getSelectedMethodCode()
+    {
+        if ($this->getSession()->isLoggedIn()) {
+            $customer = $this->getSession()->getCustomer();
+
+            if ($customer instanceof Mage_Customer_Model_Customer) {
+                $info = $customer->getLastUsedPayment();
+                if (is_array($info) && isset($info['method'])) {
+                    $method = $info['method'];
+                    return $method;
+                }
+            }
+        }
+        return false;
+    }
 }
