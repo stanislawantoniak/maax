8f8b7d62800f93b0171ed3b2bda3e8ed60a896c3
Maciej Babol <maciej.babol@orba.pl>
2014-09-17 11:04:25 +0200

    Image size for listing

--- app/code/local/Zolago/Solrsearch/Model/Catalog/Product.php
+++ app/code/local/Zolago/Solrsearch/Model/Catalog/Product.php
@@ -5,6 +5,23 @@
 class Zolago_Solrsearch_Model_Catalog_Product extends Mage_Catalog_Model_Product {
 	
 	/**
+	 * @return null | array
+	 */
+	public function getListingResizedImageInfo() {
+		$urlPath = $this->getListingResizedImageUrl();
+		// Extract cached image URI
+		if($urlPath){
+			$filePath = substr($urlPath, strpos($urlPath, "//")+2);
+			$filePath = substr($filePath, strpos($filePath, "/")+1);
+			$filePath = str_replace("/", DS, $filePath);
+			if($info=@getimagesize($filePath)){
+				return array("width"=>$info[0], "height"=>$info[1]);
+			}
+		}
+		return null;
+	}
+	
+	/**
 	 * @todo test it
 	 * @param type $qty
 	 * @return type
@@ -32,21 +49,26 @@ class Zolago_Solrsearch_Model_Catalog_Product extends Mage_Catalog_Model_Product
 	 * @return string | null;
 	 */
 	public function getListingResizedImageUrl() {
-		/** 
-		 * @todo move configurable values of helpert do admin conif per website
-		 */
-		$return = null;
-		try{
-			$return = Mage::helper('catalog/image')->
-				init($this, 'image')->
-				keepAspectRatio(true)->
-				constrainOnly(true)->
-				keepFrame(false)->
-				resize(300,null);
-		} catch (Exception $ex) {
-			Mage::logException($ex);
+		if(!$this->hasData("listing_resized_image_url")){
+			/** 
+			* @todo move configurable values of helpert do admin conif per website
+			*/
+		   $return = null;
+		   try{
+			   $return = Mage::helper('catalog/image')->
+				   init($this, 'image')->
+				   keepAspectRatio(true)->
+				   constrainOnly(true)->
+				   keepFrame(false)->
+				   resize(300,null);
+		   } catch (Exception $ex) {
+			   Mage::logException($ex);
+		   }
+		   $this->setData("listing_resized_image_url", $return . ""); // Cast to string
 		}
-		return $return;
+		
+		return $this->getData("listing_resized_image_url");
+		
 	}
 	
 	/**
