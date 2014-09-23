9ddc3eec7cdec9419d5cfe342966f9f96d14460e
Maciej Babol <maciej.babol@orba.pl>
2014-09-23 16:41:39 +0200

    Szkielket zmoawien

--- app/code/local/Zolago/Sales/controllers/OrderController.php
+++ app/code/local/Zolago/Sales/controllers/OrderController.php
@@ -1,25 +1,8 @@
 <?php
-require_once Mage::getConfig()->getModuleDir('controllers', 'Mage_Sales') . DS . "OrderController.php";
-die("TAL");
-class Zolago_Sales_OrderController extends Mage_Sales_OrderController
+require_once Mage::getConfig()->getModuleDir('controllers', 'Unirgy_Rma') .
+		DS . "OrderController.php";
+
+class Zolago_Sales_OrderController extends Unirgy_Rma_OrderController
 {
-
-
-    /**
-     * Customer order history
-     */
-    public function historyAction()
-    {
-		die("TAK");
-        $this->loadLayout();
-        $this->_initLayoutMessages('catalog/session');
-
-        $this->getLayout()->getBlock('head')->setTitle($this->__('My Orders'));
-
-        if ($block = $this->getLayout()->getBlock('customer.account.link.back')) {
-            $block->setRefererUrl($this->_getRefererUrl());
-        }
-        $this->renderLayout();
-    }
-
+	
 }
