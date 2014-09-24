a0facb3203f69e234ccba7de9e216da6f0e97092
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-09-24 12:54:32 +0200

    vendor name in campaigns

--- app/code/local/Zolago/Campaign/Model/Resource/Campaign.php
+++ app/code/local/Zolago/Campaign/Model/Resource/Campaign.php
@@ -110,6 +110,7 @@ class Zolago_Campaign_Model_Resource_Campaign extends Mage_Core_Model_Resource_D
                 'campaign_date_from' => 'campaign.date_from',
                 'campaign_date_to' => 'campaign.date_to',
                 'campaign_status' => 'campaign.status',
+                'campaign_vendor' => 'campaign.vendor_id',
             )
         );
         $select->joinLeft(
