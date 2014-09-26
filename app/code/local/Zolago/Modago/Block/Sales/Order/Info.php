fdb66f99f97c0d2847320fa9657259c09c613452
Adam Wilk <adam.wilk@convertica.pl>
2014-09-26 17:18:48 +0200

    added shipping address and sale document box refs#503

--- app/code/local/Zolago/Modago/Block/Sales/Order/Info.php
+++ app/code/local/Zolago/Modago/Block/Sales/Order/Info.php
@@ -7,4 +7,12 @@ class Zolago_Modago_Block_Sales_Order_Info extends Mage_Core_Block_Template {
         $this->setTemplate('sales/order/info.phtml');
         parent::_construct();
     }
+	public function getAddress() {
+		$id = $this->getItem()->getShippingAddressId();
+		return Mage::getModel("sales/order_address")->load($id);
+	}
+	public function getSaleDocument() {
+		$id = $this->getItem()->getBillingAddressId();
+		return Mage::getModel("sales/order_address")->load($id)->getNeedInvoice() ? 'Invoice' : 'Receipt';
+	}
 }
