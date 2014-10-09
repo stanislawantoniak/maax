9462efa2cec1d3b59ff0a498db828ea1a831848c
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-10-09 15:41:30 +0200

    Refs#558 styling: cms menu for desktop new styling
    menu update

--- app/code/local/Zolago/Modago/data/zolagomodago_setup/data-upgrade-0.4.1-0.4.2.php
+++ app/code/local/Zolago/Modago/data/zolagomodago_setup/data-upgrade-0.4.1-0.4.2.php
@@ -17,66 +17,77 @@ $cms = array(
 
 <div class="box col-sm-3 col-xs-6 col-md-3 col-lg-2">
 <dl>
-<dt><a href="#ona" rel="category" data-description="Kategory">Sport i turystyka</a></dt>
-<dd><a href="/dla-niej/ubrania-dla-kobiet/bluzy.html/" data-description="Odzież sportowa, buty i dodatki">Odzież sportowa, buty i dodatki</a></dd>
-<dd><a href="/dla-niej/ubrania-dla-kobiet/swetry.html/" data-description="Bieganie">Bieganie</a></dd>
-<dd><a href="#" data-description="Broń i akcesoria">Broń i akcesoria</a></dd>
-<dd><a href="/dla-niej/ubrania-dla-kobiet/spodnie.html/" data-description="Elektronika Sportowa">Elektronika Sportowa</a></dd>
-<dd><a href="#" data-description="Gimnastyka i Fitness">Gimnastyka i Fitness</a></dd>
-<dd><a href="#" data-description="Gry i zabawy sportowe">Gry i zabawy sportowe</a></dd>
-<dd><a href="#" data-description="Medycyna sportowa i dieta">Medycyna sportowa i dieta</a></dd>
-<dd><a href="#" data-description="Tenis, Badminton, Squash">Tenis, Badminton, Squash</a></dd>
-<dd><a href="#" data-description="Rowery i akcesoria">Rowery i akcesoria</a></dd>
-</dl>
-</div>
-
-
-
-<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
-<dl>
-<dd><a href="#" data-description="Rajstopy">Spondnice</a></dd>
-<dd><a href="#" data-description="IQ Kask rowerowy HEDOX Biało-czarny Rozm. L (54-59)">IQ Kask rowerowy HEDOX Biało-czarny Rozm. L (54-59)</a></dd>
-<dd><a href="#" data-description="Mactronic Lampa rowerowa przednia Falcon Eye w uchwycie 9 LED">Mactronic Lampa rowerowa przednia Falcon Eye w uchwycie 9 LED</a></dd>
-<dd><a href="#" data-description="Akcesoria rowerowe">Akcesoria rowerowe</a></dd>
-<dd><a href="#" data-description="ReadMore">Czytaj więcej <em class="fa fa-angle-right"></em></a></dd>
-</dl>
-</div>
-
-
-
-<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
-<dl>
-<dt><span data-description="Kategory">MĘSKA BLUZA 5 KOLORÓW OKAZJA MILANO114 ox172 - XXL</span></dt>
-<dd><a href="#" data-description="Bluzy">MASKOTKAPLUSZAK KŁAPOUCHY OSIOŁ + NIEBIESKI FERBI</a></dd>
-<dd><a href="#" data-description="Jeans">Jeans</a></dd><dd><a href="#" data-description="Koszule">Koszule</a></dd>
-<dd><a href="#" data-description="ReadMore">Czytaj więcej <em class="fa fa-angle-right"></em></a></dd>
-</dl>
-</div>
-
-
-<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
-<dl>
-<dt><a href="#" rel="category" data-description="Kategory">Łóżeczko Turystyczne Kojec+Przewijak 2 Poziomy Łóżeczko Turystyczne Kojec+Przewijak 2 Poziomy Łóżeczko Turystyczne Kojec+Przewijak 2 Poziomy</a></dt>
-<dd><a href="#" data-description="Bluzy">Bluzy</a></dd>
-<dd><a href="#" data-description="Jeans">Jeans</a></dd>
+<dt><a href="#ona" rel="category" data-description="Odzież">ODZIEŻ</a></dt>
+<dd><a href="/dla-niej/ubrania-dla-kobiet/bluzy.html/" data-description="Bielizna">Bielizna</a></dd>
+<dd><a href="/dla-niej/ubrania-dla-kobiet/swetry.html/" data-description="Bluzki">Bluzki</a></dd>
+<dd><a href="/dla-niej/ubrania-dla-kobiet/swetry.html/" data-description="Bluzy">Bluzy</a></dd>
 <dd><a href="#" data-description="Koszule">Koszule</a></dd>
-<dd><a href="#" data-description="Spodnie">UNIWERSALNA KAMIZELKA DAMSKA DIAMENCIKI WYS GRATIS</a></dd>
-<dd><a href="#" data-description="Spodnie">Spodnie</a></dd><dd><a href="#" data-description="Rajstopy">Rajstopy</a></dd>
-<dd><a href="#" data-description="ReadMore">Czytaj więcej <em class="fa fa-angle-right"></em></a></dd>
-</dl>
-</div>
-
-
-
-<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2  hidden-sm hidden-md"><dl><dt><a href="#" rel="category" data-description="Kategory">Inspiration</a></dt>
-<dd><a href="#" data-description="IQ Compression LS Power Plus 20 Spodnie">IQ Compression LS Power Plus 20 Spodnie</a></dd>
-<dd><a href="#" data-description="Laubr Orbitrek z oporem magnetycznym">Laubr Orbitrek z oporem magnetycznym</a></dd>
-<dd><a href="#" data-description="Koszule">Koszule</a></dd>
-<dd><a href="#" data-description="Koszule">Koszule</a></dd>
-<dd><a href="#"  data-description="Adidas Buty do biegania Duramo męskie">Adidas Buty do biegania Duramo męskie</a></dd>
+<dd><a href="/dla-niej/ubrania-dla-kobiet/spodnie.html/" data-description="T-shirty i topy">T-shirty i topy</a></dd>
+<dd><a href="/dla-niej/ubrania-dla-kobiet/spodnie.html/" data-description="Swetry">Swetry</a></dd>
 <dd><a href="#" data-description="Spodnie">Spodnie</a></dd>
-<dd><a href="#" data-description="Rajstopy">Rajstopy</a></dd>
-<dd><a href="#" data-description="ReadMore">Czytaj więcej <em class="fa fa-angle-right"></em></a></dd>
+<dd><a href="#" data-description="Sukienki">Sukienki</a></dd>
+
+</dl>
+</div>
+
+
+
+<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
+<dl>
+<dd><a href="#" data-description="Spódnice">Spódnice</a></dd>
+<dd><a href="#" data-description="Kurtki i płaszcze">Kurtki i płaszcze</a></dd>
+<dd><a href="#" data-description="Kamizelki i bezrękawniki">Kamizelki i bezrękawniki</a></dd>
+<dd><a href="#" data-description="Marynarki i żakiety">Marynarki i żakiety</a></dd>
+<dd><a href="#" data-description="Stroje kąpielowe">Stroje kąpielowe</a></dd>
+<dd><a href="#" data-description="ReadMore">Zobacz wszystkie ubrania <em class="fa fa-angle-right"></em></a></dd>
+</dl>
+</div>
+
+
+
+<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
+<dl>
+<dt><a href="#ona" rel="category" data-description="Obuwie">OBUWIE</a></dt>
+<dd><a href="#" data-description="Baleriny">Baleriny</a></dd>
+<dd><a href="#" data-description="Czółenka">Czółenka</a></dd>
+<dd><a href="#" data-description="Szpilki">Szpilki</a></dd>
+<dd><a href="#" data-description="Półbuty sznurowane">Półbuty sznurowane</a></dd>
+<dd><a href="#" data-description="Botki">Botki</a></dd>
+<dd><a href="#" data-description="Kozaki">Kozaki</a></dd>
+<dd><a href="#" data-description="Akcesoria rowerowe">Trampki</a></dd>
+<dd><a href="#" data-description="Buty Sportowe">Buty Sportowe</a></dd>
+<dd><a href="#" data-description="ReadMore">Zobacz wszystkie buty <em class="fa fa-angle-right"></em></a></dd>
+</dl>
+</div>
+
+
+<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
+<dl>
+<dt><a href="#ona" rel="category" data-description="Dodatki">DODATKI</a></dt>
+<dd><a href="#" data-description="Chusty i szaliki">Chusty i szaliki</a></dd>
+<dd><a href="#" data-description="Rękawiczki">Rękawiczki</a></dd>
+<dd><a href="#" data-description="Czapki i kapelusze">Czapki i kapelusze</a></dd>
+<dd><a href="#" data-description="Plecaki">Plecaki</a></dd>
+<dd><a href="#" data-description="Torebki">Torebki</a></dd>
+<dd><a href="#" data-description="Portfele">Portfele</a></dd>
+<dd><a href="#" data-description="Zegarki">Zegarki</a></dd>
+<dd><a href="#" data-description="Biżuteria">Biżuteria</a></dd>
+<dd><a href="#" data-description="ReadMore">Zobacz wszystkie dodatki <em class="fa fa-angle-right"></em></a></dd>
+</dl>
+</div>
+
+
+
+<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2  hidden-sm hidden-md"><dl><dt><a href="#" rel="category" data-description="Inspiracje">Inspiracje</a></dt>
+<dd><a href="#" data-description="Business look">Business look</a></dd>
+<dd><a href="#" data-description="Romantic">Romantic</a></dd>
+<dd><a href="#" data-description="Stylowy luz">Stylowy luz</a></dd>
+<dd><a href="#" data-description="Wieczorową porą">Wieczorową porą</a></dd>
+<dd><a href="#" data-description="Wytarty jeans">Wytarty jeans</a></dd>
+<dd><a href="#" data-description="Top kolor: fuksja">Top kolor: fuksja</a></dd>
+<dd><a href="#" data-description="Odzież ciążowa">Odzież ciążowa</a></dd>
+<dd><a href="#" data-description="Mała czarna">Mała czarna</a></dd>
+<dd><a href="#" data-description="Woman in red">Woman in red</a></dd>
 </dl>
 
 </div>
@@ -140,7 +151,7 @@ EOD
 <div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
 <dl>
 <dt><span data-description="Kategory">ODIEZ</span></dt>
-<dd><a  href="#" data-description="Bluzy">MASKOTKAPLUSZAK KŁAPOUCHY OSIOŁ + NIEBIESKI FERBI</a></dd>
+<dd><a href="#" data-description="Bluzy">MASKOTKAPLUSZAK KŁAPOUCHY OSIOŁ + NIEBIESKI FERBI</a></dd>
 <dd><a href="#" data-description="Jeans">Jeans</a></dd><dd><a href="#" data-description="Koszule">Koszule</a></dd>
 <dd><a href="#" data-description="ReadMore">Czytaj więcej <em class="fa fa-angle-right"></em></a></dd>
 </dl>
@@ -149,9 +160,10 @@ EOD
 
 <div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2">
 <dl>
-<dt><a  href="#" rel="category" data-description="ODIEZ</a></dt>
+<dt><a href="#" rel="category" data-description="ODIEZ</a></dt>
 <dd><a href="#" data-description="Bluzy">Bluzy</a></dd><dd><a href="#" data-description="Jeans">Jeans</a></dd>
-<dd><a href="#" data-description="Koszule">Koszule</a></dd><dd><a href="#" data-description="Spodnie" >UNIWERSALNA KAMIZELKA DAMSKA DIAMENCIKI WYS GRATIS</a></dd>
+<dd><a href="#" data-description="Koszule">Koszule</a></dd>
+<dd><a href="#" data-description="Spodnie">UNIWERSALNA KAMIZELKA DAMSKA DIAMENCIKI WYS GRATIS</a></dd>
 <dd><a href="#" data-description="Spodnie">Spodnie</a></dd><dd><a href="#" data-description="Rajstopy">Rajstopy</a></dd>
 <dd><a href="#" data-description="ReadMore">Czytaj więcej <em class="fa fa-angle-right"></em></a></dd>
 </dl>
@@ -159,10 +171,14 @@ EOD
 
 
 
-<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2  hidden-sm hidden-md"><dl><dt><a href="#" rel="category" data-description="Kategory">Inspiration</a></dt>
-<dd><a href="#" data-description="Bluzy">Bluzy</a></dd><dd><a href="#" data-description="Jeans">Jeans</a></dd>
+<div class="box col-sm-3 col-xs-4 col-md-3 col-lg-2  hidden-sm hidden-md">
+<dl>
+<dt><a href="#" rel="category" data-description="Kategory">Inspiration</a></dt>
+<dd><a href="#" data-description="Bluzy">Bluzy</a></dd>
+<dd><a href="#" data-description="Jeans">Jeans</a></dd>
 <dd><a href="#" data-description="Koszule">Koszule</a></dd>
-<dd><a href="#" data-description="Koszule">Koszule</a></dd><dd><a href="#" data-description="Spodnie" >UNIWERSALNA KAMIZELKA DAMSKA DIAMENCIKI WYS GRATIS</a></dd>
+<dd><a href="#" data-description="Koszule">Koszule</a></dd>
+<dd><a href="#" data-description="Spodnie">UNIWERSALNA KAMIZELKA DAMSKA DIAMENCIKI WYS GRATIS</a></dd>
 <dd><a href="#" data-description="Spodnie">Spodnie</a></dd>
 <dd><a href="#" data-description="Rajstopy">Rajstopy</a></dd>
 <dd><a href="#" data-description="ReadMore">Czytaj więcej <em class="fa fa-angle-right"></em></a></dd>
