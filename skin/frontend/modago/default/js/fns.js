1de567468e96df4a0df87221f30da73a4f795633
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-10-10 16:04:21 +0200

    Refs#558 styling: cms menu for desktop new styling
    comment:606545993

--- skin/frontend/modago/default/js/fns.js
+++ skin/frontend/modago/default/js/fns.js
@@ -1503,14 +1503,21 @@ jQuery.noConflict();
 //            });
 
         }
-        $(document).ready(function(){
-            $(".menu-5columns dt a, .menu-5columns dt span,.menu-5columns dd a").each(function(i, item){
-                console.log($(item).html().length);
-                if(parseInt($(item).html().length, 10) >= 17){
+
+        $(document).ready(function () {
+            $(".menu-5columns dt a, .menu-5columns dt span,.menu-5columns dd a").each(function (i, item) {
+                console.log($(item).textWidth());
+                if ($(item).textWidth() >= 152) {
                     $(item).addClass("long-wrap");
                 }
             })
         })
+
+        $.fn.textWidth = function (text, font) {
+            if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+            $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+            return $.fn.textWidth.fakeEl.width();
+        };
 
 
 //        var scroll = $('body').find('.mCustomScrollbar');
