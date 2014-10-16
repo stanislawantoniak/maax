4df1985baf85eb5bf6c5ba1127386b8175484cdd
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-10-16 17:16:54 +0200

    Refs#581 price filter: styling and intervals
    comment:610414133
    
    1. Check regular range
    2. Start entering number to field
    3. You have 2 cheboxes selected

--- skin/frontend/modago/default/js/mall/listing.js
+++ skin/frontend/modago/default/js/mall/listing.js
@@ -1994,7 +1994,7 @@ Mall.listing = {
 				slide: function(event, ui) {
 					jQuery("#zakres_min").val(ui.values[0]);
 					jQuery("#zakres_max").val(ui.values[1]);
-					var checkSlider = jQuery('#checkSlider').find('input');
+//					var checkSlider = jQuery('#checkSlider').find('input');
 //					if (!checkSlider.is(':checked')) {
 //						checkSlider.prop('checked', true).change();
 //						jQuery('#filter_price').find('.action').removeClass('hidden');
@@ -2056,6 +2056,9 @@ Mall.listing = {
 			"use strict";
 			var code = e.keyCode || e.which;
 			Mall.listing.unmarkPrice();
+
+            jQuery("#filter_price").find('input[name="fq[price]"]').prop('checked', false);
+
 			var checkSlider = jQuery('#checkSlider').find('input');
 			if (!checkSlider.is(':checked')) {
 				checkSlider.prop('checked', true);
