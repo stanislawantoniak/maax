d1fb460bb5410e5e87eddf1630291beb925fc6f4
Adam Wilk <adam.wilk@convertica.pl>
2014-10-24 14:01:19 +0200

    refs#579 added customer notes handle

--- app/code/local/Zolago/Rma/Block/List.php
+++ app/code/local/Zolago/Rma/Block/List.php
@@ -41,4 +41,12 @@ class Zolago_Rma_Block_List extends Mage_Core_Block_Template
 	public function getProductAttributes($options = array()) {
 		return isset($options['attributes_info']) ? $options['attributes_info'] : array();
 	}
+
+	/**
+	 * @param Zolago_Rma_Model_Rma $rma
+	 * @return string
+	 */
+	public function getStatusCustomerText(Zolago_Rma_Model_Rma $rma) {
+		return $this->__($rma->getStatusCustomerNotes() ? $rma->getStatusCustomerNotes() : $rma->getRmaStatusName());
+	}
 }
