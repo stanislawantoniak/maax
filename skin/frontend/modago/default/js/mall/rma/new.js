08596c8d8c3d4e3536c123d601b1a2d57d0b1df1
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2014-10-30 16:56:04 +0100

    ajax after page load part 1 refs#608

--- skin/frontend/modago/default/js/mall/rma/new.js
+++ skin/frontend/modago/default/js/mall/rma/new.js
@@ -274,9 +274,11 @@ jQuery(function($){
 
 			this.addressbook.init();
 
+            var zip = Mall.customer.Address._data.postcode;
+            //_rma.getDateList(zip);
+
             jQuery(this.addressbook.content).on("selectedAddressChange", function(e, address) {
                 //console.log(address.getData());
-                var poId = parseInt(jQuery("#new-rma input[name='po_id']").val());
                 var zip = address.getData().postcode;
                 _rma.getDateList(zip);
             });
