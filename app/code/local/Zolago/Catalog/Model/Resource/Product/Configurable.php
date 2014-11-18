4a28cb894e4ee2e3c452f029cc9c5b49833df556
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-11-18 10:10:53 +0100

    configurable update time

--- app/code/local/Zolago/Catalog/Model/Resource/Product/Configurable.php
+++ app/code/local/Zolago/Catalog/Model/Resource/Product/Configurable.php
@@ -271,7 +271,7 @@ class Zolago_Catalog_Model_Resource_Product_Configurable
             foreach ($productRelations as $i) {
                 $productMinPrice[] = $i['child_price'];
             }
-            Zend_Debug::dump($productMinPrice);
+            //Zend_Debug::dump($productMinPrice);
             $productMinimalPrice = min($productMinPrice);
             Mage::getSingleton('catalog/product_action')->updateAttributesNoIndex(
                 array($productConfigurableId), array('price' => $productMinimalPrice), $store
