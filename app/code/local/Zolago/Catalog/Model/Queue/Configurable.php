c4fa8711b864abe9a6914dda223294208012a8c0
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-11-19 13:01:53 +0100

    configurable update time

--- app/code/local/Zolago/Catalog/Model/Queue/Configurable.php
+++ app/code/local/Zolago/Catalog/Model/Queue/Configurable.php
@@ -48,7 +48,7 @@ class Zolago_Catalog_Model_Queue_Configurable extends Zolago_Common_Model_Queue_
 
         //define parent products (configurable) by child (simple)
         $configurableSimpleRelation = $zolagoCatalogModelProductConfigurableData->getConfigurableSimpleRelation($listUpdatedProducts);
-        Zend_Debug::dump($configurableSimpleRelation);
+        //Zend_Debug::dump($configurableSimpleRelation);
 
         if (empty($configurableSimpleRelation)) {
             //Mage::log("Found 0 configurable products ", 0, "configurable_update.log");
