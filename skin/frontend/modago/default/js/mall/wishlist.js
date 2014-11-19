b7782ea6a1680ac830bd2a38da0099a6aba1e109
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2014-11-19 11:22:16 +0100

    js cleaning and plugins to plugins.js refs#519

--- skin/frontend/modago/default/js/mall/wishlist.js
+++ skin/frontend/modago/default/js/mall/wishlist.js
@@ -519,38 +519,6 @@ Mall.wishlist = {
 
     likePriceView: function(){
 
-        //var listProducts = jQuery('#items-product');
-        //var listItemsProducts = listProducts.find('.item');
-        //listItemsProducts.each(function(index, el) {
-        //    var children = jQuery(this).find('.box_listing_product');
-        //    var widthThis = children.innerWidth()-15;
-        //
-        //    var childrenPrice = children.find('.col-price').innerWidth();
-        //    var childrenLike = children.find('.like').innerWidth();
-        //
-        //    var widthBlock = parseInt(childrenPrice + childrenLike);
-        //
-        //    var widthThisHalf = parseInt(widthThis/2);
-        //
-        //    if(widthBlock > 0){
-        //        if (widthBlock > widthThis) {
-        //            if (childrenPrice > widthThisHalf) {
-        //                if(jQuery(this).find('.col-price .old').length) {
-        //                    jQuery(this).find('.price').addClass('price-two-line');
-        //                }
-        //            } else {
-        //                jQuery(this).find('.price').removeClass('price-two-line');
-        //            }
-        //        };
-        //
-        //        if(jQuery(window).width() <= 970 ){
-        //            jQuery(this).find('.price').addClass('like-two-line');
-        //        }else {
-        //            jQuery(this).find('.price').removeClass('like-two-line');
-        //        }
-        //    }
-        //});
-
         var listProducts = jQuery('#items-product, #wishlist-items');
         var listItemsProducts = listProducts.find('.item');
         listItemsProducts.each(function(index, el) {
