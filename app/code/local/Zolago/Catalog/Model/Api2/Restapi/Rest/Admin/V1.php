b9b5d574c4e8258a8b9f2b5c55942491b17de859
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-11-19 14:41:41 +0100

    configurable update time

--- app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
+++ app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
@@ -291,11 +291,11 @@ class Zolago_Catalog_Model_Api2_Restapi_Rest_Admin_V1
                             $marginSelected = 0;
 
                             if (isset($marginByStore[$productId][$storeId])) {
-                                $marginSelected = $marginByStore[$productId][$storeId];
+                                $marginSelected = (float) str_replace(",", ".", $marginByStore[$productId][$storeId]);
                             } else {
                                 $marginDefault = isset($marginByStore[$productId][Mage_Core_Model_App::ADMIN_STORE_ID])
                                     ? $marginByStore[$productId][Mage_Core_Model_App::ADMIN_STORE_ID] : $marginSelected;
-                                $marginSelected = $marginDefault;
+                                $marginSelected = (float) str_replace(",", ".", $marginDefault);
                             }
 
                             $insert[] = array(
@@ -321,4 +321,6 @@ class Zolago_Catalog_Model_Api2_Restapi_Rest_Admin_V1
             $model->savePriceValues($insert, $ids);
         }
     }
+
+
 }
