55de3a2b9615a6811766356c8e5aa8ff9641e19a
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2014-11-21 12:18:09 +0100

    tests refs#652

--- app/code/local/Zolago/Sizetable/Model/Resource/Sizetable.php
+++ app/code/local/Zolago/Sizetable/Model/Resource/Sizetable.php
@@ -10,6 +10,21 @@ class Zolago_Sizetable_Model_Resource_Sizetable extends Mage_Core_Model_Resource
 
     public function getSizetableCMS($vendor_id, $store_id, $attribute)
     {
-        return 'jakis cms [dev]';
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
+
+
+        $resource = Mage::getSingleton('zolagosizetable/resource');
+        $read = $resource->getConnection('core_read');
+
+        $tableName = $resource->getTableName('catalog/product');
+
+
+        $query = 'SELECT * FROM ' . $tableName;
+
+        $results = $read->fetchAll($query);
+
+
+        return $results;
     }
 }
