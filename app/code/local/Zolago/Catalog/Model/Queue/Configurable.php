19583106d4fcc138aec8a3c1fe467721c3398786
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-11-24 11:54:50 +0100

    configurable update time
    remove simple products push to solr queue on fly

--- app/code/local/Zolago/Catalog/Model/Queue/Configurable.php
+++ app/code/local/Zolago/Catalog/Model/Queue/Configurable.php
@@ -23,22 +23,16 @@ class Zolago_Catalog_Model_Queue_Configurable extends Zolago_Common_Model_Queue_
     protected function _execute()
     {
         $collection = $this->_collection;
-        //Mage::log('Size: ' . $collection->getSize(), 0, "configurable_update_collection.log");
 
         $listUpdatedProducts = array();
 
 
         foreach ($collection as $colItem) {
-            //Zend_Debug::dump($colItem->getData());
             $productId = $colItem->getProductId();
 
             $listUpdatedProducts[$productId] = $productId;
-            //Mage::log(print_r($colItem->getData(),true), 0, "configurable_update_collection.log");
         }
-//        Zend_Debug::dump($listUpdatedProducts);
         unset($productId);
-        //Mage::log('Simple from queue', 0, "configurable_update.log");
-        //Mage::log(print_r($listUpdatedProducts,true), 0, "configurable_update.log");
         $storeId = array(Mage_Core_Model_App::ADMIN_STORE_ID);
         $allStores = Mage::app()->getStores();
         foreach ($allStores as $_eachStoreId => $val) {
@@ -49,9 +43,6 @@ class Zolago_Catalog_Model_Queue_Configurable extends Zolago_Common_Model_Queue_
 
         //define parent products (configurable) by child (simple)
         $configurableSimpleRelation = $zolagoCatalogModelProductConfigurableData->getConfigurableSimpleRelation($listUpdatedProducts);
-        //Mage::log('zolagoCatalogModelProductConfigurableData', 0, "configurable_update.log");
-        //Mage::log(print_r($configurableSimpleRelation,true), 0, "configurable_update.log");
-//        Zend_Debug::dump($configurableSimpleRelation);
         if (empty($configurableSimpleRelation)) {
             //Mage::log("Found 0 configurable products ", 0, "configurable_update.log");
             //return;
@@ -62,7 +53,6 @@ class Zolago_Catalog_Model_Queue_Configurable extends Zolago_Common_Model_Queue_
         //super attribute ids
         $superAttributes = $zolagoCatalogModelProductConfigurableData->getSuperAttributes($configurableProducts);
         //--super attribute ids
-//        Zend_Debug::dump($superAttributes);
         $productConfigurableIds = array();
 
         foreach ($configurableSimpleRelation as $productConfigurableId => $configurableSimpleRelationItem) {
@@ -91,14 +81,14 @@ class Zolago_Catalog_Model_Queue_Configurable extends Zolago_Common_Model_Queue_
         //1. reindex products
         Mage::getResourceModel('catalog/product_indexer_price')->reindexProductIds($productsToReindex);
 
-//        if (Mage::helper('catalog/category_flat')->isEnabled()) {
-//            $fI = new Mage_Catalog_Model_Resource_Product_Flat_Indexer();
-//            $entityTypeID = Mage::getModel('catalog/product')->getResource()->getTypeId();
-//            $attribute = Mage::getModel('eav/entity_attribute')->loadByCode($entityTypeID, 'price');
-//            foreach ($storeId as $storesId) {
-//                $fI->updateAttribute($attribute, $storesId, $productsToReindex);
-//            }
-//        }
+        if (Mage::helper('catalog/category_flat')->isEnabled()) {
+            $fI = new Mage_Catalog_Model_Resource_Product_Flat_Indexer();
+            $entityTypeID = Mage::getModel('catalog/product')->getResource()->getTypeId();
+            $attribute = Mage::getModel('eav/entity_attribute')->loadByCode($entityTypeID, 'price');
+            foreach ($storeId as $storesId) {
+                $fI->updateAttribute($attribute, $storesId, $productsToReindex);
+            }
+        }
 
         //2. put products to solr queue
         //zolago_catalog_after_update_price_type
