b0347ad4caa9f3a15e86d3a11fb80d8610f31eba
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-11-25 16:42:17 +0100

    configurable update time
    custom queue

--- app/code/local/Zolago/Catalog/Model/Queue/Configurable.php
+++ app/code/local/Zolago/Catalog/Model/Queue/Configurable.php
@@ -115,7 +115,7 @@ class Zolago_Catalog_Model_Queue_Configurable extends Zolago_Common_Model_Queue_
 
         //2. put products to solr queue
         //catalog_converter_price_update_after
-        Mage::log('catalog_converter_price_update_after', 0, 'configurable_update_solr.log');
+
 
         Mage::dispatchEvent(
             "catalog_converter_price_update_after",
