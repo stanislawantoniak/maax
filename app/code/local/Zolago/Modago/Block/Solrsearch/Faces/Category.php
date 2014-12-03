3c1cd8171769a42311775c928f255bb5781bd262
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2014-12-03 14:36:24 +0100

    fix 3. cancelling filters refs#521

--- app/code/local/Zolago/Modago/Block/Solrsearch/Faces/Category.php
+++ app/code/local/Zolago/Modago/Block/Solrsearch/Faces/Category.php
@@ -40,7 +40,21 @@ class Zolago_Modago_Block_Solrsearch_Faces_Category extends Zolago_Solrsearch_Bl
             );
         }
         else {
-            $parentCategoryUrl = $this->getFacesUrl(array('scat' => $category->getId()));
+//            $parentCategoryUrl = $this->getFacesUrl(array('scat' => $category->getId()));
+            //because _parseQueryData(...) in app/code/local/Zolago/Solrsearch/Block/Faces.php
+            //is bugged (always take paramss from Mage::app()->getRequest()->getParams();
+            //url is need to be filter
+
+            $id = $category->getId();
+
+            $_solrDataArray = $this->getSolrData();
+            $q = Mage::app()->getRequest()->getParam('q');
+
+            if( isset($_solrDataArray['responseHeader']['params']['q']) && !empty($_solrDataArray['responseHeader']['params']['q']) ) {
+                $q = "&q=" . $_solrDataArray['responseHeader']['params']['q'];
+            }
+
+            $parentCategoryUrl = Mage::getUrl("search/index/index") . "?scat={$id}{$q}";
         }
 
         return $parentCategoryUrl;
