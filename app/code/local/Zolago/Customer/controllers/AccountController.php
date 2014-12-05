ed31fe6cecf3e68432bd9f5ee008b8b0c20640df
Adam Wilk <adam.wilk@convertica.pl>
2014-12-05 17:31:39 +0100

    re#702 added submit action

--- app/code/local/Zolago/Customer/controllers/AccountController.php
+++ app/code/local/Zolago/Customer/controllers/AccountController.php
@@ -345,4 +345,88 @@ class Zolago_Customer_AccountController extends Mage_Customer_AccountController
 		$this->renderLayout();
 	}
 
+	/**
+	 * Create customer account action
+	 */
+	public function createPostAction()
+	{
+		/** @var $session Mage_Customer_Model_Session */
+		$session = $this->_getSession();
+		if ($session->isLoggedIn()) {
+			$this->_redirect('*/*/');
+			return;
+		}
+		$session->setEscapeMessages(true); // prevent XSS injection in user input
+		if (!$this->getRequest()->isPost()) {
+			$errUrl = $this->_getUrl('*/*/create', array('_secure' => true));
+			$this->_redirectError($errUrl);
+			return;
+		}
+
+		$customer = $this->_getCustomer();
+
+		try {
+			$errors = $this->_getCustomerErrors($this->getRequest()->getPost());
+
+			if (empty($errors)) {
+				$customer->save();
+				$this->_dispatchRegisterSuccess($customer);
+				$this->_successProcessRegistration($customer);
+				return;
+			} else {
+				$this->_addSessionError($errors);
+			}
+		} catch (Mage_Core_Exception $e) {
+			$session->setCustomerFormData($this->getRequest()->getPost());
+			if ($e->getCode() === Mage_Customer_Model_Customer::EXCEPTION_EMAIL_EXISTS) {
+				$url = $this->_getUrl('customer/account/forgotpassword');
+				$message = $this->__('There is already an account with this email address. If you are sure that it is your email address, <a href="%s">click here</a> to get your password and access your account.', $url);
+				$session->setEscapeMessages(false);
+			} else {
+				$message = $e->getMessage();
+			}
+			$session->addError($message);
+		} catch (Exception $e) {
+			$session->setCustomerFormData($this->getRequest()->getPost())
+				->addException($e, $this->__('Cannot save the customer.'));
+		}
+		$errUrl = $this->_getUrl('*/*/create', array('_secure' => true));
+		$this->_redirectError($errUrl);
+	}
+
+	/**
+	 * Validate customer data and return errors if they are
+	 *
+	 * @param array $customer
+	 * @return array|string
+	 */
+	protected function _getCustomerErrors($customer)
+	{
+		/**
+		 * $required
+		 *      key - field name
+		 *      value - field length (0 means not needed)
+		 */
+		$required = array(
+			"email"         => "1",
+			"password"      => "6",
+			"is_subscribed" => "0"
+		);
+		$errors = array();
+		foreach($customer as $field => $value) {
+			if(!isset($required[$field])) {
+				$errors = array();
+				$errors[] = $this->__("Some error occured");
+				return $errors;
+			} else {
+				if(strlen($value) < $required[$field]) {
+					$errors[] = $this->__("%s is not correct",$field);
+				} elseif($field == 'email' && !Zend_Validate::is($field, 'EmailAddress')) {
+					$errors[] = $this->__("Provided email is not correct");
+				}
+			}
+		}
+		return $errors;
+	}
+
 }
