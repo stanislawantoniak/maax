647ee16367bf7fd535e2eef36aa128f4137f77c2
Adam Wilk <adam.wilk@convertica.pl>
2014-12-10 17:32:37 +0100

    re#722 ready to test on dev by me

--- app/code/local/Zolago/Newsletter/Helper/Data.php
+++ app/code/local/Zolago/Newsletter/Helper/Data.php
@@ -2,10 +2,6 @@
 
 /**
  * Zolago Newsletter Data Helper
- *
- * @category   Mage
- * @package    Mage_Newsletter
- * @author     Magento Core Team <core@magentocommerce.com>
  */
 class Zolago_Newsletter_Helper_Data extends Mage_Newsletter_Helper_Data
 {
@@ -13,7 +9,16 @@ class Zolago_Newsletter_Helper_Data extends Mage_Newsletter_Helper_Data
 	const INVITATION_EMAIL_SENDER_XML_PATH = "newsletter/subscription/invitation_email_identity";
 	const INVITATION_XML_PATH = "newsletter/subscription/invitation";
 	const INVITATION_REPEAT_XML_PATH = "newsletter/subscription/invitation_repeat";
+	protected $key;
+	protected $subscriberId;
 
+	/**
+	 * Simple function to check if newsletter invitation emails are enabled
+	 * @return bool
+	 */
+	protected function isInvitationEmailEnabled() {
+		return Mage::getStoreConfig(self::INVITATION_XML_PATH) ? true : false;
+	}
 
 	/**
 	 * simple function that gets invitation email template id from system config
@@ -23,23 +28,13 @@ class Zolago_Newsletter_Helper_Data extends Mage_Newsletter_Helper_Data
 		return Mage::getStoreConfig(self::INVITATION_EMAIL_TEMPLATE_XML_PATH);
 	}
 
-
 	/**
 	 * simple function that gets invitation email sender from config
 	 * and then returns array with it's name and email
 	 * @return string
 	 */
-	public function getInvitationEmailSender() {
+	protected function getInvitationEmailSender() {
 		return Mage::getStoreConfig(self::INVITATION_EMAIL_SENDER_XML_PATH);
-	}
-
-
-	/**
-	 * Simple function to check if newsletter invitation emails are enabled
-	 * @return bool
-	 */
-	public function isInvitationEmailEnabled() {
-		return Mage::getStoreConfig(self::INVITATION_XML_PATH) ? true : false;
 	}
 
 	/**
@@ -54,13 +49,14 @@ class Zolago_Newsletter_Helper_Data extends Mage_Newsletter_Helper_Data
 	}
 
 	/**
-	 * function that sends invitation email for customers that didn't want to subscribe to newsletter
+	 * function that sends newsletter invitation email for
+	 * customers whom didn't want to subscribe to newsletter
 	 * @param string $email
 	 * @return bool
 	 */
 	public function sendInvitationEmail($email) {
 		if ($this->isInvitationEmailEnabled()
-			&& Zend_Validate::is($email, 'EmailAddress')
+			&& $this->validateEmail($email)
 			&& $this->isEmailSuitableForInvitation($email)) {
 			/** @var Mage_Core_Model_Email_Template $model */
 			$model = Mage::getModel('core/email_template');
@@ -83,7 +79,7 @@ class Zolago_Newsletter_Helper_Data extends Mage_Newsletter_Helper_Data
 	protected function getInvitationEmailVars() {
 		return array(
 			'store_name' => Mage::app()->getStore()->getName(),
-			//todo: add more variables
+			'confirmation_url' => $this->getUrl("newsletter/subscriber/confirm",array("id"=>$this->subscriberId,"code"=>$this->code))
 		);
 	}
 
@@ -99,7 +95,7 @@ class Zolago_Newsletter_Helper_Data extends Mage_Newsletter_Helper_Data
 		/** @var Mage_Newsletter_Model_Subscriber $model */
 		$model = Mage::getModel('newsletter/subscriber');
 		$subscription = $model->loadByEmail($email);
-		if ($subscription instanceof Mage_Newsletter_Model_Subscriber && $subscription->getId()) {
+		if ($subscription->getId()) {
 			$status = $subscription->getSubscriberStatus();
 			if ($status === Mage_Newsletter_Model_Subscriber::STATUS_SUBSCRIBED) {
 				return false;
@@ -107,14 +103,55 @@ class Zolago_Newsletter_Helper_Data extends Mage_Newsletter_Helper_Data
 				return $this->canRepeatInvitation();
 			}
 		} else {
-			/** @var Mage_Newsletter_Model_Subscriber $subscriber */
-			$subscriber = Mage::getModel('newsletter/subscriber');
-			$subscriber
-				->setEmail($email)
-				->setSubscriberStatus(Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE);
-			$subscriber->save();
-			return true;
+			return $this->addInactiveSubscriber($email);
 		}
 	}
 
+	/**
+	 * function that adds email to newsletter subscribers list with status set to:
+	 * Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE
+	 * @param $email
+	 * @return bool
+	 */
+	protected function addInactiveSubscriber($email) {
+		if($this->validateEmail($email)) {
+			/** @var Mage_Customer_Model_Customer $customer */
+			$customer = Mage::getModel("customer/customer")->loadByEmail($email);
+			//if customer with this email exists then save it to subscribers as user, otherwise as guest
+			$type = $customer->getId() ? 2 : 1;
+
+			/** @var Mage_Newsletter_Model_Subscriber $subscriber */
+			$subscriber = Mage::getModel('newsletter/subscriber');
+			$this->setCode();
+			$subscriber
+				->setEmail($email)
+				->setSubscriberStatus(Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE)
+				->setSubscriberConfirmCode($this->key)
+				->setType($type);
+			$subscriber->save();
+			$this->setSubscriberId($subscriber->getId());
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * simple zend based email validation
+	 * @param $email
+	 * @return bool
+	 * @throws Exception
+	 * @throws Zend_Validate_Exception
+	 */
+	public function validateEmail($email) {
+		return Zend_Validate::is($email, 'EmailAddress');
+	}
+
+	protected function setCode() {
+		$this->key = Mage::getModel('newsletter/subscriber')->randomSequence();
+	}
+
+	protected function setSubscriberId($sid) {
+		$this->subscriberId = $sid;
+	}
 }
