3aa5b3b59494f5a57143c36932034d4fb1929d62
Adam Wilk <adam.wilk@convertica.pl>
2014-12-11 10:23:51 +0100

    re#722 fixed invitation url

---
+++ app/code/local/Zolago/Newsletter/controllers/SubscriberController.php
@@ -0,0 +1,25 @@
+<?php
+class Zolago_Newsletter_SubscriberController extends Mage_Newsletter_SubscriberController
+{
+    public function invitationAction()
+    {
+        $id    = (int) $this->getRequest()->getParam('id');
+        $code  = (string) $this->getRequest()->getParam('code');
+
+        if ($id && $code) {
+            $subscriber = Mage::getModel('newsletter/subscriber')->load($id);
+            $session = Mage::getSingleton('core/session');
+
+            if($subscriber->getId() && $subscriber->getCode()) {
+                if($subscriber->confirm($code)) {
+                    $session->addSuccess($this->__('Your have been successfully subscribed to our newsletter.'));
+                } else {
+                    $session->addError($this->__('Invalid newsletter invitation code.'));
+                }
+            } else {
+                $session->addError($this->__('Invalid subscription ID.'));
+            }
+        }
+        $this->_redirectUrl(Mage::getBaseUrl());
+    }
+}
