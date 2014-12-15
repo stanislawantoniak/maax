4fbbc72640a01d0a36443690b3754b4a0fd3ceb4
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-12-15 14:02:57 +0100

    stock update

--- app/code/local/Zolago/Catalog/Helper/Stock.php
+++ app/code/local/Zolago/Catalog/Helper/Stock.php
@@ -75,12 +75,13 @@ class Zolago_Catalog_Helper_Stock extends Mage_Core_Helper_Abstract
             if (!empty($dataStockItems)) {
                 foreach ($dataStockItems as $stockId => $posStockConverter) {
                     //false if POS is not active
-                    $minimalStockPOS = isset($minPOSValues[$stockId]) ? (int)$minPOSValues[$stockId] : 0;
-                    Mage::log($stockId, 0, "minimalStockPOS.log");
-                    Mage::log($minimalStockPOS, 0, "minimalStockPOS.log");
-                    //available stock = if [POS stock from converter]>[minimal stock from POS] then [POS stock from converter] - [minimal stock from POS] else 0
                     Mage::log(in_array($stockId, $availablePos), 0, "minimalStockPOS.log");
-                    if(in_array($stockId, $availablePos)){
+                    if (in_array($stockId, $availablePos)) {
+                        $minimalStockPOS = isset($minPOSValues[$stockId]) ? (int)$minPOSValues[$stockId] : 0;
+
+                        Mage::log($stockId, 0, "minimalStockPOS.log");
+                        Mage::log($minimalStockPOS, 0, "minimalStockPOS.log");
+                        //available stock = if [POS stock from converter]>[minimal stock from POS] then [POS stock from converter] - [minimal stock from POS] else 0
                         $data[$sku][$stockId] = ($posStockConverter > $minimalStockPOS)
                             ? ($posStockConverter - $minimalStockPOS) : 0;
                     }
