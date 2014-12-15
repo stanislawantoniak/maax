bff6894ce7871abbd2ed4301f3bc0b59f7ec5bd3
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-12-15 15:19:21 +0100

    stock update

--- app/code/local/Zolago/Catalog/Helper/Stock.php
+++ app/code/local/Zolago/Catalog/Helper/Stock.php
@@ -68,20 +68,20 @@ class Zolago_Catalog_Helper_Stock extends Mage_Core_Helper_Abstract
         $posResourceModel = Mage::getResourceModel('zolagopos/pos');
         $minPOSValues = $posResourceModel->getMinPOSStock($vendor);
         $availablePos = array_keys($minPOSValues);
-        Mage::log(print_r($availablePos, true), 0, "minimalStockPOS.log");
+        //Mage::log(print_r($availablePos, true), 0, "minimalStockPOS.log");
         //-------Prepare data
         foreach ($dataStock as $sku => $dataStockItem) {
             $dataStockItems = (array)$dataStockItem;
             if (!empty($dataStockItems)) {
                 foreach ($dataStockItems as $stockId => $posStockConverter) {
-                    Mage::log(print_r($posStockConverter, true), 0, "minimalStockPOS.log");
+                    //Mage::log(print_r($posStockConverter, true), 0, "minimalStockPOS.log");
                     //false if POS is not active
-                    Mage::log(in_array($stockId, $availablePos), 0, "minimalStockPOS.log");
+                    //Mage::log(in_array($stockId, $availablePos), 0, "minimalStockPOS.log");
                     if (in_array($stockId, $availablePos)) {
                         $minimalStockPOS = isset($minPOSValues[$stockId]) ? (int)$minPOSValues[$stockId] : 0;
 
-                        Mage::log($stockId, 0, "minimalStockPOS.log");
-                        Mage::log($minimalStockPOS, 0, "minimalStockPOS.log");
+                        //Mage::log($stockId, 0, "minimalStockPOS.log");
+                        //Mage::log($minimalStockPOS, 0, "minimalStockPOS.log");
                         //available stock = if [POS stock from converter]>[minimal stock from POS] then [POS stock from converter] - [minimal stock from POS] else 0
                         $data[$sku][$stockId] = ($posStockConverter > $minimalStockPOS)
                             ? ($posStockConverter - $minimalStockPOS) : 0;
