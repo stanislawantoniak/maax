e3a0a505de73ed90f13915d459ad0d040fd70bbc
Maciej Babol <maciej.babol@orba.pl>
2014-12-16 17:24:07 +0100

    Akacja zapomnij - gosc refs #743

--- app/code/local/Zolago/Persistent/controllers/IndexController.php
+++ app/code/local/Zolago/Persistent/controllers/IndexController.php
@@ -11,8 +11,23 @@ class Zolago_Persistent_IndexController extends Mage_Persistent_IndexController
 		if(Mage::getSingleton('customer/session')->isLoggedIn()){
 			return $this->_redirect("customer/account/privacy");
 		}
+		if(!$this->_getHelper()->isPersistent()){
+			return $this->_redirectReferer();
+		}
 		$this->loadLayout();
 		$this->renderLayout();
+	}
+	
+	/**
+	 * Forget CMS page
+	 * @return type
+	 */
+    public function testAction() {
+		if($this->_getHelper()->isPersistent()){
+			echo "TAK";
+			return;
+		}
+		echo "NO";
 	}
 	
 	/**
@@ -23,20 +38,19 @@ class Zolago_Persistent_IndexController extends Mage_Persistent_IndexController
 		if(Mage::getSingleton('customer/session')->isLoggedIn()){
 			return $this->_redirect("customer/account/privacy");
 		}
-		if(!$this->_validateFormKey()){
+		
+		if(!$this->_validateFormKey() || !$this->_getHelper()->isPersistent()){
 			return $this->_redirectReferer();
 		}
 		
 		$session = Mage::getSingleton('core/session');
 		/* @var $session Mage_Core_Model_Session */
-		$helper = Mage::helper('persistent/session');
-		/* @var $helper Mage_Persistent_Helper_Session */
 		
 		// Do remove presisten quote
 		Mage::getSingleton('persistent/observer')->setQuoteGuest();
-		
-		
+
 		$session->addSuccess("Your persistent has been cleared");
 		$this->_redirect("/");
 	}
+	
 }
