0e37a5f6d821bb66a6b8033f8e89d5c8697a7827
Maciej Babol <maciej.babol@orba.pl>
2014-12-17 13:06:28 +0100

    refs #750

--- app/code/local/Zolago/Persistent/Block/Forget/Footerlink.php
+++ app/code/local/Zolago/Persistent/Block/Forget/Footerlink.php
@@ -1,19 +1,23 @@
 <?php
 class Zolago_Persistent_Block_Forget_Footerlink extends Mage_Core_Block_Html_Link
 {
+	
 	public function _construct() {
         $this->setTemplate('zolagopersistent/forget/footerlink.phtml');
 		$this->setHref($this->getUrl("persistent/index/forget", array("_no_vendor"=>true)));
 	}
 	
-	public function getAnchorText() {
-		return $this->__("Forget me");
-	}
-	
+	/**
+	 * @return bool
+	 */
 	public function canShow() {
-		return Mage::helper('persistent/session')->isPersistent() && !Mage::getSingleton('customer/session')->isLoggedIn();
+		return Mage::helper('persistent/session')->isPersistent() && 
+			!Mage::getSingleton('customer/session')->isLoggedIn();
 	}
 	
+	/**
+	 * @return string
+	 */
 	protected function _toHtml() {
 		if($this->canShow()){
 			return parent::_toHtml();
