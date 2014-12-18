a1f67374eac64488763b12ffb20072905bccf6ab
victoriasultanovska <victoria.sultanovska@convertica.pl>
2014-12-18 13:13:50 +0100

    change email process

--- app/code/local/Zolago/Modago/Block/Dropshipmicrositepro/Vendor/Banner.php
+++ app/code/local/Zolago/Modago/Block/Dropshipmicrositepro/Vendor/Banner.php
@@ -29,7 +29,6 @@ class Zolago_Modago_Block_Dropshipmicrositepro_Vendor_Banner extends Mage_Core_B
 
     public function getVendor()
     {
-        Mage::log(Mage::helper('umicrosite')->getCurrentVendor()->getData('vendor_id'));
         return Mage::helper('umicrosite')->getCurrentVendor();
     }
 
