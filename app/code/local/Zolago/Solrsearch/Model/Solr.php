3cfc5e5e1b9e896bfa674ac71bf50edd6ca94eec
Maciej Babol <maciej.babol@orba.pl>
2014-12-18 11:30:03 +0100

    refs #741

--- app/code/local/Zolago/Solrsearch/Model/Solr.php
+++ app/code/local/Zolago/Solrsearch/Model/Solr.php
@@ -20,6 +20,18 @@ class Zolago_Solrsearch_Model_Solr extends SolrBridge_Solrsearch_Model_Solr
 		parent::__construct();
 	}
 	
+	/**
+	 * If relevance use solr score
+	 * @param type $attributeCode
+	 * @param type $direction
+	 * @return boolean
+	 */
+	public function getSortFieldByCode($attributeCode, $direction){
+		if($attributeCode=="relevance"){
+			return false;
+		}
+		return parent::getSortFieldByCode($attributeCode, $direction);
+	}
 	
 	public function setSolrData($data) {
         $this->_solrData = $data;
