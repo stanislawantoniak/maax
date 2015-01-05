eedb968ba8d444aedbf2b47e36d96d460c3ed9d4
Adam Wilk <adam.wilk@convertica.pl>
2015-01-05 14:06:38 +0100

    refactoring of previous fix

--- app/code/local/Zolago/Checkout/Helper/Data.php
+++ app/code/local/Zolago/Checkout/Helper/Data.php
@@ -3,4 +3,28 @@ class Zolago_Checkout_Helper_Data extends Mage_Core_Helper_Abstract {
     public function getPaymentFromSession() {
 	    return Mage::getSingleton('checkout/session')->getPayment();
     }
+
+    public function fixCartShippingRates() {
+        $cost = array();
+
+        $q = Mage::getSingleton('checkout/cart')->getQuote();
+        $totalItemsInCart = Mage::helper('checkout/cart')->getItemsCount();
+
+        /*shipping_cost*/
+        if($totalItemsInCart > 0) {
+            $a = $q->getShippingAddress();
+
+            $qRates = $a->getGroupedAllShippingRates();
+
+            /**
+             * Fix rate quote query
+             */
+            if (!$qRates) {
+                $a->setCountryId(Mage::app()->getStore()->getConfig("general/country/default"));
+                $a->setCollectShippingRates(true);
+                $a->collectShippingRates();
+                $qRates = $a->getGroupedAllShippingRates();
+            }
+        }
+    }
 }
