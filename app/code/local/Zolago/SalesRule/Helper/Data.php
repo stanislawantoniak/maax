62273f563e4b9193a6601779533af54fd57f94d0
Adam Wilk <adam.wilk@convertica.pl>
2015-01-09 14:24:29 +0100

    re#681 testing now

--- app/code/local/Zolago/SalesRule/Helper/Data.php
+++ app/code/local/Zolago/SalesRule/Helper/Data.php
@@ -2,14 +2,48 @@
 class Zolago_SalesRule_Helper_Data extends Mage_SalesRule_Helper_Data {
 
 	public function getActiveSalesRules() {
-		$out = array();
-		$rules = Mage::getResourceModel('salesrule/rule_collection')->load();
+		$rules = Mage::getResourceModel('salesrule/rule_collection')
+			->addIsActiveFilter()
+			->load();
 
-		foreach($rules as $rule) {
-			if($rule->getIsActive()) {
-				$out[] = $rule;
-			}
+		return $rules;
+	}
+
+	protected function getStoreId() {
+		if (strlen($code = Mage::getSingleton('adminhtml/config_data')->getStore())) // store level
+		{
+			$store_id = Mage::getModel('core/store')->load($code)->getId();
 		}
-		return $out;
+		elseif (strlen($code = Mage::getSingleton('adminhtml/config_data')->getWebsite())) // website level
+		{
+			$website_id = Mage::getModel('core/website')->load($code)->getId();
+			$store_id = Mage::app()->getWebsite($website_id)->getDefaultStore()->getId();
+		}
+		else // default level
+		{
+			$store_id = 0;
+		}
+
+		return $store_id;
+	}
+
+	public function getUnusedCouponByRuleId($ruleId) {
+		$coupon = Mage::getResourceModel('salesrule/coupon_collection')
+			->addFieldToFilter('newsletter_sent', 0)
+			->addFieldToFilter('rule_id',$ruleId)
+			->setPageSize(1)
+			->load()
+			->getFirstItem();
+
+		return $coupon->getType() == 1 ? $coupon : false;
+	}
+
+	public function getSalesRuleDesc($ruleId) {
+		$rule = Mage::getResourceModel("salesrule/rule_collection")
+			->addFieldToFilter('rule_id',$ruleId)
+			->load()
+			->getFirstItem();
+
+		return $rule->getDescription();
 	}
 }
