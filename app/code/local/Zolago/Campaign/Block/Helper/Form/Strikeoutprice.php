c1764f1880c6c0496e102b7a449ec009cd59e66e
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-01-12 12:57:23 +0100

    fix strikeout price refs#371

--- app/code/local/Zolago/Campaign/Block/Helper/Form/Strikeoutprice.php
+++ app/code/local/Zolago/Campaign/Block/Helper/Form/Strikeoutprice.php
@@ -2,11 +2,11 @@
 
 class Zolago_Campaign_Block_Helper_Form_Strikeoutprice extends Varien_Data_Form_Element_Select
 {
-    public function getAfterElementHtml()
-    {
-        $html = parent::getAfterElementHtml();
-        return $html . "  <script>
-                        $('" . $this->getHtmlId() . "').disable();
-                        </script>";
-    }
+//    public function getAfterElementHtml()
+//    {
+//        $html = parent::getAfterElementHtml();
+//        return $html . "  <script>
+//                        $('" . $this->getHtmlId() . "').disable();
+//                        </script>";
+//    }
 }
