c3c08b0ce04bedab398e2e12d62626dfa9e65703
Adam Wilk <adam.wilk@convertica.pl>
2015-01-15 16:19:43 +0100

    re#814 re#826 logging

--- app/code/local/Zolago/Dotpay/controllers/NotificationController.php
+++ app/code/local/Zolago/Dotpay/controllers/NotificationController.php
@@ -9,17 +9,16 @@ class Zolago_Dotpay_NotificationController extends Dotpay_Dotpay_NotificationCon
 		$data = $this->getRequest()->getPost();
 		Mage::log($data,null,"transactions.log");
 
-		try {
-			/** @var Mage_Sales_Model_Order $order */
-			$order = Mage::getModel('sales/order');
-			$order->loadByIncrementId($data['control']);
-			if (!$order->getId()) {
-				Mage::log('order_err', null, "transactions.log");
-				die('ERR');
-			}
-		} catch(Exception $e) {
-			Mage::logException($e);
+		/** @var Mage_Sales_Model_Order $order */
+		$order = Mage::getModel('sales/order');
+		Mage::log("created_model",null,"transactions.log");
+		$order->loadByIncrementId($data['control']);
+		Mage::log("loaded_model",null,"transactions.log");
+		if (!$order->getId()) {
+			Mage::log('order_err', null, "transactions.log");
+			die('ERR');
 		}
+		Mage::log("no_order_err",null,"transactions.log");
 
 		/** @var Zolago_Dotpay_Model_Client $client */
 		$client = Mage::getModel("zolagodotpay/client");
