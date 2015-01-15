d36d0767bdebaad26196c74c0b2828288b95e695
Adam Wilk <adam.wilk@convertica.pl>
2015-01-15 12:04:16 +0100

    re#814 re#828 connect with allocation log

--- app/code/local/Zolago/Payment/Model/Observer.php
+++ app/code/local/Zolago/Payment/Model/Observer.php
@@ -7,7 +7,8 @@ class Zolago_Payment_Model_Allocation_Observer {
     }
 
     public function allocationTransaction(Varien_Event_Observer $observer) {
-
+        Mage::log("IN",null,"allocation.log");
+        Mage::log($observer->getData(),null,"allocation.log");
         $transaction_id = $observer->getData('transaction_id');
         $allocation_type = $observer->getData('allocation_type');
         $operator_id = $observer->getData('operator_id');
@@ -16,5 +17,6 @@ class Zolago_Payment_Model_Allocation_Observer {
             Mage::getModel('zolagopayment/allocation')->
                 allocationTransaction($transaction_id, $allocation_type, $operator_id , $comment);
         }
+        Mage::log("OUT",null,"allocation.log");
     }
 }
