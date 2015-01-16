d67f29d0be45274a5d599ec32f36c9bd2b86fde9
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-01-16 23:02:53 +0100

    Campaigns: Being a vendor I want campaigns be processed at starting date
    products POST

--- app/code/local/Zolago/Campaign/Model/Resource/Campaign.php
+++ app/code/local/Zolago/Campaign/Model/Resource/Campaign.php
@@ -592,7 +592,6 @@ class Zolago_Campaign_Model_Resource_Campaign extends Mage_Core_Model_Resource_D
         $select->where("products_visibility.value<>?", Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE);
         //$select->where("campaign.date_from IS NOT NULL AND campaign.date_to IS NOT NULL ");
         $select->order('campaign.date_from DESC');
-        $select->order('campaign.date_to ASC');
 
         return $this->getReadConnection()->fetchAll($select);
     }
@@ -651,7 +650,6 @@ class Zolago_Campaign_Model_Resource_Campaign extends Mage_Core_Model_Resource_D
         $select->where("products_visibility.value<>?", Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE);
         $select->where("campaign.date_from IS NOT NULL AND campaign.date_to IS NOT NULL ");
         $select->order('campaign.date_from DESC');
-        $select->order('campaign.date_to ASC');
 
 
         return $this->getReadConnection()->fetchCol($select);
@@ -736,7 +734,6 @@ class Zolago_Campaign_Model_Resource_Campaign extends Mage_Core_Model_Resource_D
         $select->where("products_visibility.value<>?", Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE);
         $select->where("campaign.date_from IS NOT NULL AND campaign.date_to IS NOT NULL ");
         $select->order('campaign.date_from DESC');
-        $select->order('campaign.date_to ASC');
 
 
         return $this->getReadConnection()->fetchAll($select);
@@ -1015,7 +1012,6 @@ class Zolago_Campaign_Model_Resource_Campaign extends Mage_Core_Model_Resource_D
         $select->where("products_visibility.value<>?", Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE);
         $select->where("campaign.date_from IS NOT NULL AND campaign.date_to IS NOT NULL ");
         $select->order('campaign.date_from DESC');
-        $select->order('campaign.date_to ASC');
 
 
         $_return = $this->getReadConnection()->fetchAll($select);
