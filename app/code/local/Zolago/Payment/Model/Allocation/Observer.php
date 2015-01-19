e2580955c123760c467bda19224cfa72c5120735
Adam Wilk <adam.wilk@convertica.pl>
2015-01-19 19:03:09 +0100

    fix for allocations

--- app/code/local/Zolago/Payment/Model/Allocation/Observer.php
+++ app/code/local/Zolago/Payment/Model/Allocation/Observer.php
@@ -7,11 +7,11 @@ class Zolago_Payment_Model_Allocation_Observer {
     }
 
     public function allocationTransaction(Varien_Event_Observer $observer) {
-        $transaction_id = $observer->getData('transaction_id');
+        $transaction = $observer->getData('transaction');
         $allocation_type = $observer->getData('allocation_type');
         $operator_id = $observer->getData('operator_id');
         $comment = $observer->getData('comment');
-        if(!empty($transaction_id) && !empty($allocation_type)) {
+        if($transaction instanceof Mage_Sales_Model_Order_Payment_Transaction && !empty($allocation_type)) {
             /** @var Zolago_Payment_Model_Allocation $model */
             $model = Mage::getModel('zolagopayment/allocation');
             $model->allocationTransaction($transaction_id, $allocation_type, $operator_id , $comment);
