715385475eda8a89ddb17aec0e8a2d3a8f29c36c
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-01-20 16:32:37 +0100

    tests refs#820

--- app/code/local/Zolago/Payment/Model/Allocation/Observer.php
+++ app/code/local/Zolago/Payment/Model/Allocation/Observer.php
@@ -9,11 +9,17 @@ class Zolago_Payment_Model_Allocation_Observer {
     public function salesOrderPaymentTransactionSaveAfter(Varien_Event_Observer $observer) {
         /** @var Mage_Sales_Model_Order_Payment_Transaction $observer */
 
+        Mage::log("salesOrderPaymentTransactionSaveAfter START", null, "aloc.log");
+
+        Mage::log($observer->getData());
+        Mage::log("salesOrderPaymentTransactionSaveAfter START---", null, "aloc.log");
         $transaction = $observer->getData('transaction');
         $allocation_type = $observer->getData('allocation_type');
         $operator_id = $observer->getData('operator_id');
         $comment = $observer->getData('comment');
-        Mage::getModel("zolagopayment/allocation")->importDataFromTransaction($transaction, $allocation_type, $operator_id, $comment);
 
+
+        Mage::getModel("zolagopayment/allocation")->importDataFromTransaction($transaction, $allocation_type, $operator_id, $comment);
+        Mage::log("salesOrderPaymentTransactionSaveAfter end", null, "aloc.log");
     }
 }
