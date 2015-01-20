a6387ecccca335fa97ff1c4d308db0c88bb23571
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-01-20 14:55:44 +0100

    refactoring refs#820

--- app/code/local/Zolago/Payment/Model/Allocation/Observer.php
+++ app/code/local/Zolago/Payment/Model/Allocation/Observer.php
@@ -2,19 +2,13 @@
 
 class Zolago_Payment_Model_Allocation_Observer {
 
-    public function __construct() {
+    /**
+     *
+     * @param Varien_Event_Observer $observer Mage_Sales_Model_Order_Payment_Transaction
+     */
+    public function salesOrderPaymentTransactionSaveAfter(Varien_Event_Observer $observer) {
+        /** @var Mage_Sales_Model_Order_Payment_Transaction $observer */
+        Mage::getModel("zolagopayment/allocation")->importDataFromTransaction($observer);
 
-    }
-
-    public function allocationTransaction(Varien_Event_Observer $observer) {
-        $transaction = $observer->getData('transaction');
-        $allocation_type = $observer->getData('allocation_type');
-        $operator_id = $observer->getData('operator_id');
-        $comment = $observer->getData('comment');
-        if($transaction instanceof Mage_Sales_Model_Order_Payment_Transaction && !empty($allocation_type)) {
-            /** @var Zolago_Payment_Model_Allocation $model */
-            $model = Mage::getModel('zolagopayment/allocation');
-            $model->allocationTransaction($transaction, $allocation_type, $operator_id , $comment);
-        }
     }
 }
