76e44b335c8664da12eb19eb728340e917f3f2d7
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-01-21 15:57:50 +0100

    tests refs#820

--- app/code/local/Zolago/Po/Model/Po.php
+++ app/code/local/Zolago/Po/Model/Po.php
@@ -666,6 +666,7 @@ class Zolago_Po_Model_Po extends Unirgy_DropshipPo_Model_Po
 		} else {
             /** @var Zolago_Po_Helper_Data $hlp */
             $hlp = Mage::helper("zolagopo");
+            Mage::log("_processStatus", null, "a.log");
             $hlp->updateStatusByAllocation($this,false);
         }
 	}
