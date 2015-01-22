3727b98217ece2b9de66fb7b63e54f5ddc9899b5
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-01-22 11:25:19 +0100

    refs#819 button and controller action for create overpayment

--- app/code/local/Zolago/Po/Block/Vendor/Po/Edit/Payments.php
+++ app/code/local/Zolago/Po/Block/Vendor/Po/Edit/Payments.php
@@ -6,5 +6,11 @@
 class Zolago_Po_Block_Vendor_Po_Edit_Payments
     extends Zolago_Po_Block_Vendor_Po_Edit
 {
-
+    public function getCreateOverpaymentUrl($action, $params=array()) {
+        $params += array(
+            "id"=> $this->getPo()->getId(),
+            "form_key" => Mage::getSingleton('core/session')->getFormKey()
+        );
+        return $this->getUrl("*/*/$action", $params);
+    }
 }
