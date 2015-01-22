39217ee0fe7e0ffab0486c11fe4504ea5aa0b331
Adam Wilk <adam.wilk@convertica.pl>
2015-01-22 11:16:43 +0100

    re#820 first try

--- app/code/local/Zolago/Po/Model/Po.php
+++ app/code/local/Zolago/Po/Model/Po.php
@@ -666,12 +666,12 @@ class Zolago_Po_Model_Po extends Unirgy_DropshipPo_Model_Po
 	protected function _processStatus() {
 		if(!$this->getId()){
 			Mage::getSingleton('zolagopo/po_status')->processNewStatus($this);
-		} else {
+		}// else {
             /** @var Zolago_Po_Helper_Data $hlp */
-            $hlp = Mage::helper("zolagopo");
-            Mage::log("_processStatus", null, "a.log");
-            $hlp->updateStatusByAllocation($this, false);
-        }
+           // $hlp = Mage::helper("zolagopo");
+          //  Mage::log("_processStatus", null, "a.log");
+          //  $hlp->updateStatusByAllocation($this, false);
+        //}
 	}
 	
 	protected function _processMaxShippingDate() {
