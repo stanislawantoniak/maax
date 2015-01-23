ce83fc111490533f24f72fb1eb04109ebc29152c
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-01-23 14:50:19 +0100

    refs#819 overpayment ACL

--- app/code/local/Zolago/Po/controllers/PaymentController.php
+++ app/code/local/Zolago/Po/controllers/PaymentController.php
@@ -13,16 +13,9 @@ class Zolago_Po_PaymentController extends Zolago_Dropship_Controller_Vendor_Abst
                 $error = $allocModel->createOverpayment($po);
 //            Mage::log("error:" . ($error ? "1" : "0"), null, "op.log");
                 if (!$error) {
-                    if (!Mage::getSingleton("zolagodropship/session")->isOperatorMode()) {
-                        //info for vendor, only operator with role "Payment manage" can do this
-                        throw new Mage_Core_Exception(Mage::helper("zolagopo")->__("You need to be operator with role `Payment manage`"));
-                    } else {
-                        throw new Mage_Core_Exception(Mage::helper("zolagopo")->__("Overpayment can not be created"));
-                    }
+                    throw new Mage_Core_Exception(Mage::helper("zolagopo")->__("Overpayment can not be created"));
                 } else {
                     $this->_getSession()->addSuccess(Mage::helper("zolagopo")->__("Overpayment created"));
-                    $this->_redirect('udpo/vendor/edit', array('id' => $po->getId()));
-                    return;
                 }
             } else {
                 throw new Mage_Core_Exception(Mage::helper("zolagopo")->__("There is no such PO"));
@@ -33,7 +26,7 @@ class Zolago_Po_PaymentController extends Zolago_Dropship_Controller_Vendor_Abst
             Mage::logException($e);
             $this->_getSession()->addError(Mage::helper("zolagopo")->__("Some error occured."));
         }
-        $this->_redirect('udropship');
+        $this->_redirect('udpo/vendor/edit', array('id' => $po->getId()));
         return;
     }
 
