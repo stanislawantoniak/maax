656c2cab7b46c9b1ab5650cc03a8e7a7c5aaa725
Adam Wilk <adam.wilk@convertica.pl>
2015-01-23 13:09:26 +0100

    re#820 payment method

--- app/code/local/Zolago/Po/Block/Vendor/Po/Edit.php
+++ app/code/local/Zolago/Po/Block/Vendor/Po/Edit.php
@@ -422,7 +422,7 @@ class Zolago_Po_Block_Vendor_Po_Edit extends Zolago_Po_Block_Vendor_Po_Info
 	}
 
 	public function getPaymentMethod(Zolago_Po_Model_Po $po) {
-		$helper = Mage::helper("zolagopo");
+		$helper = Mage::helper("zolagopayment");
 		if($po->isCod()) {
 			return $helper->__("Cash on delivery");
 		} else {
