432ba0a60ef7f27df2ddbe03b237e6ddd1aff14b
Adam Wilk <adam.wilk@convertica.pl>
2015-01-26 10:16:38 +0100

    re#821 fix in overpayment selection

--- app/code/local/Zolago/Payment/Model/Allocation.php
+++ app/code/local/Zolago/Payment/Model/Allocation.php
@@ -227,7 +227,7 @@ class Zolago_Payment_Model_Allocation extends Mage_Core_Model_Abstract {
 				->reset(Zend_Db_Select::COLUMNS)
 				->columns(array(
 					"main_table.allocation_id",
-					"DISTINCT(main_table.transaction_id)",
+					"main_table.transaction_id",
 					"main_table.po_id",
 					"allocation_amount" => "SUM(main_table.allocation_amount)",
 					"main_table.allocation_type",
@@ -243,6 +243,7 @@ class Zolago_Payment_Model_Allocation extends Mage_Core_Model_Abstract {
 				->where("main_table.allocation_type = ?",self::ZOLAGOPAYMENT_ALLOCATION_TYPE_OVERPAY)
 				->where("udpo.udropship_vendor = ?" , $udpoVendorId)
 				->having("allocation_amount > 0")
+				->group("main_table.transaction_id")
 				->order("main_table.created_at",Zend_Db_Select::SQL_DESC);
 //				->limit(1);
 			Mage::log((string)$collection->getSelect(),null,"sql.log");
