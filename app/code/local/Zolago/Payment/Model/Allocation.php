b8bdba6fa4ffccc63c26d829394968a664dd80a6
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-01-26 14:54:43 +0100

    refs#819 test

--- app/code/local/Zolago/Payment/Model/Allocation.php
+++ app/code/local/Zolago/Payment/Model/Allocation.php
@@ -243,10 +243,7 @@ class Zolago_Payment_Model_Allocation extends Mage_Core_Model_Abstract {
      * @return int
      */
     public function isAutomat() {
-        Mage::log("isAutomat", null, 'c.log');
-        Mage::log(!$this->isOperatorMode(), null, 'c.log');
-        Mage::log(!$this->isVendorMode(), null, 'c.log');
-        return (!$this->isOperatorMode() && !$this->isVendorMode()) ? 1 : 0;
+        return (!$this->getSession()->isLoggedIn()) ? 1 : 0;
     }
 
 	/**
