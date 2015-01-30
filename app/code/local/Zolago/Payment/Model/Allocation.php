dc46d606b055ce3358969180208fd4afb41cb450
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2015-01-30 15:27:05 +0100

    niepotrzebny increment_id

--- app/code/local/Zolago/Payment/Model/Allocation.php
+++ app/code/local/Zolago/Payment/Model/Allocation.php
@@ -321,7 +321,6 @@ class Zolago_Payment_Model_Allocation extends Mage_Core_Model_Abstract {
 					"main_table.comment",
                     "main_table.vendor_id",
                     "main_table.is_automat",
-					"udropship_po.increment_id"
 				))
 				->where("main_table.allocation_type = ?",self::ZOLAGOPAYMENT_ALLOCATION_TYPE_OVERPAY)
 				->where("main_table.vendor_id = ?" , $udpoVendorId)
