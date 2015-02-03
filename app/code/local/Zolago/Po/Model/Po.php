ccdf5a2c23de0cef6f8e56d0f28d6be1115887b4
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-02-03 14:50:38 +0100

    payments details visibility for udropship fnc refs#889

--- app/code/local/Zolago/Po/Model/Po.php
+++ app/code/local/Zolago/Po/Model/Po.php
@@ -559,6 +559,16 @@ class Zolago_Po_Model_Po extends Unirgy_DropshipPo_Model_Po
    public function isPaymentCheckOnDelivery() {
        return $this->getOrder()->getPayment()->getMethod() == Mage::getSingleton("payment/method_cashondelivery")->getCode();
    }
+
+    /**
+     * return true if payment method is Banktransfer
+     * if not return false
+     *
+     * @return bool
+     */
+   public function isPaymentBanktransfer() {
+       return $this->getOrder()->getPayment()->getMethod() == Mage_Payment_Model_Method_Banktransfer::PAYMENT_METHOD_BANKTRANSFER_CODE;
+   }
    
    /**
     * @see isPaymentCheckOnDelivery()
