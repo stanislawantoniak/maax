7ade5a87455c40afc293d6103f2ab79d3738bde7
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2015-02-04 17:18:17 +0100

    próba ogarnięcia persistant cookie refs #891

--- app/code/local/Zolago/Sales/Model/Observer.php
+++ app/code/local/Zolago/Sales/Model/Observer.php
@@ -12,7 +12,25 @@ class Zolago_Sales_Model_Observer extends Mage_Sales_Model_Observer {
     public function loadCustomerQuoteBefore($observer) {
         $session = $observer->getEvent()->getCheckoutSession();
         $quote = $session->getQuote();        
-        if ($quote->hasItems()) { // clear customer
+        if ($items = Mage::registry('persistent_quote_item')) {
+            // overriding items
+            $quote->removeAllItems();            
+            $quote->save();
+            foreach ($items as $item) {
+                $newItem = clone $item;
+                $quote->addItem($newItem);
+                if ($item->getHasChildren()) {
+                    foreach ($item->getChildren() as $child) {
+                        $newChild = clone $child;
+                        $newChild->setParentItem($newItem);
+                        $quote->addItem($newChild);
+                    }
+                }                                                                                                                                                                                                
+            }
+            $quote->collectTotals()
+                 ->save();
+                             
+        } elseif ($quote->hasItems()) { // clear customer
             $customerQuote = Mage::getModel('sales/quote')
                 ->setStoreId(Mage::app()->getStore()->getId())
                 ->loadByCustomer(Mage::getSingleton('customer/session')->getCustomerId());
