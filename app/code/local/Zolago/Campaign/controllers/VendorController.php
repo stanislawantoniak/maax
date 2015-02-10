ed13d5fe8ff0e3f1e4d71493409245f127e03391
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-02-10 12:35:59 +0100

    Refs#458 after adding campaign edit form should be extended to products/banners not close to grid

--- app/code/local/Zolago/Campaign/controllers/VendorController.php
+++ app/code/local/Zolago/Campaign/controllers/VendorController.php
@@ -159,6 +159,10 @@ class Zolago_Campaign_VendorController extends Zolago_Dropship_Controller_Vendor
                 return $this->_redirectReferer();
             }
             $this->_getSession()->addSuccess($helper->__("Campaign has been saved"));
+            $campaignId = $campaign->getId();
+            if ($campaign->isObjectNew() && !empty($campaignId)) {
+                return $this->_redirect("*/*/edit/id/" . $campaignId);
+            }
         } catch (Mage_Core_Exception $e) {
             $this->_getSession()->addError($e->getMessage());
             $this->_getSession()->setFormData($data);
@@ -169,8 +173,6 @@ class Zolago_Campaign_VendorController extends Zolago_Dropship_Controller_Vendor
             Mage::logException($e);
             return $this->_redirectReferer();
         }
-
-
 
         return $this->_redirect("*/*");
 	}
