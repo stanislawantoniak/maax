14dafbe57000c1456bdc4d001ebf27d5728acda3
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-02-23 12:13:56 +0100

    Refs#902 format review detail

--- app/code/local/Zolago/Review/Model/Resource/Review/Collection.php
+++ app/code/local/Zolago/Review/Model/Resource/Review/Collection.php
@@ -17,10 +17,17 @@ class Zolago_Review_Model_Resource_Review_Collection extends Mage_Review_Model_R
     protected function _initSelect()
     {
         parent::_initSelect();
+
+//        UNIX / Linux use \n for linebreaks
+//        Mac (before OSX) used \r
+//        And windows uses a combination of both
+        $replaceSpecialCharsInDetails = "REPLACE(REPLACE(det.detail,'\t','&nbsp;'),'\r\n','<br />')";
+
         $this->getSelect()
             ->join(array('det' => $this->_reviewDetailTable),
                 'main_table.review_id = det.review_id',
-                array('detail_id', 'title', 'detail', 'nickname', 'customer_id', 'recommend_product'));
+                array('detail_id', 'title', 'detail', 'nickname', 'customer_id', 'recommend_product',
+                    'detail_html' => new Zend_Db_Expr($replaceSpecialCharsInDetails)));
         return $this;
     }
 }
