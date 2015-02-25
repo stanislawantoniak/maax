66a47bc8a8da60bde8c5e5f6530563b8b9343afe
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-02-25 17:33:00 +0100

    registration new vendor time fix refs#955

---
+++ app/code/local/Varien/Data/Form/Element/Time.php
@@ -0,0 +1,48 @@
+<?php
+
+class Varien_Data_Form_Element_Time extends Varien_Data_Form_Element_Abstract
+{
+    public function getElementHtml()
+    {
+        Mage::log('tutaj', null, 'mylog.log');
+
+        $this->addClass('select');
+
+        $value_hrs = 0;
+        $value_min = 0;
+        $value_sec = 0;
+
+        if( $value = $this->getValue() ) {
+            $values = explode(',', $value);
+            if( is_array($values) && count($values) == 3 ) {
+                $value_hrs = $values[0];
+                $value_min = $values[1];
+                $value_sec = $values[2];
+            }
+        }
+
+        $html = '<input type="hidden" id="' . $this->getHtmlId() . '" />';
+        $html .= '<select name="'. $this->getName() . '" '.$this->serialize($this->getHtmlAttributes()).' style="width:40px">'."\n";
+        for( $i=0;$i<24;$i++ ) {
+            $hour = str_pad($i, 2, '0', STR_PAD_LEFT);
+            $html.= '<option value="'.$hour.'" '. ( ($value_hrs == $i) ? 'selected="selected"' : '' ) .'>' . $hour . '</option>';
+        }
+        $html.= '</select>'."\n";
+
+        $html.= '&nbsp;:&nbsp;<select name="'. $this->getName() . '" '.$this->serialize($this->getHtmlAttributes()).' style="width:40px">'."\n";
+        for( $i=0;$i<60;$i++ ) {
+            $hour = str_pad($i, 2, '0', STR_PAD_LEFT);
+            $html.= '<option value="'.$hour.'" '. ( ($value_min == $i) ? 'selected="selected"' : '' ) .'>' . $hour . '</option>';
+        }
+        $html.= '</select>'."\n";
+
+        $html.= '&nbsp;:&nbsp;<select name="'. $this->getName() . '" '.$this->serialize($this->getHtmlAttributes()).' style="width:40px">'."\n";
+        for( $i=0;$i<60;$i++ ) {
+            $hour = str_pad($i, 2, '0', STR_PAD_LEFT);
+            $html.= '<option value="'.$hour.'" '. ( ($value_sec == $i) ? 'selected="selected"' : '' ) .'>' . $hour . '</option>';
+        }
+        $html.= '</select>'."\n";
+        $html.= $this->getAfterElementHtml();
+        return $html;
+    }
+}
