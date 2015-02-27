663d9f81d2885de9c823663e58d8f32d5555222c
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-02-27 14:23:38 +0100

    tests and fix on dev Mirasvit_FeedExport refs#839

--- app/code/core/Mage/Rule/Model/Condition/Abstract.php
+++ app/code/core/Mage/Rule/Model/Condition/Abstract.php
@@ -262,9 +262,13 @@ abstract class Mage_Rule_Model_Condition_Abstract
         if ($this->hasValueOption()) {
             $valueOption = (array) $this->getValueOption();
         }
+        Mage::log($valueOption, null, 'mylog.log');
+
         foreach ($valueOption as $k => $v) {
             $opt[] = array('value' => $k, 'label' => $v);
         }
+        Mage::log($opt, null, 'mylog.log');
+
         return $opt;
     }
 
@@ -322,6 +326,8 @@ abstract class Mage_Rule_Model_Condition_Abstract
 
         Mage::log($value, null, 'mylog.log');
         Mage::log($options, null, 'mylog.log');
+        Mage::log('==================', null, 'mylog.log');
+
 
         $valueArr = array();
         if (!empty($options)) {
