7e72ba26211c80d7592a091c46453388eefec19d
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-03-06 14:12:48 +0100

    Refs#895

--- app/code/local/Zolago/Dropship/Block/Adminhtml/Vendor/Grid.php
+++ app/code/local/Zolago/Dropship/Block/Adminhtml/Vendor/Grid.php
@@ -4,100 +4,30 @@ class Zolago_Dropship_Block_Adminhtml_Vendor_Grid extends Unirgy_Dropship_Block_
     protected function _prepareColumns()
     {
         $hlp = Mage::helper('udropship');
-        $this->addColumn('vendor_id', array(
-            'header'    => $hlp->__('Vendor ID'),
+
+        $this->addColumnAfter('sequence', array(
+            'header'    => $hlp->__('Sequence'),
             'align'     => 'right',
             'width'     => '50px',
-            'index'     => 'vendor_id',
+            'index'     => 'sequence',
             'type'      => 'number',
-        ));
-
-        $this->addColumn('vendor_name', array(
-            'header'    => $hlp->__('Vendor Name'),
-            'index'     => 'vendor_name',
-        ));
-
-        $this->addColumn('email', array(
-            'header'    => $hlp->__('Email'),
-            'index'     => 'email',
-        ));
-
-        if ($hlp->isModuleActive('ustockpo')) {
-            $this->addColumn('distributor_id', array(
-                'header' => Mage::helper('ustockpo')->__('Distributor'),
-                'index' => 'distributor_id',
-                'type' => 'options',
-                'options' => Mage::getSingleton('udropship/source')->setPath('vendors')->toOptionHash(),
-            ));
-        }
-
-        $this->addColumn('carrier_code', array(
-            'header'    => $hlp->__('Used Carrier'),
-            'index'     => 'carrier_code',
-            'type'      => 'options',
-            'options'   => Mage::getSingleton('udropship/source')->setPath('carriers')->toOptionHash(),
-        ));
-        $this->addColumn('vendor_type', array(
+        ),'carrier_code');
+        $this->addColumnAfter('url_key', array(
+            'header'    => $hlp->__('URL key'),
+            'align'     => 'right',
+            'width'     => '180px',
+            'index'     => 'url_key',
+            'type'      => 'text',
+        ),'carrier_code');
+        $this->addColumnAfter('vendor_type', array(
             'header'    => $hlp->__('Vendor type'),
             'align'     => 'right',
             'width'     => '20px',
             'index'     => 'vendor_type',
             'type'      => 'options',
             'options'=> Mage::getSingleton('zolagodropship/source')->setPath('vendorstype')->toOptionHash()
-        ));
+        ),'carrier_code');
 
-        $this->addColumn('url_key', array(
-            'header'    => $hlp->__('URL key'),
-            'align'     => 'right',
-            'width'     => '180px',
-            'index'     => 'url_key',
-            'type'      => 'text',
-        ));
-        $this->addColumn('sequence', array(
-            'header'    => $hlp->__('Sequence'),
-            'align'     => 'right',
-            'width'     => '50px',
-            'index'     => 'sequence',
-            'type'      => 'number',
-        ));
-        if (Mage::helper('udropship')->isUdsprofileActive()) {
-            $this->addColumn('shipping_profile', array(
-                'header'    => $hlp->__('Shipping Profile'),
-                'index'     => 'shipping_profile',
-                'type'      => 'options',
-                'options'   => Mage::getSingleton('udsprofile/source')->setPath('profiles')->toOptionHash(),
-            ));
-        }
-
-        $this->addColumn('status', array(
-            'header'    => $hlp->__('Status'),
-            'index'     => 'status',
-            'type'      => 'options',
-            'options'   => Mage::getSingleton('udropship/source')->setPath('vendor_statuses')->toOptionHash(),
-        ));
-
-        $this->addColumn('action',
-            array(
-                'header'    => Mage::helper('sales')->__('Action'),
-                'width'     => '50px',
-                'type'      => 'action',
-                'getter'     => 'getId',
-                'actions'   => array(
-                    array(
-                        'caption' => Mage::helper('sales')->__('View'),
-                        'url'     => array('base'=>'udropshipadmin/adminhtml_vendor/edit'),
-                        'field'   => 'id'
-                    )
-                ),
-                'filter'    => false,
-                'sortable'  => false,
-                'is_system' => true
-            ));
-
-        Mage::dispatchEvent('udropship_adminhtml_vendor_grid_prepare_columns', array('grid'=>$this));
-
-        $this->addExportType('*/*/exportCsv', Mage::helper('adminhtml')->__('CSV'));
-        $this->addExportType('*/*/exportXml', Mage::helper('adminhtml')->__('XML'));
         return parent::_prepareColumns();
     }
 }
