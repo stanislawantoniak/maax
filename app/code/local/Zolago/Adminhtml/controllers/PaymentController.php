6441b6f118354f9021d3c8b6e3378f42f26266c3
Adam Wilk <adam.wilk@convertica.pl>
2015-03-16 13:40:10 +0100

    re#822 mass action controller

--- app/code/local/Zolago/Adminhtml/controllers/PaymentController.php
+++ app/code/local/Zolago/Adminhtml/controllers/PaymentController.php
@@ -2,9 +2,14 @@
 	class Zolago_Adminhtml_PaymentController extends Mage_Adminhtml_Controller_Action {
 
 		public function refundAction() {
-			Mage::log("got here!");
-			var_dump($this->getRequest()->getParam('txn'));
-			die();
+			$transactions = $this->getRequest()->getParam('txn');
+
+			foreach($transactions as $txnId) {
+				$transaction = Mage::getModel("sales/order_payment_transaction")->load($txnId);
+				var_dump($transaction);
+//				$transaction->setOrderPaymentObject($order->getPayment());
+//				$transaction->loadByTxnId($txnId);
+			}
 			/** @var Zolago_Dotpay_Model_Client $dotpay */
 			/*$dotpay = Mage::getModel("zolagodotpay/client");
 
