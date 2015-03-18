88c39aca0559918af0f9e2083ab1a3acf271513a
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2015-03-18 16:29:04 +0100

    stałe refs #1010

--- app/code/local/GH/Api/Model/Message.php
+++ app/code/local/GH/Api/Model/Message.php
@@ -1,12 +1,5 @@
 <?php
 class GH_Api_Model_Message extends Mage_Core_Model_Abstract {
-	const GH_API_MESSAGE_NEW_ORDER = 'newOrder';
-	const GH_API_MESSAGE_CANCELLED_ORDER = 'cancelledOrder';
-	const GH_API_MESSAGE_PAYMENT_DATA_CHANGED = 'paymentDataChanged';
-	const GH_API_MESSAGE_ITEMS_CHANGED = 'itemsChanged';
-	const GH_API_MESSAGE_DELIVERY_DATA_CHANGED = 'deliveryDataChanged';
-	const GH_API_MESSAGE_INVOICE_ADDRESS_CHANGED = 'invoiceAddressChanged';
-	const GH_API_MESSAGE_STATUS_CHANGED = 'statusChanged';
 
 	protected function _construct()
 	{
