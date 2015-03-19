3ac77f02fb37ead5dc0cc38c8bb18c0d072b07fa
victory <victoria.sultanovska@convertica.pl>
2015-03-19 14:31:18 +0100

    Refs#984 vendor portal settings

---
+++ app/code/local/GH/Api/controllers/Dropship/GhapiController.php
@@ -0,0 +1,13 @@
+<?php
+
+class GH_Api_Dropship_GhapiController extends Zolago_Dropship_Controller_Vendor_Abstract {
+
+	public function indexAction() {
+//        die('test');
+		$this->_renderPage(null, 'ghapi');
+	}
+
+
+}
+
+
