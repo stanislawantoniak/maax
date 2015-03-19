8b0e72ac69e9f0908333269b788aaf30a637c5c3
victory <victoria.sultanovska@convertica.pl>
2015-03-19 12:57:58 +0100

    Refs#954 vendor access

---
+++ app/code/local/GH/Api/Model/Observer.php
@@ -0,0 +1,38 @@
+<?php
+class GH_Api_Model_Observer
+{
+	/**
+     * Add tab to vendor view
+     *
+     * @param $observer Varien_Event_Observer
+	 * 
+     * @return GH_Api_Model_Observer
+     */
+    public function udropship_adminhtml_vendor_tabs_after($observer)
+    {
+        $block = $observer->getEvent()->getBlock();
+        $id = $observer->getEvent()->getId();
+        $v = Mage::helper('udropship')->getVendor($id);
+
+        if (!$block instanceof Unirgy_Dropship_Block_Adminhtml_Vendor_Edit_Tabs
+            || !Mage::app()->getRequest()->getParam('id', 0)
+        ) {
+            return;
+        }
+
+        if ($block instanceof Unirgy_Dropship_Block_Adminhtml_Vendor_Edit_Tabs) {
+            $ghApiBlock = Mage::app()
+                ->getLayout()
+                ->createBlock('ghapi/adminhtml_dropship_settings_ghapi_form', 'vendor.ghapi.form')
+                ->setVendorId($v)
+                ->toHtml();
+
+                $block->addTab('ghapi_settings', array(
+                    'label'     => Mage::helper('ghapi')->__('GH API Settings'),
+                    'after'     => 'sizetable_attributeset',
+                    'content'	=> $ghApiBlock
+                ));
+        }
+    }
+
+}
