edf18d04221f41894b4832b87f41e007bec32f7c
victory <victoria.sultanovska@convertica.pl>
2015-03-19 16:51:58 +0100

    Refs#1011 vendor portal settings

--- app/code/local/GH/Api/Block/Dropship/Settings.php
+++ app/code/local/GH/Api/Block/Dropship/Settings.php
@@ -1,44 +1,13 @@
 <?php
 class GH_Api_Block_Dropship_Settings extends Mage_Core_Block_Template {
-    protected function _construct()
+
+    protected function getGhApiVendorUser()
     {
-        parent::_construct();
+        /* @var $ghUserModel GH_Api_Model_Vendor_User */
+        $ghUserModel = Mage::getModel('ghapi/vendor_user');
+        $vendorApiUser = $ghUserModel->generateGhApiVendorUser();
+
+        return $vendorApiUser;
     }
-    public function _prepareLayout() {
-        $this->_prepareForm();
-        parent::_prepareLayout();
-    }
-    public function _prepareForm(){
-        $helper = Mage::helper('ghapi');
-        $form = Mage::getModel('zolagodropship/form');
 
-        /* @var $form Zolago_Dropship_Model_Form */
-        $form->setAction($this->getUrl("ghapi/dropship/ghapi/save"));
-
-        $general = $form->addFieldset("login_data", array(
-            "legend" => $helper->__("Login Data")
-        ));
-
-        $general->addField("ghapi_vendor_id", "text", array(
-            "name" => "ghapi_vendor_id",
-            "class" => "form-control",
-            "required" => true,
-            "label" => $helper->__('Vendor ID')
-        ));
-        $general->addField("ghapi_vendor_password", "password", array(
-            "name" => "ghapi_vendor_password",
-            "class" => "form-control",
-            "required" => true,
-            "label" => $helper->__('Password')
-        ));
-        $general->addField("ghapi_vendor_api_key", "text", array(
-            "name" => "ghapi_vendor_api_key",
-            "class" => "form-control",
-            "required" => true,
-            "label" => $helper->__('API Key')
-        ));
-
-        //$form->setValues($values);
-        $this->setForm($form);
-    }
 }
--- app/code/local/GH/Api/controllers/Dropship/GhapiController.php
+++ app/code/local/GH/Api/controllers/Dropship/GhapiController.php
@@ -7,6 +7,34 @@ class GH_Api_Dropship_GhapiController extends Zolago_Dropship_Controller_Vendor_
 	}
 
 
+    public function saveAction()
+    {
+        $helper = Mage::helper('ghapi');
+        if (!$this->getRequest()->isPost()) {
+            return $this->_redirectReferer();
+        }
+
+        $vendor = $this->_getSession()->getVendor();
+        $vendorId = (int)$vendor->getVendorId();
+
+        $ghapiVendorPassword = $this->getRequest()->getPost('ghapi_vendor_password');
+
+        if (!empty($ghapiVendorPassword)) {
+            //update password
+            /* @var $ghApiUser GH_Api_Model_User */
+            $ghApiUser = Mage::getModel('ghapi/user');
+            $vendorApiUser = $ghApiUser->loadByVendorId($vendorId);
+
+            // If Edit
+            if (is_null($vendorApiUser->getUserId()) || !($vendor->getExternalId() == $vendorApiUser->getVendorId())) {
+                throw new Mage_Core_Exception($helper->__("It is not your settings"));
+            }
+            $password = $ghApiUser->updateUserPassword($ghapiVendorPassword, $vendorId);
+
+            $this->_getSession()->addSuccess($helper->__('API Password saved'));
+        }
+        return $this->_redirectReferer();
+    }
 }
 
 
