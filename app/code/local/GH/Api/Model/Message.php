018d85275feed45050352ca95320e17181e031bd
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-03-20 15:24:27 +0100

    using gh_api vendor portal config for adding message refs#1010

--- app/code/local/GH/Api/Model/Message.php
+++ app/code/local/GH/Api/Model/Message.php
@@ -29,14 +29,16 @@ class GH_Api_Model_Message extends Mage_Core_Model_Abstract {
 	 * no new messages with same po_increment_id, same message text and status new exists
 	 * if message with same message text and po_increment_id but its status is read then it adds a new one
 	 * if message with same message text, po_increment_id and status new exist then only update updated_at field
-	 * @param $po
+	 * @param $po Zolago_Po_Model_Po
 	 * @param string $message
 	 * @return GH_Api_Model_Message
 	 */
 	public function addMessage($po,$message) {
         if(!$this->validateMessage($message)) {
 			return false;
-		} elseif(!($po instanceof Unirgy_DropshipPo_Model_Po)) {
+		} elseif(!$this->isNoticeMessageActive($po->getVendor(), $message)) {
+            return false;
+        } elseif(!($po instanceof Unirgy_DropshipPo_Model_Po)) {
 			Mage::throwException('Message could not be added because of wrong PO object');
 		} else {
             $this->unsetData();
@@ -85,6 +87,38 @@ class GH_Api_Model_Message extends Mage_Core_Model_Abstract {
 		$this->throwWrongMessageException();
 		return false;
 	}
+
+    /**
+     * @param $vendor Zolago_Dropship_Model_Vendor
+     * @param $messages @see GH_Api_Model_System_Source_Message_Type
+     * @return bool
+     */
+    protected function isNoticeMessageActive($vendor, $messages) {
+        switch ($messages) {
+            case GH_Api_Model_System_Source_Message_Type::GH_API_MESSAGE_NEW_ORDER:
+                return $vendor->getData('ghapi_message_new_order');
+                break;
+            case GH_Api_Model_System_Source_Message_Type::GH_API_MESSAGE_CANCELLED_ORDER:
+                return $vendor->getData('ghapi_message_order_canceled');
+                break;
+            case GH_Api_Model_System_Source_Message_Type::GH_API_MESSAGE_PAYMENT_DATA_CHANGED:
+                return $vendor->getData('ghapi_message_order_payment_changes');
+                break;
+            case GH_Api_Model_System_Source_Message_Type::GH_API_MESSAGE_ITEMS_CHANGED:
+                return $vendor->getData('ghapi_message_order_product_changes');
+                break;
+            case GH_Api_Model_System_Source_Message_Type::GH_API_MESSAGE_DELIVERY_DATA_CHANGED:
+                return $vendor->getData('ghapi_message_order_shipping_changes');
+                break;
+            case GH_Api_Model_System_Source_Message_Type::GH_API_MESSAGE_INVOICE_ADDRESS_CHANGED:
+                return $vendor->getData('ghapi_message_order_invoice_changes');
+                break;
+            case GH_Api_Model_System_Source_Message_Type::GH_API_MESSAGE_STATUS_CHANGED:
+                return $vendor->getData('ghapi_message_order_status_changes');
+                break;
+        }
+        return false; // Never
+    }
 
 	/**
 	 * Confirms user messages, selects from db only ids that user is allowed to delete
