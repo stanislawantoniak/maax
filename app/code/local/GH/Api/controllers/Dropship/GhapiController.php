2e379df36cfa458f85ee147e64414025cf818a0b
victory <victoria.sultanovska@convertica.pl>
2015-03-20 12:55:44 +0100

    Refs#1011 messages

--- app/code/local/GH/Api/controllers/Dropship/GhapiController.php
+++ app/code/local/GH/Api/controllers/Dropship/GhapiController.php
@@ -19,20 +19,36 @@ class GH_Api_Dropship_GhapiController extends Zolago_Dropship_Controller_Vendor_
 
         $ghapiVendorPassword = $this->getRequest()->getPost('ghapi_vendor_password');
 
+        /* @var $ghApiUser GH_Api_Model_User */
+        $ghApiUser = Mage::getModel('ghapi/user');
+        $vendorApiUser = $ghApiUser->loadByVendorId($vendorId);
+
+        // If Edit
+        if (is_null($vendorApiUser->getUserId()) || !($vendor->getExternalId() == $vendorApiUser->getVendorId())) {
+            throw new Mage_Core_Exception($helper->__("It is not your settings"));
+        }
+
         if (!empty($ghapiVendorPassword)) {
             //update password
-            /* @var $ghApiUser GH_Api_Model_User */
-            $ghApiUser = Mage::getModel('ghapi/user');
-            $vendorApiUser = $ghApiUser->loadByVendorId($vendorId);
-
-            // If Edit
-            if (is_null($vendorApiUser->getUserId()) || !($vendor->getExternalId() == $vendorApiUser->getVendorId())) {
-                throw new Mage_Core_Exception($helper->__("It is not your settings"));
-            }
             $password = $ghApiUser->updateUserPassword($ghapiVendorPassword, $vendorId);
-
             $this->_getSession()->addSuccess($helper->__('API Password saved'));
         }
+
+        $postData = $this->getRequest()->getPost();
+
+        $vendor->setData('ghapi_message_new_order', isset($postData['ghapi_message_new_order']) ? 1 : 0);
+        $vendor->setData('ghapi_message_order_canceled', isset($postData['ghapi_message_order_canceled']) ? 1 : 0);
+
+        $vendor->setData('ghapi_message_order_payment_changes', isset($postData['ghapi_message_order_payment_changes']) ? 1 : 0);
+        $vendor->setData('ghapi_message_order_product_changes', isset($postData['ghapi_message_order_product_changes']) ? 1 : 0);
+
+        $vendor->setData('ghapi_message_order_shipping_changes', isset($postData['ghapi_message_order_shipping_changes']) ? 1 : 0);
+        $vendor->setData('ghapi_message_order_invoice_changes', isset($postData['ghapi_message_order_invoice_changes']) ? 1 : 0);
+
+        $vendor->setData('ghapi_message_order_status_changes', isset($postData['ghapi_message_order_status_changes']) ? 1 : 0);
+
+        $vendor->save();
+
         return $this->_redirectReferer();
     }
 }
