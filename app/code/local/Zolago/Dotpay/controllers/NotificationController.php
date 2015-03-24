66fb30e4552049575623851a1882df2aff1fccd4
Adam Wilk <adam.wilk@convertica.pl>
2015-03-24 14:12:36 +0100

    Testing in regress ;) rollback to before debugo

--- app/code/local/Zolago/Dotpay/controllers/NotificationController.php
+++ app/code/local/Zolago/Dotpay/controllers/NotificationController.php
@@ -7,38 +7,23 @@ class Zolago_Dotpay_NotificationController extends Dotpay_Dotpay_NotificationCon
 	{
 		$data = $this->getRequest()->getPost();
 
-		Mage::log('DATA:', null, 'dotpay.log');
-		Mage::log($data, null, 'dotpay.log');
-
-		if(isset($data['control'])) {
-			/** @var Mage_Sales_Model_Order $order */
-			$order = Mage::getModel('sales/order');
-			$order->loadByIncrementId($data['control']);
-			if (!$order->getId()) {
-				Mage::log('WRONG ORDER', null, 'dotpay.log');
-				die(Zolago_Dotpay_Model_Client::DOTPAY_STATUS_ERROR);
-			}
-		} else {
+		/** @var Mage_Sales_Model_Order $order */
+		$order = Mage::getModel('sales/order');
+		$order->loadByIncrementId($data['control']);
+		if (!$order->getId()) {
 			die(Zolago_Dotpay_Model_Client::DOTPAY_STATUS_ERROR);
-		}
-
-		if(isset($data['operation_original_currency']) && isset($data['operation_original_amount'])) {
-			if (!($order->getOrderCurrencyCode() == $data['operation_original_currency']
-				&& round($order->getGrandTotal(), 2) == $data['operation_original_amount'])
-			) {
-				Mage::log('WORNG CURRENCY:', null, 'dotpay.log');
-				Mage::log('1ST CHECK:' . ($order->getOrderCurrencyCode() != $data['operation_original_currency'] ? 'FAILED' : 'OK'), null, 'dotpay.log');
-				Mage::log('2ND CHECK:' . (round($order->getGrandTotal(), 2) == $data['operation_original_amount'] ? 'FAILED' : 'OK'), null, 'dotpay.log');
-				die(Zolago_Dotpay_Model_Client::DOTPAY_STATUS_ERROR);
-			}
 		}
 
 		/** @var Zolago_Dotpay_Model_Client $client */
 		$client = Mage::getModel("zolagodotpay/client");
+
+		if (!($order->getOrderCurrencyCode() == $data['operation_original_currency']
+			&& round($order->getGrandTotal(), 2) == $data['operation_original_amount'])) {
+			die(Zolago_Dotpay_Model_Client::DOTPAY_STATUS_ERROR);
+		}
+
 		//Save transaction
 		$transaction = $client->saveTransactionFromPing($order,$data);
-		Mage::log('TRANSACTION OBJECT:',null,'dotpay.log');
-		Mage::log($transaction,null,'dotpay.log');
 		if($transaction !== false) {
 			if ($data['operation_status'] == Zolago_Dotpay_Model_Client::DOTPAY_OPERATION_STATUS_COMPLETED) {
 				$order->addStatusHistoryComment(
