fec8b30dd730884f35ebb531c86f9159dcca55e2
victory <victoria.sultanovska@convertica.pl>
2015-04-07 17:14:14 +0200

    Refs#1054

--- app/code/local/Zolago/Po/Model/Observer.php
+++ app/code/local/Zolago/Po/Model/Observer.php
@@ -150,6 +150,7 @@ class Zolago_Po_Model_Observer extends Zolago_Common_Model_Log_Abstract{
 	 * @param Mage_Core_Model_Observer $observer
 	 */
 	public function poChangeStatus($observer) {
+        Mage::log('poChangeStatus', null, 'reservation.log');
 		/* @var $po Zolago_Po_Model_Po */
 		$po = $observer->getEvent()->getData('po');
 		if($po instanceof Zolago_Po_Model_Po && $po->getId()){
@@ -182,6 +183,7 @@ class Zolago_Po_Model_Observer extends Zolago_Common_Model_Log_Abstract{
 	}
 
     public function updatePoStatusByAllocation($observer){
+        Mage::log('updatePoStatusByAllocation', null, 'reservation.log');
         /* @var $po Zolago_Po_Model_Po */
         $po = $observer->getEvent()->getData('po');
         if(!$po->getId()) {
@@ -435,6 +437,7 @@ class Zolago_Po_Model_Observer extends Zolago_Common_Model_Log_Abstract{
      * cancel orders with all po canceled
      */
     public function cronCancelOrders() {
+        Mage::log('cronCancelOrders', null, 'reservation.log');
         $order_collection = Mage::getModel('sales/order')->getCollection(); 
         $order_collection->addFieldToFilter('state',	
             array('in'=>array (
@@ -520,7 +523,31 @@ class Zolago_Po_Model_Observer extends Zolago_Common_Model_Log_Abstract{
             }
         }
     }
+    public function setReservationFlag($observer) {
+        $po = $observer->getPo();
+        $oldStatus = $observer->getOldStatus();
+        $newStatus = $observer->getNewStatus();
+        $vendor = $po->getVendor();
+        $ghapiAccess = $vendor->getData('ghapi_vendor_access_allow');
 
+        //Mage::log($ghapiAccess, null, 'setReservation.log');
+        if(($oldStatus !== $newStatus) && ($ghapiAccess == 0)){
+            Mage::log($newStatus, null, 'setReservation.log');
+            $poOpenOrder = Mage::getStoreConfig('zolagocatalog/config/po_open_order');
+            Mage::log($poOpenOrder, null, 'setReservation.log');
+
+            if(in_array($newStatus, explode(',', $poOpenOrder))){
+                //set reservation=1
+                $po->setReservation(1);
+                $po->getResource()->saveAttribute($po, 'reservation');
+            } else {
+                //set reservation=0
+                $po->setReservation(0);
+                $po->getResource()->saveAttribute($po, 'reservation');
+            }
+            Mage::log('----------------', null, 'setReservation.log');
+        }
+    }
     /**
      * Adding messages ITEMS_CHANGED to GH_API queue
      *
