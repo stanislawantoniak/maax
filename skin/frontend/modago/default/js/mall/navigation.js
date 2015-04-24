948ba0a2e0b387e80c28a89f27f505d711eac756
Adam Wilk <awilk@convertica.pl>
2015-04-24 11:25:42 +0200

    re#1092 added line

--- skin/frontend/modago/default/js/mall/navigation.js
+++ skin/frontend/modago/default/js/mall/navigation.js
@@ -9,8 +9,7 @@ Mall.Navigation = {
                 var arr = jQuery(this).attr('data-catids').split(',');
                 for (i = 0; i < arr.length; i++) {
                     if (self.currentCategoryId.indexOf(arr[i]) != -1) {
-                        //jQuery(this).addClass('forceActive'); //todo
-                        jQuery(this).css('background', 'red');
+                        jQuery(this).addClass('forceActive');
                         self.highligted = true;
                         break;
                     }
@@ -22,8 +21,7 @@ Mall.Navigation = {
     destroy: function() {
         jQuery("#navigation a[data-catids]").each(function(index, value) {
             if (self.highligted) {
-                //jQuery(this).removeClass('forceActive'); //todo
-                jQuery(this).css('background', '');
+                jQuery(this).removeClass('forceActive');
                 self.highligted = false;
             }
         });
