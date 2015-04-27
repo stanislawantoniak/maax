f0ecbdd873385b79e6920cec96d88939964b8b0b
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-04-27 16:03:54 +0200

    refs#1092 fix behaviour of highlighted element in desktop navigation

--- skin/frontend/modago/default/js/mall/navigation.js
+++ skin/frontend/modago/default/js/mall/navigation.js
@@ -1,31 +1,50 @@
 Mall.Navigation = {
     currentCategoryId: [],
-    highligted: false,
+    highlighted: false,
 
     init: function() {
         var self = this;
         jQuery("#navigation a[data-catids]").each(function(index, value) {
-            if (!self.highligted) {
+            if (!self.highlighted) {
                 var arr = jQuery(this).attr('data-catids').split(',');
                 for (i = 0; i < arr.length; i++) {
                     if (self.currentCategoryId.indexOf(arr[i]) != -1) {
                         jQuery(this).addClass('forceActive');
-                        self.highligted = true;
+                        jQuery(this).attr('data-flagForceActive', 1);
+                        self.highlighted = true;
                         break;
                     }
                 }
             }
         });
+        self.attachOnChangeDesktopSubmenu();
     },
 
     destroy: function() {
         jQuery("#navigation a[data-catids]").each(function(index, value) {
-            if (self.highligted) {
+            if (self.highlighted) {
                 jQuery(this).removeClass('forceActive');
-                self.highligted = false;
+                jQuery(this).removeAttr('data-flagForceActive');
+                self.highlighted = false;
             }
         });
     },
+
+    /**
+     * When click in main category from desktop navigation menu
+     * highlighted element (current category context) should temporary be disabled
+     */
+    attachOnChangeDesktopSubmenu: function() {
+        var customToggle = function () {
+            if (jQuery('#nav_desc a').hasClass('active')) {
+                jQuery('#nav_desc a[data-flagForceActive="1"]').removeClass('forceActive');
+            } else {
+                jQuery('#nav_desc a[data-flagForceActive="1"]').addClass('forceActive');
+            }
+        };
+        jQuery(document).delegate('#nav_desc a'  , 'click', customToggle);
+        jQuery(document).delegate('.closeSubMenu', 'click', customToggle);
+    }
 };
 
 jQuery(document).ready(function() {
