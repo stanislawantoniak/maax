1f9bf786b553382fb0e414ce4711993d29b19e8d
victory <victoria.sultanovska@convertica.pl>
2015-05-06 12:04:37 +0200

    Refs#1126 hamburger menu

--- skin/frontend/modago/default/js/mall.js
+++ skin/frontend/modago/default/js/mall.js
@@ -1315,9 +1315,11 @@ jQuery(document).ready(function() {
         swipeLeft:function(event) {
             closeHamburgerMenu(event);
         },
-        //swipeStatus:function(event, phase) {
-        //    return false;
-        //},
+        swipeStatus:function(event, phase) {
+            if(!Mall.getIsBrowserMobile){
+                return false;
+            }
+        },
         excludedElements: "label, button, input, select, textarea, .noSwipe",
         //Default is 75px, set to 0 for demo so any distance triggers swipe
         threshold: 5
