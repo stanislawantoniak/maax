617be2dbef4a80d3d028628dd88b7c6cc7fbc03c
victory <victoria.sultanovska@convertica.pl>
2015-05-06 11:26:14 +0200

    hamburger menu

--- skin/frontend/modago/default/js/mall/listing.js
+++ skin/frontend/modago/default/js/mall/listing.js
@@ -1325,6 +1325,15 @@ Mall.listing = {
 		this.initActiveEvents();
 		this.initListingLinksEvents();
         this.initImagesHeight();
+        jQuery("#solr_search_facets, #solr_search_facets .section, #solr_search_facets .section a").swipe({
+            swipeLeft:function(event, direction) {
+                console.log(direction);
+                Mall.listing.closeMobileFilters();
+            },
+            excludedElements: "label, button, input, select, textarea, .noSwipe",
+            //Default is 75px, set to 0 for demo so any distance triggers swipe
+            threshold: 5
+        });
 	},
 
 	replaceProducts: function(data){
