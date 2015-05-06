e4b4b68b2d67dd036b3d7de7bb14ae45c3cebf46
victory <victoria.sultanovska@convertica.pl>
2015-05-06 11:56:49 +0200

    Refs#1126 hamburger menu

--- skin/frontend/modago/default/js/mall.js
+++ skin/frontend/modago/default/js/mall.js
@@ -1309,6 +1309,20 @@ jQuery(document).ready(function() {
 	initToggleSearch();
     Mall.disableSearchNoQuery();
 
+    // Slidebars Submenus
+    jQuery(".sb-slidebar ul li a").swipe( {
+        //Generic swipe handler for all directions
+        swipeLeft:function(event) {
+            closeHamburgerMenu(event);
+        },
+        //swipeStatus:function(event, phase) {
+        //    return false;
+        //},
+        excludedElements: "label, button, input, select, textarea, .noSwipe",
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold: 5
+    });
+
     //hack for vendor main page (turpentine shows global messages only one time)
     if(jQuery(".page-messages-block ul.messages").length > 0){
         var messages = jQuery(".page-messages-block ul.messages").clone();
