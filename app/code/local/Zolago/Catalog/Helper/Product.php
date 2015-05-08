5de06d44a8830993a05143323b0e6b970d4de223
victory <victoria.sultanovska@convertica.pl>
2015-05-08 15:45:40 +0200

    Refs#1143 test

--- app/code/local/Zolago/Catalog/Helper/Product.php
+++ app/code/local/Zolago/Catalog/Helper/Product.php
@@ -87,7 +87,88 @@ class Zolago_Catalog_Helper_Product extends Mage_Catalog_Helper_Product {
         }
         return null;
     }
+    /**
+     * Inits product to be used for product controller actions and layouts
+     * $params can have following data:
+     *   'category_id' - id of category to check and append to product as current.
+     *     If empty (except FALSE) - will be guessed (e.g. from last visited) to load as current.
+     *
+     * @param int $productId
+     * @param Mage_Core_Controller_Front_Action $controller
+     * @param Varien_Object $params
+     *
+     * @return false|Mage_Catalog_Model_Product
+     */
+    public function initProduct($productId, $controller, $params = null)
+    {
+        // Prepare data for routine
+        if (!$params) {
+            $params = new Varien_Object();
+        }
 
+        // Init and load product
+        Mage::dispatchEvent('catalog_controller_product_init_before', array(
+            'controller_action' => $controller,
+            'params' => $params,
+        ));
+
+        if (!$productId) {
+            return false;
+        }
+
+        $product = Mage::getModel('catalog/product')
+            ->setStoreId(Mage::app()->getStore()->getId())
+            ->load($productId);
+
+        if (!$this->canShow($product)) {
+            return false;
+        }
+        if (!in_array(Mage::app()->getStore()->getWebsiteId(), $product->getWebsiteIds())) {
+            return false;
+        }
+
+        // Load product current category
+        $categoryId = $params->getCategoryId();
+        if (!$categoryId && ($categoryId !== false)) {
+            $rootId = Mage::helper("zolagosolrsearch")->getRootCategoryId();
+            $categoryD = Mage::helper("zolagosolrsearch")->getDefaultCategory($product, $rootId);
+            if($categoryD && $categoryD->getId()){
+                $lastId = $categoryD->getId();
+            } else {
+                $lastId = Mage::getSingleton('catalog/session')->getLastVisitedCategoryId();
+            }
+
+            if ($product->canBeShowInCategory($lastId)) {
+                $categoryId = $lastId;
+            }
+        } elseif (!$product->canBeShowInCategory($categoryId)) {
+            $categoryId = null;
+        }
+
+        if ($categoryId) {
+            $category = Mage::getModel('catalog/category')->load($categoryId);
+            $product->setCategory($category);
+            Mage::register('current_category', $category);
+        }
+
+        // Register current data and dispatch final events
+        Mage::register('current_product', $product);
+        Mage::register('product', $product);
+
+        try {
+            Mage::dispatchEvent('catalog_controller_product_init', array('product' => $product));
+            Mage::dispatchEvent('catalog_controller_product_init_after',
+                array('product' => $product,
+                    'controller_action' => $controller
+                )
+            );
+        } catch (Mage_Core_Exception $e) {
+            Mage::logException($e);
+            return false;
+        }
+
+        return $product;
+    }
     /**
      * @param Mage_Catalog_Model_Product $model
      * @return null|string
--- skin/frontend/modago/default/js/mall/listing.js
+++ skin/frontend/modago/default/js/mall/listing.js
@@ -2570,10 +2570,17 @@ Mall.listing = {
 
     delegateSaveContextForProductPage: function() {
         jQuery(document).delegate('.box_listing_product a','mousedown',function(e) {
+            console.log(jQuery('ol.breadcrumb').attr('data-search'));
             if (jQuery('ol.breadcrumb').attr('data-search') == "0") {
                 e.preventDefault();
                 localStorage.setItem(jQuery(this).attr("data-entity"), jQuery('#breadcrumbs-header ol').html());
+                localStorage.setItem(jQuery(this).attr("data-entity")+ "_search_params", "");
             }
+            if (jQuery('ol.breadcrumb').attr('data-search') == "1") {
+                e.preventDefault();
+                localStorage.setItem(jQuery(this).attr("data-entity")+ "_search_params", window.location.search);
+            }
+
         });
     },
 
