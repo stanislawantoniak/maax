715a411f9169619f344c00f94b5a9ef84c0d5555
victory <victoria.sultanovska@convertica.pl>
2015-05-11 12:26:31 +0200

    invitation

--- app/code/local/Zolago/SalesManago/Helper/Data.php
+++ app/code/local/Zolago/SalesManago/Helper/Data.php
@@ -35,7 +35,7 @@ class Zolago_SalesManago_Helper_Data extends SalesManago_Tracking_Helper_Data
         if (isset($data['name']) && !empty($data['name'])) {
             $data_to_json['contact']['name'] = $data['name'];
         }
-
+        Mage::log("Zolago_SalesManago_Helper_Data isset is_subscribed" . (int)isset($data['is_subscribed']), null, "salesmanago.log");
         if ($register || !isset($data['is_subscribed'])) {
             $data_to_json['forceOptIn'] = false;
             $data_to_json['forceOptOut'] = true;
