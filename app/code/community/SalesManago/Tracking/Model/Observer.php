88892d3b00dc1fb74d58e50c9ab3d759e061d4ba
victory <victoria.sultanovska@convertica.pl>
2015-05-11 17:52:41 +0200

    subscribe

--- app/code/community/SalesManago/Tracking/Model/Observer.php
+++ app/code/community/SalesManago/Tracking/Model/Observer.php
@@ -140,60 +140,60 @@ class SalesManago_Tracking_Model_Observer {
 		}
 	}
 	
-	public function newsletter_subscriber_save_before($observer){
-        Mage::log("SalesManago_Tracking_Model_Observer:newsletter_subscriber_save_before", null, "salesmanago.log");
-		$request        = Mage::app()->getRequest();
-		$moduleName     = $request->getModuleName();
-		$controllerName = $request->getControllerName();
-		$actionName     = $request->getActionName();
-		
-		if( ($moduleName=='newsletter' && $controllerName=='manage' && $actionName=='save') ||
-			($moduleName=='newsletter' && $controllerName=='subscribe' && $actionName=='unsubscribe')){
-
-			$clientId = Mage::getStoreConfig('salesmanago_tracking/general/client_id');
-			$apiSecret = Mage::getStoreConfig('salesmanago_tracking/general/api_secret');
-			$ownerEmail = Mage::getStoreConfig('salesmanago_tracking/general/email');
-			$tags = Mage::getStoreConfig('salesmanago_tracking/general/tags');
-			$endPoint = Mage::getStoreConfig('salesmanago_tracking/general/endpoint');
-			
-			$subscriber = $observer->getEvent()->getDataObject();
-			$data = $subscriber->getData();
-			$email = $data['subscriber_email'];
-			$statusChange = $subscriber->getIsStatusChanged();
-			
-			$id    = (int) Mage::app()->getFrontController()->getRequest()->getParam('id');
-			$code  = (string) Mage::app()->getFrontController()->getRequest()->getParam('code');
-
-			$apiKey = md5(time().$apiSecret);
-
-			$data_to_json = array(
-				'apiKey' => $apiKey,
-				'clientId' => $clientId,
-				'requestTime' => time(),
-				'email' => $email,
-				'sha' => sha1($apiKey . $clientId . $apiSecret),
-				'owner' => $ownerEmail,
-			);
-			
-			if ($data['subscriber_status'] == "1" && ($statusChange == true || ($id && $code))) {
-				$data_to_json['forceOptIn'] = true;
-			} elseif ($data['subscriber_status'] == "3" && ($statusChange == true || ($id && $code))) {
-				$data_to_json['forceOptOut'] = true;
-			}
-
-			$json = json_encode($data_to_json);
-			$result = $this->_getHelper()->_doPostRequest('https://'.$endPoint.'/api/contact/update', $json);
-			
-			$r = json_decode($result, true);
-			
-			if($r==false || (isset($r['success']) && $r['success']==false)){
-				$data['customerEmail'] = $email;
-				$data['status'] = 0;
-				$data['action'] = 4; //zapis / wypis z newslettera
-				$this->_getHelper()->setSalesmanagoCustomerSyncStatus($data);
-			}
-			
-			return $r;
-		}
-	}
+//	public function newsletter_subscriber_save_before($observer){
+//        Mage::log("SalesManago_Tracking_Model_Observer:newsletter_subscriber_save_before", null, "salesmanago.log");
+//		$request        = Mage::app()->getRequest();
+//		$moduleName     = $request->getModuleName();
+//		$controllerName = $request->getControllerName();
+//		$actionName     = $request->getActionName();
+//
+//		if( ($moduleName=='newsletter' && $controllerName=='manage' && $actionName=='save') ||
+//			($moduleName=='newsletter' && $controllerName=='subscribe' && $actionName=='unsubscribe')){
+//
+//			$clientId = Mage::getStoreConfig('salesmanago_tracking/general/client_id');
+//			$apiSecret = Mage::getStoreConfig('salesmanago_tracking/general/api_secret');
+//			$ownerEmail = Mage::getStoreConfig('salesmanago_tracking/general/email');
+//			$tags = Mage::getStoreConfig('salesmanago_tracking/general/tags');
+//			$endPoint = Mage::getStoreConfig('salesmanago_tracking/general/endpoint');
+//
+//			$subscriber = $observer->getEvent()->getDataObject();
+//			$data = $subscriber->getData();
+//			$email = $data['subscriber_email'];
+//			$statusChange = $subscriber->getIsStatusChanged();
+//
+//			$id    = (int) Mage::app()->getFrontController()->getRequest()->getParam('id');
+//			$code  = (string) Mage::app()->getFrontController()->getRequest()->getParam('code');
+//
+//			$apiKey = md5(time().$apiSecret);
+//
+//			$data_to_json = array(
+//				'apiKey' => $apiKey,
+//				'clientId' => $clientId,
+//				'requestTime' => time(),
+//				'email' => $email,
+//				'sha' => sha1($apiKey . $clientId . $apiSecret),
+//				'owner' => $ownerEmail,
+//			);
+//
+//			if ($data['subscriber_status'] == "1" && ($statusChange == true || ($id && $code))) {
+//				$data_to_json['forceOptIn'] = true;
+//			} elseif ($data['subscriber_status'] == "3" && ($statusChange == true || ($id && $code))) {
+//				$data_to_json['forceOptOut'] = true;
+//			}
+//
+//			$json = json_encode($data_to_json);
+//			$result = $this->_getHelper()->_doPostRequest('https://'.$endPoint.'/api/contact/update', $json);
+//
+//			$r = json_decode($result, true);
+//
+//			if($r==false || (isset($r['success']) && $r['success']==false)){
+//				$data['customerEmail'] = $email;
+//				$data['status'] = 0;
+//				$data['action'] = 4; //zapis / wypis z newslettera
+//				$this->_getHelper()->setSalesmanagoCustomerSyncStatus($data);
+//			}
+//
+//			return $r;
+//		}
+//	}
 }
