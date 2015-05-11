b5c901b24ff38cbe0f44a0a5273e5c480b25cd4a
victory <victoria.sultanovska@convertica.pl>
2015-05-11 13:15:53 +0200

    invitation

--- app/code/local/Zolago/Newsletter/Model/Inviter.php
+++ app/code/local/Zolago/Newsletter/Model/Inviter.php
@@ -73,7 +73,9 @@ class Zolago_Newsletter_Model_Inviter extends Zolago_Newsletter_Model_Subscriber
         Mage::log("sendInvitationEmail function _isInvitationEmailEnabled: " . $this->_isInvitationEmailEnabled(), null, "salesmanago.log");
         Mage::log("sendInvitationEmail function validateEmail: " . $this->validateEmail($email), null, "salesmanago.log");
         Mage::log("sendInvitationEmail function _isEmailSuitableForInvitation: " . $this->_isEmailSuitableForInvitation($email), null, "salesmanago.log");
-		if ($this->_isInvitationEmailEnabled()
+        Mage::log("sendInvitationEmail function if send email: " . (int)($this->_isInvitationEmailEnabled() && $this->validateEmail($email) && $this->_isEmailSuitableForInvitation($email)), null, "salesmanago.log");
+
+        if ($this->_isInvitationEmailEnabled()
 			&& $this->validateEmail($email)
 			&& $this->_isEmailSuitableForInvitation($email)) {
             Mage::log("sendInvitationEmail function sendEmailTemplate1", null, "salesmanago.log");
