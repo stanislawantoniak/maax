20559d5781dbd87544978731c286d31983f67dd5
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-05-20 15:21:52 +0200

    refs#1135 grid for GH rewrite

--- app/code/local/GH/Rewrite/Block/Adminhtml/Ghrewrite/Csv/Form.php
+++ app/code/local/GH/Rewrite/Block/Adminhtml/Ghrewrite/Csv/Form.php
@@ -2,6 +2,16 @@
 
 class GH_Rewrite_Block_Adminhtml_Ghrewrite_Csv_Form extends Mage_Adminhtml_Block_Widget_Form
 {
+    public function __construct()
+    {
+        /** @var GH_Rewrite_Helper_Data $rewriteHelper */
+        $rewriteHelper = Mage::helper('ghrewrite');
+
+        parent::__construct();
+        $this->setId('ghrewrite_form');
+        $this->setTitle($rewriteHelper->__('CSV Loader'));
+    }
+
     protected function _prepareForm() {
 
         /** @var GH_Rewrite_Helper_Data $rewriteHelper */
@@ -9,16 +19,16 @@ class GH_Rewrite_Block_Adminhtml_Ghrewrite_Csv_Form extends Mage_Adminhtml_Block
 
         $form = new Varien_Data_Form(array(
                 'id'      => 'edit_form',
-                'action'  => $this->getUrl('*/*/savecsv', array('id' => $this->getRequest()->getParam('id'))),
+                'action'  => $this->getUrl('*/*/csv/import'),
                 'method'  => 'post',
                 'enctype' => 'multipart/form-data'
             )
         );
-
+        $form->setUseContainer(true);
         $this->setForm($form);
         $fieldset = $form->addFieldset('loadcsv_form', array('legend' => $rewriteHelper->__('CSV Loader')));
 
-        $fieldset->addField('filename', 'image', array(
+        $fieldset->addField('file', 'file', array(
             'label' => $rewriteHelper->__('CSV File'),
             'required' => false,
             'name' => 'filename',
