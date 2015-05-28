ac928ee2a7d3a09cd571e019828dee670c75b72b
victory <victoria.sultanovska@convertica.pl>
2015-05-28 12:56:16 +0200

    Refs#1155 save single attribute salesmanago_contact_id without saving the entity

--- app/code/local/Zolago/SalesManago/Model/Observer.php
+++ app/code/local/Zolago/SalesManago/Model/Observer.php
@@ -18,7 +18,7 @@ class Zolago_SalesManago_Model_Observer extends SalesManago_Tracking_Model_Obser
 
             if(isset($r['contactId']) && !empty($r['contactId'])){
                 try{
-                    //Single attribute salesmanago_contact_id without saving the entity
+                    //Save single attribute salesmanago_contact_id without saving the entity
                     //to avoid call Zolago_Newsletter_Model_Observer::subscribeCustomer twice
                     //$observer->getCustomer()->setData('salesmanago_contact_id', $r['contactId'])->save();
                     $observer->getCustomer()
