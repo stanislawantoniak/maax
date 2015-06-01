8de174c312ba40f0ee1fe06288791eb0a1f5bf9c
victory <victoria.sultanovska@convertica.pl>
2015-06-01 14:08:20 +0200

    1154

--- app/code/local/Zolago/Catalog/Block/Product/View.php
+++ app/code/local/Zolago/Catalog/Block/Product/View.php
@@ -92,6 +92,7 @@ class Zolago_Catalog_Block_Product_View extends Mage_Catalog_Block_Product_View
 //        $collection->addAttributeToFilter("basic_category", 1);
         $collection->addPathFilter("/$rootId/");
         $collection->setOrder("basic_category", "DESC");
+        $collection->setOrder("level", "DESC");
         $collection->setOrder("position", "ASC");
         //$collection->addAttributeToSelect("*");
         Mage::log($collection->getSelect()->__toString(), null, "dcat1.log");
