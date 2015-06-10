55df4f31eaddd3521ff0e5d92abc0b47f0f5b99d
Adam Wilk <adam.wilk@convertica.pl>
2015-06-10 10:52:55 +0200

    re#1164 re#1183 logging

--- app/code/local/Zolago/Payment/Model/Refund.php
+++ app/code/local/Zolago/Payment/Model/Refund.php
@@ -33,16 +33,16 @@ class Zolago_Payment_Model_Refund extends Zolago_Payment_Model_Allocation
             ->reset(Zend_Db_Select::COLUMNS)
             ->columns(
                 array(
-                    'max_allocation_amount' => new Zend_Db_Expr('SUM(allocation_amount)'),
-                    'customer_id',
-                    'vendor_id',
+                    'max_allocation_amount' => new Zend_Db_Expr('SUM(main_table.allocation_amount)'),
+                    'main_table.customer_id',
+                    'main_table.vendor_id',
                     'created_at_hours_past' => new Zend_Db_Expr('(UNIX_TIMESTAMP()-UNIX_TIMESTAMP(MAX(main_table.created_at))) /3600'),
-                    'allocation_type',
-                    'po_id',
+                    'main_table.allocation_type',
+                    'main_table.po_id',
                     'po.order_id',
-                    'transaction_id',
+                    'main_table.transaction_id',
                     'payment_transaction.txn_id',
-	                'rma_id' => 'rma_allocations.rma_id'
+	                'rma_allocations.rma_id'
                 )
             )
             ->group(array('transaction_id'))
