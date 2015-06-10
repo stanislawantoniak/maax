3f154decd82e9df26a3cab256cf1b4b357634737
Adam Wilk <adam.wilk@convertica.pl>
2015-06-10 10:57:01 +0200

    re#1164 re#1183 logging

--- app/code/local/Zolago/Payment/Model/Refund.php
+++ app/code/local/Zolago/Payment/Model/Refund.php
@@ -28,7 +28,7 @@ class Zolago_Payment_Model_Refund extends Zolago_Payment_Model_Allocation
 	        ->join(
 		        array(
 			        'rma_allocations' => 'zolago_payment_allocation'),
-		        'main_table.transaction_id = rma_allocations.transaction_id && NOT_NULL(rma_allocations.rma_id)'
+		        '(main_table.transaction_id = rma_allocations.transaction_id && NOT_NULL rma_allocations.rma_id)'
 	        )
             ->reset(Zend_Db_Select::COLUMNS)
             ->columns(
