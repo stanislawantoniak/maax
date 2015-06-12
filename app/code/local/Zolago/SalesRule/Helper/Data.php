8bc5c05f0fcdbacb7e236bb296405961519d1318
Adam Wilk <adam.wilk@convertica.pl>
2015-06-12 14:14:03 +0200

    re#1141 re#1188 coupons view done

--- app/code/local/Zolago/SalesRule/Helper/Data.php
+++ app/code/local/Zolago/SalesRule/Helper/Data.php
@@ -148,6 +148,18 @@ class Zolago_SalesRule_Helper_Data extends Mage_SalesRule_Helper_Data {
          $path = Mage::getBaseDir('media') . DS . Zolago_SalesRule_Model_Promotion_Image::PROMOTION_IMAGE_PATH;
          return $path;
      }
+
+	/**
+	 * returns path to catalog with resized promotions images
+	 *
+	 * @return string
+	 */
+	public function getPromotionResizedImagePath() {
+		$path = $this->getPromotionImagePath() . DS . 'resized';
+		return $path;
+	}
+
+
     /**
      * returns url to catalog with promotions images
      * 
@@ -157,6 +169,57 @@ class Zolago_SalesRule_Helper_Data extends Mage_SalesRule_Helper_Data {
          $path = Mage::getBaseUrl('media') . DS . Zolago_SalesRule_Model_Promotion_Image::PROMOTION_IMAGE_PATH;
          return $path;
      }
+
+
+	/**
+	 * returns url to catalog with resized promotions images
+	 *
+	 * @return string
+	 */
+	public function getPromotionResizedImageUrl() {
+		$path = $this->getPromotionImageUrl() . DS . "resized";
+		return $path;
+	}
+
+	public function getResizedPromotionImage($fileName,$width=480) {
+		$folderURL = $this->getPromotionImageUrl();
+		$imageURL = $folderURL . $fileName;
+
+		$basePath = $this->getPromotionImagePath() . DS . $fileName;
+		$newPath = $this->getPromotionResizedImagePath() . DS . $fileName;
+		//if width empty then return original size image's URL
+		if ($width != '') {
+			//if image has already resized then just return URL
+			if (file_exists($basePath) && is_file($basePath) && !file_exists($newPath)) {
+				$imageObj = new Varien_Image($basePath);
+				$imageObj->constrainOnly(true);
+				$imageObj->keepAspectRatio(true);
+				$imageObj->keepFrame(false);
+				$imageObj->resize($width, null);
+				$imageObj->save($newPath);
+			}
+			$resizedURL = $this->getPromotionResizedImageUrl() . DS . $fileName;
+		} else {
+			$resizedURL = $imageURL;
+		}
+		return $resizedURL;
+	}
+
+
+	/**
+	 * @param Mage_Catalog_Model_Product $model
+	 * @return array|null
+	 */
+	public function getResizedPromotionImageInfo($fileName) {
+		$path = $this->getPromotionResizedImagePath() . DS . $fileName;
+		// Extract cached image URI
+		if(file_exists($path)){
+			if($info=@getimagesize($path)){
+				return array("width"=>$info[0], "height"=>$info[1], "ratio"=>100 * round(($info[1]/$info[0]),2));
+			}
+		}
+		return null;
+	}
      
     /**
      * send mail to customer with coupons_id
