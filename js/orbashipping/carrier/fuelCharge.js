aff0f2cb3782b13e24304df0f1d8b972e2be657f
victory <victoria.sultanovska@convertica.pl>
2015-06-17 14:42:40 +0200

    Refs#1206

--- js/orbashipping/carrier/fuelCharge.js
+++ js/orbashipping/carrier/fuelCharge.js
@@ -2,9 +2,12 @@
 
 jQuery(document).ready(function ($) {
 
-    $("#addFuelChargeItem").click(function(){
-        var maxIndex = getMaxIndex();
-        $("#"+fuelChargeTableId).find("tbody").append(getFuelRow(maxIndex+1));
+    $(".addFuelChargeItem").click(function(){
+       var table = jQuery(this).closest("table");
+
+        var fieldName = table.parents("tr[id^=row_carriers]").find("input[type=hidden]").attr("id");
+        var maxIndex = getMaxIndex(table);
+        table.find("tbody").append(getFuelRow(fieldName,maxIndex+1));
         initDatepicker();
     })
 
@@ -15,20 +18,20 @@ jQuery(document).ready(function ($) {
 function initDatepicker(){
     jQuery("[class^=datepicker]").datepicker({
         dateFormat: 'dd-mm-yy',
-        onSelect: function(dateText) {
+        onSelect: function() {
             var borderOk = "1px solid #ccc";
             var borderError = "1px solid red";
 
             var tr = jQuery(this).closest("tr");
 
-            var datepicker_from = tr.find(".datepicker_from");
-            var datepicker_to = tr.find(".datepicker_to");
+            var datepicker_from = tr.find("input[name*=fuel_percent_date_from]");
+
+            var datepicker_to = tr.find("input[name*=fuel_percent_date_to]");
+            var f = datepicker_from.val();
+            var t = datepicker_to.val();
 
             datepicker_from.css({"border": borderOk});
             datepicker_to.css({"border": borderOk});
-
-            var f = datepicker_from.val();
-            var t = datepicker_to.val();
 
             var begD = jQuery.datepicker.parseDate('dd-mm-yy', f);
             var endD = jQuery.datepicker.parseDate('dd-mm-yy', t);
@@ -45,16 +48,16 @@ function initDatepicker(){
     });
 }
 
-function getMaxIndex(){
+function getMaxIndex(table){
     var indexes = [];
-    var tr = jQuery("#"+fuelChargeTableId).find("tbody tr");
+    var tr = table.find("tbody tr");
     tr.each(function(i,val){
         indexes.push(parseInt(jQuery(val).data("index")));
     })
     return (indexes.length > 0) ? Math.max.apply(Math,indexes) : 0;
 }
 
-function getFuelRow(N){
+function getFuelRow(fieldName, N){
     var onclickAttribute = 'jQuery(this).closest("tr").remove()';
     return "<tr data-index='"+N+"'>" +
     "<td><input type='text' name='"+fieldName+"["+N+"][fuel_percent]' /></td>" +
