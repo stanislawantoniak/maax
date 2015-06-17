ee163051df5a9c89ad8265f4256e18b32df75b39
Adam Wilk <adam.wilk@convertica.pl>
2015-06-17 12:20:04 +0200

    re#1201 re#1202 changed labels and package sizes and package weights

--- app/code/local/Orba/Shipping/Model/System/Source/PkgSizes.php
+++ app/code/local/Orba/Shipping/Model/System/Source/PkgSizes.php
@@ -2,14 +2,14 @@
 class Orba_Shipping_Model_System_Source_PkgSizes {
 	/* VENDOR'S DHL PARCEL SIZES */
 	/* sizes in cm splitted during packages making */
-	const DHL_PARCEL_SIZE_SMALL = '10x20x40';
-	const DHL_PARCEL_SIZE_MEDIUM = '20x40x60';
-	const DHL_PARCEL_SIZE_BIG = '40x50x60';
+	const DHL_PARCEL_SIZE_SMALL = '30x40x45';
+	const DHL_PARCEL_SIZE_MEDIUM = '40x50x60';
+	const DHL_PARCEL_SIZE_BIG = '50x60x75';
 
 	protected $_hashes = array(
-		self::DHL_PARCEL_SIZE_SMALL => "Small parcel",
-		self::DHL_PARCEL_SIZE_MEDIUM => "Medium parcel",
-		self::DHL_PARCEL_SIZE_BIG => "Big parcel"
+		self::DHL_PARCEL_SIZE_SMALL => "Small parcel (biggest dimension is less than 45cm)",
+		self::DHL_PARCEL_SIZE_MEDIUM => "Medium parcel (biggest dimension is less than 60cm)",
+		self::DHL_PARCEL_SIZE_BIG => "Big parcel (biggest dimension is less than 75cm)"
 	);
 
 	public function toOptionHash() {
