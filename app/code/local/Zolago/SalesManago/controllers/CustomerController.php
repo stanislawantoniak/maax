dfc6f09793d64e5cb6985a549b1436690aeadeba
victory <victoria.sultanovska@convertica.pl>
2015-06-23 14:22:04 +0200

    Salesmanago smi d to mail

--- app/code/local/Zolago/SalesManago/controllers/CustomerController.php
+++ app/code/local/Zolago/SalesManago/controllers/CustomerController.php
@@ -11,7 +11,7 @@ class Zolago_SalesManago_CustomerController extends Mage_Core_Controller_Front_A
         header("Pragma: no-cache");
         header('Content-Type: text/html');
 
-        $email = $this->getRequest()->getParam('email');
+        $email = urlencode($this->getRequest()->getParam('email'));
         echo $email;
         $block = $this->getLayout()
             ->createBlock('zolagosalesmanago/customer_cart')
