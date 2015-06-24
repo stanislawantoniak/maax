1b36674038ccaa504c62f42a041dfae5bd7e4c50
victory <victoria.sultanovska@convertica.pl>
2015-06-24 14:01:56 +0200

    Refs#1178

--- app/code/local/GH/Dhl/Helper/Data.php
+++ app/code/local/GH/Dhl/Helper/Data.php
@@ -1,5 +1,12 @@
 <?php
 
-class GH_Dhl_Helper_Data extends Mage_Core_Helper_Abstract {
-
+class GH_Dhl_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function getGalleryDHLAccountData($account)
+    {
+        $dhlAccounts = Mage::getModel("ghdhl/dhl")->getCollection();
+        $dhlAccounts->addFieldToFilter("dhl_account", $account);
+        $galleryDHLAccount = $dhlAccounts->getFirstItem();
+        return $galleryDHLAccount;
+    }
 }
