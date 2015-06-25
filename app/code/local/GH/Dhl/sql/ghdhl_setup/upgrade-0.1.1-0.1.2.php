14f15262bcea21b51f0f7df07f3b6afcf526f1a4
victory <victoria.sultanovska@convertica.pl>
2015-06-25 16:19:42 +0200

    Refs#1178 refs#1238 add foreighkeys

--- app/code/local/GH/Dhl/sql/ghdhl_setup/upgrade-0.1.1-0.1.2.php
+++ app/code/local/GH/Dhl/sql/ghdhl_setup/upgrade-0.1.1-0.1.2.php
@@ -30,14 +30,6 @@ $table = $installer->getConnection()
         $installer->getIdxName('ghdhl/dhl', array('dhl_id')),
         array('dhl_id')
     )
-//    ->addForeignKey(
-//        $installer->getFkName('ghdhl/dhl_vendor', 'vendor_id', 'udropship/vendor', 'vendor_id'),
-//        'vendor_id', $installer->getTable('udropship_vendor'), 'vendor_id',
-//        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
-//    ->addForeignKey(
-//        $installer->getFkName('ghdhl/dhl_vendor', 'dhl_id', 'ghdhl/dhl', 'id'),
-//        'dhl_id', $installer->getTable('ghdhl/dhl'), 'id',
-//        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
 ;
 
 $installer->getConnection()->createTable($table);
