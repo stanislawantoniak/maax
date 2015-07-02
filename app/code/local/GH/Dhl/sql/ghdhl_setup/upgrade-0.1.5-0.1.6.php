cda9f90e637625592780536e4501d8f479102d09
victory <victoria.sultanovska@convertica.pl>
2015-07-02 14:10:52 +0200

    Refs#1254
    0 (vendor)

---
+++ app/code/local/GH/Dhl/sql/ghdhl_setup/upgrade-0.1.5-0.1.6.php
@@ -0,0 +1,19 @@
+<?php
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+$installer->getConnection()
+    ->changeColumn(
+        $installer->getTable('sales_flat_shipment_track'),
+        'gallery_shipping_source', 'gallery_shipping_source',
+        array(
+            'type' => Varien_Db_Ddl_Table::TYPE_SMALLINT,
+            "length" => 1,
+            "default" => 0
+        ));
+
+
+$this->endSetup();
