f13bf1baa7b5b12a8481534b2580070d71a7246c
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2015-07-13 17:20:02 +0200

    poprawka do timestampa

--- app/code/local/Orba/Shipping/Helper/Carrier/Dhl.php
+++ app/code/local/Orba/Shipping/Helper/Carrier/Dhl.php
@@ -277,7 +277,7 @@ class Orba_Shipping_Helper_Carrier_Dhl extends Orba_Shipping_Helper_Carrier {
         $login = $this->getDhlLogin();
         $password = $this->getDhlPassword();
         $dhlClient->setAuth($login, $password);
-        $ret = $dhlClient->getPostalCodeServices($zip, date("Y-m-d", Mage::getModel('core/date')->timestamp(time())));
+        $ret = $dhlClient->getPostalCodeServices($zip, date("Y-m-d", $timestamp));
         if (is_object($ret) && property_exists($ret, 'getPostalCodeServicesResult')) {
             $empty = new StdClass;
             $empty->domesticExpress9 = false;
