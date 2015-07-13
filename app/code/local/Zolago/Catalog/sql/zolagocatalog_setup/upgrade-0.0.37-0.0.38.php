b680d9df3066052eb54e6578d1b637c4b1f130da
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-07-13 13:09:17 +0200

    refs#1224 fix for attribute description_status

--- app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.0.37-0.0.38.php
+++ app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.0.37-0.0.38.php
@@ -4,6 +4,7 @@ $installer = new Mage_Eav_Model_Entity_Setup('core_setup');;
 
 $installer->startSetup();
 
-$installer->updateAttribute('catalog_product', 'description_status', 'required', false);
+$installer->updateAttribute('catalog_product', 'description_status', 'required', false); // not working
+// fix for this in upgrade-0.0.38-0.0.39.php with note
 
 $installer->endSetup();
