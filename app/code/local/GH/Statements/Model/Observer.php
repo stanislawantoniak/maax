4aad457d16b75e5e89672e7e2e78b2de80191bb4
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-07-21 17:14:44 +0200

    refs948 cron for statements

--- app/code/local/GH/Statements/Model/Observer.php
+++ app/code/local/GH/Statements/Model/Observer.php
@@ -3,6 +3,9 @@
 class GH_Statements_Model_Observer
 {
 
+    /**
+     * todo description here
+     */
     public static function processStatements() {
 
         /* @var $transaction Varien_Db_Adapter_Interface */
@@ -11,14 +14,42 @@ class GH_Statements_Model_Observer
         try {
             $transaction->beginTransaction();
 
-            self::initStatement();
+            /* Format our dates */
+            /** @var Mage_Core_Model_Date $dateModel */
+            $dateModel = Mage::getModel('core/date');
+            $today     = $dateModel->date('Y-m-d H:i:s');
+            $yesterday = date('Y-m-d', strtotime('yesterday',strtotime($today)));
 
-            self::processStatementsOrders();
-            self::processStatementsRma();
-            self::processStatementsRefunds();
-            self::processStatementsTracks();
+            // Collection of active vendors who have statement calendar
+            /* @var $collection Unirgy_Dropship_Model_Mysql4_Vendor_Collection */
+            $VendorsSollection = Mage::getResourceModel('udropship/vendor_collection');
+            $VendorsSollection->addStatusFilter(Unirgy_Dropship_Model_Source::VENDOR_STATUS_ACTIVE);
+            $VendorsSollection->addFieldToFilter('statements_calendar', array('neq' => null));
 
-            self::populateStatement();
+            foreach($VendorsSollection as $vendor) {
+                /** @var Zolago_Dropship_Model_Vendor $vendor */
+                $calendarId = (int)$vendor->getStatementsCalendar();
+
+                /* @var GH_Statements_Model_Resource_Calendar_Item_Collection $itemCollection */
+                $itemCollection = Mage::getResourceModel('ghstatements/calendar_item_collection');
+                $itemCollection->addFieldToFilter('calendar_id', $calendarId);
+                $itemCollection->addFieldToFilter('event_date', array('eq' => $yesterday));
+
+                if ($itemCollection->getFirstItem()->getId()) {
+                    /** @var GH_Statements_Model_Calendar_Item $calendarItem */
+                    $calendarItem = $itemCollection->getFirstItem();
+
+
+                    self::initStatement($vendor, $calendarItem);
+
+                    self::processStatementsOrders();
+                    self::processStatementsRma();
+                    self::processStatementsRefunds();
+                    self::processStatementsTracks();
+
+                    self::populateStatement();
+                }
+            }
 
             $transaction->commit();
         } catch (Exception $ex) {
@@ -29,9 +60,23 @@ class GH_Statements_Model_Observer
 
     /**
      * This create row for statement
+     * @param Zolago_Dropship_Model_Vendor $vendor
+     * @param GH_Statements_Model_Calendar_Item $calendarItem
      */
-    private function initStatement() {
+    private function initStatement($vendor, $calendarItem) {
 
+        /** @var GH_Statements_Model_Calendar $calendar */
+        $calendar = Mage::getModel('ghstatements/calendar')->load($calendarItem->getItemId());
+
+        /** @var GH_Statements_Model_Statement $statement */
+        $statement = Mage::getModel('ghstatements/statement');
+        $statement->setData(array(
+            "vendor_id"         => (int)$vendor->getId(),
+            "calendar_id"       => (int)$calendarItem->getCalendarId(),
+            "calendar_item_id"  => (int)$calendarItem->getItemId(),
+            "name"              => $vendor->getVendorName() . ' ' . $calendar->getName() . '( ' . $calendarItem->getEventDate() . ' )'
+        ));
+        $statement->save();
     }
 
     /**
