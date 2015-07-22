e6c03d32d5d27ba6c90687f9e614a7613a84a6aa
victory <victoria.sultanovska@convertica.pl>
2015-07-22 17:19:15 +0200

    Refs#1280 Refs#948
    set statement_id NULL
    in tables
    `udropship_po`
    `sales_flat_shipment_track`
    `urma_rma_track`

--- app/code/local/GH/Statements/Model/Resource/Statement.php
+++ app/code/local/GH/Statements/Model/Resource/Statement.php
@@ -8,4 +8,36 @@ class GH_Statements_Model_Resource_Statement extends Mage_Core_Model_Resource_Db
     protected function _construct() {
         $this->_init('ghstatements/statement','id');
     }
+
+    /**
+     * Perform actions after object delete
+     *
+     * @param Varien_Object $object
+     * @return Mage_Core_Model_Resource_Db_Abstract
+     */
+    protected function _afterDelete(Mage_Core_Model_Abstract $object)
+    {
+        $statementId = $object->getId();
+
+
+        $write = $this->_getWriteAdapter();
+
+        $write->update(
+            $write->getTableName('udropship_po'),
+            array("statement_id" => NULL),
+            "statement_id=" . $statementId
+        );
+        $write->update(
+            $write->getTableName("sales_flat_shipment_track"),
+            array("statement_id" => NULL),
+            "statement_id=" . $statementId
+        );
+        $write->update(
+            $write->getTableName("urma_rma_track"),
+            array("statement_id" => NULL),
+            "statement_id=" . $statementId
+        );
+
+        return $this;
+    }
 }
