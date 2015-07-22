c3cefe8a4258c35e31caa4e0ab3504c12e59a33e
victory <victoria.sultanovska@convertica.pl>
2015-07-22 15:49:51 +0200

    Refs#1278 Refs#948
    Track

--- app/code/local/GH/Statements/Block/Adminhtml/Vendor/Statements/Edit/Tab/Track.php
+++ app/code/local/GH/Statements/Block/Adminhtml/Vendor/Statements/Edit/Tab/Track.php
@@ -36,6 +36,35 @@ class GH_Statements_Block_Adminhtml_Vendor_Statements_Edit_Tab_Track
             'width' => '60',
             'index' => 'po_increment_id'
         ));
+        $this->addColumn('rma_increment_id', array(
+            'header' => Mage::helper('ghstatements')->__('RMA number'),
+            'sortable' => true,
+            'width' => '60',
+            'index' => 'rma_increment_id'
+        ));
+        $this->addColumn('shipped_date', array(
+            'header' => Mage::helper('ghstatements')->__('Shipping Date'),
+            'sortable' => true,
+            'width' => '60',
+            'index' => 'shipped_date',
+            'type' => 'date',
+        ));
+
+
+        $this->addColumn('charge_subtotal', array(
+            'header' => Mage::helper('ghstatements')->__('Total netto shipment charge'),
+            'index' => 'charge_subtotal',
+            'type'  => 'price',
+            'currency' => 'base_currency_code',
+            'currency_code' => Mage::getStoreConfig('currency/options/base'),
+        ));
+        $this->addColumn('charge_total', array(
+            'header' => Mage::helper('ghstatements')->__('Total brutto shipment charge'),
+            'index' => 'charge_total',
+            'type'  => 'price',
+            'currency' => 'base_currency_code',
+            'currency_code' => Mage::getStoreConfig('currency/options/base'),
+        ));
 
     }
 
