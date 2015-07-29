768f633de5cf98521df9b41e914e25e14267aaaf
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-07-29 12:11:01 +0200

    refs#1246 gh statements translate

--- app/code/local/GH/Statements/Block/Adminhtml/Vendor/Statements/Edit/Tab/Order.php
+++ app/code/local/GH/Statements/Block/Adminhtml/Vendor/Statements/Edit/Tab/Order.php
@@ -140,7 +140,7 @@ class GH_Statements_Block_Adminhtml_Vendor_Statements_Edit_Tab_Order
         ));
         $this->addColumn("value", array(
             "index" => "value",
-            "header" => Mage::helper("ghstatements")->__("Amount"),
+            "header" => Mage::helper("ghstatements")->__("Order amount"),
             'type' => 'price',
             'currency' => 'base_currency_code',
             'currency_code' => Mage::getStoreConfig('currency/options/base')
