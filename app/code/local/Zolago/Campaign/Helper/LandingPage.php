b961305ec8eb6726f6c681fc0690ccb1b7b5383d
victory <victoria.sultanovska@convertica.pl>
2015-08-04 22:38:47 +0200

    websites

--- app/code/local/Zolago/Campaign/Helper/LandingPage.php
+++ app/code/local/Zolago/Campaign/Helper/LandingPage.php
@@ -24,6 +24,7 @@ class Zolago_Campaign_Helper_LandingPage extends Mage_Core_Helper_Abstract
             $campaign = Mage::getModel("zolagocampaign/campaign")
                 ->load($landingPageCampaign,"name_customer");
 
+            $campaignWebsites = $campaign->getAllowedWebsites();
             $campaignId = $campaign->getId();
             if ($campaignId !== NULL) {
 
@@ -31,15 +32,13 @@ class Zolago_Campaign_Helper_LandingPage extends Mage_Core_Helper_Abstract
                     ($campaign->getStatus() == Zolago_Campaign_Model_Campaign_Status::TYPE_ACTIVE)
                     && $campaign->getIsLandingPage()
                     && $campaign->getLandingPageCategory()
+                    && in_array(Mage::app()->getWebsite()->getId(), $campaignWebsites)
                 ) {
                     //context landing_page_context
                     $landing_page_context = $campaign->getData("landing_page_context");
                     $landing_page_category_id = $campaign->getData("landing_page_category");
 
                     $landingPageUrl = $campaign->getData("landing_page_url");
-
-
-
 
                     if($vendor && ($campaign->getContextVendorId() == $vendor->getVendorId()) && $landing_page_context == Zolago_Campaign_Model_Attribute_Source_Campaign_LandingPageContext::LANDING_PAGE_CONTEXT_VENDOR){
                         //if vendor context
