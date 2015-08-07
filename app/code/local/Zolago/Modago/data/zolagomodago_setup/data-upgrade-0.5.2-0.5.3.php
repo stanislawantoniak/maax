2332d4f9e386f9fa60767eacef68a4ca478efecc
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-08-07 12:09:17 +0200

    refs#1313 checkout success thank you page

--- app/code/local/Zolago/Modago/data/zolagomodago_setup/data-upgrade-0.5.2-0.5.3.php
+++ app/code/local/Zolago/Modago/data/zolagomodago_setup/data-upgrade-0.5.2-0.5.3.php
@@ -1,27 +1,30 @@
 <?php
 
-// installation of bottom header cms blocks
-$cmsNavigationBlocks = array(
-
-	array(
-		'title'         => 'Twoje promocje nagłówek formularza logowania',
-		'identifier'    => 'promotions-login-header',
-		'content'       => <<<EOD
-<h2>Chcesz skorzystać z super promocji?</h2>
-<p class="fz_11">Załóż konto i zapisz się do newslettera żeby kupować taniej!</p>
+$cms = array(
+    array(
+        'title'         => 'Checkout thank you page',
+        'identifier'    => 'checkout-thank-you-page',
+        'content'       =>
+            <<<EOD
+<header>
+    <h2>Dziękujemy za złożenie zamówienia!</h2>
+</header>
+<p>Twoje zamówienie zostało przyjęte. Jeśli zamówienie opłacałaś/eś online zostanie ono zrealizowane po potwierdzeniu otrzymania płatności.<br />
+    Stan zamówienia możesz monitorować logując się do swojego konta w serwisie. Szczegóły zamówienia zostały właśnie wysłane mailowo.</p>
 EOD
-	,
-		'is_active'     => 1,
-		'stores'        => 0
-	)
+    ,
+        'is_active'     => 1,
+        'stores'        => 0
+    )
 );
 
-foreach ($cmsNavigationBlocks as $data) {
-	$block = Mage::getModel('cms/block')->load($data['identifier']);
-	if ($block->getBlockId()) {
-		$oldData = $block->getData();
-		$data = array_merge($oldData,$data);
-	}
-	$block->setData($data)->save();
+foreach ($cms as $data) {
+    $block = Mage::getModel('cms/block')->load($data['identifier']);
+    if ($block->getBlockId()) {
+        $oldData = $block->getData();
+        $data = array_merge($oldData,$data);
+    }
+
+    $block->setData($data)->save();
 }
 
