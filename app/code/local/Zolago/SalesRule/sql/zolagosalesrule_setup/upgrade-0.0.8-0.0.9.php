ea3aa23801da377ad16c7680a76e7622214238bd
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-08-25 15:25:34 +0200

    refs#1291 salesrule cart instalator; added memory table for processing products in campaigns from coupons rules

--- app/code/local/Zolago/SalesRule/sql/zolagosalesrule_setup/upgrade-0.0.8-0.0.9.php
+++ app/code/local/Zolago/SalesRule/sql/zolagosalesrule_setup/upgrade-0.0.8-0.0.9.php
@@ -15,4 +15,22 @@ $installer->getConnection()->addColumn(
 	)
 );
 
+
+// Memory table for processing products in campaigns from coupons rules
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('zolagosalesrule/product_in_campaign_tmp'))
+    ->addColumn('product_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'nullable'  => false,
+    ), 'Product ID')
+    ->addColumn('campaign_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'nullable'  => false,
+    ), 'Title')
+    ->addIndex($installer->getIdxName($installer->getTable('zolagosalesrule/product_in_campaign_tmp'),
+        array("product_id")),array("product_id"))
+    ->addIndex($installer->getIdxName($installer->getTable('zolagosalesrule/product_in_campaign_tmp'),
+        array("campaign_id")),array("campaign_id"))
+    ->setOption("type", Varien_Db_Adapter_Pdo_Mysql::ENGINE_MEMORY);
+
+$installer->getConnection()->createTable($table);
+
 $installer->endSetup();
