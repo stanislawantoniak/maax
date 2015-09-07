b7b1740b2ddc50b2d74abd67197295e709ea730d
victory <victoria.sultanovska@convertica.pl>
2015-09-07 12:57:11 +0200

    Refs#1354 refs#1364  breadcrumbs

--- app/code/local/Zolago/Modago/Block/Mypromotions/Header.php
+++ app/code/local/Zolago/Modago/Block/Mypromotions/Header.php
@@ -6,27 +6,5 @@
 class Zolago_Modago_Block_Mypromotions_Header extends Zolago_Modago_Block_Mypromotions
 {
 
-    protected function _prepareLayout()
-    {
-        $breadcrumbs = $this->getLayout()->getBlock('breadcrumbs');
-        $breadcrumbs->addCrumb(
-            'home',
-            array(
-                'label' => $this->__('Home Page'),
-                'title' => $this->__('Home Page'),
-                'link' => Mage::getBaseUrl()
-            )
-        );
-        // add second item without link
-        $breadcrumbs->addCrumb(
-            'mypromotions',
-            array(
-                'label' => $this->__('Your coupons'),
-                'title' => $this->__('Your coupons')
-            )
-        );
-
-        parent::_prepareLayout();
-    }
 
 } 
