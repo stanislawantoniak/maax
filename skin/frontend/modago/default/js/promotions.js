c5545967e751361fa9c641733ffaf970b1ad2d16
Adam Wilk <adam.wilk@convertica.pl>
2015-09-07 17:47:59 +0200

    re#1354 re#1374 added copy to clipboard (experimental)

--- skin/frontend/modago/default/js/promotions.js
+++ skin/frontend/modago/default/js/promotions.js
@@ -1,10 +1,14 @@
 Mall.promotions = function (couponId) {
+	Mall.promotions.jsCopySupported = document.queryCommandSupported('copy');
+
     Mall.promotions.populatePromotionContent(couponId);
 
     jQuery("#myPromotionsModal").on("hide.bs.modal", function () {
         Mall.promotions.clearPromotionContent();
     });
 };
+
+Mall.promotions.jsCopySupported = false;
 
 /**
  * Populate promotion popup content
@@ -32,14 +36,25 @@ Mall.promotions.populatePromotionContent = function (couponId) {
             modal.find(".promo-banner-wrapper").html(bannerImg);
         }
 
-
+	    var code = promoItem.find(".promo_popup_data").data("code");
 
         modal.find(".promo-description").html(promoItem.find(".promo_popup_data").data("description"));
         modal.find(".promo-expiration span").html(promoItem.find(".promo_popup_data").data("term"));
-        modal.find(".promo-code span").html(promoItem.find(".promo_popup_data").data("code"));
+        modal.find(".promo-code span").html(code);
 
         modal.find(".promo-link a").attr("href",promoItem.find(".promo_popup_data").data("url"));
 
+	    var copy = modal.find(".promo-code-copy"),
+		    copySuccess = modal.find('.promo-code-copy-success'),
+		    copyError = modal.find('.promo-code-copy-error');
+
+	    if(!Mall.promotions.jsCopySupported) {
+		    copy.hide();
+	    } else {
+		    copy.click(function() {
+			    Mall.promotions.copyTextToClipboard(code,copyError,copySuccess);
+		    });
+	    }
 
         if(promoItem.find(".promo_popup_data").data("pdf").length > 0){
             modal.find(".promo-pdf a").attr("href",promoItem.find(".promo_popup_data").data("pdf"));
@@ -48,7 +63,7 @@ Mall.promotions.populatePromotionContent = function (couponId) {
         }
 
     }
-}
+};
 
 
 /**
@@ -67,8 +82,47 @@ Mall.promotions.clearPromotionContent = function () {
 
     modal.find(".promo-link a").attr("href", "");
     modal.find(".promo-pdf a").attr("href", "").show();
-}
+};
 
 Mall.promotions.openModal = function () {
     jQuery("#myPromotionsModal").modal();
-}
+};
+
+Mall.promotions.copyTextToClipboard = function(text,error,success) {
+	if(Mall.promotions.jsCopySupported) {
+		error = typeof error != 'undefined' && error.length ? error : false;
+		success = typeof success != 'undefined' && success.length ? success : false;
+
+		var textArea = document.createElement("textarea");
+
+		textArea.style.position = 'fixed';
+		textArea.style.top = 0;
+		textArea.style.left = 0;
+		textArea.style.width = '2em';
+		textArea.style.height = '2em';
+		textArea.style.padding = 0;
+		textArea.style.border = 'none';
+		textArea.style.outline = 'none';
+		textArea.style.boxShadow = 'none';
+		textArea.style.background = 'transparent';
+
+		textArea.value = text;
+
+		document.body.appendChild(textArea);
+
+		textArea.select();
+
+		try {
+			var successful = document.execCommand('copy');
+			if(successful && success) {
+				error.hide();
+				success.show();
+			} else if(!successful && error) {
+				success.hide();
+				error.show();
+			}
+		} catch (err) {}
+
+		document.body.removeChild(textArea);
+	}
+};
