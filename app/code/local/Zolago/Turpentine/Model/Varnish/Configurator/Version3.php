3be6065b9a06440b48b49b6e8d539eb1423b532e
Adam Wilk <adam.wilk@convertica.pl>
2015-09-14 10:08:52 +0200

    re#1380 removed varnish changes that made listing cache work

--- app/code/local/Zolago/Turpentine/Model/Varnish/Configurator/Version3.php
+++ app/code/local/Zolago/Turpentine/Model/Varnish/Configurator/Version3.php
@@ -20,33 +20,7 @@ class Zolago_Turpentine_Model_Varnish_Configurator_Version3
 		// Override esi private ttl - is real magento cookie time - always!
         $vars['esi_private_ttl'] = 
 				Mage::helper( 'turpentine/esi' )->getSystemCookieLifeTime();
-		$vars['url_category_nopragma'] = $this->_getCategoryUrlExcludes();
         return $vars;
-	}
-
-	protected function _getCategoryUrlExcludes() {
-		$categories = Mage::getModel('catalog/category')
-			->getCollection()
-			->addAttributeToSelect('*');
-
-		$out = array();
-		$baseUrl = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_DIRECT_LINK,true);
-		foreach($categories as $cat) {
-			/** @var $cat Zolago_Catalog_Model_Category */
-			$out[] = str_replace($baseUrl,'',$cat->getUrl());
-		}
-
-		/** @var Mage_Core_Model_Url_Rewrite $rewriteModel */
-		$rewriteModel = Mage::getModel('core/url_rewrite');
-		$rewriteCollection = $rewriteModel->getCollection()->addFieldToFilter('target_path',array('like'=>'catalog/category/view/id/%'));
-
-		foreach($rewriteCollection as $rewrite) {
-			$out[] = $rewrite->getRequestPath();
-		}
-
-		$out = array_unique($out);
-
-		return implode('|',$out);
 	}
 
 }
