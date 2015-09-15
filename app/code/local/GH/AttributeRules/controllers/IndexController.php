77273da0e88940bfc4ed749d0f69014f77eaf073
victory <victoria.sultanovska@convertica.pl>
2015-09-15 15:43:30 +0200

    save controller

--- app/code/local/GH/AttributeRules/controllers/IndexController.php
+++ app/code/local/GH/AttributeRules/controllers/IndexController.php
@@ -5,8 +5,57 @@
  */
 class GH_AttributeRules_IndexController extends Mage_Core_Controller_Front_Action
 {
+    /**
+     * Save Attribute rule action
+     */
     public function saveAction()
     {
+        if (!$this->getRequest()->isPost()) {
+            return false;
+        }
+
+        $_helper = Mage::helper("gh_attributerules");
+        $id = $this->getRequest()->getPost('attribute_rule_id');
+
+        /* @var $attributeRule GH_AttributeRules_Model_Rule */
+        $attributeRule = $this->_initModel($id);
+
+        $data = $this->getRequest()->getParams();
+
+        // If Edit
+        if (!empty($modelId) && !$attributeRule->getId()) {
+            throw new Mage_Core_Exception($_helper->__("Attribute Rule not found"));
+        }
+
+        // Fix empty value
+        if ($attributeRule->getId() == "") {
+            $attributeRule->setId(null);
+        }
+
+        $attributeRule->addData($data);
+        $attributeRule->save();
 
     }
+
+
+    /**
+     * @param $modelId
+     * @return GH_AttributeRules_Model_Rule
+     */
+    protected function _initModel($modelId)
+    {
+        if (Mage::registry('current_attribute_rule') instanceof GH_AttributeRules_Model_Rule) {
+            return Mage::registry('current_attribute_rule');
+        }
+
+        $model = Mage::getModel("gh_attributerules/attribute_rules");
+        /* @var $model GH_AttributeRules_Model_Rule */
+        if ($modelId) {
+            $model->load($modelId);
+        }
+
+        Mage::register('current_attribute_rule', $model);
+        return $model;
+    }
+
 }
