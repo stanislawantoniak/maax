5277bd4bf0f814e492740e190e905cbb3e8c6506
victory <victoria.sultanovska@convertica.pl>
2015-09-15 15:15:54 +0200

    model moved to a good place))
    controller

---
+++ app/code/local/GH/AttributeRules/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class GH_AttributeRules_IndexController
+ */
+class GH_AttributeRules_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function saveAction()
+    {
+
+    }
+}
