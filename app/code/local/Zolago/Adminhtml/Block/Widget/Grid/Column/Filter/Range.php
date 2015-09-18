f0df1e2d4df213a315f59d37a96f63ba44222fde
victory <victoria.sultanovska@convertica.pl>
2015-09-18 11:40:32 +0200

    grid

---
+++ app/code/local/Zolago/Adminhtml/Block/Widget/Grid/Column/Filter/Range.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ */
+class Zolago_Adminhtml_Block_Widget_Grid_Column_Filter_Range extends Mage_Adminhtml_Block_Widget_Grid_Column_Filter_Abstract
+{
+    public function getHtml()
+    {
+        $html = '<div class="range"><div class="range-line"><span class="label">' . Mage::helper('adminhtml')->__('From').':</span> <input type="text" name="'.$this->_getHtmlName().'[from]" id="'.$this->_getHtmlId().'_from" value="'.$this->getEscapedValue('from').'" class="input-text no-changes form-control"/></div>';
+        $html .= '<div class="range-line"><span class="label">' . Mage::helper('adminhtml')->__('To').' : </span><input type="text" name="'.$this->_getHtmlName().'[to]" id="'.$this->_getHtmlId().'_to" value="'.$this->getEscapedValue('to').'" class="input-text no-changes form-control"/></div></div>';
+        return $html;
+    }
+
+    public function getValue($index=null)
+    {
+        if ($index) {
+            return $this->getData('value', $index);
+        }
+        $value = $this->getData('value');
+        if ((isset($value['from']) && strlen($value['from']) > 0) || (isset($value['to']) && strlen($value['to']) > 0)) {
+            return $value;
+        }
+        return null;
+    }
+    
+
+    public function getCondition()
+    {
+        $value = $this->getValue();
+        return $value;
+    }
+
+}
