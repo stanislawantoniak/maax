db6b7c8db11ccd248bcecb8a2a342cd7800097dc
Adam Wilk <adam.wilk@convertica.pl>
2015-09-23 11:43:34 +0200

    re#1419 cms block for logged in nocoupons page

--- app/code/local/Zolago/Modago/Block/Mypromotions.php
+++ app/code/local/Zolago/Modago/Block/Mypromotions.php
@@ -11,6 +11,7 @@ class Zolago_Modago_Block_Mypromotions extends Mage_Core_Block_Template
 	protected $_cms_block;
 	protected $_logged;
 	protected $_persistent;
+	protected $_list;
 
     protected function _prepareLayout() {
          $this->_logged = Mage::getSingleton('customer/session')->isLoggedIn();
@@ -19,7 +20,11 @@ class Zolago_Modago_Block_Mypromotions extends Mage_Core_Block_Template
              $customer = Mage::getModel('customer/customer')->load($this->_customer_id);
              $email = $customer->getEmail();
              $this->_subscribed = Mage::getModel('newsletter/subscriber')->loadByEmail($email)->isSubscribed();
-             $this->_cms_block = 'mypromotions_logged';
+	         if(count($this->getPromotionList())) {
+		         $this->_cms_block = 'mypromotions_logged';
+	         } else {
+		         $this->_cms_block = 'mypromotions_logged_nocoupons';
+	         }
          } else {
              $helper = Mage::helper('persistent/session');         
              $this->_persistent = $helper->isPersistent();         
@@ -40,126 +45,129 @@ class Zolago_Modago_Block_Mypromotions extends Mage_Core_Block_Template
      */
     public function getPromotionList()
     {
-        /* Coupons collection */
-        $collection = Mage::getModel('salesrule/coupon')->getCollection();
-        $collection->addFieldToFilter('customer_id', $this->_customer_id);
-        $collection->getSelect()
-            ->join(array('salesrule' => 'salesrule'), 'salesrule.rule_id = main_table.rule_id', array('salesrule.use_auto_generation'))
-            ->where('expiration_date > ?', date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time())))
-            ->where('(main_table.times_used < main_table.usage_limit) OR (main_table.usage_limit = 0)')
-            ->where('salesrule.use_auto_generation = 1');
-        $out = array();
-        $rules = array();
-        foreach ($collection as $item) {
-            $rules[$item['rule_id']] = $item['rule_id'];
-        }
-        if (empty($rules)) {
-            return array();
-        }
+	    if(!$this->_list) {
+		    /* Coupons collection */
+		    $collection = Mage::getModel('salesrule/coupon')->getCollection();
+		    $collection->addFieldToFilter('customer_id', $this->_customer_id);
+		    $collection->getSelect()
+			    ->join(array('salesrule' => 'salesrule'), 'salesrule.rule_id = main_table.rule_id', array('salesrule.use_auto_generation'))
+			    ->where('expiration_date > ?', date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time())))
+			    ->where('(main_table.times_used < main_table.usage_limit) OR (main_table.usage_limit = 0)')
+			    ->where('salesrule.use_auto_generation = 1');
+		    $out = array();
+		    $rules = array();
+		    foreach ($collection as $item) {
+			    $rules[$item['rule_id']] = $item['rule_id'];
+		    }
+		    if (empty($rules)) {
+			    return array();
+		    }
 
-        //TODO clear unused fields from sales_rule
-        $rulesCollection = Mage::getModel('salesrule/rule')->getCollection();
-        $rulesCollection->addFieldToFilter('rule_id', array('in', $rules));
-        $rulesCollection->addFieldToFilter('is_active', array('eq', 1));
+		    //TODO clear unused fields from sales_rule
+		    $rulesCollection = Mage::getModel('salesrule/rule')->getCollection();
+		    $rulesCollection->addFieldToFilter('rule_id', array('in', $rules));
+		    $rulesCollection->addFieldToFilter('is_active', array('eq', 1));
 
-        $rulesCollection->getSelect()
-            ->join(
-                array("campaign" => "zolago_campaign"),
-                "main_table.campaign_id = campaign.campaign_id",
-                array(
-                    "campaign_id" => "campaign.campaign_id",
-                    //"is_landing_page" => "campaign.is_landing_page",
-                    "coupon_image" => "campaign.coupon_image",
-                    "landing_page_context" => "campaign.landing_page_context",
-                    "context_vendor_id" => "campaign.context_vendor_id",
-                    "coupon_pdf" => "campaign.coupon_conditions",
-                    "campaign_url" => "campaign.campaign_url"
-                )
-            );
+		    $rulesCollection->getSelect()
+			    ->join(
+				    array("campaign" => "zolago_campaign"),
+				    "main_table.campaign_id = campaign.campaign_id",
+				    array(
+					    "campaign_id" => "campaign.campaign_id",
+					    //"is_landing_page" => "campaign.is_landing_page",
+					    "coupon_image" => "campaign.coupon_image",
+					    "landing_page_context" => "campaign.landing_page_context",
+					    "context_vendor_id" => "campaign.context_vendor_id",
+					    "coupon_pdf" => "campaign.coupon_conditions",
+					    "campaign_url" => "campaign.campaign_url"
+				    )
+			    );
 
-        //jeśli kupon nie jest powiązany z kampanią (bo ktoś źle zdefiniował)
-        // lub brakuje obrazka - nie pokazujemy kuponu
-        $localtime = date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time()));
-        $rulesCollection->addFieldToFilter('status', array("eq" => Zolago_Campaign_Model_Campaign_Status::TYPE_ACTIVE));
-        $rulesCollection->addFieldToFilter('is_landing_page', array("eq" => 1));
-        $rulesCollection->addFieldToFilter('campaign.coupon_image', array('notnull' => true));
-        $rulesCollection->addFieldToFilter('campaign.coupon_image', array('neq' => ''));
+		    //jeśli kupon nie jest powiązany z kampanią (bo ktoś źle zdefiniował)
+		    // lub brakuje obrazka - nie pokazujemy kuponu
+		    $localtime = date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time()));
+		    $rulesCollection->addFieldToFilter('status', array("eq" => Zolago_Campaign_Model_Campaign_Status::TYPE_ACTIVE));
+		    $rulesCollection->addFieldToFilter('is_landing_page', array("eq" => 1));
+		    $rulesCollection->addFieldToFilter('campaign.coupon_image', array('notnull' => true));
+		    $rulesCollection->addFieldToFilter('campaign.coupon_image', array('neq' => ''));
 
-        $rulesCollection->addFieldToFilter('date_from', array('lteq' => $localtime));
-        $rulesCollection->addFieldToFilter('date_to', array('gteq' => $localtime));
+		    $rulesCollection->addFieldToFilter('date_from', array('lteq' => $localtime));
+		    $rulesCollection->addFieldToFilter('date_to', array('gteq' => $localtime));
 
-        //Mage::log($rulesCollection->getSelect()->__toString());
+		    //Mage::log($rulesCollection->getSelect()->__toString());
 
-        $rules = array(); // clear
+		    $rules = array(); // clear
 
-        $vendorIds = array();
-
-
-        $campaignIds = array();
-        foreach ($rulesCollection as $rule) {
-            $rules[$rule['rule_id']] = $rule;
-            $vendorIds[] = $rule->getContextVendorId();
-            $campaignIds[] = $rule->getCampaignId();
-        }
-
-        $vendorIds = array_unique($vendorIds);
-        $campaignIds = array_unique($campaignIds);
+		    $vendorIds = array();
 
 
-        $vendorLogos = array();
-        if (!empty($vendorIds)) {
-            foreach ($vendorIds as $vendorId) {
-                $campaignVendor = Mage::getModel("udropship/vendor")->load($vendorId);
-                if ($campaignVendor) {
-                    $vendorLogos[$vendorId] = $campaignVendor->getLogo();
-                }
-                unset($campaignVendor);
-            }
-        }
+		    $campaignIds = array();
+		    foreach ($rulesCollection as $rule) {
+			    $rules[$rule['rule_id']] = $rule;
+			    $vendorIds[] = $rule->getContextVendorId();
+			    $campaignIds[] = $rule->getCampaignId();
+		    }
 
-        //Get campaign landing page banners
-        if(!empty($campaignIds)){
-            $landingPageBanners = Mage::helper("zolagocampaign/landingPage")->getLandingPageBanners($campaignIds);
-        }
+		    $vendorIds = array_unique($vendorIds);
+		    $campaignIds = array_unique($campaignIds);
 
 
-        $campaignDataForRule = array();
-        foreach ($rulesCollection as $ruleData) {
+		    $vendorLogos = array();
+		    if (!empty($vendorIds)) {
+			    foreach ($vendorIds as $vendorId) {
+				    $campaignVendor = Mage::getModel("udropship/vendor")->load($vendorId);
+				    if ($campaignVendor) {
+					    $vendorLogos[$vendorId] = $campaignVendor->getLogo();
+				    }
+				    unset($campaignVendor);
+			    }
+		    }
 
-            $campaignDataForRule[$ruleData->getRuleId()]["image"] = $ruleData->getCouponImage();
-            if(isset($landingPageBanners[$ruleData->getCampaignId()])){
-                $ruleData->setLandingPageBanner($landingPageBanners[$ruleData->getCampaignId()]);
-            }
-            if ($ruleData->getLandingPageContext() == Zolago_Campaign_Model_Attribute_Source_Campaign_LandingPageContext::LANDING_PAGE_CONTEXT_VENDOR) {
-                $campaignDataForRule[$ruleData->getRuleId()]["logo_vendor"] = isset($vendorLogos[$ruleData->getContextVendorId()]) ? Mage::getBaseUrl("media") . $vendorLogos[$ruleData->getContextVendorId()] : "";
-            }
-            if ($ruleData->getCouponPdf())
-                $ruleData->setCouponPdf(Mage::getBaseUrl("media") . Zolago_Campaign_Model_Campaign::LP_COUPON_PDF_FOLDER . DS . $ruleData->getCouponPdf());
+		    //Get campaign landing page banners
+		    if (!empty($campaignIds)) {
+			    $landingPageBanners = Mage::helper("zolagocampaign/landingPage")->getLandingPageBanners($campaignIds);
+		    }
 
 
-            $ruleData->setExpirationDate(date('d.m.Y', Mage::getModel('core/date')->timestamp(
-                strtotime($ruleData->getToDate())
-            )));
-            $ruleData->setCampaignUrl(Mage::helper("zolagocampaign/landingPage")->getLandingPageUrl($ruleData->getCampaignId()), FALSE);
-        }
+		    $campaignDataForRule = array();
+		    foreach ($rulesCollection as $ruleData) {
 
-        foreach ($collection as $item) {
-            if (isset($rules[$item['rule_id']])) {
-                $ruleItem = $rules[$item['rule_id']];
-                $ruleItem->setCouponImage($campaignDataForRule[$item['rule_id']]["image"]);
+			    $campaignDataForRule[$ruleData->getRuleId()]["image"] = $ruleData->getCouponImage();
+			    if (isset($landingPageBanners[$ruleData->getCampaignId()])) {
+				    $ruleData->setLandingPageBanner($landingPageBanners[$ruleData->getCampaignId()]);
+			    }
+			    if ($ruleData->getLandingPageContext() == Zolago_Campaign_Model_Attribute_Source_Campaign_LandingPageContext::LANDING_PAGE_CONTEXT_VENDOR) {
+				    $campaignDataForRule[$ruleData->getRuleId()]["logo_vendor"] = isset($vendorLogos[$ruleData->getContextVendorId()]) ? Mage::getBaseUrl("media") . $vendorLogos[$ruleData->getContextVendorId()] : "";
+			    }
+			    if ($ruleData->getCouponPdf())
+				    $ruleData->setCouponPdf(Mage::getBaseUrl("media") . Zolago_Campaign_Model_Campaign::LP_COUPON_PDF_FOLDER . DS . $ruleData->getCouponPdf());
 
-                if (isset($campaignDataForRule[$item['rule_id']]["logo_vendor"])) {
-                    $ruleItem->setLogoVendor($campaignDataForRule[$item['rule_id']]["logo_vendor"]);
-                }
 
-                $item['ruleItem'] = $ruleItem;
-                $out[] = $item;
+			    $ruleData->setExpirationDate(date('d.m.Y', Mage::getModel('core/date')->timestamp(
+				    strtotime($ruleData->getToDate())
+			    )));
+			    $ruleData->setCampaignUrl(Mage::helper("zolagocampaign/landingPage")->getLandingPageUrl($ruleData->getCampaignId()), FALSE);
+		    }
 
-                unset($ruleItem);
-            }
-        }
+		    foreach ($collection as $item) {
+			    if (isset($rules[$item['rule_id']])) {
+				    $ruleItem = $rules[$item['rule_id']];
+				    $ruleItem->setCouponImage($campaignDataForRule[$item['rule_id']]["image"]);
 
-        return $out;
+				    if (isset($campaignDataForRule[$item['rule_id']]["logo_vendor"])) {
+					    $ruleItem->setLogoVendor($campaignDataForRule[$item['rule_id']]["logo_vendor"]);
+				    }
+
+				    $item['ruleItem'] = $ruleItem;
+				    $out[] = $item;
+
+				    unset($ruleItem);
+			    }
+		    }
+		    $this->_list = $out;
+	    }
+
+        return $this->_list;
     }	
     /**
      * cms block 
--- app/code/local/Zolago/SalesRule/data/zolagosalesrule_setup/data-upgrade-0.0.11-0.0.12.php
+++ app/code/local/Zolago/SalesRule/data/zolagosalesrule_setup/data-upgrade-0.0.11-0.0.12.php
@@ -3,23 +3,6 @@
 // installation privacy-settings-remember-me-description cms blocks
 $cmsNavigationBlocks = array(
 	array(
-		'title'         => 'Account promotions not logged in',
-		'identifier'    => 'mypromotions_not_logged',
-		'content'       => <<<EOD
-	<div class="mypromotions-register-header">
-		<div class="mypromotions-register-header-title">Twoje ulubione marki do <span class="mypromotions-register-bold">50%&nbsp;taniej!</span></div>
-		<div class="mypromotions-register-header-subtitle">Załóż konto i&nbsp;odkryj specjalne rabaty.</div>
-		<div class="mypromotions-register-header-text">
-			Wyraź zgodę na&nbsp;mailing i&nbsp;odbierz swoje pierwsze kupony.<br />
-			Im&nbsp;więcej przeglądasz, im&nbsp;więcej kupujesz ty&nbsp;lepiej dobierzemy dla&nbsp;Ciebie kupony rabatowe.
-		</div>
-	</div>
-EOD
-	,
-		'is_active'     => 1,
-		'stores'        => 0
-	),
-	array(
 		'title'         => 'Account promotions logged in',
 		'identifier'    => 'mypromotions_logged',
 		'content'       => <<<EOD
@@ -48,7 +31,7 @@ EOD
 		'stores'        => 0
 	),
 	array(
-		'title'         => 'Account promotions persitent',
+		'title'         => 'Account promotions persistent',
 		'identifier'    => 'mypromotions_persistance',
 		'content'       => <<<EOD
 <div class="mypromotions-cms-container">
@@ -87,53 +70,30 @@ EOD
 		'stores'        => 0
 	),
 	array(
-		'title'         => 'Account promotions go to login',
-		'identifier'    => 'mypromotions_gotologin',
+		'title'         => 'Account promotions logged in no coupons',
+		'identifier'    => 'mypromotions_logged_nocoupons',
 		'content'       => <<<EOD
-<header class="title-section">
-	<h2>Masz już konto?</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-</header>
-<div class="row">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-		<div class="row action">
-			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-				Jeżeli masz już konto w naszym sklepie i chcesz zobaczyć kupony musisz się zalogować
-			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-				<a href="{{store url='customer/account'}}" class="button button-primary large pull-right">
-					Logowanie
-				</a>
-			</div>
+<div class="mypromotions-cms-container">
+	<section class="main bg-w mypromotions-cms col-xs-12">
+		<div class="mypromotions-cms-pretitle">
+			W tej chwili nie masz żadnych rabatów
 		</div>
-	</div>
-</div>
-EOD
-	,
-		'is_active'     => 1,
-		'stores'        => 0
-	),
-	array(
-		'title'         => 'Account promotions go to register',
-		'identifier'    => 'mypromotions_gotoregister',
-		'content'       => <<<EOD
-<header class="title-section">
-	<h2>Nie masz konta?</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-</header>
-<div class="row">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-		<div class="row action">
-			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-				Jeżeli nie masz konta w naszym sklepie musisz się zalogować i zapisać do newslettera żeby otrzymać kody rabatowe.
-			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-				<a href="{{store url='customer/account/create'}}" class="button button-primary large pull-right">
-					Rejestracja
-				</a>
-			</div>
+		<div class="mypromotions-cms-title">
+			Jak zdobyć dodatkowe rabaty?
 		</div>
-	</div>
+		<div class="mypromotions-cms-subtitle">
+			Nagradzamy Twoją aktywność.
+		</div>
+		<ul class="mypromotions-cms-list">
+			<li>Przeglądaj produkty</li>
+			<li>Dodawaj je do ulubionych</li>
+			<li>Dziel się nimi ze znajomymi na Facebooku</li>
+			<li>Kupuj produkty ulubionych sklepów</li>
+		</ul>
+		<div class="mypromotions-cms-text">
+			Im więcej przeglądasz, im więcej kupujesz tym więcej otrzymasz kodów rabatowych i tym lepiej dobierzemy je do Ciebie.
+		</div>
+	</section>
 </div>
 EOD
 	,
@@ -144,47 +104,14 @@ EOD
 		'title'         => 'Account promotions registration header',
 		'identifier'    => 'mypromotions_registration_header',
 		'content'       => <<<EOD
-<h2>Utwórz konto</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-EOD
-	,
-		'is_active'     => 1,
-		'stores'        => 0
-	),
-	array(
-		'title'         => 'Account promotions no coupons',
-		'identifier'    => 'mypromotions_nocoupons',
-		'content'       => <<<EOD
-<p>W tej chwili nie masz żadnych kodów rabatowych</p>
-EOD
-	,
-		'is_active'     => 1,
-		'stores'        => 0
-	),
-	array(
-		'title'         => 'Account promotions not subscribed, coupons available',
-		'identifier'    => 'mypromotions_notsubscribed',
-		'content'       => <<<EOD
-<header class="title-section">
-	<h2>Zapisz się do newslettera!</h2>
-	<p>Po zapisie natychmiast dostaniesz nowe kody rabatowe!</p>
-</header>
-<div class="row">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus feugiat dui, in bibendum est pellentesque a. Quisque placerat felis elit, quis interdum est ullamcorper nec. Donec diam lectus, viverra in mauris in, pretium convallis nisl. Proin eget eros vitae velit maximus rhoncus. Aliquam erat volutpat. Nulla sed sem a nisi pretium mollis. Pellentesque venenatis eros nisl, in imperdiet dolor volutpat in.</p>
+<div class="mypromotions-register-header">
+	<div class="mypromotions-register-header-title">Twoje ulubione marki do <span class="mypromotions-register-bold">50%&nbsp;taniej!</span></div>
+	<div class="mypromotions-register-header-subtitle">Załóż konto i&nbsp;odkryj specjalne rabaty.</div>
+	<div class="mypromotions-register-header-text">
+		Wyraź zgodę na&nbsp;mailing i&nbsp;odbierz swoje pierwsze kupony.<br />
+		Im&nbsp;więcej przeglądasz, im&nbsp;więcej kupujesz ty&nbsp;lepiej dobierzemy dla&nbsp;Ciebie kupony rabatowe.
 	</div>
 </div>
-EOD
-	,
-		'is_active'     => 1,
-		'stores'        => 0
-	),
-	array(
-		'title'         => 'My promotions list header',
-		'identifier'    => 'mypromotions_list_header',
-		'content'       => <<<EOD
-<h2>Twoje kody rabatowe</h2>
-<p>Super promocje, kupuj z mega zniżkami!</p>
 EOD
 	,
 		'is_active'     => 1,
