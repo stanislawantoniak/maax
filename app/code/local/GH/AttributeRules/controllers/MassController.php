dcfb83fa75b6b0422f0f43d772df24dd6ee54c5e
victory <victoria.sultanovska@convertica.pl>
2015-09-28 13:06:04 +0200

    test reindex attributes

--- app/code/local/GH/AttributeRules/controllers/MassController.php
+++ app/code/local/GH/AttributeRules/controllers/MassController.php
@@ -207,7 +207,7 @@ class GH_AttributeRules_MassController extends Zolago_Dropship_Controller_Vendor
                     if (!empty($ids)) {
                         $idsForReindex = array_merge($idsForReindex, $ids);
                         //$attrDataForReindex = array_merge($attrDataForReindex, array($code => $value));
-                        //$productAction->updateAttributesNoIndex($ids, array($code => $value), $storeId);
+                        $productAction->updateAttributesNoIndex($ids, array($code => $value), $storeId);
                         //$productAction->updateAttributesPure($ids, array($code => $value), $storeId);
                     }
                 }
@@ -224,9 +224,9 @@ class GH_AttributeRules_MassController extends Zolago_Dropship_Controller_Vendor
 //                );
 //                $productAction->reindexAfterMassAttributeChange();
 
-                $indexer = new Mage_Catalog_Model_Resource_Product_Indexer_Eav_Source();
-                /* @var $indexer Mage_Catalog_Model_Resource_Product_Indexer_Eav_Source */
-                $indexer->reindexEntities($idsForReindex);
+//                $indexer = new Mage_Catalog_Model_Resource_Product_Indexer_Eav_Source();
+//                /* @var $indexer Mage_Catalog_Model_Resource_Product_Indexer_Eav_Source */
+//                $indexer->reindexEntities($idsForReindex);
 
                 // Push to solr and ban varnish
                 Mage::dispatchEvent(
