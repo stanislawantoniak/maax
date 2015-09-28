2217eaac019c087f854250e62d7b8a4b966ace67
victory <victoria.sultanovska@convertica.pl>
2015-09-28 12:49:19 +0200

    test reindex attributes

--- app/code/local/GH/AttributeRules/controllers/MassController.php
+++ app/code/local/GH/AttributeRules/controllers/MassController.php
@@ -224,7 +224,7 @@ class GH_AttributeRules_MassController extends Zolago_Dropship_Controller_Vendor
 //                );
 //                $productAction->reindexAfterMassAttributeChange();
 
-                $indexer = Mage::getResourceModel('catalog/product_indexer_eav_source');
+                $indexer = new Mage_Catalog_Model_Resource_Product_Indexer_Eav_Source();
                 /* @var $indexer Mage_Catalog_Model_Resource_Product_Indexer_Eav_Source */
                 $indexer->reindexEntities($idsForReindex);
 
