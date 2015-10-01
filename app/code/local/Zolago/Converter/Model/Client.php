dcf2c8ca1f5381a6658b01073bdf5601b0231b80
victory <victoria.sultanovska@convertica.pl>
2015-10-01 10:11:28 +0200

    test POST

--- app/code/local/Zolago/Converter/Model/Client.php
+++ app/code/local/Zolago/Converter/Model/Client.php
@@ -242,6 +242,7 @@ class Zolago_Converter_Model_Client {
             curl_setopt($process, CURLOPT_RETURNTRANSFER, true);
             $return = curl_exec($process);
             Mage::log("curl_exec", null, "_makeConnection.log");
+            Mage::log("POST", null, "_makeConnection.log");
             Mage::log($return, null, "_makeConnection.log");
             Mage::log("---------------------------", null, "_makeConnection.log");
             curl_close($process);
