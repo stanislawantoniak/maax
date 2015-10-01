834ba905409fe2eeb99a374004183377540dc81e
victory <victoria.sultanovska@convertica.pl>
2015-10-01 11:16:54 +0200

    timeout test

--- app/code/local/Zolago/Converter/Model/Client.php
+++ app/code/local/Zolago/Converter/Model/Client.php
@@ -233,11 +233,13 @@ class Zolago_Converter_Model_Client {
         try {
             $process = curl_init($url);
             Mage::log($url, null, "_makeConnection.log");
-            //curl_setopt($process, CURLOPT_HTTPHEADER, array('Accept: application/json'));
-            curl_setopt($process, CURLOPT_HTTPHEADER, 0);
+            curl_setopt($process, CURLOPT_HTTPHEADER, array('Accept: application/json'));
             curl_setopt($process, CURLOPT_USERPWD, $this->getConfig('login') . ":" . $this->getConfig('password'));
             curl_setopt($process, CURLOPT_TIMEOUT, 30);
-            curl_setopt($process, CURLOPT_HTTPGET, 1);
+            curl_setopt($process, CURLOPT_HTTPGET, true);
+
+            curl_setopt($process, CURLOPT_FOLLOWLOCATION, true);
+
             curl_setopt($process, CURLOPT_RETURNTRANSFER, true);
             $return = curl_exec($process);
             Mage::log($process, null, "_makeConnection.log");
