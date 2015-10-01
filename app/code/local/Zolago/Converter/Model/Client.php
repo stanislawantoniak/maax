a2a28028b9d16954ab380e2a1e761202e9300541
victory <victoria.sultanovska@convertica.pl>
2015-10-01 11:45:45 +0200

    timeout test

--- app/code/local/Zolago/Converter/Model/Client.php
+++ app/code/local/Zolago/Converter/Model/Client.php
@@ -254,7 +254,7 @@ class Zolago_Converter_Model_Client {
             Mage::logException($e);
         }
 
-        Mage::log("RETURN", null, "_makeConnection.log");
+        Mage::log("RETURN " . gettype($return), null, "_makeConnection.log");
 
         return Zend_Json::decode($return);
     }
