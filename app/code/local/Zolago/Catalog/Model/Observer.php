ac885bf981c1db67b7e590fa9eec967b45cb87a7
victory <victoria.sultanovska@convertica.pl>
2015-10-02 09:26:21 +0200

    limit

--- app/code/local/Zolago/Catalog/Model/Observer.php
+++ app/code/local/Zolago/Catalog/Model/Observer.php
@@ -54,7 +54,7 @@ class Zolago_Catalog_Model_Observer
 
     static public function processConfigurableQueue()
     {
-        Mage::getModel('zolagocatalog/queue_configurable')->process(4000);
+        Mage::getModel('zolagocatalog/queue_configurable')->process(5000);
     }
 
 
