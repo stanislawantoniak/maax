8c5dffbb8776ccacd6e98672636eed1ad908efb8
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-10-09 10:33:32 +0200

    refs#1449 refs#1343 gh regulation grids

--- app/code/local/GH/Regulation/Model/Regulation/Document.php
+++ app/code/local/GH/Regulation/Model/Regulation/Document.php
@@ -2,6 +2,10 @@
 
 /**
  * Class GH_Regulation_Model_Regulation_Document
+ * @method string getId()
+ * @method string getRegulationTypeId()
+ * @method string getDocumentLink()
+ * @method string getDate()
  */
 class GH_Regulation_Model_Regulation_Document extends Mage_Core_Model_Abstract
 {
@@ -11,4 +15,7 @@ class GH_Regulation_Model_Regulation_Document extends Mage_Core_Model_Abstract
         $this->_init('ghregulation/regulation_document');
     }
 
+    public function getFileName() {
+        return $this->getDocumentLink();// TODO
+    }
 }
