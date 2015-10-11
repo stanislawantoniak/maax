811dcf3cf1e83b15d77552a3aefb854989a02f5f
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-10-11 20:10:49 +0200

    bcc copy

--- app/code/local/Zolago/DropshipMicrosite/Helper/Datarewrite.php
+++ app/code/local/Zolago/DropshipMicrosite/Helper/Datarewrite.php
@@ -40,6 +40,7 @@ class Zolago_DropshipMicrosite_Helper_Datarewrite extends Unirgy_DropshipMicrosi
 
         /** @var Zolago_Common_Helper_Data $mailer */
         $mailer = Mage::helper('zolagocommon');
+        $confirmation_email_send_copy_to = Mage::getStoreConfig('udropship/microsite/confirmation_email_send_copy_to');
         $mailer->sendEmailTemplate(
             $vendor->getEmail(),
             $vendor->getVendorName(),
@@ -50,7 +51,9 @@ class Zolago_DropshipMicrosite_Helper_Datarewrite extends Unirgy_DropshipMicrosi
                 'use_attachments' => true
             ),
             $store->getId(),
-            $store->getConfig('udropship/vendor/vendor_email_identity')
+            $store->getConfig('udropship/vendor/vendor_email_identity'),
+            null,
+            $confirmation_email_send_copy_to
         );
         Mage::helper('udropship')->setDesignStore();
 
