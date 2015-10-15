603239faab4089cc2893f24d73ef04387c2333ec
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2015-10-15 16:39:53 +0200

    zamiana forward na redirect refs #1448

--- app/code/local/Zolago/Dropship/controllers/IndexController.php
+++ app/code/local/Zolago/Dropship/controllers/IndexController.php
@@ -8,6 +8,6 @@ class Zolago_Dropship_IndexController extends Unirgy_Dropship_IndexController {
 
     public function indexAction()
     {
-        $this->_forward('index', 'vendor', "udropship");
+        $this->_redirect('*/vendor');
     }
 }
