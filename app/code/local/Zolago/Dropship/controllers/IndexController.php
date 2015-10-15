cfd25b2e220e8fac577118a042d2c768c4226249
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-10-15 14:28:13 +0200

    refs#1448 fix; magento way

--- app/code/local/Zolago/Dropship/controllers/IndexController.php
+++ app/code/local/Zolago/Dropship/controllers/IndexController.php
@@ -2,7 +2,7 @@
 /**
  * Zolago_Dropship_IndexController
  */
-require_once 'Unirgy/Dropship/controllers/IndexController.php';
+require_once Mage::getModuleDir('controllers', "Unirgy_Dropship") . DS . "IndexController.php";
 
 class Zolago_Dropship_IndexController extends Unirgy_Dropship_IndexController {
 
