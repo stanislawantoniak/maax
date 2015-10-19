4e29d6a11c4b6c1c71ed7f8b3d4ac94a16dfd1b5
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2015-10-19 14:34:01 +0200

    year w template refs #1343

--- app/code/local/Zolago/Common/Helper/Data.php
+++ app/code/local/Zolago/Common/Helper/Data.php
@@ -36,7 +36,8 @@ class Zolago_Common_Helper_Data extends Mage_Core_Helper_Abstract {
 			if(is_null($sender)){
 				$sender = Mage::getStoreConfig(self::XML_PATH_DEFAULT_IDENTITY, $storeId);
 			}
-
+            $templateParams['year'] = Mage::getModel('core/date')->date('Y');
+			
             /* @var $mailer Zolago_Common_Model_Core_Email_Template_Mailer */
 			$mailer = Mage::getModel('zolagocommon/core_email_template_mailer');
             /** @var Mage_Core_Model_Email_Info $emailInfo */
