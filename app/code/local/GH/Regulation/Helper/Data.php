4d4023f8be772085bef894696f229fe6271b13d1
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2015-10-26 16:50:24 +0100

    kalkulacja historii dokumentów refs #1450

--- app/code/local/GH/Regulation/Helper/Data.php
+++ app/code/local/GH/Regulation/Helper/Data.php
@@ -157,163 +157,79 @@ class GH_Regulation_Helper_Data extends Mage_Core_Helper_Abstract
         $acceptDate = strtotime($vendor->getRegulationAcceptDocumentDate());
         $acceptDate = strtotime(date('Y-m-d',$acceptDate));
 
-/***********************/
-
-
-        /** @var GH_Regulation_Model_Regulation_Vendor_Kind $vendorKindModel */
-//        $vendorKindModel = Mage::getModel('ghregulation/regulation_vendor_kind');
-        /** @var GH_Regulation_Model_Resource_Regulation_Vendor_Kind_Collection $vendorKindCollection */
-//        $vendorKindCollection = $vendorKindModel->getCollection();
-        /* 
-        $vendorKindCollection
-            ->addFieldToFilter('vendor_id',$vendor->getId())
-            ->getSelect()
-                ->join(
-                    'gh_regulation_kind',
-                    'main_table.regulation_kind_id = gh_regulation_kind.regulation_kind_id',
-                    'gh_regulation_kind.name as regulation_kind_name');
-        if($vendorKindCollection->getSize()) {
-            $vendorKinds = array(); //gathers kinds assigned to vendor
-            $kinds = array(); //gathers kinds names
-            foreach($vendorKindCollection as $vendorKind) {
-                $vendorKinds[] = $vendorKind->getRegulationKindId();
-                $kinds[$vendorKind->getRegulationKindId()] = $vendorKind->getRegulationKindName();
-            }
-        } else {
-            return array(); //vendor has no selected document kinds
-        }
-
-        */ // do not use vendor_kind
+        $typeTable = Mage::getSingleton('core/resource')->getTableName('ghregulation/regulation_type');
+        $kindTable = Mage::getSingleton('core/resource')->getTableName('ghregulation/regulation_kind');
+        $documentTable = Mage::getSingleton('core/resource')->getTableName('ghregulation/regulation_document');
         
+        // prepare array with assigned types
+        $collection = Mage::getResourceModel('ghregulation/regulation_document_vendor_collection');
+        $collection->getSelect()
+            ->join (
+                array('type' => $typeTable),
+                'main_table.regulation_type_id = type.regulation_type_id',
+                array()
+            )
+            -> join (
+                array('kind' => $kindTable),
+                'type.regulation_kind_id = kind.regulation_kind_id',
+                array('kindname' => 'kind.name','kindid'=>'kind.regulation_kind_id')
+            )
+            ->order('type.regulation_kind_id')
+            ->order('main_table.date','DESC')
+            ->where('main_table.vendor_id=?',$vendor->getId())
+            ->where('main_table.date<=?',Mage::getModel('core/date')->date('Y-m-d'));
         
-/***************************/
-        $kindCollection = Mage::getResourceModel('ghregulation/regulation_kind_collection');
-        $vendorKinds = array();
-        $kinds = array();
-        foreach ($kindCollection as $kind) {
-            $vendorKinds[] = $kind->getRegulationKindId();
-            $kinds[$kind->getRegulationKindId()] = $kind->getName();
-        }
-
-        /** @var GH_Regulation_Model_Regulation_Type $typeModel */
-        $typeModel = Mage::getModel('ghregulation/regulation_type');
-        /** @var GH_Regulation_Model_Resource_Regulation_Type_Collection $typeCollection */
-        $typeCollection = $typeModel->getCollection();
-        $typeCollection
-            ->addFieldToFilter('regulation_kind_id',array('in'=>$vendorKinds))
-            ->getSelect();
-
-        if($typeCollection->getSize()) {
-            $vendorPossibleTypes = array(); //gathers all types that vendor *COULD* be assigned to
-            $typesKinds = array(); //gathers types to kinds connections
-            foreach($typeCollection as $type) {
-                $vendorPossibleTypes[] = $type->getRegulationTypeId();
-                $typesKinds[$type->getRegulationTypeId()] = $type->getRegulationKindId();
+        $assign = array();
+        $types = array();
+        foreach ($collection as $pos) {
+            if (!isset($assign[$pos->getKindid()])) {
+                $assign[$pos->getKindid()] = array();
             }
-        } else {
-            return array(); //there is no document types for selected document kinds
+            $assign[$pos->getKindid()][] = $pos->getData();
+            $types[$pos->getRegulationTypeId()] = $pos->getRegulationTypeId();
         }
-
-        /** @var GH_Regulation_Model_Regulation_Document_Vendor $documentVendorModel */
-        $documentVendorModel = Mage::getModel('ghregulation/regulation_document_vendor');
-        /** @var GH_Regulation_Model_Resource_Regulation_Document_Vendor_Collection $documentVendorCollection */
-        $documentVendorCollection = $documentVendorModel->getCollection();
-        $documentVendorCollection
-            ->addFieldToFilter('vendor_id',$vendor->getId())
-            ->addFieldToFilter('regulation_type_id',array('in'=>$vendorPossibleTypes))
-            ->setOrder('date',Zend_Db_Select::SQL_ASC);
-        if($documentVendorCollection->getSize()) {
-            $vendorFinalTypes = array();
-            foreach($documentVendorCollection as $documentVendor) {
-                $vendorFinalTypes[] = $documentVendor->getRegulationTypeId();
-            }
-            $vendorFinalTypes = array_unique($vendorFinalTypes);
-        } else {
-            return array(); //vendor has no assigned document types in admin
+        // prepare array with document versions
+        
+        $collection = Mage::getResourceModel('ghregulation/regulation_document_collection');
+        $collection->addFieldToFilter('regulation_type_id',array('in'=>$types));
+        $collection->addOrder('date','ASC');
+        $documents = array();
+        foreach ($collection as $document) {
+            $documents[$document->getRegulationTypeId()][] = array (
+                'id' => $document->getId(),
+                'filename' => $document->getFilename(),
+                'url' => $document->getVendorUrl(),
+                'date' => $document->getDate(),
+            ); 
         }
-
-        //get all documents of selected types
-        /** @var GH_Regulation_Model_Regulation_Document $documentModel */
-        $documentModel = Mage::getModel('ghregulation/regulation_document');
-        /** @var GH_Regulation_Model_Resource_Regulation_Document_Collection $documentCollection */
-        $documentCollection = $documentModel->getCollection();
-        $documentCollection
-            ->addFieldToFilter('regulation_type_id',array('in'=>$vendorFinalTypes))
-            ->addFieldToFilter('date',array('lteq'=>date('Y-m-d')))
-            ->setOrder('date',Zend_Db_Select::SQL_ASC);
-        if($documentCollection->getSize()) {
-            $preReturnDocuments = array(); //array of all possible documents that can be assigned to vendor
-            $startDateForKinds = array();
-            foreach($documentCollection as $document) {
-                /** @var GH_Regulation_Model_Regulation_Document $document */
-                $docArray = array(
-                    'id' => $document->getId(),
-                    'filename' => $document->getFileName(),
-                    'url' => $document->getVendorUrl(),
-                    'date' => $document->getDate()
-                );
-                $typeId = $document->getRegulationTypeId();
-                $preReturnDocuments[$typesKinds[$typeId]][$document->getRegulationTypeId()][] = $docArray;
-
-                $kindId = $typesKinds[$document->getRegulationTypeId()];
-                if(!isset($startDateForKinds[$kindId]) || strtotime($document->getDate()) <= $acceptDate) {
-                    $startDateForKinds[$kindId] = $document->getDate();
+        
+        // calculate final table
+        $final = array();
+        foreach ($assign as $kindId => $typeset) {
+            foreach ($typeset as $typeKey => $type) {
+                $docTmp = empty($documents[$type['regulation_type_id']])? array(): $documents[$type['regulation_type_id']]; 
+                $key = 0;
+                // skip older documents
+                while (isset($docTmp[$key]) && ($docTmp[$key]['date']<$type['date'])) {
+                    $key ++;                    
+                }
+                       // get latest document element
+                if (isset($docTmp[$key-1])) {
+                    $tmp = $docTmp[$key-1];
+                    $tmp['date'] = $type['date'];
+                    $final[$type['kindname']][$tmp['id']] = $tmp;
+                }
+                // add same type documents
+                $nextDate = isset($typeset[$typeKey+1])? $typeset[$typeKey+1]['date']: '9999-31-12';
+                while (isset($docTmp[$key]) && ($docTmp[$key]['date']<$nextDate)) {
+                    $tmp = $docTmp[$key++];
+                    $final[$type['kindname']][$tmp['id']] = $tmp;
                 }
             }
-        } else {
-            return array(); //there are no documents for selected document types
         }
-
-
-        //timeframes holds start and end dates for each document type
-        $timeframes = array();
-        $lastkeys = array();
-        foreach($documentVendorCollection as $k=>$documentVendor) {
-            $typeId = $documentVendor->getRegulationTypeId();
-            $kindId = $typesKinds[$typeId];
-            $timeframes[$kindId][$k] = array(
-                'type' => $typeId,
-                'start' => ($k == 1 ? $startDateForKinds[$kindId] : $documentVendor->getDate()),
-                'end' => date('Y-m-d')
-            );
-            if(isset($lastkeys[$kindId]) && isset($timeframes[$kindId][$lastkeys[$kindId]])) {
-                $daybefore = date('Y-m-d',strtotime($documentVendor->getDate()) - 1);
-                if($timeframes[$kindId][$lastkeys[$kindId]]['start'] > $daybefore) {
-                    unset($timeframes[$kindId][$lastkeys[$kindId]]);
-                } else {
-                    $timeframes[$kindId][$lastkeys[$kindId]]['end'] = $daybefore;
-                }
-            }
-            $lastkeys[$kindId] = $k;
-        }
-
-        //return proper documents for displaying in vendor history
-        $returnDocuments = array(); //hold return documents in chronological order
-        foreach ($timeframes as $kindId => $types) {
-            $kindName = $kinds[$kindId];
-            $returnDocuments[$kindName] = array();
-            foreach($types as $typeTimeframe) {
-                if (isset($preReturnDocuments[$kindId][$typeTimeframe['type']])) {
-                    $tmpDocuments = array();
-                    foreach ($preReturnDocuments[$kindId][$typeTimeframe['type']] as $finalDocument) {
-                        if (strtotime($finalDocument['date']) <= strtotime($typeTimeframe['start'])) {
-                            $tmpDocuments[0] = $finalDocument;
-                            continue;
-                        } elseif (strtotime($finalDocument['date']) <= strtotime($typeTimeframe['end'])) {
-                            $tmpDocuments[] = $finalDocument;
-                            continue;
-                        }
-                        break; //breaks loop because all documents for this timeframe has been assigned - no need to process next ones
-                    }
-                    $returnDocuments[$kindName] = array_merge($returnDocuments[$kindName], $tmpDocuments);
-                }
-                $returnDocuments[$kindName] = array_reverse($returnDocuments[$kindName]);
-            }
-        }
-
         if($idsOnly) {
             $ids = array();
-            foreach($returnDocuments as $kind=>$documents) {
+            foreach($final as $kind=>$documents) {
                 foreach($documents as $document) {
                     $ids[] = $document['id'];
                 }
@@ -323,8 +239,10 @@ class GH_Regulation_Helper_Data extends Mage_Core_Helper_Abstract
         if(isset($ids)) {
             return $ids;
         } else {
-            return $returnDocuments;
+            return $final;
         }
+        
+        
     }
 
 
