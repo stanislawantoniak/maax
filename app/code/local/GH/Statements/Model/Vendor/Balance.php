9ce9c299fcbe066026401a8734e0e755ca0b1f19
victory <victoria.sultanovska@convertica.pl>
2015-10-27 14:25:41 +0100

    Refs#1518 refs#1483 instalator + model triad

--- app/code/local/GH/Statements/Model/Vendor/Balance.php
+++ app/code/local/GH/Statements/Model/Vendor/Balance.php
@@ -4,6 +4,18 @@
  * Class GH_Statements_Model_Vendor_Balance
  *
  * @method int getStatus()
+ * @method string getDate()
+ * @method int getVendorId()
+ *
+ * @method string getPaymentToClient()
+ * @method string getReturnToClient()
+ *
+ * @method string getVendorPaymentCost()
+ * @method string getVendorInvoiceCost()
+ *
+ * @method string getBalancePerMonth()
+ * @method string getBalanceCumulative()
+ * @method string getBalanceDue()
  *
  */
 class GH_Statements_Model_Vendor_Balance extends Mage_Core_Model_Abstract
