abac985ef24c96980a59a0e557bcd85b7f21cd29
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-10-27 22:46:31 +0100

    change column

--- app/code/local/GH/Statements/Model/Resource/Vendor/Balance.php
+++ app/code/local/GH/Statements/Model/Resource/Vendor/Balance.php
@@ -8,7 +8,7 @@ class GH_Statements_Model_Resource_Vendor_Balance extends Mage_Core_Model_Resour
 
     protected function _construct()
     {
-        $this->_init('ghstatements/vendor_balance', 'balance_id');
+        $this->_init('ghstatements/vendor_balance', 'id');
     }
 
 }
