9d7995f9523f414ee9c883890d5cf0ab27e6655a
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-10-27 20:42:18 +0100

    change column

--- app/code/local/GH/Statements/sql/ghstatements_setup/upgrade-0.1.5-0.1.6.php
+++ app/code/local/GH/Statements/sql/ghstatements_setup/upgrade-0.1.5-0.1.6.php
@@ -8,26 +8,31 @@ $installer->startSetup();
 /**
  * Saldo sprzedawcy
  */
-$installer->getConnection()
-    ->changeColumn(
-        $this->getTable('ghstatements/vendor_balance'),
-        "payment_to_client",
-        "payment_from_client",
-        array(
-            'type' => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-            'nullable' => false,
-            "length" => "12,4",
-            'comment' => 'Płatności od klientów'
-        )
+$table = $this->getTable('ghstatements/vendor_balance');
+if ($installer->getConnection()->tableColumnExists($table, "payment_to_client")) {
+    $installer->getConnection()
+        ->changeColumn(
+            $table,
+            "payment_to_client",
+            "payment_from_client",
+            array(
+                'type' => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+                'nullable' => false,
+                "length" => "12,4",
+                'comment' => 'Płatności od klientów'
+            )
+        );
+}
+
+
+$installer->getConnection()->modifyColumn(
+    $table,
+    "date",
+    array(
+        'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+        'nullable' => false,
+        'comment' => 'Statement month (Miesiąc rozliczeniowy)'
     )
-    ->modifyColumn(
-        $this->getTable('ghstatements/vendor_balance'),
-        "date",
-        array(
-            'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-            'nullable' => false,
-            'comment' => 'Statement month (Miesiąc rozliczeniowy)'
-        )
-    );
+);
 
 $installer->endSetup();
