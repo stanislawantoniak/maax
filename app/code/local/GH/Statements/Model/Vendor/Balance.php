263b743eba8d10aeb75218cceca3b0fe6f7c0911
victory <victoria.sultanovska@convertica.pl>
2015-10-29 10:38:44 +0100

    saldo accumulative

--- app/code/local/GH/Statements/Model/Vendor/Balance.php
+++ app/code/local/GH/Statements/Model/Vendor/Balance.php
@@ -4,7 +4,7 @@
  * Class GH_Statements_Model_Vendor_Balance
  *
  * @method int getStatus()
- * @method string getBalanceMonth()
+ * @method string getDate()
  * @method int getVendorId()
  *
  * @method string getPaymentToClient()
