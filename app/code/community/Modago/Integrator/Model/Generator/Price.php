5942bbc3575afe523cb864531a2ddb3976a08aab
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-11-02 22:51:11 +0100

    price

--- app/code/community/Modago/Integrator/Model/Generator/Price.php
+++ app/code/community/Modago/Integrator/Model/Generator/Price.php
@@ -1,11 +1,17 @@
 <?php
+
 /**
  * generating price file
  */
 class Modago_Integrator_Model_Generator_Price
-    extends Modago_Integrator_Model_Generator {
+    extends Modago_Integrator_Model_Generator
+{
+    protected $_getList;
+    protected $_header;
+    protected $_footer;
 
-    protected function _construct() {
+    protected function _construct()
+    {
         $this->setFileNamePrefix('PRICES');
     }
 
@@ -14,37 +20,65 @@ class Modago_Integrator_Model_Generator_Price
      *
      * @return array
      */
-     protected function _prepareList() {
-         /// todo                  
-         return array();
-     }
-     
+    protected function _prepareList()
+    {
+        if ($this->_getList) {
+            return false;
+        }
+        $collection = Mage::getModel("catalog/product")->getCollection();
+        $collection->addAttributeToSelect("price");
+        $collection->addAttributeToSelect("special_price");
+
+        //$collection->addAttributeToFilter('visibility', array('neq' => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE));
+        //$collection->addAttributeToFilter('status', Mage_Catalog_Model_Product_Status::STATUS_ENABLED);
+
+
+        $res = array();
+        foreach($collection as $collectionItem){
+            $res[] = array("sku" => $collectionItem->getSku(), "price" => $collectionItem->getPrice());
+        }
+
+        $this->_getList = true;
+        return $res;
+    }
+
     /**
-     *	prepare xml block 
+     * prepare xml block
      *
+     * @param array $item
      * @return string
      */
-     protected function _prepareXmlBlock($item) {
-         return ''; // todo
-     }
-     
+    protected function _prepareXmlBlock($item)
+    {
+        $price = number_format($item['price'], 2);
+        return '<product price="' . $price . '">' . $item['sku'] . '</product>';
+    }
+
     /**
      * prepare header
      *
      * @return string
      */
-     protected function _getHeader() {
-         return ''; // todo
-     }
-     
+    protected function _getHeader()
+    {
+        if (!$this->_header) {
+            $this->_header = '<mall><merchant>' . $this->getExternalId() . '</merchant><priceList priceId="A">';
+        }
+        return $this->_header;
+    }
+
     /**
      * prepare footer
      *
      * @return string
      */
-     protected function _getFooter() {
-         return ''; // todo
-     }
+    protected function _getFooter()
+    {
+        if (!$this->_footer) {
+            $this->_footer = "</priceList></mall>";
+        }
+        return $this->_footer;
+    }
 
-    
+
 }
