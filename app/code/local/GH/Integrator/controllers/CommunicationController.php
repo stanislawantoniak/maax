f0967114ad5ba09fa99246a38d4d7a4b71a2a171
Adam Wilk <adam.wilk@convertica.pl>
2015-11-02 10:10:55 +0100

    re#1536 re#1539 finished communication controller

--- app/code/local/GH/Integrator/controllers/CommunicationController.php
+++ app/code/local/GH/Integrator/controllers/CommunicationController.php
@@ -2,34 +2,31 @@
 
 class GH_Integrator_CommunicationController extends Mage_Core_Controller_Front_Action {
 	public function indexAction() {
-		$data = $this->getRequest();
+		$data = $this->getRequest()->getParams();
 
-		$vendorId = isset($data['external_id']) ? $data['external_id'] : null;
-		$secret = isset($data['secret']) ? $data['secret'] : null;
+		$vendorId = 5;//isset($data['external_id']) ? $data['external_id'] : null;
+		$secret = "7b7a53e239400a13bd6be6c91c4f6c4e";//isset($data['secret']) ? $data['secret'] : null;
 
 		/** @var GH_Integrator_Helper_Data $helper */
 		$helper = Mage::helper('ghintegrator');
 
 		try {
 			if (!$vendorId || !$secret) {
-				$helper->throwException(
-					"Incorrect input data provided! \n external_id: " .
-					print_r($vendorId) . "\n secret: " . print_r($secret)
-				);
+				$helper->throwException("Incorrect input data provided!\n external_id: $vendorId\n secret: $secret");
 			}
 
 			/** @var Zolago_Dropship_Model_Vendor $vendor */
 			$vendor = Mage::getModel('udropship/vendor')->load($vendorId);
 			if (!$vendor->getId()) {
-				$helper->throwException("Provided external id is not valid! (" . print_r($vendorId) . ")");
+				$helper->throwException("Provided external id is not valid! ($vendorId)");
 			}
 
 			if (!$vendor->getIntegratorEnabled()) {
-				$helper->throwException("Integrator is disabled for vendor with ID: {$vendorId}", $vendorId);
+				$helper->throwException("Integrator is disabled for vendor with ID: $vendorId", $vendorId);
 			}
 
-			if ($data['secret'] !== $vendor->getIntegratorSecret()) {
-				$helper->throwException("Provided secret is not valid for this vendor! (" . print_r($secret) . ")", $vendorId);
+			if ($secret !== $vendor->getIntegratorSecret()) {
+				$helper->throwException("Provided secret is not valid for this vendor! ($secret)", $vendorId);
 			}
 
 
@@ -52,7 +49,42 @@ class GH_Integrator_CommunicationController extends Mage_Core_Controller_Front_A
 			//everything is checked let's continue
 			$filesToUpdate = array();
 			$lastIntegrationTime = $vendor->getLastIntegration() ? strtotime($vendor->getLastIntegration()) : 0;
-			$currentTime = Mage::getModel('core/date')->timestamp(time());
+			$currentTime = Mage::getModel('core/date')->timestamp();
+
+			Mage::log($vendor->getLastIntegration());
+			Mage::log(date('Y-m-d H:i',$lastIntegrationTime));
+			Mage::log(date('Y-m-d H:i',$currentTime));
+
+			//check if descriptions are candidate to update
+			foreach($descriptionTimes as $descriptionTime) { //array is sorted from latest to earliest hour
+				Mage::log('description time: '.date('Y-m-d H:i',$descriptionTime));
+				if($descriptionTime <= $currentTime && $descriptionTime > $lastIntegrationTime) {
+					$filesToUpdate[] = $helper::FILE_DESCRIPTIONS;
+					break;
+				}
+			}
+
+			//check if prices are candidate to update
+			foreach($priceTimes as $priceTime) { //array is sorted from latest to earliest hour
+				if($priceTime <= $currentTime && $priceTime > $lastIntegrationTime) {
+					$filesToUpdate[] = $helper::FILE_PRICES;
+					break;
+				}
+			}
+
+			//check if stocks are candidate to update
+			foreach($stockTimes as $stockTime) { //array is sorted from latest to earliest hour
+				if($stockTime <= $currentTime && $stockTime > $lastIntegrationTime) {
+					$filesToUpdate[] = $helper::FILE_STOCKS;
+					break;
+				}
+			}
+
+			/** @var Mage_Core_Model_Date $dateModel */
+			$dateModel = Mage::getModel('core/date');
+			$vendor
+				->setLastIntegration($dateModel->timestamp())
+				->save();
 
 			echo $this->returnResponse($helper::STATUS_OK,$filesToUpdate);
 		} catch(GH_Integrator_Exception $exception) {
