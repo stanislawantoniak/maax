90f821a36df43ab5fbea3c0dbfc279de608caa37
Adam Wilk <adam.wilk@convertica.pl>
2015-11-02 15:18:58 +0100

    re#1536 file write functions

--- app/code/community/Modago/Integrator/Helper/Data.php
+++ app/code/community/Modago/Integrator/Helper/Data.php
@@ -12,7 +12,41 @@ class Modago_Integrator_Helper_Data extends Mage_Core_Helper_Abstract
 	const FILE_PRICES = 'PRICES';
 	const FILE_STOCKS = 'STOCKS';
 
+	private $_file;
+	private $_path;
+
 	public function getFileTypes() {
 		return array(self::FILE_DESCRIPTIONS,self::FILE_PRICES,self::FILE_STOCKS);
+	}
+
+	public function createFile($path) {
+		$this->_file = fopen($path,'w');
+		if($this->_file === false) {
+			$this->throwException('Cannot create file '.$path);
+		} else {
+			$this->_path = $path;
+			$this->addToFile('<?xml version="1.0" encoding="UTF-8"?>');
+		}
+		return $this;
+	}
+
+	public function addToFile($data) {
+		if(is_null($this->_file) || is_null($this->_path)) {
+			$this->throwException('You have to create file first!');
+		}
+
+		$written = fwrite($this->_file,$data);
+		if($written === false) {
+			$this->throwException('Could not write to file '.$this->_path);
+		}
+	}
+
+	public function closeFile() {
+		fclose($this->_file);
+		$this->_file = null;
+	}
+
+	public function throwException($msg,$code=0) {
+		throw Mage::exception("Modago_Integrator",$msg,$code);
 	}
 }
