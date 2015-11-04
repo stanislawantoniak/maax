39cc2f03a9256f554f176196f9a41d070236a08e
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-11-04 10:21:05 +0100

    re#1322 re#1483 fix: do poprawki angielske teksty - w arkusze wpisałem poprawne

--- app/code/local/GH/Statements/Block/Adminhtml/Vendor/Balance/Grid.php
+++ app/code/local/GH/Statements/Block/Adminhtml/Vendor/Balance/Grid.php
@@ -38,7 +38,7 @@ class GH_Statements_Block_Adminhtml_Vendor_Balance_Grid extends Mage_Adminhtml_B
         ));
         // Status miesiąca
         $this->addColumn('status', array(
-            'header' => $helper->__('Status of monthly balance'),
+            'header' => $helper->__('Status'),
             'index' => 'status',
             //'width' => '100px',
             'type' => 'options',
@@ -50,7 +50,7 @@ class GH_Statements_Block_Adminhtml_Vendor_Balance_Grid extends Mage_Adminhtml_B
         ));
         // Statement month (Miesiąc rozliczeniowy)
         $this->addColumn('date', array(
-            'header' => $helper->__("Statement month"),
+            'header' => $helper->__('Month'),
             //'type'   => 'date',
             //'format' => 'Y-M',
             'index' => 'date'
@@ -66,49 +66,49 @@ class GH_Statements_Block_Adminhtml_Vendor_Balance_Grid extends Mage_Adminhtml_B
         );
         // Płatności od klientów
         $this->addColumn('payment_from_client', array(
-            'header' => $helper->__('Payment from client'),
+            'header' => $helper->__('Customer payments'),
             'index' => 'payment_from_client',
             'type' => 'currency',
             'currency_code' => $currency
         ));
         // Zwroty płatności do klientów
         $this->addColumn('payment_return_to_client', array(
-            'header' => $helper->__('Payment return to client'),
+            'header' => $helper->__('Customer refunds'),
             'index' => 'payment_return_to_client',
             'type' => 'currency',
             'currency_code' => $currency
         ));
         // Wypłaty
         $this->addColumn('vendor_payment_cost', array(
-            'header' => $helper->__("Vendor payment cost"),
+            'header' => $helper->__('Payouts to vendor'),
             'index' => 'vendor_payment_cost',
             'type' => 'currency',
             'currency_code' => $currency
         ));
         // Faktury i korekty faktur
         $this->addColumn('vendor_invoice_cost', array(
-            'header' => $helper->__("Vendor invoice cost"),
+            'header' => $helper->__('Invoices and credit notes'),
             'index' => 'vendor_invoice_cost',
             'type' => 'currency',
             'currency_code' => $currency
         ));
         // Bilans miesiąca
         $this->addColumn('balance_per_month', array(
-            'header' => $helper->__("Balance per month"),
+            'header' => $helper->__('Monthly balance'),
             'index' => 'balance_per_month',
             'type' => 'currency',
             'currency_code' => $currency
         ));
         // Saldo narastająco
         $this->addColumn('balance_cumulative', array(
-            'header' => $helper->__("Balance cumulative"),
+            'header' => $helper->__('Cumulative balance'),
             'index' => 'balance_cumulative',
             'type' => 'currency',
             'currency_code' => $currency
         ));
         // Saldo wymagalne
         $this->addColumn('balance_due', array(
-            'header' => $helper->__("Balance due"),
+            'header' => $helper->__('Due balance'),
             'index' => 'balance_due',
             'type' => 'currency',
             'currency_code' => $currency
