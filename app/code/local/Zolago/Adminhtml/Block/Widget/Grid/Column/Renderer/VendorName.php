dae15d31006ae98d2ca3bc2015638dc842cc98dd
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-11-16 12:45:18 +0100

    re#1567 better way

--- app/code/local/Zolago/Adminhtml/Block/Widget/Grid/Column/Renderer/VendorName.php
+++ app/code/local/Zolago/Adminhtml/Block/Widget/Grid/Column/Renderer/VendorName.php
@@ -6,12 +6,7 @@ class Zolago_Adminhtml_Block_Widget_Grid_Column_Renderer_VendorName
 
     public function render(Varien_Object $row)
     {
-        $attribute = Mage::getModel('eav/config')->getAttribute('catalog_product', "udropship_vendor");
-
-        $options = array();
-        foreach ($attribute->getSource()->getAllOptions(false, true) as $option) {
-            $options[(int)$option['value']] = $option['label'];
-        }
+        $options = $this->getColumn()->getData('options');
         return $options[(int)$row->getData("udropship_vendor")];
     }
 
