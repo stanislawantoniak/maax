02dfc12b379caac9cc3eb543585202e5d532e84f
victory <victoria.sultanovska@convertica.pl>
2015-11-27 17:13:03 +0100

    Refs#1607

---
+++ app/code/local/Zolago/Modago/controllers/MapController.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once Mage::getConfig()->getModuleDir('controllers', "Unirgy_DropshipMicrosite") . DS . "IndexController.php";
+
+class Zolago_Modago_MapController extends Unirgy_DropshipMicrosite_IndexController
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
