1e2de58c9c5fe9f09053c95e57b426d6b971fc1e
victoriasultanovska <victoria.sultanovska@convertica.pl>
2015-12-02 22:01:09 +0100

    stores list

--- js/gh/storemap/storefinder.js
+++ js/gh/storemap/storefinder.js
@@ -85,12 +85,13 @@ function initialize() {
     data = jQuery.parseJSON(data);
 
 
-
+    refreshMap();
+    buildStoresList();
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(
             function (position) {
                 console.log("I'm tracking you!");
-
+                gmarkers = [];
                 showPosition(position);
             },
             function (error) {
