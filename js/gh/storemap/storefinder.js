70fb22f1e54a7e5dc6b4481f1d04e6d0ce62496e
victory <victoria.sultanovska@convertica.pl>
2015-12-03 09:20:28 +0100

    Link "zobacz wszystkie sklepy nie powinien być widoczny jak widać wszystkie sklepy.

--- js/gh/storemap/storefinder.js
+++ js/gh/storemap/storefinder.js
@@ -382,9 +382,9 @@ jQuery(document).ready(function () {
         enteredSearchValue = jQuery(this).val();
         searchOnMap(enteredSearchValue);
         if (enteredSearchValue.length > 0) {
-            showAllLink.show();
+            showAllLink.removeClass("hidden");
         } else {
-            showAllLink.hide();
+            showAllLink.addClass("hidden");
         }
     });
 
