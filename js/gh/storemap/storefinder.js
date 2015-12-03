58ac8d3b31a7332a98cdc4049d1f01a9a1263d1b
victory <victoria.sultanovska@convertica.pl>
2015-12-03 09:38:40 +0100

    zoom

--- js/gh/storemap/storefinder.js
+++ js/gh/storemap/storefinder.js
@@ -191,19 +191,12 @@ function refreshMap(filteredData) {
         google.maps.event.addListener(marker, "click", function () {
             infowindow.setContent(this.html);
             //$screen-sm: 768px
-            //if (window.innerWidth >= 768) {
-            //    map.setCenter(this.getPosition()); // set map center to marker position
-            //    smoothZoom(map, 10, map.getZoom()); //call smoothZoom, parameters map, final zoomLevel, and starting zoom level
-            //} else {
-            //    map.setCenter(this.getPosition());
-            //    map.setZoom(9);
-            //}
-            if (window.innerWidth < 768) {
-                map.setZoom(5);
-                map.setCenter(new google.maps.LatLng(defaultCenterLangMobile, defaultCenterLatMobile));
+            if (window.innerWidth >= 768) {
+                map.setCenter(this.getPosition()); // set map center to marker position
+                smoothZoom(map, 10, map.getZoom()); //call smoothZoom, parameters map, final zoomLevel, and starting zoom level
             } else {
-                map.setZoom(6);
-                map.setCenter(new google.maps.LatLng(defaultCenterLang, defaultCenterLat));
+                map.setCenter(this.getPosition());
+                map.setZoom(9);
             }
             //$screen-md: 992px
             if (window.innerWidth <= 992) {
