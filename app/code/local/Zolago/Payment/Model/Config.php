44f8df8235f5a6b67628eeaeb704d85a5aef0cdd
victory <victoria.sultanovska@convertica.pl>
2015-12-04 16:55:06 +0100

    test dotpay

--- app/code/local/Zolago/Payment/Model/Config.php
+++ app/code/local/Zolago/Payment/Model/Config.php
@@ -71,6 +71,8 @@ class Zolago_Payment_Model_Config extends Varien_Simplexml_Config
         Mage::log($type, null, "getProviderConfig.log");
         Mage::log($provider, null, "getProviderConfig.log");
         Mage::log($website, null, "getProviderConfig.log");
+        Mage::log($data, null, "getProviderConfig.log");
+        Mage::log("------------------------------", null, "getProviderConfig.log");
 		return (is_array($data) && isset($data[0])) ? $data[0]->asArray() : null;
 	}
 }
