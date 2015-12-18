2e93eaf4d463d5d2f76064a5fcaa345601b7b644
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2015-12-18 14:37:23 +0100

    re#1668 jako admin chcę mieć możliwość wysłania do solara produktów z grida produktowego (mass akcja)

--- app/code/local/Zolago/Adminhtml/controllers/Catalog/Product/Action/SolrController.php
+++ app/code/local/Zolago/Adminhtml/controllers/Catalog/Product/Action/SolrController.php
@@ -14,13 +14,18 @@ class Zolago_Adminhtml_Catalog_Product_Action_SolrController extends Mage_Adminh
                 "check_parents" => true
             )
         );
-
+        $this->_getSession()->addSuccess($this->getHelper()->__('%s successfully push to solr.', count($ids)));
         $this->_redirectReferer();
-        var_dump($ids);
-        die("happy die!");
     }
 
     protected function _isAllowed() {
         return true; //todo
+    }
+
+    /**
+     * @return Zolago_Adminhtml_Helper_Data
+     */
+    public function getHelper() {
+        return Mage::helper("zolagoadminhtml");
     }
 }
