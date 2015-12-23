679b1bdc6025ce4767683c1cb220a19c99388fa3
victory <victoria.sultanovska@convertica.pl>
2015-12-23 11:12:09 +0100

    test

--- test/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1Test.php
+++ test/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1Test.php
@@ -92,37 +92,49 @@ class Zolago_Catalog_Model_Api2_Restapi_Rest_Admin_V1Test extends Zolago_TestCas
         Mage::log("nowa cena: ".$randomek, null, 'mylog.log');
 
         $param = array(
-        '5-25678-CZERWONY-XXXXL' => array(
+        '25-1AU41410440G' => array(
                 'A' => $randomek,
                 'B' => 6,
                 'C' => 18,
                 'Z' => 88
             ),
-        '5-25680-BIALY-XXXXL' => array(
+        '25-16X83123136O' => array(
                 'A' => $randomek+1,
                 'B' => 6,
                 'C' => 18,
                 'Z' => 88
             ),
-//        '5-24939-BIALY-S' => array(
-//                'A' => $randomek+2,
-//                'B' => 6,
-//                'C' => 18,
-//                'Z' => 88
-//            ),
-//        '5-24939-BIALY-L' => array(
-//                'A' => $randomek+3,
-//                'B' => 6,
-//                'C' => 18,
-//                'Z' => 88
-//            ),
+        '25-19E40694342D' => array(
+                'A' => $randomek+2,
+                'B' => 6,
+                'C' => 18,
+                'Z' => 88
+            ),
+        '25-19E40694344F' => array(
+                'A' => $randomek+3,
+                'B' => 6,
+                'C' => 18,
+                'Z' => 88
+            ),
 
-//        '5123124' => array(
-//            'A' => $randomek+3,
-//            'B' => 6,
-//            'C' => 18,
-//            'Z' => 88
-//        )
+        '25-19E40694340B' => array(
+            'A' => $randomek+3,
+            'B' => 6,
+            'C' => 18,
+            'Z' => 88
+        ),
+            '25-19E40694338I' => array(
+                'A' => $randomek+3,
+                'B' => 6,
+                'C' => 18,
+                'Z' => 88
+            ),
+            '25-19E40694338I' => array(
+                'A' => $randomek+3,
+                'B' => 6,
+                'C' => 18,
+                'Z' => 88
+            )
         );
         //$param = json_encode($json);
         $obj::updatePricesConverter($param);
