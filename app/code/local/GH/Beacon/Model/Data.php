91b2058bda08d8ba7893c464d11ebfe8f421f386
Adam Wilk <adam.wilk@convertica.pl>
2016-01-04 14:34:04 +0100

    re#1685 module + table

---
+++ app/code/local/GH/Beacon/Model/Data.php
@@ -0,0 +1,9 @@
+<?php
+class GH_Beacon_Model_Data extends Mage_Core_Model_Abstract {
+
+    protected function _construct() {
+        $this->_init('ghbeacon/data');
+        parent::_construct();
+    }
+}
+
