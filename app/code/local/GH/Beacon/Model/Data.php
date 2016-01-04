2e08394c06adfea41b32ec0c488c2b26b2ebf2f9
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2016-01-04 15:51:48 +0100

    re#1685 admin grid

--- app/code/local/GH/Beacon/Model/Data.php
+++ app/code/local/GH/Beacon/Model/Data.php
@@ -1,4 +1,17 @@
 <?php
+
+/**
+ * Model for data from beacons - offline history of customer
+ *
+ * Class GH_Beacon_Model_Data
+ *
+ * @method string getId()
+ * @method string getBeaconId()
+ * @method string getEmail()
+ * @method string getDistance()
+ * @method string getDate()
+ * @method string getEventType()
+ */
 class GH_Beacon_Model_Data extends Mage_Core_Model_Abstract {
 
     protected function _construct() {
