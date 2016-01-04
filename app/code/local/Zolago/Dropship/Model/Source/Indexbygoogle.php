05e1f2d2a5a0ea87111afb93832b9c1a792f89d0
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-01-04 17:37:57 -0500

    product attribute

---
+++ app/code/local/Zolago/Dropship/Model/Source/Indexbygoogle.php
@@ -0,0 +1,43 @@
+<?php
+
+class Zolago_Dropship_Model_Source_Indexbygoogle
+    extends Zolago_Catalog_Model_Product_Source_Abstract
+{
+    const PRODUCT_INDEX_BY_GOOGLE_USE_CONFIG = 0;
+    const PRODUCT_INDEX_BY_GOOGLE_NO = 1;
+    const PRODUCT_INDEX_BY_GOOGLE_YES = 2;
+
+
+    /**
+     * Retrieve all attribute options
+     *
+     * @return array
+     */
+    public function getAllOptions()
+    {
+        if (!$this->_options) {
+            $this->_options = array(
+                array(
+                    'label' => Mage::helper('catalog')->__('Use config'),
+                    'value' => self::PRODUCT_INDEX_BY_GOOGLE_USE_CONFIG
+                ),
+                array(
+                    'label' => Mage::helper('catalog')->__('Yes'),
+                    'value' => self::PRODUCT_INDEX_BY_GOOGLE_YES
+                ),
+                array(
+                    'label' => Mage::helper('catalog')->__('No'),
+                    'value' => self::PRODUCT_INDEX_BY_GOOGLE_NO
+                )
+            );
+        }
+        return $this->_options;
+    }
+
+    public function toOptionArray()
+    {
+        return $this->getAllOptions();
+    }
+
+
+}
