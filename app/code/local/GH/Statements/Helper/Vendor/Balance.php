497d7095680aba9dfe3177b164cc36f75babee49
victory <victoria.sultanovska@convertica.pl>
2016-01-07 14:49:03 +0100

    Refs#1697 fix for actual_balance

--- app/code/local/GH/Statements/Helper/Vendor/Balance.php
+++ app/code/local/GH/Statements/Helper/Vendor/Balance.php
@@ -120,7 +120,7 @@ class GH_Statements_Helper_Vendor_Balance extends Mage_Core_Helper_Abstract
         $statements = Mage::getModel("ghstatements/statement")
             ->getCollection();
         $statements->getSelect()->reset(Zend_Db_Select::COLUMNS)
-            ->columns("vendor_id, DATE_FORMAT(event_date,'%Y-%m') AS balance_month, (last_statement_balance+to_pay-payment_value) AS amount")
+            ->columns("vendor_id, DATE_FORMAT(event_date,'%Y-%m') AS balance_month, actual_balance AS amount")
             ->group("vendor_id")
             ->group("balance_month")
             ->order("event_date DESC");
