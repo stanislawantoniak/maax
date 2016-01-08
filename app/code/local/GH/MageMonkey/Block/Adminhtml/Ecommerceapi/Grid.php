9aa7d027b4f4887770f07224a24dd53fb90e06e5
victory <victoria.sultanovska@convertica.pl>
2016-01-08 11:08:33 +0100

    Refs#1688 You must specify a apikey value for the ecommOrders method ERR message

--- app/code/local/GH/MageMonkey/Block/Adminhtml/Ecommerceapi/Grid.php
+++ app/code/local/GH/MageMonkey/Block/Adminhtml/Ecommerceapi/Grid.php
@@ -3,7 +3,8 @@
 /**
  * Class GH_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid
  */
-class GH_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid extends Ebizmarts_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid {
+class GH_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid extends Ebizmarts_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid
+{
 
     /**
      * @return Mage_Adminhtml_Block_Widget_Grid
@@ -12,27 +13,35 @@ class GH_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid extends Ebizmarts_MageMonk
     {
         $orders = array();
 
+
         foreach (Mage::app()->getStores() as $storeId => $store) {
             $api = Mage::getModel('monkey/api', array('store' => $storeId));
             $result = $api->ecommOrders(0, 500);
 
-            try{
-                if(isset($result['data'])){
+            try {
+                if (isset($result['data'])) {
                     $orders += $result['data'];
                     $collection = Mage::getModel('monkey/custom_collection', array($orders));
                 } else {
-                    echo $result . "STORE_ID : {$storeId} <br />";
+                    echo $result . " STORE_ID : {$storeId} <br />";
                 }
 
-            } catch(Mage_Adminhtml_Exception $e){
+            } catch (Mage_Adminhtml_Exception $e) {
                 Mage::logException($e);
             }
         }
 
 
+        try {
+            $this->setCollection($collection);
 
-        $this->setCollection($collection);
+        } catch (Mage_Adminhtml_Exception $e) {
+            Mage::logException($e);
+        }
 
-        return parent::_prepareCollection();
+        if (!empty($orders)) {
+            return parent::_prepareCollection();
+        }
+
     }
 }
