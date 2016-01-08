d2e5ee7b55e1b751f0947ba8abba077ed48fb283
victory <victoria.sultanovska@convertica.pl>
2016-01-08 10:57:31 +0100

    test

--- app/code/local/GH/MageMonkey/Block/Adminhtml/Ecommerceapi/Grid.php
+++ app/code/local/GH/MageMonkey/Block/Adminhtml/Ecommerceapi/Grid.php
@@ -5,6 +5,9 @@
  */
 class GH_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid extends Ebizmarts_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid {
 
+    /**
+     * @return Mage_Adminhtml_Block_Widget_Grid
+     */
     protected function _prepareCollection()
     {
         $orders = array();
@@ -12,10 +15,21 @@ class GH_MageMonkey_Block_Adminhtml_Ecommerceapi_Grid extends Ebizmarts_MageMonk
         foreach (Mage::app()->getStores() as $storeId => $store) {
             $api = Mage::getModel('monkey/api', array('store' => $storeId));
             $result = $api->ecommOrders(0, 500);
-            $orders += $result['data'];
+
+            try{
+                if(isset($result['data'])){
+                    $orders += $result['data'];
+                    $collection = Mage::getModel('monkey/custom_collection', array($orders));
+                } else {
+                    echo $result . "STORE_ID : {$storeId} <br />";
+                }
+
+            } catch(Mage_Adminhtml_Exception $e){
+                Mage::logException($e);
+            }
         }
 
-        $collection = Mage::getModel('monkey/custom_collection', array($orders));
+
 
         $this->setCollection($collection);
 
