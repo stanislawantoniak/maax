c9f86eca7dd1453b4684000e500a451b80c8b623
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2016-01-08 09:54:33 +0100

    re#1649 re#1690 config; aktualizacja kurierow modago

--- app/code/community/Modago/Integrator/Model/Shipping/Source/Modagocarrier.php
+++ app/code/community/Modago/Integrator/Model/Shipping/Source/Modagocarrier.php
@@ -11,8 +11,14 @@ class Modago_Integrator_Model_Shipping_Source_Modagocarrier {
 		$data = array(
 			array('value' => 'dhl', 'label' => 'DHL'),
 			array('value' => 'ups', 'label' => 'UPS'),
-			array('value' => 'dpd', 'label' => 'DPD')
+			array('value' => 'dpd', 'label' => 'DPD'),
+			array('value' => 'siodemka', 'label' => 'Siódemka'),
+			array('value' => 'gls', 'label' => 'GLS'),
+			array('value' => 'fedex', 'label' => 'Fedex'),
+			array('value' => 'poczta_polska', 'label' => 'Poczta Polska'),
+			array('value' => 'inpost', 'label' => 'Inpost'),
 		);
+
 		if(!$isMultiselect){
 			array_unshift($data, array('value'=>'', 'label'=>''));
 		}
