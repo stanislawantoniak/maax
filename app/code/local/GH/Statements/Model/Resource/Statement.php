5d322cb8b2c00b11e1d3ad073dd8e1c05c0cc57e
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-01-10 14:55:14 -0500

    calculate saldo one function

--- app/code/local/GH/Statements/Model/Resource/Statement.php
+++ app/code/local/GH/Statements/Model/Resource/Statement.php
@@ -18,41 +18,11 @@ class GH_Statements_Model_Resource_Statement extends Mage_Core_Model_Resource_Db
      */
     public function _afterDelete(Mage_Core_Model_Abstract $object)
     {
-//        $this->_recalculateBalanceDue($object->getData("vendor_id"), $object->getData("event_date"));
         Mage::helper("ghstatements/vendor_balance")
             ->calculateVendorBalance(
                 date("Y-m", strtotime($object->getData("event_date"))),
                 $object->getData("vendor_id")
             );
         return parent::_afterDelete($object);
-    }
-
-    /**
-     * @param $vendorId
-     * @param $date
-     */
-    protected function _recalculateBalanceDue($vendorId, $date)
-    {
-        $dateFormatted = date("Y-m", strtotime($date));
-        $statements = Mage::getModel("ghstatements/statement")
-            ->getCollection()
-            ->addFieldToFilter("vendor_id", $vendorId);
-        $statements->getSelect()->reset(Zend_Db_Select::COLUMNS)
-            ->columns("DATE_FORMAT(event_date,'%Y-%m') statement_month, actual_balance")
-            ->having("statement_month=?", $dateFormatted)
-            ->order("event_date DESC");
-
-        $actualBalance = $statements
-            ->getFirstItem()
-            ->getData("actual_balance");
-
-
-        Mage::helper("ghstatements/vendor_balance")
-            ->updateVendorBalanceData(
-                $vendorId,
-                "balance_due",
-                (float)$actualBalance,
-                $date
-            );
     }
 }
