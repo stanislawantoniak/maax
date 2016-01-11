77a956166c35f7ccfd9a975aab77fccb013a0350
Adam Wilk <adam.wilk@convertica.pl>
2016-01-11 10:38:35 +0100

    re#1718 debug fix

--- app/code/community/Modago/Integrator/Model/Order.php
+++ app/code/community/Modago/Integrator/Model/Order.php
@@ -125,7 +125,7 @@ class Modago_Integrator_Model_Order
 			->setGrandTotal($this->_subTotal)
 			->setBaseGrandTotal($this->_subTotal);
 
-		$this->_order->setUdpoNoSplitPoFlag(1); //todo: remove it's for testing on local!
+		$this->_order->setData('noautopo_flag',1); //todo: remove it's for testing on local!
 
 		$transaction->addObject($this->_order);
 		$transaction->addCommitCallback(array($this->_order, 'place'));
