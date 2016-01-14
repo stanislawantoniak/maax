8bccd9c460e240339de2ad832f4b58cfccd90289
Adam Wilk <adam.wilk@convertica.pl>
2016-01-14 14:07:31 +0100

    re#1718 changed methodology

--- app/code/community/Modago/Integrator/Model/Api.php
+++ app/code/community/Modago/Integrator/Model/Api.php
@@ -146,7 +146,7 @@ class Modago_Integrator_Model_Api
                 $item = current($item);
                 /** @var Modago_Integrator_Model_Order $integratorOrders */
                 $integratorOrders = Mage::getModel('modagointegrator/order');
-                $orderId = $integratorOrders->createOrderFromApi($item);
+                $orderId = $integratorOrders->createOrder($item);
                 $helper->log($helper->__('Success: order %s (%s) was created', $orderId, $item->order_id));
             } catch (Exception $e) {
                 Mage::logException($e);
