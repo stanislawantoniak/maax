49e13a610951f33309861f34bd8849de0c269449
victory <victoria.sultanovska@convertica.pl>
2016-01-25 11:09:23 +0100

    Refs#1746 sort by vendor name on Wypłaty do sprzedawców

--- app/code/local/Zolago/Payment/Block/Adminhtml/Vendor/Payment/Grid.php
+++ app/code/local/Zolago/Payment/Block/Adminhtml/Vendor/Payment/Grid.php
@@ -18,14 +18,17 @@ class Zolago_Payment_Block_Adminhtml_Vendor_Payment_Grid extends Mage_Adminhtml_
     protected function _prepareCollection()
     {
         $model = Mage::getModel('zolagopayment/vendor_payment');
+
+        /* @var $collection Zolago_Payment_Model_Resource_Vendor_Payment_Collection */
         $collection = $model->getCollection();
         $this->setCollection($collection);
 
         $collection->getSelect()->join(
             array("vendors" => $model->getResource()->getTable('udropship/vendor')), //$name
             "main_table.vendor_id=vendors.vendor_id", //$cond
-            array("vendor_name")//$cols = '*'
+            array("vendor_name" => "vendor_name")//$cols = '*'
         );
+
         return parent::_prepareCollection();
     }
 
@@ -52,10 +55,12 @@ class Zolago_Payment_Block_Adminhtml_Vendor_Payment_Grid extends Mage_Adminhtml_
                 'header' => $this->__('Vendor'),
                 'width' => '50px',
                 "type" => "options",
-                'index' => 'vendor_name',
+                'index' => 'vendor_id',
+                'filter_index' => 'vendor_name',
                 "options" => Mage::getSingleton('zolagodropship/source')
                     ->setPath('allvendorswithdisabled')
-                    ->toOptionHash()
+                    ->toOptionHash(),
+                'filter_condition_callback' => array($this, '_sortByVendorName'),
             )
         );
         $this->addColumn('cost',
@@ -63,7 +68,7 @@ class Zolago_Payment_Block_Adminhtml_Vendor_Payment_Grid extends Mage_Adminhtml_
                 'header' => $this->__('Cost'),
                 'width' => '50px',
                 'index' => 'cost',
-                'type'  => 'price',
+                'type' => 'price',
                 'currency' => 'base_currency_code',
                 'currency_code' => Mage::getStoreConfig('currency/options/base')
 
@@ -88,7 +93,7 @@ class Zolago_Payment_Block_Adminhtml_Vendor_Payment_Grid extends Mage_Adminhtml_
                         'caption' => Mage::helper('zolagopayment')->__('Delete'),
                         'url' => array('base' => '*/*/delete'),
                         'field' => 'id',
-                        'confirm'  => Mage::helper('zolagopayment')->__('Are you sure?')
+                        'confirm' => Mage::helper('zolagopayment')->__('Are you sure?')
                     ),
                 ),
                 'filter' => false,
@@ -97,8 +102,46 @@ class Zolago_Payment_Block_Adminhtml_Vendor_Payment_Grid extends Mage_Adminhtml_
                 'is_system' => true,
             ));
 
-
         return parent::_prepareColumns();
+    }
+
+    /**
+     * @param $collection
+     * @param $column
+     * @return $this
+     */
+    function _sortByVendorName($collection, $column)
+    {
+        /* @var $collection Zolago_Payment_Model_Resource_Vendor_Payment_Collection */
+        $direction = strtoupper($column->getDir());
+
+        if ($direction) {
+            $collection->getSelect()->order($column->getFilterIndex(), $direction);
+        }
+        if (!$value = $column->getFilter()->getValue()) {
+            return $this;
+        }
+
+        $index = $column->getIndex();
+        $collection->getSelect()->where("main_table.{$index} = ?", $value);
+
+        return $this;
+    }
+
+    /**
+     * Sets sorting order by some column
+     *
+     * @param Mage_Adminhtml_Block_Widget_Grid_Column $column
+     * @return Mage_Adminhtml_Block_Widget_Grid
+     */
+    protected function _setCollectionOrder($column)
+    {
+        if ($column->getOrderConditionCallback()) {
+            call_user_func($column->getOrderConditionCallback(), $this->getCollection(), $column);
+            return $this;
+        }
+
+        return parent::_setCollectionOrder($column);
     }
 
     public function getRowUrl($row)
