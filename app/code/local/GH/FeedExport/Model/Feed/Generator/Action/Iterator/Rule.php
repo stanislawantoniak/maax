e4bb34c3f1a55d0e3ff7db5cbe86b58f1fd5511e
victory <victoria.sultanovska@convertica.pl>
2016-01-26 16:13:50 +0100

    Refs#1761 Refs#1776 Generate feeds for enabled visible individual (should be)

--- app/code/local/GH/FeedExport/Model/Feed/Generator/Action/Iterator/Rule.php
+++ app/code/local/GH/FeedExport/Model/Feed/Generator/Action/Iterator/Rule.php
@@ -11,8 +11,9 @@ class GH_FeedExport_Model_Feed_Generator_Action_Iterator_Rule extends Mirasvit_F
         Mage::app()->getStore()->setId(0);
         $collection = Mage::getResourceModel('catalog/product_collection')
             ->setStoreId($this->getFeed()->getStore()->getId())
-            ->addAttributeToFilter("status", array("neq" => Mage_Catalog_Model_Product_Status::STATUS_DISABLED))
-            ->addAttributeToFilter("visibility", array("neq" => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE));
+            ->addAttributeToFilter("status", array("eq" => Mage_Catalog_Model_Product_Status::STATUS_ENABLED))
+            //->addAttributeToFilter("visibility", array("neq" => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE))
+        ;
 
         $this->_rule->getConditions()->collectValidatedAttributes($collection);
         return $collection;
