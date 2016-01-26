92a6c222eba3a88da2e80b3961a3e682706c3c30
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2016-01-26 16:51:10 +0100

    re#11761 run cron with args

--- shell/feedexport.sh
+++ shell/feedexport.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
+#
+# USAGE EXAMPLE
+# sh shell/feedexport.sh --cron 1
+#
 # location of the php binary
+CRONSCRIPT=feedexport.php
+
+ARG1=""
 if [ ! "$1" = "" ] ; then
-    CRONSCRIPT=$1
-else
-    CRONSCRIPT=feedexport.php
+	ARG1=" $1"
 fi
 
-MODE=""
+ARG2=""
 if [ ! "$2" = "" ] ; then
-	MODE=" $2"
+	ARG2=" $2"
+fi
+
+ARG3=""
+if [ ! "$3" = "" ] ; then
+	ARG3=" $3"
 fi
 
 PHP_BIN=`which php`
@@ -18,11 +28,11 @@ INSTALLDIR=`echo $0 | sed 's/feedexport\.sh//g'`
 
 #	prepend the intallation path if not given an absolute path
 if [ "$INSTALLDIR" != "" -a "`expr index $CRONSCRIPT /`" != "1" ];then
-    if ! ps auxwww | grep "$INSTALLDIR$CRONSCRIPT$MODE" | grep -v grep 1>/dev/null 2>/dev/null ; then
-    	$PHP_BIN $INSTALLDIR$CRONSCRIPT$MODE &
+    if ! ps auxwww | grep "$INSTALLDIR$CRONSCRIPT$ARG1$ARG2$ARG3" | grep -v grep 1>/dev/null 2>/dev/null ; then
+    	$PHP_BIN $INSTALLDIR$CRONSCRIPT$ARG1$ARG2$ARG3 &
     fi
 else
-    if  ! ps auxwww | grep "$CRONSCRIPT$MODE" | grep -v grep | grep -v feedexport.sh 1>/dev/null 2>/dev/null ; then
-        $PHP_BIN $CRONSCRIPT$MODE &
+    if  ! ps auxwww | grep "$CRONSCRIPT$ARG1$ARG2$ARG3" | grep -v grep | grep -v feedexport.sh 1>/dev/null 2>/dev/null ; then
+        $PHP_BIN $CRONSCRIPT$ARG1$ARG2$ARG3 &
     fi
 fi
