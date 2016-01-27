f446621e1fc705ce07ecb204c6683ba421a1db40
victory <victoria.sultanovska@convertica.pl>
2016-01-27 12:05:15 +0100

    Refs#1746 Admin: Sprzedawca -> Zarządzanie sprzedażą -> zamówienia do sklepów: żle sortuje

--- app/code/local/Zolago/Po/Block/Adminhtml/Po/Grid.php
+++ app/code/local/Zolago/Po/Block/Adminhtml/Po/Grid.php
@@ -1,7 +1,7 @@
 <?php
 class Zolago_Po_Block_Adminhtml_Po_Grid extends Unirgy_DropshipPo_Block_Adminhtml_Po_Grid
 {
-	
+
     protected function _prepareColumns()
     {
         $this->addColumnAfter('default_pos_name', array(
@@ -13,5 +13,31 @@ class Zolago_Po_Block_Adminhtml_Po_Grid extends Unirgy_DropshipPo_Block_Adminhtm
         return parent::_prepareColumns();
     }
 
+    /**
+     * Sets sorting order by some column
+     *
+     * @param Mage_Adminhtml_Block_Widget_Grid_Column $column
+     * @return Mage_Adminhtml_Block_Widget_Grid
+     */
+    protected function _setCollectionOrder($column)
+    {
+        $collection = $this->getCollection();
+
+        if ($collection) {
+            $columnIndex = $column->getFilterIndex() ?
+                $column->getFilterIndex() : $column->getIndex();
+
+            if ($columnIndex = "udropship_vendor") {
+                $collection->getSelect()->join(
+                    array("vendors" => $collection->getTable('udropship/vendor')), //$name
+                    "main_table.udropship_vendor=vendors.vendor_id", //$cond
+                    array("vendor_name" => "vendor_name")//$cols = '*'
+                );
+                $columnIndex = "vendor_name";
+            }
+            $collection->setOrder($columnIndex, strtoupper($column->getDir()));
+        }
+        return $this;
+    }
 
 }
