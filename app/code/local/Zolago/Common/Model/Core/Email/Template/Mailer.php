a34cfa8b1d9e6d169cb32dcaa0dcd56ffeb74218
Adam Wilk <adam.wilk@convertica.pl>
2016-02-03 14:02:13 +0100

    re#1779 re#1781 email logo fixes, tnf email template

--- app/code/local/Zolago/Common/Model/Core/Email/Template/Mailer.php
+++ app/code/local/Zolago/Common/Model/Core/Email/Template/Mailer.php
@@ -9,9 +9,14 @@ class Zolago_Common_Model_Core_Email_Template_Mailer extends Mage_Core_Model_Ema
 	public function setTemplateParams(array $templateParams = array()) {
 		// Append logo if needed
 		if(isset($templateParams['use_attachments']) || isset($templateParams['_ATTACHMENTS'])){
+			/** @var Zolago_Common_Helper_Data $_hlp */
+			$_hlp = Mage::helper("zolagocommon");
+			$filename = $_hlp->getDesignFileByStore($this->getLogoFile(), $this->getStoreId());
+
+			Mage::log($this->getLogoFile(),null,'superlog.log');
+
 			$templateParams['_ATTACHMENTS'][] = array(
-				"filename"		=> Mage::helper("zolagocommon")->getDesignFileByStore(
-					$this->getLogoFile(), $this->getStoreId()),
+				"filename"		=> $this->getLogoFile(),
 				"id"			=> "logo.png",
 				"disposition"	=> "inline"
 			);
@@ -23,7 +28,10 @@ class Zolago_Common_Model_Core_Email_Template_Mailer extends Mage_Core_Model_Ema
 	 * @return string
 	 */
 	public function getLogoFile() {
-		return "images/logo_email.png";
+		$fileName = Mage::getStoreConfig(Mage_Core_Model_Email_Template::XML_PATH_DESIGN_EMAIL_LOGO,$this->getStoreId());
+		$uploadDir = Mage_Adminhtml_Model_System_Config_Backend_Email_Logo::UPLOAD_DIR;
+		$fullFileName = Mage::getBaseDir('media') . DS . $uploadDir . DS . $fileName;
+		return $fullFileName;
 	}
 	
     /**
