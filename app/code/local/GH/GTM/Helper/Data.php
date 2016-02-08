1dbe5cc4e940bbafe726ece3dc26c02609c87ecc
Adam Wilk <adam.wilk@convertica.pl>
2016-02-08 15:57:36 +0100

    re#1791 re#1799 change dataLayer visitor_data to ajax with event visitorDataReady

--- app/code/local/GH/GTM/Helper/Data.php
+++ app/code/local/GH/GTM/Helper/Data.php
@@ -5,4 +5,52 @@
  */
 class GH_GTM_Helper_Data extends Shopgo_GTM_Helper_Data {
 
+	public function getVisitorData(){
+		$data = array();
+		/** @var Zolago_Customer_Model_Session $customerSession */
+		$customerSession = Mage::getSingleton('customer/session');
+
+		// visitorHasSubscribed - by default 'no'
+		$data['visitorHasSubscribed'] = 'no';
+
+		// visitorId, visitorHasAccount
+		$customerId = $customerSession->getCustomerId();
+		if($customerId) {
+			$data['visitorId'] = (string)$customerId;
+			$data['visitorHasAccount'] = 'yes';
+
+			//visitorHasSubscribed
+			$resource = Mage::getSingleton('core/resource');
+			$readConnection = $resource->getConnection('core_read');
+
+			$query =
+				'SELECT `subscriber_status` FROM `' .
+				$resource->getTableName('newsletter/subscriber') .
+				'` WHERE `customer_id` = ' . $customerId;
+
+			$result = $readConnection->fetchAll($query);
+			if(count($result)) {
+				$newsletterStatus = current(current($result));
+				switch($newsletterStatus) {
+					case Zolago_Newsletter_Model_Subscriber::STATUS_SUBSCRIBED:
+						$data['visitorHasSubscribed'] = 'yes';
+						break;
+					case Zolago_Newsletter_Model_Subscriber::STATUS_UNSUBSCRIBED:
+						$data['visitorHasSubscribed'] = 'unsubscribed';
+						break;
+				}
+			}
+		} else {
+			$data['visitorHasAccount'] = 'no';
+		}
+
+		//visitorLogged
+		$data['visitorLogged'] = $customerSession->isLoggedIn() ? 'yes' : 'no';
+
+
+		$data['event'] = 'visitorDataReady';
+
+		return $data;
+	}
+
 }
