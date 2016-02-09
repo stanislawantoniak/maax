06db44ebcd71512ec08c1780c9a0b517cfbc06c7
victory <victoria.sultanovska@convertica.pl>
2016-02-09 09:12:40 +0100

    TEST

--- app/code/local/Mirasvit/FeedExport/Model/Feed/Generator/Action/Iterator/Entity.php
+++ app/code/local/Mirasvit/FeedExport/Model/Feed/Generator/Action/Iterator/Entity.php
@@ -35,7 +35,6 @@ class Mirasvit_FeedExport_Model_Feed_Generator_Action_Iterator_Entity
             $collection = Mage::getModel('catalog/product')->getCollection()
                 ->joinField('qty', 'cataloginventory/stock_item', 'qty',
                     'product_id=entity_id', '{{table}}.stock_id=1', 'left')
-                ->addFieldToFilter('sku', "9-T0A0QLN2N")
                 ->addStoreFilter();
 
             if (count($this->getFeed()->getRuleIds()) || Mage::app()->getRequest()->getParam('skip')) {
