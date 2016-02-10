60703cfe9e3f9d7fcd61c38fc3ab769ceeae4ffe
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2016-02-10 14:37:08 +0100

    modele i resoruce refs #1437

---
+++ app/code/local/GH/Marketing/Model/Marketing/Budget.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Marketing cost
+ */
+class GH_Marketing_Model_Marketing_Budget extends Mage_Core_Model_Abstract {
+    
+    protected function _construct()
+    {
+        $this->_init("ghmarketing/marketing_budget");
+    }
+
+}
