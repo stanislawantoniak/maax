2b07c241bc795408f04a93b9648726e99d55b634
victory <victoria.sultanovska@convertica.pl>
2016-02-11 09:23:15 +0100

    Refs#1808

--- app/code/local/Zolago/Sizetable/sql/zolagosizetable_setup/upgrade-0.0.6-0.0.7.php
+++ app/code/local/Zolago/Sizetable/sql/zolagosizetable_setup/upgrade-0.0.6-0.0.7.php
@@ -4,35 +4,57 @@ $installer = $this;
 
 $installer->startSetup();
 
-$defaultCollection = Mage::getModel("zolagosizetable/sizetable")->getCollection();
-foreach ($defaultCollection as $defaultCollectionItem) {
+$resource = Mage::getSingleton('core/resource');
+$readConnection = $resource->getConnection('core_read');
+$writeConnection = $resource->getConnection('core_write');
 
-    $resave = false;
-    if (!is_serialized($defaultCollectionItem->getDefaultValue())) {
-        $defaultCollectionItem->setData("default_value", serialize(array("C" => $defaultCollectionItem->getDefaultValue())));
-    }
-    $scopes = Mage::getModel("zolagosizetable/sizetable")
-        ->load($defaultCollectionItem->getId())
-        ->getScopes();
+$sizeTableTable = $resource->getTableName("zolagosizetable/sizetable");
 
-    $sizetableOfItem = $scopes->getData("sizetable");
-    $postData = array();
-    if (!empty($sizetableOfItem)) {
-        foreach ($sizetableOfItem as $storeId => $sizetableItem) {
-            if (!is_serialized($sizetableItem)) {
-                $postData[$storeId]["C"] = $sizetableItem;
-            }
+//Default values
+$query = "SELECT * FROM {$sizeTableTable};";
+$results = $readConnection->fetchAll($query);
+
+if (!empty($results)) {
+    $toUpdate = array();
+    foreach ($results as $result) {
+        if (!is_serialized($result["default_value"])) {
+            $toUpdate[] = $result;
         }
     }
-    $defaultCollectionItem->setPostData($postData);
 
-    //Stores
-    try {
-        $defaultCollectionItem->save();
-    } catch (Exception $e) {
-        Mage::logException($e);
+    if (!empty($toUpdate)) {
+        foreach ($toUpdate as $toUpdateItem) {
+            $defaultValue = serialize(array("C" => $toUpdateItem["default_value"]));
+            $queryUpdate = "UPDATE {$sizeTableTable} SET default_value = '{$defaultValue}' WHERE sizetable_id = " . (int)$toUpdateItem["sizetable_id"];
+            $writeConnection->query($queryUpdate);
+        }
     }
 }
+unset($toUpdate, $result, $toUpdateItem);
+
+
+//Scope values
+$sizeTableScopeTable = $resource->getTableName("zolagosizetable/sizetable_scope");
+$query = "SELECT * FROM {$sizeTableScopeTable};";
+$results = $readConnection->fetchAll($query);
+
+if (!empty($results)) {
+    $toUpdate = array();
+    foreach ($results as $result) {
+        if (!is_serialized($result["value"])) {
+            $toUpdate[] = $result;
+        }
+    }
+
+    if (!empty($toUpdate)) {
+        foreach ($toUpdate as $toUpdateItem) {
+            $value = serialize(array("C" => $toUpdateItem["value"]));
+            $queryUpdate = "UPDATE {$sizeTableScopeTable} SET value = '{$value}' WHERE sizetable_id = " . (int)$toUpdateItem["sizetable_id"]. " AND store_id=". (int)$toUpdateItem["store_id"];
+            $writeConnection->query($queryUpdate);
+        }
+    }
+}
+
 
 
 function is_serialized($str)
