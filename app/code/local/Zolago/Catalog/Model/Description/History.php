d18b778f2b15b1c59d0c3452f05f69de74c0147e
victory <victoria.sultanovska@convertica.pl>
2016-02-22 17:08:46 +0100

    Refs#1844 refs#1847

---
+++ app/code/local/Zolago/Catalog/Model/Description/History.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class Zolago_Catalog_Model_Description_History
+ */
+class Zolago_Catalog_Model_Description_History extends Mage_Core_Model_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('zolagocatalog/product_description_history');
+    }
+
+}
---
+++ app/code/local/Zolago/Catalog/Model/Resource/Description/History/Collection.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class Zolago_Catalog_Model_Resource_Description_History_Collection
+ */
+class Zolago_Catalog_Model_Resource_Description_History_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('zolagocatalog/product_description_history');
+    }
+
+}
