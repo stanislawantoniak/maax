a13be844efa5daccc6a09e8a96078163b714732c
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2016-02-24 12:56:48 +0100

    re#777 cart ajax loading: performance tuning - part 4

--- app/code/local/Orba/Common/Model/Observer.php
+++ app/code/local/Orba/Common/Model/Observer.php
@@ -13,4 +13,10 @@ class Orba_Common_Model_Observer {
 		$cacheHelper = Mage::helper('orbacommon/ajax_customer_cache');
 		$cacheHelper->removeCacheCart();
 	}
+
+	public function invalidateSearchCustomerAjaxCache(Varien_Event_Observer $observer) {
+		/** @var Orba_Common_Helper_Ajax_Customer_Cache $cacheHelper */
+		$cacheHelper = Mage::helper('orbacommon/ajax_customer_cache');
+		$cacheHelper->removeCacheSearch();
+	}
 }
