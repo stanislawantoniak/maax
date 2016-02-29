6833a730297338e651e79984b56a0ba2e398d4cb
Adam Wilk <adam.wilk@convertica.pl>
2016-02-29 16:05:19 +0100

    re#1835 almost fixed, showmore button on mainpage left to do

--- app/code/local/Zolago/Modago/Block/Brands/Vendor.php
+++ app/code/local/Zolago/Modago/Block/Brands/Vendor.php
@@ -18,12 +18,55 @@ class Zolago_Modago_Block_Brands_Vendor extends Mage_Core_Block_Template
 
             $collection->addFieldToFilter('vendor_id', array('neq' => $localVendorId));
 			// Load serialized data
+			/** @var Zolago_Dropship_Helper_Data $helper */
+			$helper = Mage::helper('udropship');
 			foreach($collection as $vendor){
-				Mage::helper('udropship')->loadCustomData($vendor);
+				$helper->loadCustomData($vendor);
 			}
+			
+			$this->addDummyVendorsToCollection($collection);
+			
 			$this->setData("vendor_collection", $collection);
 		}
 		return $this->getData("vendor_collection");
+	}
+
+	private function addDummyVendorsToCollection(&$collection) {
+		$collectionSize = $collection->getSize();
+		$dummyVendorsCount = array();
+		$dummyVendorsCount[8] = 8 - ($collectionSize % 8); //8 = maximum number in row
+		$dummyVendorsCount[7] = 7 - ($collectionSize % 7);
+		$dummyVendorsCount[6] = 6 - ($collectionSize % 6);
+		$dummyVendorsCount[5] = 5 - ($collectionSize % 5);
+		$dummyVendorsCount[4] = 4 - ($collectionSize % 4);
+		$dummyVendorsCount[3] = 3 - ($collectionSize % 3);
+		$dummyVendorsCount[2] = $collectionSize % 2;
+
+		$dummyVendor = new Zolago_Dropship_Model_Vendor();
+		$dummyVendor
+			->setDummy(true)
+			->setVendorResizedLogoUrl($this->getSkinUrl('images/brand_comming.png'))
+			->setVendorName('Kolejne już wkrótce');
+
+		$maxDummies = max($dummyVendorsCount);
+
+		for ($i = 1; $i <= $maxDummies; $i++) {
+			$classesToAdd = "brands-dummy";
+
+			$classesToAdd .= $dummyVendorsCount[2] >= $i ? ' visible-xxs' : '';
+			$classesToAdd .= $dummyVendorsCount[3] >= $i ? ' visible-xs' : '';
+			$classesToAdd .= $dummyVendorsCount[4] >= $i ? ' visible-xss-landscape' : '';
+			$classesToAdd .= $dummyVendorsCount[5] >= $i ? ' visible-xs-landscape' : '';
+			$classesToAdd .= $dummyVendorsCount[6] >= $i ? ' visible-md visible-sm' : '';
+			$classesToAdd .= $dummyVendorsCount[7] >= $i ? ' visible-sm-landscape' : '';
+			$classesToAdd .= $dummyVendorsCount[8] >= $i ? ' visible-lg' : '';
+
+			$clonedDummy = clone($dummyVendor);
+			$clonedDummy->setClasses($classesToAdd);
+
+			$collection->addItem($clonedDummy);
+		}
+
 	}
 	
 	/**
