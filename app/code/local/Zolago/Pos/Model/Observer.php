ffccb0023d9c1d185aef47d0699d900a0ca387ab
victory <victoria.sultanovska@convertica.pl>
2016-03-03 10:40:13 +0100

    Refs#461 refs#1865 shell

--- app/code/local/Zolago/Pos/Model/Observer.php
+++ app/code/local/Zolago/Pos/Model/Observer.php
@@ -61,7 +61,7 @@ class Zolago_Pos_Model_Observer {
 
 	}
 
-	protected function getVendorPOSes($vendorId){
+	public function getVendorPOSes($vendorId){
 		$vendor = Mage::getModel("udropship/vendor")->load($vendorId);
 		/* @var $vendor Unirgy_Dropship_Model_Vendor */
 		$collection = Mage::getResourceModel("zolagopos/pos_collection");
@@ -72,7 +72,7 @@ class Zolago_Pos_Model_Observer {
 		return $collection;
 	}
 
-    public function setAppropriatePoPos(){
+    public static function setAppropriatePoPos(){
 		//1. Get POs for recalculate  POSes
 		/* @var $vendor Zolago_Po_Model_Po */
 		$collection = Mage::getModel("zolagopo/po")->getCollection();
@@ -147,7 +147,7 @@ class Zolago_Pos_Model_Observer {
 		$poses = array();
 
 		foreach ($data as $vendorId => $dataPerPO) {
-			$vendorPOSes = $this->getVendorPOSes($vendorId);
+			$vendorPOSes = self::getVendorPOSes($vendorId);
 
 			//Hm Vendor doesn't have POSes!!!
 			if ($vendorPOSes->count() == 0)
