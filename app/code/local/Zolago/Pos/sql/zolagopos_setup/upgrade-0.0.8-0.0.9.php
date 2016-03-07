b2631252db8f5a3839b315311caa5b22325cfd86
victory <victoria.sultanovska@convertica.pl>
2016-03-07 11:34:45 +0100

    Refs#1881 fix UNIQUE

--- app/code/local/Zolago/Pos/sql/zolagopos_setup/upgrade-0.0.8-0.0.9.php
+++ app/code/local/Zolago/Pos/sql/zolagopos_setup/upgrade-0.0.8-0.0.9.php
@@ -19,8 +19,11 @@ $table = $installer->getConnection()
     ->addColumn("vendor_id", Varien_Db_Ddl_Table::TYPE_INTEGER, null, array('nullable' => false))
     ->addColumn("website_id", Varien_Db_Ddl_Table::TYPE_INTEGER, null, array('nullable' => false))
     // Indexes
-    ->addIndex($installer->getIdxName('zolagopos/pos_vendor_website', array('pos_id', 'vendor_id', 'website_id')),
-        array('pos_id', 'vendor_id', 'website_id'), Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE)
+    ->addIndex($installer->getIdxName('zolagopos/pos_vendor_website', array('pos_id','vendor_id','website_id'),
+        Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE),
+        array('pos_id','vendor_id','website_id'),array('type'=>Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE))
+
+
     ->addIndex($installer->getIdxName('zolagopos/pos_vendor_website', array('pos_id')),
         array('pos_id'))
     ->addIndex($installer->getIdxName('zolagopos/pos_vendor_website', array('vendor_id')),
