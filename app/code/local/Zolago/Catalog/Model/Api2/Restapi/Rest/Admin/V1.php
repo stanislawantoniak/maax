d3df4653bab7ce0c1e8ba29edd6a471aad9246c8
victory <victoria.sultanovska@convertica.pl>
2016-03-09 14:36:42 +0100

    TEST

--- app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
+++ app/code/local/Zolago/Catalog/Model/Api2/Restapi/Rest/Admin/V1.php
@@ -169,7 +169,7 @@ class Zolago_Catalog_Model_Api2_Restapi_Rest_Admin_V1
             $s = Zolago_Catalog_Helper_Stock::getAvailableStock($stockData, $merchant); //return array("sku" => qty, ...)
             $availableStockByMerchant = $s + $availableStockByMerchant;
         }
-        Mage::log($availableStockByMerchant, null, "XXX.log");
+
         if (empty($availableStockByMerchant))
             return;
         
