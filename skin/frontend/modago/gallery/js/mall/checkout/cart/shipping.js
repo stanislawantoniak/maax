3518c35858196cb9c149e35445e921f27288ca13
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-03-21 11:53:50 +0100

    save

--- skin/frontend/modago/gallery/js/mall/checkout/cart/shipping.js
+++ skin/frontend/modago/gallery/js/mall/checkout/cart/shipping.js
@@ -1,39 +1,23 @@
 jQuery(document).ready(function () {
 
-    jQuery(".select-inpost-code").click(function (e) {
-        e.preventDefault();
-        var selectInPostCodeLink = jQuery(this);
-        var code = selectInPostCodeLink.text(),
-                inPostCodeInput = '<input type="hidden" name="inpost_code" value="' + code + '"  />',
-                selectedMethodData = [];
-        jQuery("#cart-shipping-methods .shipping-collect-inpost").html(inPostCodeInput);
-
-        jQuery("#select_inpost_point").modal("hide");
-        jQuery("input[name=_shipping_method][data-carrier-inpost=1]").click();
-        var inPostRadio = jQuery("input[name=_shipping_method][data-carrier-inpost=1]");
-
-        //jQuery(".inpost-code-holder").html(code);
-        selectedMethodData["logo"] = '<img  src="' + Mall.reg.get("inpost_logo") + '" height="52px"/>';
-        selectedMethodData["name"] = inPostRadio.attr("data-carrier-name");
-        selectedMethodData["method"] = inPostRadio.attr("data-carrier-method");
-        selectedMethodData["additional"] = selectInPostCodeLink.attr("data-inpost-address");
-
-        appendSelectedCartShipping(selectedMethodData);
-        toggleCartShippingSelectors();
-    });
-    jQuery("input[name=_shipping_method]").click(function (e) {
-        setShippingMethod(this);
+    jQuery("[data-select-shipping-method-trigger=1]").click(function (e) {
+        setShippingMethod();
+        if (jQuery(e.target).is("a")) {
+            e.preventDefault();
+            jQuery("#select_inpost_point").modal("hide");
+        }
     });
 
+
+    jQuery("[data-select-shipping-method-trigger=0]").click(function (e) {
+        jQuery("#select_inpost_point").modal("show");
+    });
 
 
     jQuery("#change-shipping-type").click(function () {
-        jQuery(".shipping-method-selector").slideToggle();
-        jQuery(".shipping-method-selected").slideToggle();
+        jQuery(".shipping-method-selector").slideDown();
+        jQuery(".shipping-method-selected").slideUp();
     });
-
-
-
 
 
     jQuery("#cart-buy").on('click', function (e) {
@@ -66,49 +50,44 @@ function appendSelectedCartShipping(selectedMethodData) {
     shippingMethodSelectedContainer.find('[data-item="logo"]').html(selectedMethodData["logo"]);
 
     shippingMethodSelectedContainer.find('[data-item="additional"]').html(selectedMethodData["additional"]);
-}
-function toggleCartShippingSelectors() {
-    jQuery(".shipping-method-selector").slideToggle();
-    jQuery(".shipping-method-selected").slideToggle();
+
+
+    jQuery(".shipping-method-selector").slideUp();
+    jQuery(".shipping-method-selected").slideDown();
 }
 
-function setShippingMethod(methodRadio) {
-    //console.log(jQuery(methodRadio).attr("data-carrier-inpost"));
+
+function setShippingMethod(target) {
     var selectedMethodData = [];
-    if (jQuery(methodRadio).attr("data-carrier-inpost") == 1) {
-        //Open modal to select paczkomat
-        console.log("Open modal");
-        jQuery("#select_inpost_point").modal("toggle");
-    } else {
-        jQuery("#cart-shipping-methods .shipping-collect-inpost").html("");
-
-        selectedMethodData["logo"] = '<figure class="logo-courier pull-right"><div class="shipment-icon"><i class="fa fa-truck fa-3x"></i></div></figure>';
-        selectedMethodData["name"] = jQuery(methodRadio).attr("data-carrier-name");
-        selectedMethodData["method"] = jQuery(methodRadio).attr("data-carrier-method");
-        selectedMethodData["additional"] = "";
-
-    }
-
 
     var vendors = Mall.reg.get("vendors"),
-            content = jQuery("#cart-shipping-methods");
+        content = jQuery("#cart-shipping-methods");
 
-    var shipping = content.find("input[name=_shipping_method]:checked").val();
+    var methodRadio = content.find("input[name=_shipping_method]:checked");
+    var shipping = methodRadio.val();
+    console.log(shipping);
+
+    console.log(jQuery(methodRadio).attr("data-carrier-name"));
+    console.log(jQuery(methodRadio).attr("data-carrier-method"));
+
+    selectedMethodData["logo"] = jQuery(methodRadio).attr("data-carrier-logo");
+    selectedMethodData["name"] = jQuery(methodRadio).attr("data-carrier-name");
+    selectedMethodData["method"] = jQuery(methodRadio).attr("data-carrier-method");
+    selectedMethodData["additional"] = jQuery(target).attr("data-carrier-additional");
+
+    var pointCode = (jQuery(target).attr("data-carrier-pointcode") !== "undefined") ? jQuery(target).attr("data-carrier-pointcode") : "";
 
     if (jQuery.type(shipping) !== "undefined") {
         var inputs = '';
         jQuery.each(vendors, function (i, vendor) {
             inputs += '<input type="hidden" name="shipping_method[' + vendor + ']" value="' + shipping + '" required="required" />';
         });
+        inputs += '<input type="hidden" name="shipping_point_code" value="' + pointCode + '"  />';
+
         content.find("form .shipping-collect").html(inputs);
     }
 
-    if (jQuery(methodRadio).attr("data-carrier-inpost") == 0) {
-        jQuery("#cart-shipping-methods .shipping-collect-inpost").html("");
-        appendSelectedCartShipping(selectedMethodData);
-        toggleCartShippingSelectors();
-    }
-
+    appendSelectedCartShipping(selectedMethodData);
 }
 
 
