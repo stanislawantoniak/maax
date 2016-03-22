1c8bac1193978e9394dbf3214aaacedba5658b18
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-03-22 17:44:27 +0100

    map template

--- app/code/local/Zolago/Modago/Block/Checkout/Cart/Sidebar/Shipping/Map.php
+++ app/code/local/Zolago/Modago/Block/Checkout/Cart/Sidebar/Shipping/Map.php
@@ -6,6 +6,12 @@ class Zolago_Modago_Block_Checkout_Cart_Sidebar_Shipping_Map
 
     public function getBuildingData()
     {
-
+        $lockers = array();
+        $collection = Mage::getModel("ghinpost/locker")->getCollection();
+        $collection->setPageSize(10);
+        foreach ($collection as $locker) {
+            $lockers[$locker->getName()] = $this->getLockerRender($locker);
+        }
+        return $lockers;
     }
 }
