9804ce4b9c8d472a9ff5e45da19cd3b64a8058b3
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2016-04-01 13:36:05 +0200

    notices refs #1881

--- app/code/local/Zolago/Catalog/Helper/Stock.php
+++ app/code/local/Zolago/Catalog/Helper/Stock.php
@@ -92,7 +92,9 @@ class Zolago_Catalog_Helper_Stock extends Mage_Core_Helper_Abstract
                     $data[$sku][$stockId] = $posStockConverter - $minimalStockPOS - $reservedOnPOStock;
 
                     //Allocate Product STOCK by POS
-                    $dataAllocateToPOS[$skuIdAssoc[$sku]][$stockId] = $data[$sku][$stockId];
+                    if (isset($data[$sku][$stockId])) {
+                        $dataAllocateToPOS[$skuIdAssoc[$sku]][$stockId] = $data[$sku][$stockId];
+                    }
 
 
                 }
