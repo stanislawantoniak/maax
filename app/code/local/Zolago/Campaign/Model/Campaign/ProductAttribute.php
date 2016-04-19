808f39f9c23c5d858fa074f99e9764845eac73f8
victory <victoria.sultanovska@convertica.pl>
2016-04-19 12:06:43 +0200

    test

--- app/code/local/Zolago/Campaign/Model/Campaign/ProductAttribute.php
+++ app/code/local/Zolago/Campaign/Model/Campaign/ProductAttribute.php
@@ -542,8 +542,8 @@ class Zolago_Campaign_Model_Campaign_ProductAttribute extends Zolago_Campaign_Mo
                 foreach ($stores[$websiteId] as $store) {
                     $actionModel->updateAttributesPure($productsIds, $attributesData, $store);
 
-                    $col = Zolago_Turpentine_Model_Observer_Ban::collectProductsBeforeBan($productsIds, $store);
-                    Mage::dispatchEvent("zolagocatalog_converter_stock_complete", array("products" => $col));
+                    //$col = Zolago_Turpentine_Model_Observer_Ban::collectProductsBeforeBan($productsIds, $store);
+                    //Mage::dispatchEvent("zolagocatalog_converter_stock_complete", array("products" => $col));
                 }
             }
         }
