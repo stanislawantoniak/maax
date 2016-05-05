66143d30fe112bda77e819a0a2d7b21d4663eb9c
victory <victoria.sultanovska@convertica.pl>
2016-05-05 17:36:55 +0200

    test

--- app/code/local/Zolago/Checkout/Controller/Abstract.php
+++ app/code/local/Zolago/Checkout/Controller/Abstract.php
@@ -331,8 +331,8 @@ abstract class Zolago_Checkout_Controller_Abstract
 		}
 
 
-		$quote = $onepage->getQuote();
-		$address = $quote->getShippingAddress();
+
+		$address = Mage::getSingleton('checkout/session')->getQuote()->getShippingAddress();
 		if ($shippingPointCode = $request->getParam("shipping_point_code")) {
 			$address->setInpostLockerName($shippingPointCode);
 			$this->_getCheckoutSession()->setInpostLockerName($shippingPointCode);
