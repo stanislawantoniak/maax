d167687bcc13184a1706af66d4fb9a86f47674c9
victory <victoria.sultanovska@convertica.pl>
2016-05-10 12:56:44 +0200

    Refs#2028 grooming

--- app/code/local/Zolago/Adminhtml/controllers/Sales/Order/ChangeController.php
+++ app/code/local/Zolago/Adminhtml/controllers/Sales/Order/ChangeController.php
@@ -4,24 +4,33 @@ class Zolago_Adminhtml_Sales_Order_ChangeController extends Mage_Adminhtml_Contr
 {
     public function changeToCodAction()
     {
-        $orderId = $this->getRequest()->getParam("order_id", null);
-        if ($orderId) {
-            $paymentModel = Mage::getModel("sales/order_payment");
-            $paymentModel->load($orderId, "parent_id");
-
-            if ($paymentModel->getId() !== NULL) {
-                $paymentModel->setData('method', 'cashondelivery');
-                $paymentModel->save();
-
-                $this->_getSession()->addSuccess($this->__('Payment has been successfully changes to COD'));
-                $this->_redirectReferer();
-            } else {
-                $this->_getSession()->addError($this->__('Payment for order not found'));
-                $this->_redirectReferer();
-            }
-
+        $orderId = $this->getRequest()->getParam("order_id", NULL);
+        if ($orderId== NULL) {
+            $this->_getSession()->addError($this->__('Order not found'));
+            $this->_redirectReferer();
+            return;
         }
+        $paymentModel = Mage::getModel("sales/order_payment");
+        $paymentModel->load($orderId, "parent_id");
+
+        if ($paymentModel->getId() == NULL) {
+            $this->_getSession()->addError($this->__('Payment for order not found'));
+            $this->_redirectReferer();
+        }
+
+        try {
+            $paymentModel->setData('method', 'cashondelivery');
+            $paymentModel->save();
+            $this->_getSession()->addSuccess($this->__('Payment has been successfully changes to COD'));
+        } catch (Mage_Core_Exception $e) {
+            $this->_getSession()->addError($e->getMessage());
+        } catch (Exception $e) {
+            $this->_getSession()->addError($this->__('An error occurred on change to COD action.'));
+            Mage::logException($e);
+        }
+
         $this->_redirectReferer();
+
 
     }
 }
