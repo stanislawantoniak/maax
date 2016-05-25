b1752964728247e5dbab5fcc52370a67ad40a671
victory <victoria.sultanovska@convertica.pl>
2016-05-25 17:59:07 +0200

    customer account

--- app/code/local/Wf/Wf/data/wfwf_setup/data-upgrade-0.1.0-0.1.1.php
+++ app/code/local/Wf/Wf/data/wfwf_setup/data-upgrade-0.1.0-0.1.1.php
@@ -10,8 +10,8 @@ $cmsNavigationBlocks = array(
                     <div class="top-bar-left">
                         <div class="top-bar-left-content">
                             <ul class="links">
-                                <li><a href="#">DOSTAWA GRATIS 24h</a></li>
-                                <li><a href="#">DARMOWY ZWROT 30 dni</a></li>
+                                <li><a href="{{store direct_url='' _no_vendor='1'}}">DOSTAWA GRATIS 24h</a></li>
+                                <li><a href="{{store direct_url='' _no_vendor='1'}}">DARMOWY ZWROT 30 dni</a></li>
                             </ul>
                         </div>
                     </div>
@@ -19,10 +19,10 @@ $cmsNavigationBlocks = array(
                         <div id="nav_menu-4" class="topbar-widget widget_nav_menu">
                             <div class="menu-right-top-menu-container">
                                 <ul id="menu-right-top-menu" class="menu">
-                                    <li><a href="/">Znajdź sklep</a></li>
-                                    <li><a href="/">Kontakt</a></li>
-                                    <li><a href="/">Ulubione</a></li>
-                                    <li><a href="/">Twoje Konto</a></li>
+                                    <li><a href="{{store direct_url='storesmap' _no_vendor='1'}}">Znajdź sklep</a></li>
+                                    <li><a href="{{store direct_url='help/contact' _no_vendor='1'}}">Kontakt</a></li>
+                                    <li><a href="{{store direct_url='wishlist' _no_vendor='1'}}">Ulubione</a></li>
+                                    <li><a href="{{store direct_url='customer/account' _no_vendor='1'}}">Twoje Konto</a></li>
                                 </ul>
                             </div>
                         </div>
