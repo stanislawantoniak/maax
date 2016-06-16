b5d5ce2e48c256de57b10afd786cef45862e4777
www-data <www-data@lorante.com>
2016-06-16 15:50:04 +0200

    porawki js do lookbooka

--- skin/frontend/modago/wf/js/mall.js
+++ skin/frontend/modago/wf/js/mall.js
@@ -1947,19 +1947,19 @@ jQuery(document).ready(function() {
     if (jQuery('.messages i').length) {
         jQuery('#content').css('margin-top', '0px');
     }
-
-    jQuery(".no-size").tooltip({
-        template: '<div class="tooltip top" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner" style="color: #ea687e"></div></div>'
-    });
-    jQuery("#add-to-cart").tooltip({
-        template: '<div class="tooltip top" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner" style="color: #ea687e"></div></div>'
-    });
-    jQuery("#add-to-cart").on('mouseover', function() {
-        if(Mall._current_superattribute != null) {
-            jQuery("#add-to-cart").tooltip('destroy');
-        }
-    });
-
+    if (jQuery(".no-size").tooltip) {
+        jQuery(".no-size").tooltip({
+            template: '<div class="tooltip top" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner" style="color: #ea687e"></div></div>'
+        });
+        jQuery("#add-to-cart").tooltip({
+            template: '<div class="tooltip top" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner" style="color: #ea687e"></div></div>'
+        });
+        jQuery("#add-to-cart").on('mouseover', function() {
+            if(Mall._current_superattribute != null) {
+                jQuery("#add-to-cart").tooltip('destroy');
+            }
+        });
+    }
 
     jQuery('#popup-after-add-to-cart').on('shown.bs.modal', function (e) {
         var backdrop =  jQuery('#sb-site').find('.modal-backdrop');
