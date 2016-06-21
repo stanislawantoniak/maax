78992269889968ba0faa055739476310e659cc08
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-06-21 09:03:01 +0200

    Refs#2070 product attribures

---
+++ app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.1.4-0.1.5.php
@@ -0,0 +1,115 @@
+<?php
+/** @var Zolago_Catalog_Model_Resource_Setup $installer */
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->addAttribute('catalog_product', 'external_price_a', array(
+	'group'             => 'Prices',
+	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+	'backend'           => '',
+	'frontend'          => '',
+	'label'             => 'Converter price A',
+	'input'             => 'text',
+	'class'             => '',
+	'source'            => '',
+	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+	'visible'           => true,
+	'required'          => false,
+	'user_defined'      => true,
+	'searchable'        => false,
+	'filterable'        => false,
+	'comparable'        => false,
+	'visible_on_front'  => false,
+	'unique'            => false,
+	'is_configurable'   => false
+));
+
+$installer->addAttribute('catalog_product', 'external_price_b', array(
+	'group'             => 'Prices',
+	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+	'backend'           => '',
+	'frontend'          => '',
+	'label'             => 'Converter price B',
+	'input'             => 'text',
+	'class'             => '',
+	'source'            => '',
+	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+	'visible'           => true,
+	'required'          => false,
+	'user_defined'      => true,
+	'searchable'        => false,
+	'filterable'        => false,
+	'comparable'        => false,
+	'visible_on_front'  => false,
+	'unique'            => false,
+	'is_configurable'   => false
+));
+
+$installer->addAttribute('catalog_product', 'external_price_c', array(
+	'group'             => 'Prices',
+	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+	'backend'           => '',
+	'frontend'          => '',
+	'label'             => 'Converter price C',
+	'input'             => 'text',
+	'class'             => '',
+	'source'            => '',
+	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+	'visible'           => true,
+	'required'          => false,
+	'user_defined'      => true,
+	'searchable'        => false,
+	'filterable'        => false,
+	'comparable'        => false,
+	'visible_on_front'  => false,
+	'unique'            => false,
+	'is_configurable'   => false
+));
+$installer->addAttribute('catalog_product', 'external_price_z', array(
+	'group'             => 'Prices',
+	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+	'backend'           => '',
+	'frontend'          => '',
+	'label'             => 'Converter price Z',
+	'input'             => 'text',
+	'class'             => '',
+	'source'            => '',
+	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+	'visible'           => true,
+	'required'          => false,
+	'user_defined'      => true,
+	'searchable'        => false,
+	'filterable'        => false,
+	'comparable'        => false,
+	'visible_on_front'  => false,
+	'unique'            => false,
+	'is_configurable'   => false
+));
+
+$installer->addAttribute('catalog_product', 'external_price_salePriceBefore', array(
+	'group'             => 'Prices',
+	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+	'backend'           => '',
+	'frontend'          => '',
+	'label'             => 'Converter price salePriceBefore',
+	'input'             => 'text',
+	'class'             => '',
+	'source'            => '',
+	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+	'visible'           => true,
+	'required'          => false,
+	'user_defined'      => true,
+	'searchable'        => false,
+	'filterable'        => false,
+	'comparable'        => false,
+	'visible_on_front'  => false,
+	'unique'            => false,
+	'is_configurable'   => false
+));
+
+$installer->endSetup();
+
+
+
+
