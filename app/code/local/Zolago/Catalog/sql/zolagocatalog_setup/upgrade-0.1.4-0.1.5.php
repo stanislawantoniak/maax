f3beb6038b2a83ca63bc0b4148f3aeb5b72871f0
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-06-21 14:19:52 +0200

    chenge setup

--- app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.1.4-0.1.5.php
+++ app/code/local/Zolago/Catalog/sql/zolagocatalog_setup/upgrade-0.1.4-0.1.5.php
@@ -4,109 +4,31 @@ $installer = $this;
 
 $installer->startSetup();
 
-$installer->addAttribute('catalog_product', 'external_price_A', array(
-	'group'             => 'Prices',
-	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-	'backend'           => '',
-	'frontend'          => '',
-	'label'             => 'Converter price A',
-	'input'             => 'text',
-	'class'             => '',
-	'source'            => '',
-	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-	'visible'           => true,
-	'required'          => false,
-	'user_defined'      => true,
-	'searchable'        => false,
-	'filterable'        => false,
-	'comparable'        => false,
-	'visible_on_front'  => false,
-	'unique'            => false,
-	'is_configurable'   => false
-));
+$priceLabels = array("A", "B", "C", "Z", "salePriceBefore");
+foreach ($priceLabels as $priceLabel) {
 
-$installer->addAttribute('catalog_product', 'external_price_B', array(
-	'group'             => 'Prices',
-	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-	'backend'           => '',
-	'frontend'          => '',
-	'label'             => 'Converter price B',
-	'input'             => 'text',
-	'class'             => '',
-	'source'            => '',
-	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-	'visible'           => true,
-	'required'          => false,
-	'user_defined'      => true,
-	'searchable'        => false,
-	'filterable'        => false,
-	'comparable'        => false,
-	'visible_on_front'  => false,
-	'unique'            => false,
-	'is_configurable'   => false
-));
+	$installer->addAttribute('catalog_product', "external_price_{$priceLabel}", array(
+		'group'             => 'Prices',
+		'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+		'backend'           => '',
+		'frontend'          => '',
+		'label'             => "Converter price {$priceLabel}",
+		'input'             => 'text',
+		'class'             => '',
+		'source'            => '',
+		'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+		'visible'           => true,
+		'required'          => false,
+		'user_defined'      => true,
+		'searchable'        => false,
+		'filterable'        => false,
+		'comparable'        => false,
+		'visible_on_front'  => false,
+		'unique'            => false,
+		'is_configurable'   => false
+	));
 
-$installer->addAttribute('catalog_product', 'external_price_C', array(
-	'group'             => 'Prices',
-	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-	'backend'           => '',
-	'frontend'          => '',
-	'label'             => 'Converter price C',
-	'input'             => 'text',
-	'class'             => '',
-	'source'            => '',
-	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-	'visible'           => true,
-	'required'          => false,
-	'user_defined'      => true,
-	'searchable'        => false,
-	'filterable'        => false,
-	'comparable'        => false,
-	'visible_on_front'  => false,
-	'unique'            => false,
-	'is_configurable'   => false
-));
-$installer->addAttribute('catalog_product', 'external_price_Z', array(
-	'group'             => 'Prices',
-	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-	'backend'           => '',
-	'frontend'          => '',
-	'label'             => 'Converter price Z',
-	'input'             => 'text',
-	'class'             => '',
-	'source'            => '',
-	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-	'visible'           => true,
-	'required'          => false,
-	'user_defined'      => true,
-	'searchable'        => false,
-	'filterable'        => false,
-	'comparable'        => false,
-	'visible_on_front'  => false,
-	'unique'            => false,
-	'is_configurable'   => false
-));
-
-$installer->addAttribute('catalog_product', 'external_price_salePriceBefore', array(
-	'group'             => 'Prices',
-	'type'              => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-	'backend'           => '',
-	'frontend'          => '',
-	'label'             => 'Converter price salePriceBefore',
-	'input'             => 'text',
-	'class'             => '',
-	'source'            => '',
-	'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-	'visible'           => true,
-	'required'          => false,
-	'user_defined'      => true,
-	'searchable'        => false,
-	'filterable'        => false,
-	'comparable'        => false,
-	'visible_on_front'  => false,
-	'unique'            => false,
-	'is_configurable'   => false
-));
+}
 
 $installer->endSetup();
 
