dbcfa04a8345cb5d4a3b5e58ee6cdee7ee353c91
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-06-29 16:28:16 +0200

    FOOTER

--- skin/frontend/modago/wf/js/mall.js
+++ skin/frontend/modago/wf/js/mall.js
@@ -1469,7 +1469,7 @@ jQuery(window).resize(function() {
 
 Mall.Footer = {
 	footerId: '#footer',
-	footerMargin: 0,
+	footerMargin: 20,
 	containerId: '#sb-site',
 	init: function() {
 		var _ = this;
