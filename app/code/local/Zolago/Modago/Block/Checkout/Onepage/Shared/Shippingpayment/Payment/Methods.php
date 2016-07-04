30d57db55e6cb501f779560a37b8ebd6ba41639a
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-07-04 17:09:00 +0200

    Refs#1928

--- app/code/local/Zolago/Modago/Block/Checkout/Onepage/Shared/Shippingpayment/Payment/Methods.php
+++ app/code/local/Zolago/Modago/Block/Checkout/Onepage/Shared/Shippingpayment/Payment/Methods.php
@@ -48,14 +48,25 @@ class Zolago_Modago_Block_Checkout_Onepage_Shared_Shippingpayment_Payment_Method
             $store = $quote ? $quote->getStoreId() : null;
             $methods = array();
             foreach ($this->helper('payment')->getStoreMethods($store, $quote) as $method) {
-                if ($this->_canUseMethod($method) && $method->isApplicableToQuote(
-                        $quote,
-                        Mage_Payment_Model_Method_Abstract::CHECK_ZERO_TOTAL
-                    ) && $this->_getIsVisibleInCheckout($method)
+                if (
+                    $this->_canUseMethod($method)
+                    && $method->isApplicableToQuote($quote, Mage_Payment_Model_Method_Abstract::CHECK_ZERO_TOTAL)
+                    && $this->_getIsVisibleInCheckout($method)
+
                 ) {
                     $this->_assignMethod($method);
-                    $methods[] = $method;
+                    if ($method->getCode() !== "cashondelivery"){
+                        $methods[] = $method;
+                    } else {
+                        if ($codTitle = $this->_getIsCODCompatibleWithShippingMethod($method)) {
+                            $method->setData("cod_shipping_dependent_title", $codTitle);
+                            $methods[] = $method;
+                        }
+
+                    }
+
                 }
+
             }
             $this->setData('methods', $methods);
         }
@@ -65,7 +76,6 @@ class Zolago_Modago_Block_Checkout_Onepage_Shared_Shippingpayment_Payment_Method
 
     protected function _canUseMethod($method)
     {
-        Mage::log($method->getData(), null, "payment.log");
         return $method && $method->canUseCheckout() && parent::_canUseMethod($method);
     }
 	
@@ -104,6 +114,39 @@ class Zolago_Modago_Block_Checkout_Onepage_Shared_Shippingpayment_Payment_Method
         return (bool)$_method->getConfigData("visible");
     }
 
+    /**
+     * @param $_method
+     * @return bool
+     */
+    protected function _getIsCODCompatibleWithShippingMethod()
+    {
+
+        $selectedShipping = Mage::helper("zolagocheckout")->getSelectedShipping();
+        $methods = array_values($selectedShipping["methods"]);
+
+        $udropshipMethod = array_shift($methods);
+
+        $info = $this->getOmniChannelMethodInfoByMethod($udropshipMethod);
+        $carrier = $info->getDeliveryCode();
+
+        $storeId = Mage::app()->getStore()->getId();
+        $path = 'carriers/' . $carrier . '/' . "cod_allowed";
+        $codAllowed = (bool)Mage::getStoreConfig($path, $storeId);
+        if (!$codAllowed)
+            return false;
+
+        $pathTitle = 'carriers/' . $carrier . '/' . "cod_checkout_title";
+        $codCheckoutTitle = (string)Mage::getStoreConfig($pathTitle, $storeId);
+
+        if (!empty($codCheckoutTitle))
+            return $codCheckoutTitle;
+
+        $pathTitleDefault = 'payment/cashondelivery/title';
+        $codCheckoutTitleDefault = (string)Mage::getStoreConfig($pathTitleDefault, $storeId);
+
+        return $codCheckoutTitleDefault;
+    }
+
 
     /**
      * @param $code
@@ -130,6 +173,48 @@ class Zolago_Modago_Block_Checkout_Onepage_Shared_Shippingpayment_Payment_Method
                 break;
         }
         return $icon;
+    }
+
+
+
+    /**
+     * @return Varien_Object
+     */
+    public function getOmniChannelMethodInfoByMethod($udropshipMethod)
+    {
+        // udropship_method (example udtiership_1)
+        $storeId = Mage::app()->getStore()->getId();
+
+        $collection = Mage::getModel("udropship/shipping")->getCollection();
+        $collection->getSelect()
+            ->join(
+                array('udropship_shipping_method' => $collection->getTable('udropship/shipping_method')),
+                "main_table.shipping_id = udropship_shipping_method.shipping_id",
+                array(
+                    'udropship_method' => new Zend_Db_Expr('CONCAT_WS(\'_\',    udropship_shipping_method.carrier_code ,udropship_shipping_method.method_code)'),
+                    "udropship_method_title" => "IF(udropship_shipping_title_store.title IS NOT NULL, udropship_shipping_title_store.title, udropship_shipping_title_default.title)"
+                )
+            );
+        $collection->getSelect()->join(
+            array('udtiership_delivery_type' => $collection->getTable('udtiership/delivery_type')),
+            "udropship_shipping_method.method_code = udtiership_delivery_type.delivery_type_id",
+            array("delivery_code")
+        );
+
+        $collection->getSelect()->joinLeft(
+            array('udropship_shipping_title_default' => $collection->getTable('udropship/shipping_title')),
+            "main_table.shipping_id = udropship_shipping_title_default.shipping_id AND udropship_shipping_title_default.store_id=0",
+            array()
+        );
+        $collection->getSelect()->joinLeft(
+            array('udropship_shipping_title_store' => $collection->getTable('udropship/shipping_title')),
+            "main_table.shipping_id = udropship_shipping_title_store.shipping_id AND udropship_shipping_title_store.store_id={$storeId}",
+            array()
+        );
+
+        $collection->getSelect()->having("udropship_method=?", $udropshipMethod);
+
+        return $collection->getFirstItem();
     }
 
 }
