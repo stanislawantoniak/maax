ad63832051ac77343705593caffa51e019c69584
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-07-11 21:14:06 +0200

    update to 1.9.2.4

--- app/code/core/Mage/Payment/Model/Info.php
+++ app/code/core/Mage/Payment/Model/Info.php
@@ -10,18 +10,18 @@
  * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * to license@magento.com so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade Magento to newer
  * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
+ * needs please refer to http://www.magento.com for more information.
  *
  * @category    Mage
  * @package     Mage_Payment
- * @copyright   Copyright (c) 2013 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
@@ -72,11 +72,7 @@ class Mage_Payment_Model_Info extends Mage_Core_Model_Abstract
     {
         if (!$this->hasMethodInstance()) {
             if ($this->getMethod()) {
-                Mage::log($this->getMethod(), null, "payment.log");
                 $instance = Mage::helper('payment')->getMethodInstance($this->getMethod());
-                Mage::log((bool)$instance, null, "payment.log");
-                Mage::log($instance, null, "payment.log");
-                Mage::log("---------------------", null, "payment.log");
                 if ($instance) {
                     $instance->setInfoInstance($this);
                     $this->setMethodInstance($instance);
