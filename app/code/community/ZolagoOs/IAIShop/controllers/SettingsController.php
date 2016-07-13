8c948720b9cc0a9e1b7ac9ab34ddfb4a9191cf02
Patryk Dobrosz <patryk.dobrosz@convertica.pl>
2016-07-13 09:46:21 +0200

    Refs#2143 IAI-Shop configuration in the Vendor Panel - saving serialized fields

--- app/code/community/ZolagoOs/IAIShop/controllers/SettingsController.php
+++ app/code/community/ZolagoOs/IAIShop/controllers/SettingsController.php
@@ -5,6 +5,10 @@ class ZolagoOS_IAIShop_SettingsController extends Zolago_Dropship_Controller_Ven
 
     public function indexAction()
     {
+        $vendor = Mage::getModel("udropship/vendor")->load(83);
+        Zend_Debug::dump(json_decode($vendor->getData("custom_vars_combined"))->iaishop_login);
+        Zend_Debug::dump(json_decode($vendor->getData("custom_vars_combined"))->iaishop_pass);
+        die("test");
         Mage::register('as_frontend', true);
         $this->_renderPage(null, 'zolagoosiaishop');
     }
@@ -12,12 +16,27 @@ class ZolagoOS_IAIShop_SettingsController extends Zolago_Dropship_Controller_Ven
 
     public function saveAction()
     {
-        $this->getRequest()->getParam("login");
-        $this->getRequest()->getParam("password");
 
-        
 
-        $this->_getSession()->addSuccess(Mage::helper("zosiaishop")->__("Settings saved!"));
-        $this->_redirect('iaishop/settings');
+        // ACL no access
+        $vendor = $this->_getSession()->getVendor();
+        if (!$vendor->getData('ghapi_vendor_access_allow')) {
+            return $this->_redirect('udropship/vendor/dashboard');
+        }
+
+        $helper = Mage::helper('zosiaishop');
+        if (!$this->getRequest()->isPost()) {
+            return $this->_redirectReferer();
+        }
+
+
+        $vendor->setData('iaishop_login', $this->getRequest()->getPost('login'));
+        $vendor->setData('iaishop_pass', $this->getRequest()->getPost('password'));
+
+        $vendor->save();
+
+        $this->_getSession()->addSuccess($helper->__('Settings saved!'));
+
+        return $this->_redirectReferer();
     }
 }
