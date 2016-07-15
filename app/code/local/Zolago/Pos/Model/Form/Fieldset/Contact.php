146b7f93da7cf4bcc2d278546db738b6ee277101
andrzejspolnicki <andrzej.spolnicki@convertica.pl>
2016-07-15 14:48:18 +0200

    re#2130 udropship fixes: - POSy mają wymagany numer telefonu - nie dla każdego sklepu mamy numer - chodzi o mapkę

--- app/code/local/Zolago/Pos/Model/Form/Fieldset/Contact.php
+++ app/code/local/Zolago/Pos/Model/Form/Fieldset/Contact.php
@@ -7,18 +7,4 @@ class Zolago_Pos_Model_Form_Fieldset_Contact extends Zolago_Common_Model_Form_Fi
     protected function _getHelper() {
         return Mage::helper('zolagopos');
     }
-
-    /**
-     * field phone
-     */
-    protected function _addFieldPhone() {
-        $this->_fieldset->addField('phone', 'text', array(
-            'name'          => 'phone',
-            'label'         => $this->_helper->__('Phone'),
-            'required'      => false,
-            'class'         => 'form-control',
-            "maxlength"     => 50
-        ));
-
-    }
 }
