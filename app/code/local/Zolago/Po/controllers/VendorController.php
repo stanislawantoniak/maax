867789d75ff3554344f6cbbb39a1627789024f02
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-07-22 12:00:25 +0200

    Refs#2152 refs#2175 instead of Wydrukuj list przewozowy - Wydaj zamówienie (cherry picked from commit bbe4fd5)

--- app/code/local/Zolago/Po/controllers/VendorController.php
+++ app/code/local/Zolago/Po/controllers/VendorController.php
@@ -1483,7 +1483,7 @@ class Zolago_Po_VendorController extends Zolago_Dropship_Controller_Vendor_Abstr
         try {
             $udpo = $this->_registerPo();
             if (!$udpo->isPaid()) {
-                $this->_getSession()->addError(Mage::helper("zolagopo")->__("The order should be paid before."));
+                $this->_getSession()->addError(Mage::helper("zolagopo")->__("The order should be paid."));
                 return $this->_redirectReferer();
             }
 
