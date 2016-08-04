23ea55a401e87a0caa7d2d6f7ec9698a954908a6
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-08-04 21:58:19 +0200

    test

--- app/code/local/GH/FeedExport/Model/Feed/Generator/Action/Iterator/Rule.php
+++ app/code/local/GH/FeedExport/Model/Feed/Generator/Action/Iterator/Rule.php
@@ -29,14 +29,14 @@ class GH_FeedExport_Model_Feed_Generator_Action_Iterator_Rule extends Mirasvit_F
 
 
 
-        if ($productInventoryIsInStock) {
-            if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_IN_STOCK){
-                $collection->getSelect()->where("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_IN_STOCK);
-            }
-            if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_OUT_OF_STOCK){
-                $collection->getSelect()->where("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_OUT_OF_STOCK);
-            }
-        }
+//        if ($productInventoryIsInStock) {
+//            if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_IN_STOCK){
+//                $collection->getSelect()->where("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_IN_STOCK);
+//            }
+//            if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_OUT_OF_STOCK){
+//                $collection->getSelect()->where("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_OUT_OF_STOCK);
+//            }
+//        }
 
         $collection->addStoreFilter();
 
