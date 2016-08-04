6e466fa33adf927746a5dc00fe9bd7da78699d62
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-08-04 21:31:55 +0200

    test

--- app/code/local/GH/FeedExport/Model/Feed/Generator/Action/Iterator/Entity.php
+++ app/code/local/GH/FeedExport/Model/Feed/Generator/Action/Iterator/Entity.php
@@ -40,14 +40,14 @@ class GH_FeedExport_Model_Feed_Generator_Action_Iterator_Entity
 
 //
 
-            if ($productInventoryIsInStock) {
-                if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_IN_STOCK){
-                    $collection->addFieldToFilter("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_IN_STOCK);
-                }
-                if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_OUT_OF_STOCK){
-                    $collection->addFieldToFilter("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_OUT_OF_STOCK);
-                }
-            }
+//            if ($productInventoryIsInStock) {
+//                if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_IN_STOCK){
+//                    $collection->addFieldToFilter("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_IN_STOCK);
+//                }
+//                if ($productInventoryIsInStock == GH_FeedExport_Model_Observer::FILTER_STOCK_OUT_OF_STOCK){
+//                    $collection->addFieldToFilter("is_in_stock", Mage_CatalogInventory_Model_Stock::STOCK_OUT_OF_STOCK);
+//                }
+//            }
 
             $collection->addStoreFilter();
 
