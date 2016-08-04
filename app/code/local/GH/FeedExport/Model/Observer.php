f58870a7b59d97884c1e8a51084277036f86d1c8
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-08-04 21:36:30 +0200

    test

--- app/code/local/GH/FeedExport/Model/Observer.php
+++ app/code/local/GH/FeedExport/Model/Observer.php
@@ -40,8 +40,7 @@ class GH_FeedExport_Model_Observer
             "e.entity_id = cataloginventory_stock_table.product_id",
             array()
         );
-        $collection->addExpressionAttributeToSelect('politics',
-            "IF(e.type_id IN ('configurable', 'grouped'), (cataloginventory_stock_table.manage_stock = 1 AND cataloginventory_stock_table.is_in_stock = 0) , (cataloginventory_stock_table.min_qty>999999) )", array());
+
         // Join all children count
         $subSelect = $adapter->select();
         $subSelect->from(array("link_all" => $linkTable), array("COUNT(link_all.link_id)"));
