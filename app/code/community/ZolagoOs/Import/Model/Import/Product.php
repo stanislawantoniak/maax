bce25b35323f1c9343c123b304ad5c4a06fad0b3
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-08-19 17:00:00 +0200

    test

--- app/code/community/ZolagoOs/Import/Model/Import/Product.php
+++ app/code/community/ZolagoOs/Import/Model/Import/Product.php
@@ -155,13 +155,13 @@ class ZolagoOs_Import_Model_Import_Product
 
 
             //Start update configurable with children session
-            //$dp->beginImportSession($importProfile, "update", new ZolagoOs_Import_Model_ImportProductsLogger());
-            //$this->updateRelations($dp,$skusCreated);
-            //$dp->endImportSession();
+            $dp->beginImportSession($importProfile, "update", new ZolagoOs_Import_Model_ImportProductsLogger());
+            $this->updateRelations($dp,$skusCreated);
+            $dp->endImportSession();
 
 
             //3. Set additional attributes
-            //$this->updateAdditionalAttributes();
+            $this->updateAdditionalAttributes();
 
         } catch (Exception $e) {
             Mage::logException($e);
