e9c7947a2c52f48acabd53ef13742bcd80c6450b
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-08-26 12:18:33 +0200

    cost

--- app/code/local/Zolago/Modago/Block/Checkout/Onepage/Abstract.php
+++ app/code/local/Zolago/Modago/Block/Checkout/Onepage/Abstract.php
@@ -254,6 +254,7 @@ abstract class Zolago_Modago_Block_Checkout_Onepage_Abstract extends Mage_Checko
 			foreach ($methodDataArr as $methodData) {
 				$vendorId = $methodData['vendor_id'];
 				$methodToFind[$code][$vendorId] = $vendorId;
+				$cost[$code][] = $methodData['cost'];
 			}
 		}
 
