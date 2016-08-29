a65d279a297e1b80625305abc274e89166f86d4f
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-08-29 17:42:57 +0200

    Refs#2240
    blog posts on the product page

---
+++ app/code/local/Zolago/Blog/Block/Catalog/Product/Posts.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Class Zolago_Blog_Block_Catalog_Product_Posts
+ */
+class Zolago_Blog_Block_Catalog_Product_Posts extends Mage_Catalog_Block_Product_Abstract
+{
+
+    protected $_productBlogItems;
+
+    protected function _prepareData()
+    {
+        /* @var $product Mage_Catalog_Model_Product */
+        $product = Mage::registry('product');
+
+        $productBlogIds = [];
+
+        $range = range(1, 3);
+
+        foreach ($range as $i) {
+            if (!empty($product->getData("product_blog_post_id_$i"))) {
+                $productBlogIds[] = $product->getData("product_blog_post_id_$i");
+            }
+        }
+
+
+        $storeId = Mage::app()->getStore()->getId();
+
+        $blogPostModel = Mage::getModel("mpblog/post");
+        $blogPostCollection = $blogPostModel->getCollection()
+            ->addFieldToFilter("post_id", array("in" => $productBlogIds))
+            ->addFieldToFilter("status", Magpleasure_Blog_Model_Post::STATUS_ENABLED);
+
+        $blogPostCollection->getSelect()
+            ->joinLeft(
+                array(
+                    'post_store' => 'mp_blog_posts_store'
+                ),
+                "post_store.post_id = main_table.post_id",
+                array()
+            )
+            ->where("post_store.store_id=?", $storeId);
+
+
+        $this->_productBlogItems = $blogPostCollection;
+
+        return $this;
+    }
+
+    protected function _beforeToHtml()
+    {
+        $this->_prepareData();
+        return parent::_beforeToHtml();
+    }
+
+}
