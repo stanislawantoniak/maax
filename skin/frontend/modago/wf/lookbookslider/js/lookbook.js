ab6348a65a4d57e1870324121a60e599b41f79de
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-09-14 15:30:59 +0200

    Refs#2268

---
+++ skin/frontend/modago/wf/lookbookslider/js/lookbook.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function () {
+    Mall.lookbook.init();
+});
+
+Mall.lookbook = {
+
+    init: function () {
+        Mall.lookbook.iconsManage();
+    },
+    iconsManage: function () {
+        var iconMinus = Mall.reg.get("lookbookslider_minus_icon"),
+            iconPlus = Mall.reg.get("lookbookslider_plus_icon");
+
+        jQuery(".lookbookslider-container").delegate(".cycle-slideshow .hotspot", {
+            hover: function () {
+                jQuery(this).find("img").attr("src", iconMinus);
+            },
+            mouseleave: function () {
+                jQuery(this).find("img").attr("src", iconPlus);
+            }
+        });
+    }
+};
+
