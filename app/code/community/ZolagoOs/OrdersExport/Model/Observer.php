85e30ee30afae9c4e0f9579f51407017a4bfa242
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-09-21 15:06:53 +0200

    Refs#2238 crontab

---
+++ app/code/community/ZolagoOs/OrdersExport/Model/Observer.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class ZolagoOs_Import_Model_Observer
+ */
+class ZolagoOs_OrdersExport_Model_Observer
+{
+    /**
+     * Export Orders
+     */
+    public function cronExportOrders(){
+        
+    }
+
+}
