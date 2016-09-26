3502a7796800d64c0092a61314074214b566efbf
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-09-26 16:17:41 +0200

    Refs#2282 11. 404 Not Found dla popup_rehabilitacja.css (strona główna).

---
+++ app/code/local/Zolago/Freshmail/Model/Observer.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class Zolago_Freshmail_Model_Observer
+ */
+class Zolago_Freshmail_Model_Observer extends Snowdog_Freshmail_Model_Observer
+{
+    /**
+     * Append dynamically generated css files
+     */
+    public function layoutGenerateBlocksAfter()
+    {
+    }
+}
