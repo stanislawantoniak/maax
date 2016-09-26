62f76f8f2d9c401d09e90a1b3de1e8063cf3e264
Przemek Siwik <przemyslaw.siwik@convertica.pl>
2016-09-26 16:17:32 +0200

    usunięcie modago ze źródeł refs #2282

---
+++ app/code/local/StandAlone/Ipson/controllers/InpostController.php
@@ -0,0 +1,5 @@
+<?php
+require_once Mage::getConfig()->getModuleDir('controllers', "Zolago_Modago") . DS . "InpostController.php";
+
+class StandAlone_Ipson_InpostController extends Zolago_Modago_InpostController {
+}
