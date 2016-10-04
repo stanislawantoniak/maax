46454c8e2580ebb7b8eb3e805531e9128c04760a
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-04 10:00:05 +0200

    code reformat

--- app/code/community/ZolagoOs/OrdersExport/Helper/Data.php
+++ app/code/community/ZolagoOs/OrdersExport/Helper/Data.php
@@ -34,6 +34,29 @@ class ZolagoOs_OrdersExport_Helper_Data extends Mage_Core_Helper_Abstract
         return $this->getConfig('export_files_folder');
     }
 
+
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function formatToDocNumber($value)
+    {
+        //Format liczb    : bez separatora tysiêcy, separator dziesiêtny "."
+        $decPoint = '.';
+        $thousandsSep = '';
+        return number_format($value, 2, $decPoint, $thousandsSep);
+    }
+
+
+    /**
+     * 
+     */
+    public function toWindows1250($text)
+    {
+        return iconv('UTF-8', "Windows-1250", $text);
+    }
+
     /**
      * create log (file in var/log)
      *
