3d36eee3ee009d4105ede3d205124b4bbdbcd56a
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-12 15:07:02 +0200

    test message duplicate

--- app/code/local/Zolago/DropshipVendorAskQuestion/controllers/CustomerController.php
+++ app/code/local/Zolago/DropshipVendorAskQuestion/controllers/CustomerController.php
@@ -80,11 +80,9 @@ class Zolago_DropshipVendorAskQuestion_CustomerController extends ZolagoOs_OmniC
 
 
         //END PARRENT::POSTACTION
-//        $obj = Zend_Uri_Http::fromString($this->_getRefererUrl());
-//        $obj->addReplaceQueryParameters(array('salt'=>uniqid()));
-//        $this->getResponse()->setRedirect($obj->getUri());
-
-        $this->_redirectReferer();
+        $obj = Zend_Uri_Http::fromString($this->_getRefererUrl());
+        $obj->addReplaceQueryParameters(array('salt'=>uniqid()));
+        $this->getResponse()->setRedirect($obj->getUrl());
 	return $this;
     }
 }
