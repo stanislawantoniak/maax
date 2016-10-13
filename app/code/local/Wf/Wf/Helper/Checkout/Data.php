a71ce7cbaa542965590d4bd64b2ece85772bb6b8
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-13 15:36:36 +0200

    Refs#2288

--- app/code/local/Wf/Wf/Helper/Checkout/Data.php
+++ app/code/local/Wf/Wf/Helper/Checkout/Data.php
@@ -16,8 +16,11 @@ class Wf_Wf_Helper_Checkout_Data extends Zolago_Checkout_Helper_Data
 
         $carrierLogo = parent::getCarrierLogo($deliveryType);
         switch ($deliveryType) {
-            case 'std': // carrier
+            case Orba_Shipping_Model_Carrier_Default::CODE: // carrier
                 $carrierLogo = '<img class="checkout-logo"  src="' . Mage::getDesign()->getSkinUrl('images/dhl/checkout-logo.png') . '" />';
+                break;
+            case Orba_Shipping_Model_Post::CODE: //zolagopp
+                $carrierLogo = '<img class="checkout-logo"  src="' . Mage::getDesign()->getSkinUrl('images/poczta_polska/poczta_polska.png') . '" />';
                 break;
         }
 
