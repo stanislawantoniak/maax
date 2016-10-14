59f99359b76163f39d6b360aef607cad452eaf71
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-14 12:24:49 +0200

    Refs#2284 shell/wf_import_subscribers.php

---
+++ shell/wf_import_subscribers.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'shell/abstract.php';
+
+class Wf_Import_Subscribers extends Mage_Shell_Abstract {
+
+    public function run() {
+
+
+    }
+}
+
+
+$obj = new Wf_Import_Subscribers();
+$obj->run();
