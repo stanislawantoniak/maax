98d8f6976d06cdb4799290b43d3c8efbffddd108
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-14 12:01:38 +0200

    Refs#2284 db table, model triad

---
+++ app/code/local/Wf/OldStoreCustomer/Model/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class Wf_OldStoreCustomer_Model_Customer
+ */
+class Wf_OldStoreCustomer_Model_Customer extends Mage_Core_Model_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('wfoldstorecustomer/customer');
+    }
+
+}
