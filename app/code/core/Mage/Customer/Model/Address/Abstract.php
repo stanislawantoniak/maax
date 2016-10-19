e6f3a0fd55209e15bc6c0e4085de7d177873bfd6
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-19 16:14:21 +0200

    test 2326

--- app/code/core/Mage/Customer/Model/Address/Abstract.php
+++ app/code/core/Mage/Customer/Model/Address/Abstract.php
@@ -329,6 +329,8 @@ class Mage_Customer_Model_Address_Abstract extends Mage_Core_Model_Abstract
 
     public function format($type)
     {Mage::log($type, null, "address1.log");
+        Mage::log($this->getConfig()->getFormatByCode($type), null, "address1.log");
+        Mage::log($this->getConfig()->getFormatByCode($type)->getRenderer(), null, "address1.log");
         if(!($formatType = $this->getConfig()->getFormatByCode($type))
             || !$formatType->getRenderer()) {
             return null;
