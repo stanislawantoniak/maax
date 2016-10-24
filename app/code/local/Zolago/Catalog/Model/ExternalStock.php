bc7ea5753ad819c7bc654aad45755d575059be5c
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-24 16:05:17 +0200

    Refs#2357

---
+++ app/code/local/Zolago/Catalog/Model/ExternalStock.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class Zolago_Catalog_Model_ExternalStock
+ */
+class Zolago_Catalog_Model_ExternalStock extends Mage_Core_Model_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('zolagocatalog/external_stock');
+    }
+
+}
