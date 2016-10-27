97e5d7a6640b4656db7d65738beef873c5a04c95
victoriasultanovska <victoria.sultanovska@convertica.pl>
2016-10-27 13:42:21 +0200

    Refs#2402 image resize

--- app/code/local/Orba/Common/Helper/Ajax/Customer/Cache.php
+++ app/code/local/Orba/Common/Helper/Ajax/Customer/Cache.php
@@ -418,8 +418,7 @@ class Orba_Common_Helper_Ajax_Customer_Cache extends Mage_Core_Helper_Abstract {
 				/* @var $product Zolago_Catalog_Model_Product */
 				$image = Mage::helper("zolago_image")
 					->init($product, 'small_image')
-					->setCropPosition(Zolago_Image_Model_Catalog_Product_Image::POSITION_CENTER)
-					->adaptiveResize(200, 312);
+					->resize(312, 312);
 				$recentlyViewedContent[(int)$product->getId()] = array(
 					'title' => Mage::helper('catalog/output')->productAttribute($product, $product->getName(), 'name'),
 					'image_url' => (string)$image,
