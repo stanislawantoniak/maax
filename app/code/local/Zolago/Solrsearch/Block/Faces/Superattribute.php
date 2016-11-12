affa74ae82c2c78130e3df5b3bbc22dd1dda4ea4
Victoria Sultanovska <viktoriia.sultanovska@orba.co>
2016-11-12 04:19:46 +0100

    custom filter

--- app/code/local/Zolago/Solrsearch/Block/Faces/Superattribute.php
+++ app/code/local/Zolago/Solrsearch/Block/Faces/Superattribute.php
@@ -14,22 +14,22 @@ class Zolago_Solrsearch_Block_Faces_Superattribute extends Zolago_Solrsearch_Blo
     }
 
 
-    public function getItems() {
-        if(!$this->hasData("items")) {
-            $hiddenItems = array();
-            $items = $this->getAllItems();
-            krumo($items);
-            if($this->getFilterModel()) {
-                $items =  $this->filterAndSortOptions(
-                    $this->getAllItems(),
-                    $this->getFilterModel(),
-                    $hiddenItems
-                );
-            }
-
-            $this->setData("items", $items);
-            $this->setData("hidden_items", $hiddenItems);
-        }
-        return $this->getData("items");
-    }
+//    public function getItems() {
+//        if(!$this->hasData("items")) {
+//            $hiddenItems = array();
+//            $items = $this->getAllItems();
+//            krumo($items);
+//            if($this->getFilterModel()) {
+//                $items =  $this->filterAndSortOptions(
+//                    $this->getAllItems(),
+//                    $this->getFilterModel(),
+//                    $hiddenItems
+//                );
+//            }
+//
+//            $this->setData("items", $items);
+//            $this->setData("hidden_items", $hiddenItems);
+//        }
+//        return $this->getData("items");
+//    }
 }
